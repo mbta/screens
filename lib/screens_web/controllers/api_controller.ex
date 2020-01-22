eeaf97b7d7a0b5255b2473aab302273e46b1a7dd
Paul Handorff <handorff.paul@gmail.com>
2020-01-22 10:44:06 -0500

    set up basic screen data api

--- lib/screens_web/controllers/api_controller.ex
+++ lib/screens_web/controllers/api_controller.ex
@@ -2,6 +2,9 @@ defmodule ScreensWeb.ApiController do
   use ScreensWeb, :controller
 
   def show(conn, %{"id" => id}) do
-    json(conn, %{screen_id: id})
+    screens_data = Application.get_env(:screens, :screen_data)
+    screen_data = Map.get(screens_data, id)
+
+    json(conn, %{screen_id: id, stop_id: Map.get(screen_data, :stop_id)})
   end
 end
