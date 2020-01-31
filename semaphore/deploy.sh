459bff44b55a29aeff31d3430f95aa4543e36ae8
Ian Westcott <iwestcott@mbta.com>
2020-01-31 12:11:06 -0500

    modify deploy process to pull from ECR

--- semaphore/deploy.sh
+++ semaphore/deploy.sh
@@ -1,53 +1,38 @@
 #!/bin/bash
 set -e -x -u
 
-# bash script should be called with aws environment (e.g.: ./semaphore/deploy.sh dev)
+# bash script should be called with aws environment (dev / dev-green / prod)
 # other required configuration:
 # * APP
 # * DOCKER_REPO
+# * S3_BUCKET_NAME
 
-function get_running_taskdef_arns() {
-  # returns an array of the task definition ARNs for each running task in the cluster, or "none"
-  runningtasks="$(aws ecs list-tasks --region us-east-1 --cluster $APP --service-name $awsenv --desired-status RUNNING | jq -r '.taskArns[]')"
-  if [ "${runningtasks}" != "" ]; then
-    echo "$(aws ecs describe-tasks --region us-east-1 --cluster $APP --tasks $runningtasks | jq -r -c '.tasks | map(.taskDefinitionArn)')"
-  else
-    echo "none"
-  fi
-}
-
-awsenv=$APP-$1
+awsenv=$1
+appenv=$APP-$awsenv
 
 githash=$(git rev-parse --short HEAD)
 gitmsg=$(git log -1 --pretty=%s)
 
-# ensure the image exists on AWS. This command will fail if it does not.
-aws ecr describe-images --region us-east-1 --repository-name $APP --image-ids "imageTag=git-$githash"
+# Create Dockerrun file pointing to ECR image, and zip it up
+mkdir $githash
+sed "s/GITHASH/$githash/g" ./semaphore/Dockerrun.aws.json > $githash/Dockerrun.aws.json
+pushd $githash
+zip -r ../$githash.zip .
+popd
 
-# get JSON describing task definition currently running on AWS
-# use it as basis for new revision, but replace image with the one built above
-taskdefinition=$(aws ecs describe-task-definition --region us-east-1 --task-definition $awsenv)
-taskdefinition=$(echo $taskdefinition | jq ".taskDefinition | del(.status) | del(.taskDefinitionArn) | del(.requiresAttributes) | del(.revision) | del(.compatibilities)")
-newcontainers=$(echo $taskdefinition | jq ".containerDefinitions | map(.image=\"$DOCKER_REPO:git-$githash\")")
-aws ecs register-task-definition --region us-east-1 --family $awsenv --cli-input-json "$taskdefinition" --container-definitions "$newcontainers"
-newtaskdef=$(aws ecs describe-task-definition --region us-east-1 --task-definition $awsenv)
-newrevision=$(echo $newtaskdef | jq -r '.taskDefinition.revision')
-newtaskdefarn=$(echo $newtaskdef | jq -r '.taskDefinition.taskDefinitionArn')
+rm -r $githash
 
-# Update the ECS service to use the new revision of the task definition. Check periodically to see
-# if the task is running yet, and signal deploy failure if it doesn't start up in a reasonable time.
-aws ecs update-service --region us-east-1 --cluster $APP --service $awsenv --task-definition $awsenv:$newrevision
+# Put zip on S3 and use to create application version
+aws s3 cp $githash.zip s3://$S3_BUCKET_NAME/$APP/$githash.zip
+aws elasticbeanstalk delete-application-version --application-name $APP --version-label $githash --delete-source-bundle
+aws elasticbeanstalk create-application-version --application-name $APP --version-label $githash --source-bundle S3Bucket=$S3_BUCKET_NAME,S3Key=$APP/$githash.zip --description "$gitmsg"
 
-checks=0
-# the right-hand side of this test is intentionally formatted as an array
-# to match the output of the function call on the left-hand side
-until [[ `get_running_taskdef_arns` = "[\"${newtaskdefarn}\"]" ]]; do
-  echo "waiting for task to be updated to new revision"
-  # wait for up to 5 minutes
-  if [[ $checks -ge 15 ]]; then
-    echo "$awsenv took too long to update"
-    exit 1
-  fi
-  sleep 20
-  checks=$((checks+1))
-done
+rm $githash.zip
+
+# Deploy application version
+aws elasticbeanstalk update-environment --environment-name $appenv --version-label $githash
+echo "Environment status: `aws elasticbeanstalk describe-environments --environment-names $appenv | grep '"Status"' | cut -d: -f2  | sed -e 's/^[^"]*"//' -e 's/".*$//'`"
+echo "Your environment is currently updating"; while [[ `aws elasticbeanstalk describe-environments --environment-names $appenv | grep '"Status"' | cut -d: -f2  | sed -e 's/^[^"]*"//' -e 's/".*$//'` = "Updating" ]]; do sleep 10; printf "."; done
+if [[ `aws elasticbeanstalk describe-environments --environment-names $appenv | grep VersionLabel | cut -d: -f2 | sed -e 's/^[^"]*"//' -e 's/".*$//'` = $githash ]]; then echo "The version of application code on Elastic Beanstalk matches the version that Semaphore sent in this deployment."; echo "Your environment info:"; aws elasticbeanstalk describe-environments --environment-names $appenv; else echo "The version of application code on Elastic Beanstalk does not match the version that Semaphore sent in this deployment. Please check your AWS Elastic Beanstalk Console for more information."; echo "Your environment info:"; aws elasticbeanstalk describe-environments --environment-names $appenv; false; fi
+sleep 5; a="0"; echo "Waiting for environment health to turn Green"; while [[ `aws elasticbeanstalk describe-environments --environment-names $appenv | grep '"Health":' | cut -d: -f2  | sed -e 's/^[^"]*"//' -e 's/".*$//'` != "Green" && $a -le 30 ]]; do sleep 2; a=$[$a+1]; printf "."; done; if [[ `aws elasticbeanstalk describe-environments --environment-names $appenv | grep '"Health":' | cut -d: -f2 | sed -e 's/^[^"]*"//' -e 's/".*$//'` = "Green" ]]; then echo "Your environment status is Green, congrats!"; else echo "Your environment status is not Green, sorry."; false; fi;
+echo "Your environment info:"; aws elasticbeanstalk describe-environments --environment-names $appenv
