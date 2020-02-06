d4531c9ae3eddf0b45b2c46068b9d7cdf954f4b8
Ian Westcott <iwestcott@mbta.com>
2020-02-06 11:58:43 -0500

    Secrets Manager Take 2 (#14)
    * revert: don't use env var instead of secrets manager
    
    Ref: 552d38b39e99b4af19fd55ce941716fb81eccb7d
    
    * feat: ensure ExAWS.SecretsManager and its dependencies are started
    
    * add TODO for removing environment name from code

--- config/releases.exs
+++ config/releases.exs
@@ -4,8 +4,22 @@
 # remember to add this file to your .gitignore.
 import Config
 
+# make sure ExAWS.SecretsManager and its dependencies are available
+Application.ensure_all_started(:poison)
+Application.ensure_all_started(:hackney)
+Application.ensure_all_started(:ex_aws)
+Application.ensure_all_started(:ex_aws_secretsmanager)
+
+# TODO source the environment name from somewhere else (env var?)
+secret_key_base =
+  "screens-dev-secret-key-base"
+  |> ExAws.SecretsManager.get_secret_value()
+  |> ExAws.request!()
+  |> Map.fetch!("SecretString")
+
 config :screens, ScreensWeb.Endpoint,
-  http: [:inet6, port: String.to_integer(System.get_env("PORT") || "4000")]
+  http: [:inet6, port: String.to_integer(System.get_env("PORT") || "4000")],
+  secret_key_base: secret_key_base
 
 # ## Using releases (Elixir v1.9+)
 #
