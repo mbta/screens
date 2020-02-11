968c946e21838528fe7f8dc66014afb76f4d9bcf
Paul Handorff <handorff.paul@gmail.com>
2020-02-11 12:02:49 -0500

    chore: move Header into its own scss file

--- assets/src/components/header.tsx
+++ assets/src/components/header.tsx
@@ -2,7 +2,12 @@ import moment from "moment";
 import "moment-timezone";
 import React, { useLayoutEffect, useRef, useState } from "react";
 
+import { classWithSize } from "../util";
+
 const Header = ({ stopName, currentTimeString }): JSX.Element => {
+  const SIZES = ["small", "large"];
+  const MAX_HEIGHT = 216;
+
   const ref = useRef(null);
   const [stopSize, setStopSize] = useState(1);
   const currentTime = moment(currentTimeString)
@@ -11,25 +16,25 @@ const Header = ({ stopName, currentTimeString }): JSX.Element => {
 
   useLayoutEffect(() => {
     const height = ref.current.clientHeight;
-    if (height > 216) {
+    if (height > MAX_HEIGHT) {
       setStopSize(stopSize - 1);
     }
   });
 
-  const SIZES = ["small", "large"];
-  const stopClassName = "header-stopname header-stopname-" + SIZES[stopSize];
-
   return (
     <div className="header">
-      <div className="header-time">{currentTime}</div>
-      <div className="header-realtime-indicator">
+      <div className="header__time">{currentTime}</div>
+      <div className="header__realtime-indicator">
         <img
-          className="header-realtime-indicator-icon"
+          className="header__realtime-indicator-icon"
           src="images/live-data-small.svg"
         ></img>
         UPDATED LIVE EVERY MINUTE
       </div>
-      <div className={stopClassName} ref={ref}>
+      <div
+        className={classWithSize("header__stop-name", SIZES[stopSize])}
+        ref={ref}
+      >
         {stopName}
       </div>
     </div>
