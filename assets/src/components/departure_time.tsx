6031783b7fdc596beae7d797ea791cc166f7477b
Paul Handorff <handorff.paul@gmail.com>
2020-02-11 08:43:37 -0500

    chore: factor out classes for DepartureTime

--- assets/src/components/departure_time.tsx
+++ assets/src/components/departure_time.tsx
@@ -2,44 +2,52 @@ import moment from "moment";
 import "moment-timezone";
 import React from "react";
 
+const classWithSize = (baseClass, size) => {
+  return `${baseClass} ${baseClass}--${size}`;
+};
+
 const DepartureTime = ({ time, currentTimeString, modifier }): JSX.Element => {
   const departureTime = moment(time);
   const currentTime = moment(currentTimeString);
   const secondDifference = departureTime.diff(currentTime, "seconds");
   const minuteDifference = Math.round(secondDifference / 60);
 
-  let prefix;
-  if (modifier) {
-    prefix = "later-";
-  } else {
-    prefix = "";
-  }
+  const size = modifier ? "small" : "large";
 
+  let inner;
   if (secondDifference < 60) {
-    return (
-      <div className={prefix + "departure-time"}>
-        <span className={prefix + "departure-time-now"}>Now</span>
+    inner = (
+      <div>
+        <span className={classWithSize("departure-time__now", size)}>Now</span>
       </div>
     );
   } else if (minuteDifference < 60) {
-    return (
-      <div className={prefix + "departure-time"}>
-        <span className={prefix + "departure-time-minutes"}>
+    inner = (
+      <div>
+        <span className={classWithSize("departure-time__minutes", size)}>
           {minuteDifference}
         </span>
-        <span className={prefix + "departure-time-minutes-label"}>m</span>
+        <span className={classWithSize("departure-time__minutes-label", size)}>
+          m
+        </span>
       </div>
     );
   } else {
     const timestamp = departureTime.tz("America/New_York").format("h:mm");
     const ampm = departureTime.tz("America/New_York").format("A");
-    return (
-      <div className={prefix + "departure-time"}>
-        <span className={prefix + "departure-time-timestamp"}>{timestamp}</span>
-        <span className={prefix + "departure-time-ampm"}>{ampm}</span>
+    inner = (
+      <div>
+        <span className={classWithSize("departure-time__timestamp", size)}>
+          {timestamp}
+        </span>
+        <span className={classWithSize("departure-time__ampm", size)}>
+          {ampm}
+        </span>
       </div>
     );
   }
+
+  return <div className={classWithSize("departure-time", size)}>{inner}</div>;
 };
 
 export default DepartureTime;
