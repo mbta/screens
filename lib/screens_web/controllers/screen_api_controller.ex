9b90f8df59423b93eb206ef0a5befa4aa0c2a7ad
Paul Handorff <handorff.paul@gmail.com>
2020-03-06 10:03:57 -0500

    feat: check override before sending API response

--- lib/screens_web/controllers/screen_api_controller.ex
+++ lib/screens_web/controllers/screen_api_controller.ex
@@ -7,7 +7,7 @@ defmodule ScreensWeb.ScreenApiController do
       |> Application.get_env(:screen_data)
       |> Map.get(screen_id)
       |> Map.get(:stop_id)
-      |> Screens.ScreenData.by_stop_id_with_version(version)
+      |> Screens.ScreenData.by_stop_id_with_override_and_version(screen_id, version)
 
     json(conn, data)
   end
