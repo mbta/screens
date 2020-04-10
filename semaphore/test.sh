72fa256e6b4a919f46683b108c94a667a8844d8d
Paul Handorff <handorff.paul@gmail.com>
2020-04-10 14:08:47 -0400

    chore: add codecov

--- semaphore/test.sh
+++ semaphore/test.sh
@@ -3,4 +3,4 @@ set -e
 
 mix coveralls.json &&
 npm --prefix assets test
-# bash <(curl -s https://codecov.io/bash) -t $ARROW_CODECOV_TOKEN
+bash <(curl -s https://codecov.io/bash) -t $SCREENS_CODECOV_TOKEN
