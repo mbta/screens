a9417bb5bba5d33db568520393eb0b4139d6bc97
Paul Handorff <handorff.paul@gmail.com>
2020-04-24 14:52:03 -0400

    feat: factor out destination logic and styling (#125)

--- assets/src/components/eink/bus/departure_destination.tsx
+++ assets/src/components/eink/bus/departure_destination.tsx
@@ -1,52 +1,12 @@
 import React from "react";
 
 import { classWithModifier } from "Util";
+import BaseDepartureDestination from "Components/eink/base_departure_destination";
 
 const DepartureDestination = ({ destination, size }): JSX.Element => {
-  let inner;
-  if (destination === undefined) {
-    inner = <div></div>;
-  } else if (destination.includes("via")) {
-    const parts = destination.split(" via ");
-    const primaryDestination = parts[0];
-    const secondaryDestination = "via " + parts[1];
-
-    inner = (
-      <div
-        className={classWithModifier("departure-destination__container", size)}
-      >
-        <div
-          className={classWithModifier("departure-destination__primary", size)}
-        >
-          {primaryDestination}
-        </div>
-        <div
-          className={classWithModifier(
-            "departure-destination__secondary",
-            size
-          )}
-        >
-          {secondaryDestination}
-        </div>
-      </div>
-    );
-  } else {
-    inner = (
-      <div
-        className={classWithModifier("departure-destination__container", size)}
-      >
-        <div
-          className={classWithModifier("departure-destination__primary", size)}
-        >
-          {destination}
-        </div>
-      </div>
-    );
-  }
-
   return (
     <div className={classWithModifier("departure-destination", size)}>
-      {inner}
+      <BaseDepartureDestination destination={destination} />
     </div>
   );
 };
