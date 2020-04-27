1547742cfec846f8b1221ec916b13091c349a6b5
Paul Handorff <handorff.paul@gmail.com>
2020-04-27 11:48:41 -0400

    fix: cr response

--- assets/src/components/eink/green_line/line_map.tsx
+++ assets/src/components/eink/green_line/line_map.tsx
@@ -9,6 +9,32 @@ const COLOR_BLACK = "#000000";
 const COLOR_LIGHT_GRAY = "#CCCCCC";
 const COLOR_DARK_GRAY = "#999999";
 
+interface LineMapSchedule {
+  time: string;
+}
+
+interface LineMapStops {
+  count_before: number;
+  current: string;
+  following: string;
+  next: string;
+  origin: string;
+}
+
+interface LineMapVehicle {
+  id: string;
+  index: number;
+  time: string | null;
+}
+
+interface LineMapData {
+  schedule: LineMapSchedule;
+  stops: LineMapStops;
+  vehicles: LineMapVehicle[];
+}
+
+const LineMapContext = createContext();
+
 const ScheduledDepartureIcon = ({ x, y, iconRadius }): JSX.Element => {
   return (
     <g>
@@ -18,7 +44,7 @@ const ScheduledDepartureIcon = ({ x, y, iconRadius }): JSX.Element => {
         r={iconRadius}
         fill={COLOR_DARK_GRAY}
         strokeWidth="0"
-      ></circle>
+      />
       <LineMapVehicleIcon
         x={x - iconRadius * 0.8}
         y={y - iconRadius * 0.8}
@@ -57,17 +83,7 @@ const ScheduledDepartureDescription = ({
 };
 
 const ScheduledDeparture = ({ stopName, schedule }): JSX.Element => {
-  const {
-    showScheduledDeparture,
-    lineBottomY,
-    dy,
-    radius,
-    lineWidth,
-  } = useContext(LineMapContext);
-
-  if (!showScheduledDeparture) {
-    return null;
-  }
+  const { lineBottomY, dy, radius, lineWidth } = useContext(LineMapContext);
 
   const time = schedule.time;
   const x = lineWidth / 2;
@@ -175,7 +191,7 @@ const LineMapVehicleDroplet = ({ x, y }): JSX.Element => {
         stroke={COLOR_WHITE}
         strokeWidth="8"
         strokeLinejoin="round"
-      ></path>
+      />
       <LineMapVehicleIcon
         x={centerX - iconSize / 2}
         y={centerY - iconSize / 2}
@@ -191,41 +207,37 @@ const LineMapVehicleLabel = ({
   time,
   currentTimeString,
 }): JSX.Element => {
-  if (time === null) {
-    return null;
+  const timeRep = timeRepresentation(time, currentTimeString);
+  if (timeRep.type === "TIME_NOW") {
+    return (
+      <text
+        x={x + 20 + 18} // lineWidth / 2 + textMargin
+        y={y + 44} // eyeballed it...
+        fontFamily="neue-haas-grotesk-text"
+        fontSize="40"
+        fontWeight="700"
+        textAnchor="right"
+      >
+        Now
+      </text>
+    );
+  } else if (timeRep.type === "TIME_MINUTES") {
+    return (
+      <text
+        x={x + 20 + 18} // lineWidth / 2 + textMargin
+        y={y + 44} // eyeballed it...
+        fontFamily="neue-haas-grotesk-text"
+      >
+        <tspan fontSize="40" fontWeight="700" textAnchor="right">
+          {timeRep.minutes}
+        </tspan>
+        <tspan fontSize="30" fontWeight="400" textAnchor="right" dx="3">
+          m
+        </tspan>
+      </text>
+    );
   } else {
-    const timeRep = timeRepresentation(time, currentTimeString);
-    if (timeRep.type === "TIME_NOW") {
-      return (
-        <text
-          x={x + 20 + 18} // lineWidth / 2 + textMargin
-          y={y + 44} // eyeballed it...
-          fontFamily="neue-haas-grotesk-text"
-          fontSize="40"
-          fontWeight="700"
-          textAnchor="right"
-        >
-          Now
-        </text>
-      );
-    } else if (timeRep.type === "TIME_MINUTES") {
-      return (
-        <text
-          x={x + 20 + 18} // lineWidth / 2 + textMargin
-          y={y + 44} // eyeballed it...
-          fontFamily="neue-haas-grotesk-text"
-        >
-          <tspan fontSize="40" fontWeight="700" textAnchor="right">
-            {timeRep.minutes}
-          </tspan>
-          <tspan fontSize="30" fontWeight="400" textAnchor="right" dx="3">
-            m
-          </tspan>
-        </text>
-      );
-    } else {
-      return null;
-    }
+    return null;
   }
 };
 
@@ -238,7 +250,7 @@ const LineMapVehicleIcon = ({ x, y, size }): JSX.Element => {
         <path
           d="M44.8825019,13.9985834 L45.200153,14.0009999 L83.117284,14.0000017 C91.2380941,14.0585408 97.8713422,20.576518 98.0335851,28.7341526 L98.0335851,28.7341526 L98.0335851,83.1671083 C97.9844822,90.1165254 93.1479625,96.113536 86.3665559,97.6335637 L86.3665559,97.6335637 L104,124 L94.000075,124 L81.4333436,105.566684 L46.7667569,105.566684 L34.2000255,124 L24,124 L41.5668439,97.6335637 C34.7855565,96.1134158 29.9491856,90.1164357 29.9001148,83.1671083 L29.9001148,83.1671083 L29.9001148,28.700853 C30.082492,20.4239828 36.9225061,13.8522867 45.200153,14.0009999 Z M47.3176012,76.8409304 C44.8263083,75.8090489 41.9587251,76.3794898 40.0520239,78.286253 C38.1453227,80.1930162 37.5750036,83.0605893 38.6070102,85.5518045 C39.6390167,88.0430198 42.0701085,89.6671433 44.7666519,89.6671433 C48.4485712,89.6669776 51.4332686,86.6821832 51.4332686,83.00031 C51.4332686,80.3038003 49.808894,77.872812 47.3176012,76.8409304 Z M82.9667974,76.2669608 C79.2728998,76.2669608 76.2758223,79.2565289 76.2665802,82.9503689 C76.2573808,86.6442089 79.2394957,89.6487022 82.9333473,89.6671433 L82.9333473,89.6671433 L83.0002475,89.6671433 C86.6940991,89.6487022 89.676214,86.6442089 89.6670146,82.9503689 C89.6577726,79.2565289 86.6606951,76.2669608 82.9667974,76.2669608 Z M82.5928589,29.1915756 L82.3000458,29.200948 L45.6333541,29.200948 C43.580738,29.0932838 41.5792601,29.864029 40.1290268,31.3205986 C38.6787935,32.7771681 37.9167965,34.7819715 38.0334351,36.8340716 L38.0334351,36.8340716 L38.0334351,46.6340736 C37.9428296,48.5591437 38.6207068,50.4413552 39.9179302,51.8666122 C41.2151536,53.2918691 43.0254527,54.1434122 44.9505524,54.2338976 C45.1779529,54.2446975 45.4059535,54.2446975 45.6333541,54.2338976 L45.6333541,54.2338976 L82.3000458,54.2338976 C84.2251399,54.3245019 86.1073749,53.6466332 87.5326497,52.349426 C88.9579245,51.0522188 89.8094782,49.2419425 89.8999647,47.3168668 C89.9107648,47.0890691 89.9107648,46.8614714 89.8999647,46.6340736 L89.8999647,46.6340736 L89.8999647,36.8340716 C90.0162165,34.7820752 89.2540943,32.7775009 87.8039495,31.3210202 C86.3538047,29.8645396 84.3525564,29.0936702 82.3000458,29.200948 Z M71.5000188,17.2343668 L56.6334816,17.2343668 C55.2724793,17.2333028 54.1538025,18.3077224 54.0999752,19.6676433 L54.0999752,19.6676433 L54.0999752,24.0010999 C54.108748,24.6641795 54.3806231,25.2966045 54.8557695,25.7591984 C55.3309159,26.2217922 55.9703964,26.4766462 56.6334816,26.4676753 L56.6334816,26.4676753 L71.4001185,26.500975 C72.7797632,26.5560897 73.9428956,25.4824218 73.998125,24.1027989 L73.998125,24.1027989 L73.999925,24.0010999 L73.999925,19.6676433 C73.9468627,18.3205696 72.8480507,17.2510446 71.5000188,17.2343668 L71.5000188,17.2343668 Z M58.4339861,4 C60.7721541,4.00011046 62.6675862,5.89551885 62.6676966,8.23365765 C62.6678071,10.5717964 60.7725541,12.4673839 58.4343861,12.4677153 L58.4343861,12.4677153 L58.4334861,12.4677153 C57.3110185,12.4692339 56.2335871,12.0230424 55.4393587,11.2285989 C54.6451304,10.4341554 54.1992386,9.35661107 54.1998748,8.23325762 C54.2002069,5.89511884 56.095818,3.99988954 58.4339861,4 Z M69.6667142,4.00119994 C72.004707,4.00119994 73.9000248,5.89649396 73.9000248,8.23445761 C73.9000248,10.5724213 72.004707,12.4677153 69.6667142,12.4677153 C68.543946,12.4677153 67.4671407,12.0218178 66.6732237,11.2279107 C65.8793067,10.4340036 65.4334036,9.35721174 65.4334036,8.23445761 C65.4334036,5.89649396 67.3287213,4.00119994 69.6667142,4.00119994 Z"
           fill={COLOR_WHITE}
-        ></path>
+        />
       </g>
     </g>
   );
@@ -260,7 +272,9 @@ const LineMapVehicle = ({ vehicle, currentTimeString }): JSX.Element => {
   return (
     <g>
       <LineMapVehicleDroplet x={x} y={y} />
-      <LineMapVehicleLabel {...{ x, y, time, currentTimeString }} />
+      {time !== null && (
+        <LineMapVehicleLabel {...{ x, y, time, currentTimeString }} />
+      )}
     </g>
   );
 };
@@ -294,7 +308,7 @@ const LineMapLineBefore = (): JSX.Element => {
     "Z",
   ].join(" ");
 
-  return <path d={dPast} fill={COLOR_LIGHT_GRAY}></path>;
+  return <path d={dPast} fill={COLOR_LIGHT_GRAY} />;
 };
 
 const LineMapLineAfter = (): JSX.Element => {
@@ -319,7 +333,7 @@ const LineMapLineAfter = (): JSX.Element => {
     "Z",
   ].join(" ");
 
-  return <path d={dFuture} fill={COLOR_BLACK}></path>;
+  return <path d={dFuture} fill={COLOR_BLACK} />;
 };
 
 const LineMapLine = (): JSX.Element => {
@@ -349,8 +363,8 @@ const LineMapStop = ({ i, stopName }): JSX.Element => {
         r={radius}
         fill={COLOR_WHITE}
         stroke="none"
-      ></circle>
-      {stopName === null ? null : (
+      />
+      {stopName !== null && (
         <LineMapStopLabel
           x={lineWidth + textMargin}
           y={stopMarginTop + 2 * radius + i * dy}
@@ -376,37 +390,30 @@ const LineMapCurrentStop = (): JSX.Element => {
       fill={COLOR_WHITE}
       stroke={COLOR_BLACK}
       strokeWidth={strokeWidth}
-    ></circle>
+    />
   );
 };
 
 const LineMapOriginStop = (): JSX.Element => {
-  const {
-    lineBottomY,
-    showOriginStop,
-    lineWidth,
-    strokeWidth,
-    radius,
-  } = useContext(LineMapContext);
+  const { lineBottomY, lineWidth, strokeWidth, radius } = useContext(
+    LineMapContext
+  );
 
-  if (showOriginStop) {
-    return (
-      <circle
-        cx={lineWidth / 2}
-        cy={lineBottomY}
-        r={radius + strokeWidth / 2}
-        fill={COLOR_WHITE}
-        stroke={COLOR_LIGHT_GRAY}
-        strokeWidth={strokeWidth}
-      ></circle>
-    );
-  } else {
-    return null;
-  }
+  return (
+    <circle
+      cx={lineWidth / 2}
+      cy={lineBottomY}
+      r={radius + strokeWidth / 2}
+      fill={COLOR_WHITE}
+      stroke={COLOR_LIGHT_GRAY}
+      strokeWidth={strokeWidth}
+    />
+  );
 };
 
 const LineMapStops = ({}): JSX.Element => {
   const {
+    showOriginStop,
     originStopIndex,
     stopNames,
     radius,
@@ -426,7 +433,7 @@ const LineMapStops = ({}): JSX.Element => {
         }
       })}
       <LineMapCurrentStop />
-      <LineMapOriginStop />
+      {showOriginStop && <LineMapOriginStop />}
     </g>
   );
 };
@@ -440,13 +447,16 @@ const LineMapBase = (): JSX.Element => {
   );
 };
 
-const LineMapContext = createContext();
-
 const LineMapContainer = ({
   data,
   height,
   width,
   currentTimeString,
+}: {
+  data: LineMapData;
+  height: number;
+  width: number;
+  currentTimeString: string;
 }): JSX.Element => {
   const constants = {
     radius: 14,
@@ -527,10 +537,12 @@ const LineMapContainer = ({
     <LineMapContext.Provider value={params}>
       <g transform={`translate(${constants.marginLeft}, 0)`}>
         <LineMapBase />
-        <ScheduledDeparture
-          stopName={data.stops.origin}
-          schedule={data.schedule}
-        />
+        {showScheduledDeparture && (
+          <ScheduledDeparture
+            stopName={data.stops.origin}
+            schedule={data.schedule}
+          />
+        )}
         <LineMapVehicles
           vehicles={data.vehicles}
           currentTimeString={currentTimeString}
@@ -540,7 +552,15 @@ const LineMapContainer = ({
   );
 };
 
-const LineMap = ({ data, height, currentTimeString }): JSX.Element => {
+const LineMap = ({
+  data,
+  height,
+  currentTimeString,
+}: {
+  data: LineMapData;
+  height: number;
+  currentTimeString: string;
+}): JSX.Element => {
   if (!data) {
     return <div className="line-map"></div>;
   }
