9f494a7be82e543f99741858e3a2f023ebd1dce3
Jon Zimbel <jzimbel@mbta.com>
2020-04-28 10:55:30 -0400

    Tweak functions that create Override structs

--- lib/screens/override.ex
+++ lib/screens/override.ex
@@ -18,16 +18,22 @@ defmodule Screens.Override do
             green_line_service: 1,
             headway_mode_screen_ids: MapSet.new()
 
-  @map_set_keys [:disabled_screen_ids, :headway_mode_screen_ids]
-
   @spec new :: __MODULE__.t()
   def new, do: %__MODULE__{}
 
+  @type json_map :: %{
+    optional(:globally_disabled) => boolean(),
+    optional(:disabled_screen_ids) => list(pos_integer()),
+    optional(:bus_service) => pos_integer(),
+    optional(:green_line_service) => pos_integer(),
+    optional(:headway_mode_screen_ids) => list(pos_integer())
+  }
+
   @doc """
-  Creates a new Override struct from a map.
+  Creates a new Override struct from parsed JSON.
 
   ## Example
-      iex> Screens.Override.from(%{bus_service: 2, disabled_screen_ids: [1, 2, 2], invalid_key: false})
+      iex> Screens.Override.from_json(%{bus_service: 2, disabled_screen_ids: [1, 2, 2], invalid_key: false})
       %Override{
         bus_service: 2,
         disabled_screen_ids: #MapSet<[1, 2]>,
@@ -36,14 +42,24 @@ defmodule Screens.Override do
         headway_mode_screen_ids: #MapSet<[]>
       }
   """
-  @spec from(map()) :: __MODULE__.t()
-  def from(map) do
-    struct(__MODULE__, convert_map_set_keys(map))
+  @spec from_json(json_map()) :: __MODULE__.t()
+  def from_json(%{} = json_map) do
+    converted = json_map
+    |> disabled_map_set()
+    |> headway_mode_map_set()
+
+    struct(__MODULE__, converted)
   end
 
-  @spec convert_map_set_keys(map()) :: map()
-  defp convert_map_set_keys(map) do
-    map_set_keys = for {k, v} when k in @map_set_keys <- map, into: %{}, do: {k, MapSet.new(v)}
-    Map.merge(map, map_set_keys)
+  # Converts the `disabled_screen_ids` list to a MapSet, if it exists
+  defp disabled_map_set(%{disabled_screen_ids: disabled_screen_ids} = json_map) do
+    Map.put(json_map, :disabled_screen_ids, MapSet.new(disabled_screen_ids))
   end
+  defp disabled_map_set(%{} = json_map), do: json_map
+
+  # Converts the `disabled_screen_ids` list to a MapSet, if it exists
+  defp headway_mode_map_set(%{headway_mode_screen_ids: headway_mode_screen_ids} = json_map) do
+    Map.put(json_map, :headway_mode_screen_ids, MapSet.new(headway_mode_screen_ids))
+  end
+  defp headway_mode_map_set(%{} = json_map), do: json_map
 end
