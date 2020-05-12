3d17e13f2ba46ce4ac022891443a37080ee78302
Paul Handorff <handorff.paul@gmail.com>
2020-05-12 13:36:09 -0400

    fix: log mercury error causes

--- lib/screens/mercury_data/fetch.ex
+++ lib/screens/mercury_data/fetch.ex
@@ -1,18 +1,46 @@
 defmodule Screens.MercuryData.Fetch do
   @moduledoc false
 
+  require Logger
+
   @api_url_base "https://cms.mercuryinnovation.com.au/ExtApi/devices"
 
   def fetch_data do
     headers = [{"apikey", Application.get_env(:screens, :mercury_api_key)}]
 
-    with {:ok, response} <- HTTPoison.get(@api_url_base, headers),
-         %{status_code: 200, body: body} <- response,
-         {:ok, parsed} <- Jason.decode(body) do
+    with {:http_request, {:ok, response}} <-
+           {:http_request, HTTPoison.get(@api_url_base, headers)},
+         {:response_success, %{status_code: 200, body: body}} <- {:response_success, response},
+         {:parse, {:ok, parsed}} <- {:parse, Jason.decode(body)} do
       {:ok, Enum.map(parsed, &fetch_relevant_fields/1)}
     else
-      _ -> :error
+      {:http_request, {:error, e}} ->
+        log_fetch_error(:http_fetch_error, %{message: HTTPoison.Error.message(e)})
+
+      {:response_success, %{status_code: status_code}} ->
+        log_fetch_error(:bad_response_code, %{status_code: status_code})
+
+      {:parse, {:error, e}} ->
+        log_fetch_error(:parse_error, %{message: Jason.DecodeError.message(e)})
+
+      _ ->
+        log_fetch_error(:error)
     end
+  end
+
+  defp log_fetch_error(e) do
+    _ = Logger.info("mercury_fetch_error #{e}")
+    :error
+  end
+
+  defp log_fetch_error(e, data) do
+    data_str =
+      data
+      |> Enum.map(fn {key, value} -> "#{key}=#{value}" end)
+      |> Enum.join(" ")
+
+    _ = Logger.info("mercury_fetch_error #{e} #{data_str}")
+    :error
   end
 
   defp fetch_relevant_fields(%{
