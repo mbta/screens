3c04cf2608b226eec7d44b9400fa083f9bac36a3
Jon Zimbel <jzimbel@mbta.com>
2020-06-08 19:18:07 -0400

    Initial debug-filled code with infinite loop

--- assets/src/components/solari/section_list.tsx
+++ assets/src/components/solari/section_list.tsx
@@ -1,4 +1,4 @@
-import React, { useRef, useState, useLayoutEffect } from "react";
+import React from "react";
 import _ from "lodash";
 
 import { PagedSection, Section } from "Components/solari/section";
@@ -84,59 +84,176 @@ const assignSectionSizesHelper = (
   return roundedSizes;
 };
 
-const SectionList = ({
-  sections,
-  sectionHeaders,
-  currentTimeString,
-}): JSX.Element => {
-  const MAX_DEPARTURES_HEIGHT = 1565;
+interface SectionListProps {
+  sections: object[];
+  sectionHeaders: string;
+  currentTimeString: string;
+}
 
-  const ref = useRef(null);
-  const initialRows = totalRows(sections);
-  const initialSizes = assignSectionSizes(sections, initialRows);
-  const initialState = { numRows: initialRows, sectionSizes: initialSizes };
-  const [state, setState] = useState(initialState);
+interface SectionListState {
+  numRows: number;
+  sectionSizes: number[];
+}
 
-  useLayoutEffect(() => {
-    if (ref.current) {
-      const departuresHeight = ref.current.clientHeight;
+const MAX_DEPARTURES_HEIGHT = 1565;
 
-      if (departuresHeight > MAX_DEPARTURES_HEIGHT && state.numRows > 5) {
-        const newRows = state.numRows - 1;
-        const newSizes = assignSectionSizes(sections, newRows);
-        const newState = { numRows: newRows, sectionSizes: newSizes };
-        setState(newState);
+const debug = <T extends Array<any>, U>(fn: (...args: T) => U) => (
+  ...args: T
+) => {
+  console.log(`called ${fn.name} with`, ...args);
+  const result = fn(...args);
+  if (result != undefined) {
+    console.log(`${fn.name} returned`, result);
+  }
+  return result;
+};
+
+class SectionList extends React.Component<SectionListProps, SectionListState> {
+  ref: React.RefObject<HTMLDivElement>;
+
+  constructor(props: SectionListProps) {
+    super(props);
+    this.ref = React.createRef();
+
+    this.state = debug(SectionList.getInitialStateFromProps)(props);
+  }
+
+  static getInitialStateFromProps(props: SectionListProps) {
+    const initialRows = debug(totalRows)(props.sections);
+    const initialSizes = debug(assignSectionSizes)(props.sections, initialRows);
+    return { numRows: initialRows, sectionSizes: initialSizes };
+  }
+
+  componentDidMount() {
+    debug(this.maybeAdjustSectionSizes.bind(this))();
+  }
+
+  componentDidUpdate(
+    _prevProps: SectionListProps,
+    prevState: SectionListState
+  ) {
+    if (debug(this.stateEquals.bind(this))(prevState)) {
+      this.setState((_, prevProps) =>
+        debug(SectionList.getInitialStateFromProps)(prevProps)
+      );
+    } else {
+      debug(this.maybeAdjustSectionSizes.bind(this))();
+    }
+  }
+
+  stateEquals(prevState: SectionListState) {
+    const { numRows, sectionSizes } = this.state;
+    return (
+      numRows == prevState.numRows &&
+      sectionSizes.length == prevState.sectionSizes.length &&
+      sectionSizes.every((n, i) => n == prevState.sectionSizes[i])
+    );
+  }
+
+  maybeAdjustSectionSizes() {
+    if (this.ref.current != null) {
+      const departuresHeight = this.ref.current.clientHeight;
+
+      if (departuresHeight > MAX_DEPARTURES_HEIGHT && this.state.numRows > 5) {
+        this.setState((prevState, prevProps) => {
+          const newRows = prevState.numRows - 1;
+          const newSizes = assignSectionSizes(prevProps.sections, newRows);
+          return { numRows: newRows, sectionSizes: newSizes };
+        });
       }
     }
-  });
+  }
 
-  return (
-    <div className="section-list" ref={ref}>
-      {sections.map((section, i) => {
-        if (section.paging && section.paging.is_enabled === true) {
-          return (
-            <PagedSection
-              {...section}
-              numRows={state.sectionSizes[i]}
-              sectionHeaders={sectionHeaders}
-              currentTimeString={currentTimeString}
-              key={section.name + state.sectionSizes[i] + currentTimeString}
-            />
-          );
-        } else {
-          return (
-            <Section
-              {...section}
-              numRows={state.sectionSizes[i]}
-              sectionHeaders={sectionHeaders}
-              currentTimeString={currentTimeString}
-              key={section.name + state.sectionSizes[i] + currentTimeString}
-            />
-          );
-        }
-      })}
-    </div>
-  );
-};
+  render() {
+    const { sections, sectionHeaders, currentTimeString } = this.props;
+
+    return (
+      <div className="section-list" ref={this.ref}>
+        {sections.map((section, i) => {
+          if (section.paging && section.paging.is_enabled === true) {
+            return (
+              <PagedSection
+                {...section}
+                numRows={this.state.sectionSizes[i]}
+                sectionHeaders={sectionHeaders}
+                currentTimeString={currentTimeString}
+                key={
+                  section.name + this.state.sectionSizes[i] + currentTimeString
+                }
+              />
+            );
+          } else {
+            return (
+              <Section
+                {...section}
+                numRows={this.state.sectionSizes[i]}
+                sectionHeaders={sectionHeaders}
+                currentTimeString={currentTimeString}
+                key={
+                  section.name + this.state.sectionSizes[i] + currentTimeString
+                }
+              />
+            );
+          }
+        })}
+      </div>
+    );
+  }
+}
+
+// const _SectionList = ({
+//   sections,
+//   sectionHeaders,
+//   currentTimeString,
+// }): JSX.Element => {
+//   const MAX_DEPARTURES_HEIGHT = 1565;
+
+//   const ref = useRef(null);
+//   const initialRows = totalRows(sections);
+//   const initialSizes = assignSectionSizes(sections, initialRows);
+//   const initialState = { numRows: initialRows, sectionSizes: initialSizes };
+//   const [state, setState] = useState(initialState);
+
+//   useLayoutEffect(() => {
+//     if (ref.current) {
+//       const departuresHeight = ref.current.clientHeight;
+
+//       if (departuresHeight > MAX_DEPARTURES_HEIGHT && state.numRows > 5) {
+//         const newRows = state.numRows - 1;
+//         const newSizes = assignSectionSizes(sections, newRows);
+//         const newState = { numRows: newRows, sectionSizes: newSizes };
+//         setState(newState);
+//       }
+//     }
+//   });
+
+//   return (
+//     <div className="section-list" ref={ref}>
+//       {sections.map((section, i) => {
+//         if (section.paging && section.paging.is_enabled === true) {
+//           return (
+//             <PagedSection
+//               {...section}
+//               numRows={state.sectionSizes[i]}
+//               sectionHeaders={sectionHeaders}
+//               currentTimeString={currentTimeString}
+//               key={section.name + state.sectionSizes[i] + currentTimeString}
+//             />
+//           );
+//         } else {
+//           return (
+//             <Section
+//               {...section}
+//               numRows={state.sectionSizes[i]}
+//               sectionHeaders={sectionHeaders}
+//               currentTimeString={currentTimeString}
+//               key={section.name + state.sectionSizes[i] + currentTimeString}
+//             />
+//           );
+//         }
+//       })}
+//     </div>
+//   );
+// };
 
 export default SectionList;
