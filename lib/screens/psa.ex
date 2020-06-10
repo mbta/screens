61dc98a156a513a4c1f5ae525784fca004d77b8f
Paul Handorff <handorff.paul@gmail.com>
2020-06-10 11:17:30 -0400

    fix: cr response

--- lib/screens/psa.ex
+++ lib/screens/psa.ex
@@ -1,28 +1,31 @@
 defmodule Screens.Psa do
   @moduledoc false
 
+  @eink_refresh_seconds 30
+  @solari_refresh_seconds 15
+
   alias Screens.Override.State
 
   def current_bus_psa do
-    current_psa_from_list(State.bus_psa_list(), 30)
+    choose_from_rotating_list(State.bus_psa_list(), @eink_refresh_seconds)
   end
 
   def current_green_line_psa do
-    current_psa_from_list(State.green_line_psa_list(), 30)
+    choose_from_rotating_list(State.green_line_psa_list(), @eink_refresh_seconds)
   end
 
   def show_solari_psa do
-    current_psa_from_list([true, false, false], 15)
+    choose_from_rotating_list([true, false, false], @solari_refresh_seconds)
   end
 
-  defp current_psa_from_list([], _), do: nil
-  defp current_psa_from_list([psa], _), do: psa
+  defp choose_from_rotating_list([], _), do: nil
+  defp choose_from_rotating_list([psa], _), do: psa
 
-  defp current_psa_from_list(psa_list, seconds_to_show) do
+  defp choose_from_rotating_list(list, seconds_to_show) do
     t = DateTime.utc_now()
     seconds_since_midnight = t.hour * 60 * 60 + t.minute * 60 + t.second
     periods_since_midnight = div(seconds_since_midnight, seconds_to_show)
-    psa_index = rem(periods_since_midnight, length(psa_list))
-    Enum.at(psa_list, psa_index)
+    current_index = rem(periods_since_midnight, length(list))
+    Enum.at(list, current_index)
   end
 end
