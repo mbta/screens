342d833f05372f956b1fc1dbe8811a0ec2513988
Paul Handorff <handorff.paul@gmail.com>
2020-06-12 09:03:33 -0400

    feat: progress bar for PSA component

--- assets/src/components/solari/psa.tsx
+++ assets/src/components/solari/psa.tsx
@@ -3,8 +3,9 @@ import React from "react";
 const Psa = ({ psaName, currentTimeString }): JSX.Element => {
   const imageSrc = `https://mbta-dotcom.s3.amazonaws.com/screens/images/psa/${psaName}.png`;
   return (
-    <div className="screen-container__flex-space" key={currentTimeString}>
+    <div className="psa" key={currentTimeString}>
       <img src={imageSrc} />
+      <div className="psa__progress-bar" />
     </div>
   );
 };
