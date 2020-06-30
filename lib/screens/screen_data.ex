d2c9330c236b13e0e002a48e2ccf7ec44aa730b7
Jon Zimbel <jzimbel@mbta.com>
2020-06-30 10:42:36 -0400

    Simplify by_screen_id

--- lib/screens/screen_data.ex
+++ lib/screens/screen_data.ex
@@ -10,40 +10,36 @@ defmodule Screens.ScreenData do
     "solari" => Screens.SolariScreenData
   }
 
+  @disabled_response %{force_reload: false, success: false}
+
+  @outdated_response %{force_reload: true}
+
   def by_screen_id(screen_id, is_screen, opts \\ []) do
     check_disabled = Keyword.get(opts, :check_disabled, false)
 
     client_version = Keyword.get(opts, :client_version, nil)
     check_version = not is_nil(client_version)
 
-    response = nil
+    response =
+      cond do
+        check_disabled and disabled?(screen_id) -> @disabled_response
+        check_version and outdated?(client_version) -> @outdated_response
+        true -> fetch_data(screen_id, is_screen)
+      end
+
+    _ = log(screen_id, client_version, is_screen)
 
     response
-    |> response_pipe(check_disabled and disabled?(screen_id), &disabled_response/0)
-    |> response_pipe(check_version and outdated?(client_version), &outdated_response/0)
-    |> response_pipe(true, fn -> fetch_data(screen_id, is_screen) end)
-    |> log(screen_id, client_version, is_screen)
   end
 
   def by_screen_id_with_datetime(screen_id, datetime_str) do
-    app_id = app_id_from(screen_id)
+    app_id = app_id_from_screen_id(screen_id)
 
     {:ok, naive} = Timex.parse(datetime_str, "{ISO:Extended}")
     {:ok, local} = DateTime.from_naive(naive, "America/New_York")
     {:ok, datetime} = DateTime.shift_zone(local, "Etc/UTC")
     screen_data_module = Map.get(@modules_by_app_id, app_id)
     screen_data_module.by_screen_id(screen_id, false, datetime)
-  end
-
-  defp disabled_response do
-    %{
-      force_reload: false,
-      success: false
-    }
-  end
-
-  defp outdated_response do
-    %{force_reload: true}
   end
 
   defp disabled?(screen_id) do
@@ -59,33 +55,17 @@ defmodule Screens.ScreenData do
   end
 
   defp fetch_data(screen_id, is_screen) do
-    app_id = app_id_from(screen_id)
+    app_id = app_id_from_screen_id(screen_id)
 
     screen_data_module = Map.get(@modules_by_app_id, app_id)
     screen_data_module.by_screen_id(screen_id, is_screen)
   end
 
-  defp log(response, screen_id, client_version, is_screen) do
-    _ =
-      LogScreenData.log_api_response(
-        screen_id,
-        client_version,
-        is_screen
-      )
-
-    response
+  defp log(screen_id, client_version, is_screen) do
+    LogScreenData.log_api_response(screen_id, client_version, is_screen)
   end
 
-  defp response_pipe(response, condition, func)
-       when is_boolean(condition) and is_function(func, 0) do
-    if is_nil(response) and condition do
-      func.()
-    else
-      response
-    end
-  end
-
-  defp app_id_from(screen_id) do
+  defp app_id_from_screen_id(screen_id) do
     :screens
     |> Application.get_env(:screen_data)
     |> Map.get(screen_id)
