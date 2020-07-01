3f95a87c1ad6edeccec44a5851dfab7769cf6b7b
Paul Handorff <handorff.paul@gmail.com>
2020-07-01 17:17:31 -0400

    fix: remove unused arg

--- assets/src/components/eink/takeover_alert.tsx
+++ assets/src/components/eink/takeover_alert.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 
-const TakeoverAlert = ({ name, mode }): JSX.Element => {
+const TakeoverAlert = ({ name }): JSX.Element => {
   const imageSrc = `https://mbta-dotcom.s3.amazonaws.com/screens/images/psa/${name}.png`;
   return (
     <div className="takeover-alert">
