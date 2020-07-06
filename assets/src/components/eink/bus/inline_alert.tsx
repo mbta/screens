88198e78760d4572df574d600fb84d783ba109c2
Paul Handorff <handorff.paul@gmail.com>
2020-07-06 18:19:20 -0400

    feat: show bus e-ink delays without minutes

--- assets/src/components/eink/bus/inline_alert.tsx
+++ assets/src/components/eink/bus/inline_alert.tsx
@@ -1,50 +1,12 @@
 import React from "react";
 
-const parseSeverity = (severity) => {
-  let delayDescription;
-  let delayMinutes;
-
-  if (severity < 3) {
-    severity = 3;
-  }
-  if (severity > 9) {
-    severity = 9;
-  }
-
-  if (severity >= 8) {
-    delayDescription = "more than";
-    delayMinutes = 30 * (severity - 7);
-  } else {
-    delayDescription = "up to";
-    delayMinutes = 5 * (severity - 1);
-  }
-
-  return {
-    delayDescription,
-    delayMinutes,
-  };
-};
-
-const InlineAlert = ({ inlineBadges }): JSX.Element => {
-  let severity;
-
-  if (inlineBadges && inlineBadges.length > 0) {
-    severity = inlineBadges[0].severity;
-  }
-
-  if (severity === undefined) {
-    return <div></div>;
-  }
-
-  const { delayDescription, delayMinutes } = parseSeverity(severity);
-
+const InlineAlert = ({ alert }): JSX.Element => {
   return (
     <div className="inline-alert">
-      <span className={"inline-alert__badge"}>
+      <div className="inline-alert__badge">
         <img className="inline-alert__icon" src="/images/alert.svg" />
-        Delays {delayDescription + " "}
-        <span className="inline-alert__emphasis">{delayMinutes} minutes</span>
-      </span>
+        <div className="inline-alert__text">Delays</div>
+      </div>
     </div>
   );
 };
