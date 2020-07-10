898e0b920e3bb3a5d0d82147f8c64c5cbd0af2f0
Jon Zimbel <jzimbel@mbta.com>
2020-07-10 10:18:07 -0400

    Create atoms at runtime when parsing JSON config

--- lib/screens/override/local_fetch.ex
+++ lib/screens/override/local_fetch.ex
@@ -3,7 +3,7 @@ defmodule Screens.Override.LocalFetch do
 
   def fetch_config do
     with {:ok, file_contents} <- File.read(Path.join(:code.priv_dir(:screens), "local.json")),
-         {:ok, parsed} <- Jason.decode(file_contents, keys: :atoms!) do
+         {:ok, parsed} <- Jason.decode(file_contents, keys: :atoms) do
       {:ok, Screens.Override.from_json(parsed)}
     else
       _ -> :error
