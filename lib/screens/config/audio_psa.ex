7e67209b5ebc40b0db0c22f61a3b4d062c46c339
Paul Handorff <handorff.paul@gmail.com>
2020-09-17 14:58:27 -0400

    feat: add support for takeover audio PSAs to the config

--- lib/screens/config/audio_psa.ex
+++ lib/screens/config/audio_psa.ex
@@ -1,13 +1,17 @@
 defmodule Screens.Config.AudioPsa do
   @moduledoc false
 
-  @type t :: {:plaintext | :ssml, String.t()} | nil
+  @type t :: {format, String.t(), type} | nil
 
-  @default_psa_type :plaintext
+  @typep format :: :plaintext | :ssml
+  @typep type :: :takeover | :end
+
+  @default_psa_format :plaintext
+  @default_psa_type :end
 
   @spec from_json(map() | :default) :: t()
-  def from_json(%{"type" => type, "text" => text}) when is_binary(text) do
-    {type_from_json(type), text}
+  def from_json(%{"format" => format, "text" => text, "type" => type}) when is_binary(text) do
+    {format_from_json(format), text, type_from_json(type)}
   end
 
   def from_json(_) do
@@ -15,16 +19,33 @@ defmodule Screens.Config.AudioPsa do
   end
 
   @spec to_json(t()) :: map()
-  def to_json({type, text}) do
+  def to_json({format, text, type}) do
     %{
-      "type" => type_to_json(type),
-      "text" => text
+      "format" => format_to_json(format),
+      "text" => text,
+      "type" => type_to_json(type)
     }
   end
 
   def to_json(nil), do: nil
 
-  for type <- ~w[plaintext ssml]a do
+  for format <- ~w[plaintext ssml]a do
+    format_string = Atom.to_string(format)
+
+    defp format_from_json(unquote(format_string)) do
+      unquote(format)
+    end
+
+    defp format_to_json(unquote(format)) do
+      unquote(format_string)
+    end
+  end
+
+  defp format_from_json(_) do
+    @default_psa_format
+  end
+
+  for type <- ~w[takeover end]a do
     type_string = Atom.to_string(type)
 
     defp type_from_json(unquote(type_string)) do
