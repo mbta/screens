aceac0af0d18389446976a4ff10f0e6eebe287f0
Paul Handorff <handorff.paul@gmail.com>
2020-09-23 17:21:23 -0400

    fix(solari): make parenthesized destinations secondary

--- assets/src/components/eink/base_departure_destination.tsx
+++ assets/src/components/eink/base_departure_destination.tsx
@@ -1,15 +1,31 @@
 import React from "react";
 
+const splitDestination = (destination) => {
+  if (destination.includes("via")) {
+    const parts = destination.split(" via ");
+    const primaryDestination = parts[0];
+    const secondaryDestination = "via " + parts[1];
+    return [primaryDestination, secondaryDestination];
+  } else if (destination.includes("(")) {
+    const parts = destination.split(" (");
+    const primaryDestination = parts[0].trim();
+    const secondaryDestination = "(" + parts[1];
+    return [primaryDestination, secondaryDestination];
+  } else {
+    return [destination];
+  }
+};
+
 const BaseDepartureDestination = ({ destination }): JSX.Element => {
   if (!destination) {
     return null;
   }
 
-  if (destination.includes("via")) {
-    const parts = destination.split(" via ");
-    const primaryDestination = parts[0];
-    const secondaryDestination = "via " + parts[1];
+  const [primaryDestination, secondaryDestination] = splitDestination(
+    destination
+  );
 
+  if (secondaryDestination) {
     return (
       <div className="base-departure-destination__container">
         <div className="base-departure-destination__primary">
