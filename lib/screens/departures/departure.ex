b8b9f21aef76e512d1bc060b94a137daef594d99
Paul Handorff <handorff.paul@gmail.com>
2020-09-25 12:46:03 -0400

    fix: deduplicate predictions at central

--- lib/screens/departures/departure.ex
+++ lib/screens/departures/departure.ex
@@ -1,6 +1,8 @@
 defmodule Screens.Departures.Departure do
   @moduledoc false
 
+  require Logger
+
   alias Screens.Predictions.Prediction
   alias Screens.Schedules.Schedule
   alias Screens.Trips.Trip
@@ -84,6 +86,7 @@ defmodule Screens.Departures.Departure do
   defp fetch_predictions_only(%{} = query_params) do
     query_params
     |> Prediction.fetch()
+    |> deduplicate_repeated_trips()
     |> from_predictions()
   end
 
@@ -336,6 +339,40 @@ defmodule Screens.Departures.Departure do
   defp format_query_param({:date, date}) do
     {"date", date}
   end
+
+  defp log_unexpected_groups(groups) do
+    Enum.each(groups, fn {trip_id, predictions} ->
+      route_ids = Enum.map(predictions, & &1.route.id)
+
+      if length(route_ids) > 1 and Enum.at(route_ids, 0) != "64" do
+        Logger.info(
+          "log_unexpected_groups found #{length(route_ids)} predictions on trip #{trip_id} for route #{
+            Enum.at(route_ids, 0)
+          }"
+        )
+      end
+    end)
+
+    groups
+  end
+
+  # If there are multiple predictions along the same trip, choose only the earliest one to display.
+  # This addresses a specific issue at Central where late-night outbound 64 trips which start in
+  # Central Square serve multiple stops we're displaying on the Solari screen there. This shouldn't
+  # happen anywhere else.
+  defp deduplicate_repeated_trips({:ok, predictions}) do
+    deduplicated_predictions =
+      predictions
+      |> Enum.reject(fn %{departure_time: departure_time} -> is_nil(departure_time) end)
+      |> Enum.group_by(fn %{trip: %Trip{id: trip_id}} -> trip_id end)
+      |> log_unexpected_groups()
+      |> Enum.map(fn {_trip_id, predictions} -> Enum.min_by(predictions, & &1.departure_time) end)
+      |> Enum.sort_by(& &1.departure_time)
+
+    {:ok, deduplicated_predictions}
+  end
+
+  defp deduplicate_repeated_trips(:error), do: :error
 
   # This function filters out predictions whose route ID does not equal its trip's route ID.
   #
