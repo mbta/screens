3c4f96a9e92819605d8f07069c922cef0cbc0d37
Paul Handorff <handorff.paul@gmail.com>
2020-11-19 20:04:15 -0500

    feat: DUP timestamps for commuter rail and ferry

--- assets/src/util/time_representation.tsx
+++ assets/src/util/time_representation.tsx
@@ -8,30 +8,55 @@ export type TimeRepresentation =
   | { type: "MINUTES"; minutes: number }
   | { type: "TIMESTAMP"; timestamp: string; ampm: string };
 
+export const timeRepresentationsEqual = (rep1, rep2) => {
+  if (!rep1 || !rep2) {
+    return false;
+  }
+  if (rep1.type !== rep2.type) {
+    return false;
+  }
+
+  if (rep1.type === "TIMESTAMP") {
+    return rep1.ampm === rep2.ampm && rep1.timestamp === rep2.timestamp;
+  }
+
+  if (rep1.type === "MINUTES") {
+    return rep1.minutes === rep2.minutes;
+  }
+
+  if (rep1.type === "TEXT") {
+    return rep1.text === rep2.text;
+  }
+};
+
 export const standardTimeRepresentation = (
   departureTimeString: string,
   currentTimeString: string,
   vehicleStatus: string,
-  stopType: string
+  stopType: string,
+  noMinutes: string,
+  forceTimestamp: string
 ): TimeRepresentation => {
   const departureTime = moment(departureTimeString);
   const currentTime = moment(currentTimeString);
   const secondDifference = departureTime.diff(currentTime, "seconds");
   const minuteDifference = Math.round(secondDifference / 60);
 
-  if (vehicleStatus === "stopped_at" && secondDifference <= 90) {
-    return { type: "TEXT", text: "BRD" };
-  }
-
-  if (secondDifference <= 30) {
-    if (stopType === "first_stop") {
+  if (!forceTimestamp) {
+    if (vehicleStatus === "stopped_at" && secondDifference <= 90) {
       return { type: "TEXT", text: "BRD" };
     }
-    return { type: "TEXT", text: "ARR" };
-  }
 
-  if (minuteDifference < 60) {
-    return { type: "MINUTES", minutes: minuteDifference };
+    if (secondDifference <= 30) {
+      if (stopType === "first_stop") {
+        return { type: "TEXT", text: "BRD" };
+      }
+      return { type: "TEXT", text: "ARR" };
+    }
+
+    if (minuteDifference < 60 && !noMinutes) {
+      return { type: "MINUTES", minutes: minuteDifference };
+    }
   }
 
   const timestamp = departureTime.format("h:mm");
