f6a6bc6f6b8caf6869c2f675cadc1fcc31db702b
Paul Handorff <handorff.paul@gmail.com>
2020-11-30 12:26:38 -0500

    fix: cr response

--- assets/src/components/dup/destination.tsx
+++ assets/src/components/dup/destination.tsx
@@ -32,12 +32,31 @@ const REPLACEMENTS = {
   "Needham Heights": "Needham Hts",
 };
 
-const PHASES = {
-  ONE_LINE_FULL: 0,
-  ONE_LINE_ABBREV: 1,
-  TWO_LINES_FULL: 2,
-  TWO_LINES_ABBREV: 3,
-  DONE: 4,
+enum PHASES {
+  ONE_LINE_FULL,
+  ONE_LINE_ABBREV,
+  TWO_LINES_FULL,
+  TWO_LINES_ABBREV,
+  DONE,
+}
+
+const RenderedDestination = ({ parts, index1, index2, page }) => {
+  let currentPage;
+  if (index1 === parts.length) {
+    currentPage = parts.join(" ");
+  } else {
+    const pages = [
+      parts.slice(0, index1).join(" ") + "…",
+      "…" + parts.slice(index1, index2).join(" "),
+    ];
+    currentPage = pages[page];
+  }
+
+  return (
+    <div className="base-departure-destination__container">
+      <div className="base-departure-destination__primary">{currentPage}</div>
+    </div>
+  );
 };
 
 const Destination = ({ destination }) => {
@@ -53,17 +72,15 @@ const Destination = ({ destination }) => {
   const firstLineRef = useRef(null);
   const secondLineRef = useRef(null);
 
-  destination = REPLACEMENTS[destination] || destination;
-  let parts = destination.split(" ");
+  const correctedDestination = REPLACEMENTS[destination] || destination;
+  let parts = correctedDestination.split(" ");
 
-  const [breakState, setBreakState] = useState({
-    index1: parts.length,
-    index2: parts.length,
-    abbreviate: false,
-    phase: PHASES.ONE_LINE_FULL,
-  });
+  const [index1, setIndex1] = useState(parts.length);
+  const [index2, setIndex2] = useState(parts.length);
+  const [abbreviate, setAbbreviate] = useState(false);
+  const [phase, setPhase] = useState(PHASES.ONE_LINE_FULL);
 
-  if (breakState.abbreviate) {
+  if (abbreviate) {
     parts = parts.map((p) => ABBREVIATIONS[p] || p);
   }
 
@@ -72,51 +89,40 @@ const Destination = ({ destination }) => {
       const firstLines = firstLineRef.current.clientHeight / LINE_HEIGHT;
       const secondLines = secondLineRef.current.clientHeight / LINE_HEIGHT;
 
-      switch (breakState.phase) {
+      switch (phase) {
         case PHASES.ONE_LINE_FULL:
           // Don't abbreviate if it already fits on one line.
           if (firstLines === 1 && secondLines === 0) {
-            setBreakState((s) => ({ ...s, phase: PHASES.DONE }));
+            setPhase(PHASES.DONE);
           } else {
-            setBreakState((s) => ({
-              ...s,
-              abbreviate: true,
-              phase: PHASES.ONE_LINE_ABBREV,
-            }));
+            setAbbreviate(true);
+            setPhase(PHASES.ONE_LINE_ABBREV);
           }
           break;
 
         case PHASES.ONE_LINE_ABBREV:
           // Do abbreviate if it's the difference between fitting on one line and not.
           if (firstLines === 1 && secondLines === 0) {
-            setBreakState((s) => ({ ...s, phase: PHASES.DONE }));
+            setPhase(PHASES.DONE);
           } else {
-            setBreakState((s) => ({
-              ...s,
-              abbreviate: false,
-              phase: PHASES.TWO_LINES_FULL,
-            }));
+            setAbbreviate(false);
+            setPhase(PHASES.TWO_LINES_FULL);
           }
           break;
 
         case PHASES.TWO_LINES_FULL:
           // Don't abbreviate if we fit on two lines either way
           if (firstLines === 1 && secondLines === 1) {
-            setBreakState((s) => ({ ...s, phase: PHASES.DONE }));
+            setPhase(PHASES.DONE);
           } else {
             // Try all possible positions for the line break
-            if (breakState.index1 > 1) {
-              setBreakState(({ index1, ...s }) => ({
-                ...s,
-                index1: index1 - 1,
-              }));
+            if (index1 > 1) {
+              setIndex1((n) => n - 1);
             } else {
-              setBreakState({
-                index1: parts.length,
-                index2: parts.length,
-                abbreviate: true,
-                phase: PHASES.TWO_LINES_ABBREV,
-              });
+              setIndex1(parts.length);
+              setIndex2(parts.length);
+              setAbbreviate(true);
+              setPhase(PHASES.TWO_LINES_ABBREV);
             }
           }
           break;
@@ -124,17 +130,14 @@ const Destination = ({ destination }) => {
         case PHASES.TWO_LINES_ABBREV:
           // Do abbreviate if it's the difference between fitting on two lines and not.
           // Cut off at 2 lines no matter what, so unexpected input doesn't wrap.
-          if (firstLines > 1 && breakState.index1 > 1) {
+          if (firstLines > 1 && index1 > 1) {
             // Find position of first line break
-            setBreakState(({ index1, ...s }) => ({ ...s, index1: index1 - 1 }));
-          } else if (
-            secondLines > 1 &&
-            breakState.index2 > breakState.index1 + 1
-          ) {
+            setIndex1((n) => n - 1);
+          } else if (secondLines > 1 && index2 > index1 + 1) {
             // Find position of second line break
-            setBreakState(({ index2, ...s }) => ({ ...s, index2: index2 - 1 }));
+            setIndex2((n) => n - 1);
           } else {
-            setBreakState((s) => ({ ...s, phase: PHASES.DONE }));
+            setPhase(PHASES.DONE);
           }
           break;
       }
@@ -142,35 +145,26 @@ const Destination = ({ destination }) => {
   });
 
   // Render paged version when done determining breaks
-  if (breakState.phase === PHASES.DONE) {
-    let currentPage;
-    if (breakState.index1 === parts.length) {
-      currentPage = parts.join(" ");
-    } else {
-      const pages = [
-        parts.slice(0, breakState.index1).join(" ") + "...",
-        "..." + parts.slice(breakState.index1, breakState.index2).join(" "),
-      ];
-      currentPage = pages[page];
-    }
-
+  if (phase === PHASES.DONE) {
     return (
-      <div className="base-departure-destination__container">
-        <div className="base-departure-destination__primary">{currentPage}</div>
-      </div>
+      <RenderedDestination
+        index1={index1}
+        index2={index2}
+        parts={parts}
+        page={page}
+      />
     );
   }
 
   // Version just for determining line breaks, never visible to riders
   let firstLine;
   let secondLine;
-  if (breakState.index1 === parts.length) {
+  if (index1 === parts.length) {
     firstLine = parts.join(" ");
     secondLine = "";
   } else {
-    firstLine = parts.slice(0, breakState.index1).join(" ") + "...";
-    secondLine =
-      "..." + parts.slice(breakState.index1, breakState.index2).join(" ");
+    firstLine = parts.slice(0, index1).join(" ") + "…";
+    secondLine = "…" + parts.slice(index1, index2).join(" ");
   }
 
   return (
