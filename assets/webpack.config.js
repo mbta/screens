27d0716e83708b1e6d29bbb7cd3337c62f3e2346
Paul Handorff <handorff.paul@gmail.com>
2021-03-02 14:18:34 -0500

    feat: bundle fonts with DUP app

--- assets/webpack.config.js
+++ assets/webpack.config.js
@@ -76,6 +76,20 @@ module.exports = (env, options) => ({
             loader: "sass-loader"
           }
         ]
+      },
+      {
+        test: /\.(woff(2)?|ttf|eot|svg)(\?v=\d+\.\d+\.\d+)?$/,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              name: '[name].[ext]',
+              outputPath: 'fonts/',
+              publicPath: '../fonts/',
+              useRelativePaths: true
+            }
+          }
+        ]
       }
     ]
   },
