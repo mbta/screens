ed08e8fee03c6dc64a3acfa7acf83ef5e384aed1
Jon Zimbel <63608771+jzimbel-mbta@users.noreply.github.com>
2021-03-04 12:03:37 -0500

    feat: Minimal alert instance (#708)

---
+++ lib/screens/v2/widget_instance/alert.ex
@@ -0,0 +1,83 @@
+defmodule Screens.V2.WidgetInstance.Alert do
+  @moduledoc false
+
+  alias Screens.Alerts.Alert
+
+  alias __MODULE__, as: T
+
+  defstruct screen: :ok,
+            alert: nil
+
+  @type config :: :ok
+
+  @type t :: %T{
+          screen: config(),
+          alert: Alert.t()
+        }
+
+  @base_alert_priority 1
+
+  @effect_priorities cancellation: 0,
+                     delay: 1,
+                     detour: 0,
+                     no_service: 0,
+                     shuttle: 0,
+                     suspension: 0,
+                     station_closure: 0,
+                     stop_closure: 0,
+                     stop_moved: 0
+
+  def priority(%T{} = t, alert_active? \\ &Alert.happening_now?/1) do
+    [
+      @base_alert_priority,
+      active_priority(t, alert_active?),
+      informed_entity_priority(t),
+      effect_priority(t)
+    ]
+  end
+
+  def serialize(%T{} = t, alert_active? \\ &Alert.happening_now?/1) do
+    %{
+      pill: :bus,
+      icon: :warning,
+      active_status: if(alert_active?.(t.alert), do: :active, else: :upcoming),
+      header: serialize_header(t),
+      text: serialize_text(t)
+    }
+  end
+
+  def slot_names(%T{}), do: ~w[medium_left medium_right]a
+
+  defp active_priority(%T{alert: alert}, alert_active?) do
+    if alert_active?.(alert), do: 0, else: 1
+  end
+
+  defp informed_entity_priority(%T{
+         screen: :ok,
+         alert: %Alert{informed_entities: _informed_entities}
+       }) do
+    0
+  end
+
+  for {effect, priority} <- @effect_priorities do
+    defp effect_priority(%T{alert: %Alert{effect: unquote(effect)}}) do
+      unquote(priority)
+    end
+  end
+
+  defp effect_priority(_), do: 100
+
+  defp serialize_text(%T{alert: _alert}) do
+    ["Dummy alert text"]
+  end
+
+  defp serialize_header(%T{alert: _alert}) do
+    "Dummy alert header"
+  end
+
+  defimpl Screens.V2.WidgetInstance do
+    def priority(t), do: T.priority(t)
+    def serialize(t), do: T.serialize(t)
+    def slot_names(t), do: T.slot_names(t)
+  end
+end
