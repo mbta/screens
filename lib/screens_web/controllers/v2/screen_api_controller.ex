a8875580d4bfd853040d360d1a119432b78cad93
Paul Handorff <handorff.paul@gmail.com>
2021-03-24 12:03:52 -0400

    feat: make v2 backend use app_id

--- lib/screens_web/controllers/v2/screen_api_controller.ex
+++ lib/screens_web/controllers/v2/screen_api_controller.ex
@@ -1,7 +1,25 @@
 defmodule ScreensWeb.V2.ScreenApiController do
   use ScreensWeb, :controller
 
-  def show(conn, %{"id" => screen_id}) do
+  alias Screens.Config.State
+
+  plug(:check_config)
+
+  defp check_config(conn, _) do
+    if State.ok?() do
+      conn
+    else
+      conn
+      |> put_status(:not_found)
+      |> halt()
+    end
+  end
+
+  def show(conn, %{"id" => screen_id, "last_refresh" => last_refresh}) do
+    is_screen = ScreensWeb.UserAgent.is_screen_conn?(conn)
+
+    _ = Screens.LogScreenData.log_data_request(screen_id, last_refresh, is_screen)
+
     data = Screens.V2.ScreenData.by_screen_id(screen_id)
     json(conn, data)
   end
--- lib/screens_web/controllers/v2/screen_controller.ex
+++ lib/screens_web/controllers/v2/screen_controller.ex
@@ -1,10 +1,61 @@
 defmodule ScreensWeb.V2.ScreenController do
   use ScreensWeb, :controller
 
-  def index(conn, %{"id" => _screen_id}) do
+  alias Screens.Config.{Screen, State}
+
+  @default_app_id :bus_eink
+
+  plug(:check_config)
+  plug(:environment_name)
+  plug(:last_refresh)
+
+  defp check_config(conn, _) do
+    if State.ok?() do
+      conn
+    else
+      conn
+      |> render_not_found()
+      |> halt()
+    end
+  end
+
+  defp last_refresh(conn, _) do
+    now = DateTime.utc_now() |> DateTime.to_iso8601()
+    assign(conn, :last_refresh, now)
+  end
+
+  defp environment_name(conn, _) do
+    environment_name = Application.get_env(:screens, :environment_name)
+    assign(conn, :environment_name, environment_name)
+  end
+
+  def index(conn, %{"id" => screen_id}) do
+    is_screen = ScreensWeb.UserAgent.is_screen_conn?(conn)
+
+    _ = Screens.LogScreenData.log_page_load(screen_id, is_screen)
+
+    case State.screen(screen_id) do
+      %Screen{app_id: app_id} ->
+        conn
+        |> assign(:app_id, app_id)
+        |> put_view(ScreensWeb.V2.ScreenView)
+        |> put_layout({ScreensWeb.V2.LayoutView, "app.html"})
+        |> render("index.html")
+
+      nil ->
+        render_not_found(conn)
+    end
+  end
+
+  def index(conn, _params) do
+    render_not_found(conn)
+  end
+
+  defp render_not_found(conn) do
     conn
-    |> put_view(ScreensWeb.V2.ScreenView)
-    |> put_layout({ScreensWeb.V2.LayoutView, "app.html"})
-    |> render("index.html")
+    |> assign(:app_id, @default_app_id)
+    |> put_status(:not_found)
+    |> put_view(ScreensWeb.ErrorView)
+    |> render("404.html")
   end
 end
