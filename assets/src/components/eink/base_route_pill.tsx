b9ab23494c85fabd3fc02777d612db152be01047
Jon Zimbel <63608771+jzimbel-mbta@users.noreply.github.com>
2021-03-30 17:10:14 -0400

    Handle long slashed bus route numbers on DUPs (#789)
    * Split slashed route parts into separate lines on DUP
    
    * Formatting

--- assets/src/components/eink/base_route_pill.tsx
+++ assets/src/components/eink/base_route_pill.tsx
@@ -3,10 +3,30 @@ import React from "react";
 import { classWithModifier } from "Util/util";
 
 const BaseRoutePill = ({ route }): JSX.Element => {
-  const modifier =
-    typeof route === "string" && route.includes("/")
-      ? "with-slash"
-      : "no-slash";
+  const isSlashRoute = typeof route === "string" && route.includes("/");
+
+  const modifier = isSlashRoute ? "with-slash" : "no-slash";
+
+  if (isSlashRoute) {
+    const parts = route.split("/");
+    const part1 = parts[0] + "/";
+    const part2 = parts[1];
+
+    return (
+      <div className={classWithModifier("base-route-pill__pill", modifier)}>
+        <div
+          className={classWithModifier("base-route-pill__route-text", modifier)}
+        >
+          <div className="base-route-pill__route-text--with-slash__part1">
+            {part1}
+          </div>
+          <div className="base-route-pill__route-text--with-slash__part2">
+            {part2}
+          </div>
+        </div>
+      </div>
+    );
+  }
 
   return (
     <div className={classWithModifier("base-route-pill__pill", modifier)}>
