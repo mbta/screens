015e8093f2969e836e66e85ca3d58294419d4655
Jon Zimbel <jzimbel@mbta.com>
2021-03-30 16:58:10 -0400

    Split slashed route parts into separate lines on DUP

--- assets/src/components/eink/base_route_pill.tsx
+++ assets/src/components/eink/base_route_pill.tsx
@@ -3,10 +3,29 @@ import React from "react";
 import { classWithModifier } from "Util/util";
 
 const BaseRoutePill = ({ route }): JSX.Element => {
+  const isSlashRoute = typeof route === "string" && route.includes("/");
+
   const modifier =
-    typeof route === "string" && route.includes("/")
+    isSlashRoute
       ? "with-slash"
       : "no-slash";
+
+  if (isSlashRoute) {
+    const parts = route.split("/");
+    const part1 = parts[0] + "/";
+    const part2 = parts[1];
+
+    return (
+      <div className={classWithModifier("base-route-pill__pill", modifier)}>
+        <div
+          className={classWithModifier("base-route-pill__route-text", modifier)}
+        >
+          <div className="base-route-pill__route-text--with-slash__part1">{part1}</div>
+          <div className="base-route-pill__route-text--with-slash__part2">{part2}</div>
+        </div>
+      </div>
+    );
+  }
 
   return (
     <div className={classWithModifier("base-route-pill__pill", modifier)}>
