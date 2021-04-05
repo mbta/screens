3c2d7d252fa76c909e67522a355ebbc47cc478bd
Paul Handorff <handorff.paul@gmail.com>
2021-04-05 18:15:36 -0400

    feat: add header to bus eink candidate generator

--- lib/screens/v2/candidate_generator/bus_eink.ex
+++ lib/screens/v2/candidate_generator/bus_eink.ex
@@ -1,8 +1,9 @@
 defmodule Screens.V2.CandidateGenerator.BusEink do
   @moduledoc false
 
+  alias Screens.Config.{Bus, Screen}
   alias Screens.V2.CandidateGenerator
-  alias Screens.V2.WidgetInstance.Placeholder
+  alias Screens.V2.WidgetInstance.{NormalHeader, Placeholder}
 
   @behaviour CandidateGenerator
 
@@ -26,12 +27,25 @@ defmodule Screens.V2.CandidateGenerator.BusEink do
   end
 
   @impl CandidateGenerator
-  def candidate_instances(_config) do
-    [
-      %Placeholder{color: :blue, slot_names: [:header]},
-      %Placeholder{color: :blue, slot_names: [:footer]},
-      %Placeholder{color: :green, slot_names: [:main_content]},
-      %Placeholder{color: :red, slot_names: [:medium_flex]}
-    ]
+  def candidate_instances(config) do
+    header_instances(config) ++
+      [
+        %Placeholder{color: :blue, slot_names: [:footer]},
+        %Placeholder{color: :green, slot_names: [:main_content]},
+        %Placeholder{color: :red, slot_names: [:medium_flex]}
+      ]
+  end
+
+  defp header_instances(config) do
+    %Screen{app_params: %Bus{stop_id: stop_id}} = config
+
+    case Screens.V3Api.get_json("stops", %{"filter[id]" => stop_id}) do
+      {:ok, %{"data" => [stop_data]}} ->
+        %{"attributes" => %{"name" => stop_name}} = stop_data
+        [%NormalHeader{screen: config, text: stop_name, time: DateTime.utc_now()}]
+
+      _ ->
+        []
+    end
   end
 end
