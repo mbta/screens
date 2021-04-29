d61a543f7d07a2ff8730e8f90671f3b6c1871dce
Paul Handorff <handorff.paul@gmail.com>
2021-04-29 14:48:52 -0400

    test: test departures accessor functions

--- lib/screens/v2/departure.ex
+++ lib/screens/v2/departure.ex
@@ -24,7 +24,7 @@ defmodule Screens.V2.Departure do
     end
   end
 
-  defp fetch_predictions_and_schedules(params) do
+  def fetch_predictions_and_schedules(params) do
     with {:ok, predictions} <- Prediction.fetch(params),
          {:ok, schedules} <- Schedule.fetch(params) do
       relevant_predictions = get_relevant_departures(predictions)
@@ -36,7 +36,19 @@ defmodule Screens.V2.Departure do
     end
   end
 
-  defp get_relevant_departures(predictions_or_schedules) do
+  def fetch_predictions_only(params) do
+    case Prediction.fetch(params) do
+      {:ok, predictions} ->
+        relevant_predictions = get_relevant_departures(predictions)
+        departures = Enum.map(relevant_predictions, fn p -> %__MODULE__{prediction: p} end)
+        {:ok, departures}
+
+      :error ->
+        :error
+    end
+  end
+
+  def get_relevant_departures(predictions_or_schedules) do
     predictions_or_schedules
     |> Enum.reject(&in_past_or_nil_time?/1)
     |> Enum.reject(&multi_route_duplicate?/1)
@@ -93,14 +105,12 @@ defmodule Screens.V2.Departure do
       |> Enum.group_by(fn %{trip: %Trip{id: trip_id}} -> trip_id end)
       |> Enum.map(fn {_trip_id, departures} -> Enum.min_by(departures, & &1.departure_time) end)
 
-    deduplicated_predictions =
-      (departures_without_trip ++ deduplicated_predictions_with_trip)
-      |> Enum.sort_by(& &1.departure_time)
-
-    deduplicated_predictions
+    departures_without_trip
+    |> Kernel.++(deduplicated_predictions_with_trip)
+    |> Enum.sort_by(& &1.departure_time)
   end
 
-  defp merge_predictions_and_schedules(predictions, schedules) do
+  def merge_predictions_and_schedules(predictions, schedules) do
     predicted_trip_ids =
       predictions
       |> Enum.reject(&is_nil(&1.trip))
@@ -134,71 +144,18 @@ defmodule Screens.V2.Departure do
       end)
       |> Enum.map(fn s -> %__MODULE__{schedule: s} end)
 
-    (predicted_departures ++ unpredicted_departures)
+    predicted_departures
+    |> Kernel.++(unpredicted_departures)
     |> Enum.sort_by(&time/1)
   end
 
-  defp fetch_predictions_only(params) do
-    case Prediction.fetch(params) do
-      {:ok, predictions} ->
-        relevant_predictions = get_relevant_departures(predictions)
-        departures = Enum.map(relevant_predictions, fn p -> %__MODULE__{prediction: p} end)
-        {:ok, departures}
-
-      :error ->
-        :error
-    end
-  end
-
   ### Accessor functions
-  def route_id(%__MODULE__{prediction: p}) when not is_nil(p) do
-    %Prediction{route: %Route{id: route_id}} = p
-    route_id
-  end
-
-  def route_id(%__MODULE__{prediction: nil, schedule: s}) do
-    %Schedule{route: %Route{id: route_id}} = s
-    route_id
-  end
-
-  def headsign(%__MODULE__{prediction: p, schedule: s}) when not is_nil(p) do
-    %Prediction{trip: %Trip{headsign: headsign}} = p
-
-    case s do
-      nil -> headsign
-      %{stop_headsign: nil} -> headsign
-      %{stop_headsign: stop_headsign} -> stop_headsign
-    end
-  end
-
-  def headsign(%__MODULE__{prediction: nil, schedule: s}) do
-    %Schedule{trip: %Trip{headsign: headsign}} = s
-    headsign
-  end
-
   def alerts(%__MODULE__{prediction: p}) when not is_nil(p) do
     %Prediction{alerts: alerts} = p
     alerts
   end
 
   def alerts(%__MODULE__{prediction: nil, schedule: _}), do: []
-
-  def time(%__MODULE__{prediction: p}) when not is_nil(p) do
-    select_departure_time(p)
-  end
-
-  def time(%__MODULE__{prediction: nil, schedule: s}) do
-    select_departure_time(s)
-  end
-
-  def scheduled_time(%__MODULE__{schedule: s}) when not is_nil(s) do
-    select_departure_time(s)
-  end
-
-  def scheduled_time(_), do: nil
-
-  defp select_departure_time(%{arrival_time: t, departure_time: nil}), do: t
-  defp select_departure_time(%{arrival_time: _, departure_time: t}), do: t
 
   def crowding_level(%__MODULE__{prediction: p}) when not is_nil(p) do
     case p do
@@ -215,6 +172,91 @@ defmodule Screens.V2.Departure do
   end
 
   def crowding_level(%__MODULE__{prediction: nil, schedule: _}), do: nil
+
+  def headsign(%__MODULE__{prediction: p, schedule: s}) when not is_nil(p) do
+    %Prediction{trip: %Trip{headsign: headsign}} = p
+
+    case s do
+      nil -> headsign
+      %Schedule{stop_headsign: nil} -> headsign
+      %Schedule{stop_headsign: stop_headsign} -> stop_headsign
+    end
+  end
+
+  def headsign(%__MODULE__{prediction: nil, schedule: s}) do
+    case s do
+      %Schedule{stop_headsign: stop_headsign} when not is_nil(stop_headsign) ->
+        stop_headsign
+
+      %Schedule{trip: %Trip{headsign: headsign}} ->
+        headsign
+    end
+  end
+
+  def route_id(%__MODULE__{prediction: %Prediction{route: %Route{id: route_id}}}) do
+    route_id
+  end
+
+  def route_id(%__MODULE__{prediction: nil, schedule: %Schedule{route: %Route{id: route_id}}}) do
+    route_id
+  end
+
+  def route_type(%__MODULE__{prediction: %Prediction{route: %Route{type: route_type}}}) do
+    route_type
+  end
+
+  def route_type(%__MODULE__{
+        prediction: nil,
+        schedule: %Schedule{route: %Route{type: route_type}}
+      }) do
+    route_type
+  end
+
+  def scheduled_time(%__MODULE__{schedule: s}) when not is_nil(s) do
+    select_departure_time(s)
+  end
+
+  def scheduled_time(_), do: nil
+
+  def stop_type(%__MODULE__{
+        prediction: %Prediction{arrival_time: arrival_time, departure_time: departure_time}
+      }) do
+    identify_stop_type_from_times(arrival_time, departure_time)
+  end
+
+  def stop_type(%__MODULE__{
+        prediction: nil,
+        schedule: %Schedule{arrival_time: arrival_time, departure_time: departure_time}
+      }) do
+    identify_stop_type_from_times(arrival_time, departure_time)
+  end
+
+  def time(%__MODULE__{prediction: p}) when not is_nil(p) do
+    select_departure_time(p)
+  end
+
+  def time(%__MODULE__{prediction: nil, schedule: s}) do
+    select_departure_time(s)
+  end
+
+  def track_number(%__MODULE__{prediction: %Prediction{track_number: track_number}})
+      when not is_nil(track_number) do
+    track_number
+  end
+
+  def track_number(%__MODULE__{prediction: _, schedule: s}), do: s.track_number
+
+  def vehicle_status(%__MODULE__{prediction: p}) when not is_nil(p) do
+    case p do
+      %Prediction{vehicle: %Vehicle{current_status: current_status}} ->
+        current_status
+
+      _ ->
+        nil
+    end
+  end
+
+  def vehicle_status(_), do: nil
 
   defp crowding_data_relevant?(%Trip{id: trip_trip_id, stops: [first_stop | _]}, %Vehicle{
          current_status: current_status,
@@ -234,50 +276,14 @@ defmodule Screens.V2.Departure do
   defp crowding_level_from_occupancy_status(:full), do: 3
   defp crowding_level_from_occupancy_status(nil), do: nil
 
-  def vehicle_status(%__MODULE__{prediction: p}) when not is_nil(p) do
-    case p do
-      %Prediction{vehicle: %Vehicle{current_status: current_status}} ->
-        current_status
+  defp select_departure_time(%{arrival_time: t, departure_time: nil}), do: t
+  defp select_departure_time(%{arrival_time: _, departure_time: t}), do: t
 
-      _ ->
-        nil
-    end
-  end
-
-  def vehicle_status(%__MODULE__{prediction: nil, schedule: _}), do: nil
-
-  def stop_type(%__MODULE__{
-        prediction: %Prediction{arrival_time: arrival_time, departure_time: departure_time}
-      }) do
+  defp identify_stop_type_from_times(arrival_time, departure_time) do
     case {arrival_time, departure_time} do
       {nil, _} -> :first_stop
       {_, nil} -> :last_stop
       {_, _} -> :mid_route_stop
     end
   end
-
-  def stop_type(%__MODULE__{
-        prediction: nil,
-        schedule: %Schedule{arrival_time: arrival_time, departure_time: departure_time}
-      }) do
-    case {arrival_time, departure_time} do
-      {nil, _} -> :first_stop
-      {_, nil} -> :last_stop
-      {_, _} -> :mid_route_stop
-    end
-  end
-
-  def route_type(%__MODULE__{prediction: %Prediction{route: %Route{type: route_type}}}) do
-    route_type
-  end
-
-  def route_type(%__MODULE__{
-        prediction: nil,
-        schedule: %Schedule{route: %Route{type: route_type}}
-      }) do
-    route_type
-  end
-
-  def track_number(%__MODULE__{prediction: p}) when not is_nil(p), do: p.track_number
-  def track_number(%__MODULE__{prediction: nil, schedule: s}), do: s.track_number
 end
