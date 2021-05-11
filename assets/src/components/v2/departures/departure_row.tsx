6afc2edc9700c2e5ff8d7e09b8c91bf2a8f13e9f
Paul Handorff <handorff.paul@gmail.com>
2021-05-11 16:17:27 -0400

    feat: normal section component

---
+++ assets/src/components/v2/departures/departure_row.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const DepartureRow = ({
+  headsign,
+  route,
+  times_with_crowding: timesWithCrowding,
+  inline_alerts: inlineAlerts,
+}) => {
+  return null;
+};
+
+export default DepartureRow;
--- assets/src/components/v2/departures/normal_section.tsx
+++ assets/src/components/v2/departures/normal_section.tsx
@@ -1,7 +1,15 @@
 import React from "react";
 
+import DepartureRow from "Components/v2/departures/departure_row";
+
 const NormalSection = ({ rows }) => {
-  return null;
+  return (
+    <div className="departures-section">
+      {rows.map((rowProps) => (
+        <DepartureRow {...rowProps} key={JSON.stringify(rowProps)} />
+      ))}
+    </div>
+  );
 };
 
 export default NormalSection;
