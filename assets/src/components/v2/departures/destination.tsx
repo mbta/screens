e9efd4791593004c4bb14cfcd2a421f65bfed8b3
Paul Handorff <handorff.paul@gmail.com>
2021-05-12 06:12:27 -0400

    feat: destination component with bus shelter screen styles

--- assets/src/components/v2/departures/destination.tsx
+++ assets/src/components/v2/departures/destination.tsx
@@ -1,7 +1,14 @@
 import React from "react";
 
-const Destination = () => {
-  return null;
+const Destination = ({ headsign, variation }) => {
+  return (
+    <div className="departure-destination">
+      <div className="departure-destination__headsign">{headsign}</div>
+      {variation && (
+        <div className="departure-destination__variation">{variation}</div>
+      )}
+    </div>
+  );
 };
 
 export default Destination;
