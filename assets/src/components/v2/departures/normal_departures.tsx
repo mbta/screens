b88b71c612c1ec5e5cbb2440a0e3c7f7b7ed30e6
Paul Handorff <handorff.paul@gmail.com>
2021-05-18 08:00:49 -0400

    fix: add missing onDoneSizing call

--- assets/src/components/v2/departures/normal_departures.tsx
+++ assets/src/components/v2/departures/normal_departures.tsx
@@ -50,6 +50,8 @@ const NormalDeparturesSizer = ({ sections, onDoneSizing }) => {
       setTempSectionSizes((sectionSizes) => {
         return [sectionSizes[0] - 1];
       });
+    } else {
+      onDoneSizing(tempSectionSizes);
     }
   }, [sections, tempSectionSizes]);
 
