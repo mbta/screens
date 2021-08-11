62519e0fb230a8f4d893f235626226e206d45ca2
Jon Zimbel <jzimbel@mbta.com>
2021-08-11 13:15:04 -0400

    Allow evergreen content to supply multiple slot names

--- lib/screens/config/v2/evergreen_content_instance.ex
+++ lib/screens/config/v2/evergreen_content_instance.ex
@@ -4,18 +4,18 @@ defmodule Screens.Config.V2.EvergreenContentInstance do
   alias Screens.V2.WidgetInstance
 
   @type t :: %__MODULE__{
-          slot_name: WidgetInstance.slot_id(),
+          slot_names: list(WidgetInstance.slot_id()),
           asset_path: String.t(),
           priority: WidgetInstance.priority()
         }
 
-  @enforce_keys ~w[slot_name asset_path priority]a
+  @enforce_keys ~w[slot_names asset_path priority]a
   defstruct @enforce_keys
 
   use Screens.Config.Struct
 
-  defp value_from_json("slot_name", slot_name) do
-    String.to_existing_atom(slot_name)
+  defp value_from_json("slot_names", slot_names) do
+    Enum.map(slot_names, &String.to_existing_atom/1)
   end
 
   defp value_from_json(_, value), do: value
