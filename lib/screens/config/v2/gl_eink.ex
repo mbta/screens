aa8c7d5bfae1053effcba51db3338df120736c55
Maddox <cmaddox@CTD24876CMADDOX.local>
2021-09-09 12:02:18 -0400

    Added Evergreen Content to GL eink config.

--- lib/screens/config/v2/gl_eink.ex
+++ lib/screens/config/v2/gl_eink.ex
@@ -2,7 +2,7 @@ defmodule Screens.Config.V2.GlEink do
   @moduledoc false
   # credo:disable-for-this-file Credo.Check.Design.DuplicatedCode
 
-  alias Screens.Config.V2.{Alerts, Departures, Footer, LineMap}
+  alias Screens.Config.V2.{Alerts, Departures, Footer, LineMap, EvergreenContentItem}
   alias Screens.Config.V2.Header.Destination
   alias Screens.Util
 
@@ -11,7 +11,8 @@ defmodule Screens.Config.V2.GlEink do
           footer: Footer.t(),
           header: Destination.t(),
           alerts: Alerts.t(),
-          line_map: LineMap.t()
+          line_map: LineMap.t(),
+          evergreen_content: list(EvergreenContentItem.t())
         }
 
   @enforce_keys [:departures, :footer, :header, :alerts, :line_map]
@@ -19,7 +20,8 @@ defmodule Screens.Config.V2.GlEink do
             footer: nil,
             header: nil,
             alerts: nil,
-            line_map: nil
+            line_map: nil,
+            evergreen_content: []
 
   use Screens.Config.Struct,
     children: [
@@ -27,6 +29,7 @@ defmodule Screens.Config.V2.GlEink do
       footer: Footer,
       header: Destination,
       alerts: Alerts,
-      line_map: LineMap
+      line_map: LineMap,
+      evergreen_content: {:list, EvergreenContentItem}
     ]
 end
