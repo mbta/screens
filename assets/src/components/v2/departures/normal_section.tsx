f28788ca8988392a16a4ea2bd3f4ac719c6d6805
Maddox <cmaddox@CTD24876CMADDOX.lan1>
2021-10-08 10:29:12 -0400

    Changed conditional to be in a single mapping function.

--- assets/src/components/v2/departures/normal_section.tsx
+++ assets/src/components/v2/departures/normal_section.tsx
@@ -4,17 +4,16 @@ import DepartureRow from "Components/v2/departures/departure_row";
 import NoticeRow from "./notice_row";
 
 const NormalSection = ({ rows }) => {
-  const departure_rows = rows.filter(({ type }) => type === "departure_row");
-  const notice_rows = rows.filter(({ type }) => type === "notice_row");
   return (
     <div>
       <div className="departures-section">
-        {departure_rows.filter(({ type }) => type === "departure_row").map((rowProps) => {
-          const { id, ...data } = rowProps;
-          return <DepartureRow {...data} key={id} />;
-        })}
-        {notice_rows.map((notice, index) => {
-          return <NoticeRow row={notice} key={"notice" + index} />;
+        {rows.map((row, index) => {
+          const { id, type, ...data } = row;
+          if (type === "departure_row") {
+            return <DepartureRow {...data} key={id} />;
+          } else if (type === "notice_row") {
+            return <NoticeRow row={row} key={"notice" + index} />;
+          }
         })}
       </div>
     </div>
