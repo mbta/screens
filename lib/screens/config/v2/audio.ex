2e6bc75378b97d4a1e358e4c99247d65d1c872bc
Maddox <cmaddox@CTD24876CMADDOX.local>
2021-10-12 08:21:26 -0400

    Added new config definition for audio.

---
+++ lib/screens/config/v2/audio.ex
@@ -0,0 +1,18 @@
+defmodule Screens.Config.V2.Audio do
+  @moduledoc false
+
+  @type t :: %__MODULE__{
+          start_time: Time.t(),
+          stop_time: Time.t(),
+          days_active: list(Calendar.day()),
+          volume: float()
+        }
+
+  @enforce_keys [:start_time, :stop_time, :days_active, :volume]
+  defstruct @enforce_keys
+
+  use Screens.Config.Struct
+
+  defp value_from_json(_, value), do: value
+  defp value_to_json(_, value), do: value
+end
