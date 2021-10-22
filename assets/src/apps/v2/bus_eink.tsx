2bd83199feeba75db7ad47871687eaea989a4c3e
Maddox <cmaddox@CTD24876CMADDOX.local>
2021-10-22 14:36:52 -0400

    Added NoData component to bus_eink render.

--- assets/src/apps/v2/bus_eink.tsx
+++ assets/src/apps/v2/bus_eink.tsx
@@ -16,6 +16,8 @@ import NormalHeader from "Components/v2/eink/normal_header";
 import FareInfoFooter from "Components/v2/eink/fare_info_footer";
 import NormalDepartures from "Components/v2/departures/normal_departures";
 import EvergreenContent from "Components/v2/evergreen_content";
+import { ResponseMapper, ResponseMapperContext } from "Components/v2/screen_container";
+import NoData from "Components/v2/eink/no_data";
 
 const TYPE_TO_COMPONENT = {
   normal: NormalScreen,
@@ -25,6 +27,27 @@ const TYPE_TO_COMPONENT = {
   normal_header: NormalHeader,
   departures: NormalDepartures,
   evergreen_content: EvergreenContent,
+  no_data: NoData
+};
+
+const DISABLED_LAYOUT = {
+  full_screen: {
+    type: "no_data",
+  },
+  type: "full_takeover",
+};
+
+const FAILURE_LAYOUT = DISABLED_LAYOUT;
+
+const responseMapper: ResponseMapper = (apiResponse) => {
+  switch (apiResponse.state) {
+    case "success":
+      return apiResponse.data;
+    case "disabled":
+      return DISABLED_LAYOUT;
+    case "failure":
+      return FAILURE_LAYOUT;
+  }
 };
 
 const App = (): JSX.Element => {
@@ -33,11 +56,13 @@ const App = (): JSX.Element => {
       <Switch>
         <Route path="/v2/screen/:id">
           <MappingContext.Provider value={TYPE_TO_COMPONENT}>
-            <ScreenPage />
+            <ResponseMapperContext.Provider value={responseMapper}>
+              <ScreenPage />
+            </ResponseMapperContext.Provider>
           </MappingContext.Provider>
         </Route>
       </Switch>
-    </Router>
+    </Router >
   );
 };
 
