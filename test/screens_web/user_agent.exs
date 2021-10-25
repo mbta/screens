0e11df08fb14e29be346b82ef142d2e1db86ff10
Fauble <bfauble@CTD25368BFAUBLE.local>
2021-10-25 10:53:09 -0500

    add is_real_screen query param handling on backend

---
+++ test/screens_web/user_agent.exs
@@ -0,0 +1,29 @@
+defmodule ScreensWeb.UserAgentTest do
+  use ScreensWeb.ConnCase
+
+  describe "is_screen_conn?/2" do
+    test "returns true if is_real_screen query param is set to true" do
+      conn = :get
+        |> build_conn("/v2/api/screen/1", %{ "is_real_screen" => "true" })
+        |> put_req_header("user-agent", "fake")
+
+      assert ScreensWeb.UserAgent.is_screen_conn?(conn, "1")
+    end
+
+    test "returns false if is_real_screen query param is set to false" do
+      conn = :get
+        |> build_conn("/v2/api/screen/1", %{ "is_real_screen" => "false" })
+        |> put_req_header("user-agent", "fake")
+
+      refute ScreensWeb.UserAgent.is_screen_conn?(conn, "1")
+    end
+
+    test "returns false if is_real_screen query param is not set to true and user-agent unrecognized" do
+      conn = :get
+        |> build_conn("/v2/api/screen/1")
+        |> put_req_header("user-agent", "fake")
+
+      refute ScreensWeb.UserAgent.is_screen_conn?(conn, "1")
+    end
+  end
+end
