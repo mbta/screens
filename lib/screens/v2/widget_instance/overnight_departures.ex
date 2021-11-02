56fcb739c95d0cde7eff736b9b53651d0976978b
Christian Maddox <christian.maddox93@gmail.com>
2021-11-02 09:47:59 -0400

    fix/feat: Added new widget for overnight on GL eInk. (#1108)
    * Fixed logic so nil headway does not crash the app.
    
    * Added an overnight_departures widget.
    
    * Changed logic to add overnight_departures if post_processing indicates one is needed.
    
    * Hooked up overnight_departures to gl_eink.
    
    * Removed inspect.
    
    * Removed unused attributes.
    
    * Added a test.
    
    * Changed styling of time to match normal screen layout.
    
    * Added test file for overnight_departures.
    
    * Added audio view for overnight_departures.
    
    Co-authored-by: Maddox <cmaddox@CTD24876CMADDOX.local>

---
+++ lib/screens/v2/widget_instance/overnight_departures.ex
@@ -0,0 +1,31 @@
+defmodule Screens.V2.WidgetInstance.OvernightDepartures do
+  @moduledoc false
+
+  defstruct screen: nil
+
+  @type t :: %__MODULE__{}
+
+  def priority(_instance), do: [1]
+
+  def serialize(_instance) do
+    %{}
+  end
+
+  def slot_names(_instance), do: [:full_screen]
+  def widget_type(_instance), do: :overnight_departures
+  def valid_candidate?(_instance), do: true
+
+  defimpl Screens.V2.WidgetInstance do
+    alias Screens.V2.WidgetInstance.OvernightDepartures
+
+    def priority(instance), do: OvernightDepartures.priority(instance)
+    def serialize(instance), do: OvernightDepartures.serialize(instance)
+    def slot_names(instance), do: OvernightDepartures.slot_names(instance)
+    def widget_type(instance), do: OvernightDepartures.widget_type(instance)
+    def valid_candidate?(instance), do: OvernightDepartures.valid_candidate?(instance)
+    def audio_serialize(_instance), do: %{}
+    def audio_sort_key(_instance), do: 0
+    def audio_valid_candidate?(_instance), do: false
+    def audio_view(_instance), do: ScreensWeb.V2.Audio.OvernightDeparturesView
+  end
+end
---
+++ test/screens/v2/widget_instance/overnight_departures_test.exs
@@ -0,0 +1,59 @@
+defmodule Screens.V2.WidgetInstance.OvernightDeparturesTest do
+  use ExUnit.Case, async: true
+
+  alias Screens.V2.WidgetInstance
+
+  setup do
+    %{
+      instance: %WidgetInstance.OvernightDepartures{}
+    }
+  end
+
+  describe "priority/1" do
+    test "returns 1", %{instance: instance} do
+      assert [1] == WidgetInstance.priority(instance)
+    end
+  end
+
+  describe "serialize/1" do
+    test "returns empty map", %{instance: instance} do
+      assert %{} == WidgetInstance.serialize(instance)
+    end
+  end
+
+  describe "slot_names/1" do
+    test "returns full_screen", %{instance: instance} do
+      assert [:full_screen] == WidgetInstance.slot_names(instance)
+    end
+  end
+
+  describe "widget_type/1" do
+    test "returns overnight_departures", %{instance: instance} do
+      assert :overnight_departures == WidgetInstance.widget_type(instance)
+    end
+  end
+
+  describe "audio_serialize/1" do
+    test "returns empty map", %{instance: instance} do
+      assert %{} = WidgetInstance.audio_serialize(instance)
+    end
+  end
+
+  describe "audio_sort_key/1" do
+    test "returns 0", %{instance: instance} do
+      assert 0 == WidgetInstance.audio_sort_key(instance)
+    end
+  end
+
+  describe "audio_valid_candidate?/1" do
+    test "returns false", %{instance: instance} do
+      refute WidgetInstance.audio_valid_candidate?(instance)
+    end
+  end
+
+  describe "audio_view/1" do
+    test "returns OvernightDeparturesView", %{instance: instance} do
+      assert ScreensWeb.V2.Audio.OvernightDeparturesView == WidgetInstance.audio_view(instance)
+    end
+  end
+end
