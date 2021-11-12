729449575fa0296192bf3f702869180a4e4000ec
Maddox <cmaddox@CTD24876CMADDOX.local>
2021-11-12 16:24:04 -0500

    Wrapped init in useEffect to avoid multiple inits.

--- assets/src/hooks/use_sentry.tsx
+++ assets/src/hooks/use_sentry.tsx
@@ -1,12 +1,15 @@
 import * as Sentry from "@sentry/react";
+import { useEffect } from "react";
 
 const useSentry = () => {
   const sentryDsn = document.getElementById("app")?.dataset.sentry;
-  if (sentryDsn) {
-    Sentry.init({
-      dsn: sentryDsn,
-    });
-  }
+  useEffect(() => {
+    if (sentryDsn) {
+      Sentry.init({
+        dsn: sentryDsn,
+      });
+    }
+  }, [sentryDsn]);
 };
 
 export default useSentry;
