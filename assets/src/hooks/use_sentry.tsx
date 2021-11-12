92c92a22781ac2999e19ec75f9459f5c45f8c12b
Maddox <cmaddox@CTD24876CMADDOX.local>
2021-11-12 16:22:33 -0500

    Removed console log.

--- assets/src/hooks/use_sentry.tsx
+++ assets/src/hooks/use_sentry.tsx
@@ -2,7 +2,6 @@ import * as Sentry from "@sentry/react";
 
 const useSentry = () => {
   const sentryDsn = document.getElementById("app")?.dataset.sentry;
-  console.log(sentryDsn);
   if (sentryDsn) {
     Sentry.init({
       dsn: sentryDsn,
