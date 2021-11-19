99d0267afacfd3afbb1757c9cdd5bb4a06ef7c6a
Brian Fauble <bfauble@users.noreply.github.com>
2021-11-19 15:07:26 -0600

    Add client_ip metadata to endpoint logs

---
+++ lib/screens_web/plugs/metadata.ex
@@ -0,0 +1,20 @@
+defmodule ScreensWeb.Plugs.Metadata do
+  require Logger
+
+  def init(default), do: default
+
+  def call(conn, _default) do
+    log_client_ip(conn)
+
+    conn
+  end
+
+  defp log_client_ip(conn) do
+    forwarded_for =
+      conn
+      |> Plug.Conn.get_req_header("x-forwarded-for")
+      |> List.first()
+
+    Logger.metadata(client_ip: forwarded_for || conn.remote_ip)
+  end
+end
