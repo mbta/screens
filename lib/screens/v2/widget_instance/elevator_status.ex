6c7bb2ae1e14ab0e1aa94eb9af819b5a74a02bb8
Maddox <cmaddox@CTD24876CMADDOX.local>
2022-01-07 10:34:06 -0500

    Added ElevatorStatus widget instance with stubs for now.

---
+++ lib/screens/v2/widget_instance/elevator_status.ex
@@ -0,0 +1,40 @@
+defmodule Screens.V2.WidgetInstance.ElevatorStatus do
+  @moduledoc false
+
+  alias Screens.Alerts.Alert
+
+  defstruct screen: nil,
+            now: nil,
+            alerts: nil
+
+  @type t :: %__MODULE__{
+          screen: Screens.Config.Screen.t(),
+          now: DateTime.t(),
+          alerts: list(Alert.t())
+        }
+
+  @max_height_list_container 0
+  @max_height_station_heading 0
+  @max_height_elevator_description
+  @max_height_row_separator
+
+  defimpl Screens.V2.WidgetInstance do
+    def priority(_instance), do: [2]
+
+    def serialize(_instance), do: %{}
+
+    def slot_names(_instance), do: [:main_content_right]
+
+    def widget_type(_instance), do: :elevator_status
+
+    def valid_candidate?(_instance), do: true
+
+    def audio_serialize(_instance), do: %{}
+
+    def audio_sort_key(_instance), do: 0
+
+    def audio_valid_candidate?(_instance), do: false
+
+    def audio_view(_instance), do: ScreensWeb.V2.Audio.ElevatorStatusView
+  end
+end
---
+++ lib/screens_web/views/v2/audio/elevator_status_view.ex
@@ -0,0 +1,7 @@
+defmodule ScreensWeb.V2.Audio.ElevatorStatusView do
+  use ScreensWeb, :view
+
+  def render("_widget.ssml", _) do
+    ~E""
+  end
+end
