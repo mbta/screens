12fb9873c768e8a269aa5f1d8e4355e12bc197d1
Maddox <cmaddox@CTD24876CMADDOX.local>
2022-01-13 10:14:21 -0500

    Added tests.

--- test/screens/v2/widget_instance/elevator_status_test.exs
+++ test/screens/v2/widget_instance/elevator_status_test.exs
@@ -2,10 +2,134 @@ defmodule Screens.V2.WidgetInstance.ElevatorStatusTest do
   use ExUnit.Case, async: true
 
   alias Screens.V2.WidgetInstance
+  alias Screens.Alerts.Alert
+  alias Screens.Config.Screen
+  alias Screens.Config.V2.{ElevatorStatus, PreFare}
 
   setup do
     %{
-      instance: %WidgetInstance.ElevatorStatus{}
+      instance: %WidgetInstance.ElevatorStatus{
+        screen: %Screen{
+          app_params: %PreFare{elevator_status: %ElevatorStatus{parent_station_id: "place-foo"}},
+          vendor: nil,
+          device_id: nil,
+          name: nil,
+          app_id: nil
+        },
+        alerts: [
+          %Alert{
+            effect: :elevator_closure,
+            informed_entities: [
+              %{stop: "place-bar", facility: "1"}
+            ],
+            active_period: [{~U[2022-01-01T00:00:00Z], ~U[2022-01-01T22:00:00Z]}]
+          },
+          %Alert{
+            effect: :elevator_closure,
+            informed_entities: [
+              %{stop: "place-foo", facility: "1"}
+            ],
+            active_period: [{~U[2022-01-01T00:00:00Z], ~U[2022-01-01T22:00:00Z]}]
+          }
+        ],
+        facilities: [%{name: "Elevator 1", id: "1"}],
+        station_id_to_name: %{"place-foo" => "Foo Station", "place-bar" => "Bar Station"},
+        station_id_to_icons: %{"place-foo" => [:red], "place-bar" => [:red]},
+        now: ~U[2022-01-01T10:00:00Z],
+        stop_sequences: [["place-foo"]]
+      },
+      one_active_at_home_instance: %WidgetInstance.ElevatorStatus{
+        screen: %Screen{
+          app_params: %PreFare{elevator_status: %ElevatorStatus{parent_station_id: "place-foo"}},
+          vendor: nil,
+          device_id: nil,
+          name: nil,
+          app_id: nil
+        },
+        alerts: [
+          %Alert{
+            effect: :elevator_closure,
+            informed_entities: [
+              %{stop: "place-foo", facility: "1"}
+            ],
+            active_period: [{~U[2022-01-01T00:00:00Z], ~U[2022-01-01T22:00:00Z]}]
+          }
+        ],
+        facilities: [%{name: "Elevator 1", id: "1"}],
+        station_id_to_name: %{"place-foo" => "Foo Station"},
+        station_id_to_icons: %{"place-foo" => [:red]},
+        now: ~U[2022-01-01T10:00:00Z],
+        stop_sequences: [["place-foo"]]
+      },
+      one_active_elsewhere_instance: %WidgetInstance.ElevatorStatus{
+        screen: %Screen{
+          app_params: %PreFare{elevator_status: %ElevatorStatus{parent_station_id: "place-foo"}},
+          vendor: nil,
+          device_id: nil,
+          name: nil,
+          app_id: nil
+        },
+        alerts: [
+          %Alert{
+            effect: :elevator_closure,
+            informed_entities: [
+              %{stop: "place-bar", facility: "1"}
+            ],
+            active_period: [{~U[2022-01-01T00:00:00Z], ~U[2022-01-01T22:00:00Z]}]
+          }
+        ],
+        facilities: [%{name: "Elevator 1", id: "1"}],
+        station_id_to_name: %{"place-bar" => "Bar Station"},
+        station_id_to_icons: %{"place-bar" => [:red]},
+        now: ~U[2022-01-01T10:00:00Z],
+        stop_sequences: [["place-foo"]]
+      },
+      one_upcoming_at_home_instance: %WidgetInstance.ElevatorStatus{
+        screen: %Screen{
+          app_params: %PreFare{elevator_status: %ElevatorStatus{parent_station_id: "place-foo"}},
+          vendor: nil,
+          device_id: nil,
+          name: nil,
+          app_id: nil
+        },
+        alerts: [
+          %Alert{
+            effect: :elevator_closure,
+            informed_entities: [
+              %{stop: "place-foo", facility: "1"}
+            ],
+            active_period: [{~U[2022-02-01T00:00:00Z], ~U[2022-02-01T22:00:00Z]}]
+          }
+        ],
+        facilities: [%{name: "Elevator 1", id: "1"}],
+        station_id_to_name: %{"place-foo" => "Foo Station"},
+        station_id_to_icons: %{"place-foo" => [:red]},
+        now: ~U[2022-01-01T10:00:00Z],
+        stop_sequences: [["place-foo"]]
+      },
+      one_upcoming_on_connecting_line_instance: %WidgetInstance.ElevatorStatus{
+        screen: %Screen{
+          app_params: %PreFare{elevator_status: %ElevatorStatus{parent_station_id: "place-foo"}},
+          vendor: nil,
+          device_id: nil,
+          name: nil,
+          app_id: nil
+        },
+        alerts: [
+          %Alert{
+            effect: :elevator_closure,
+            informed_entities: [
+              %{stop: "place-bar", facility: "1"}
+            ],
+            active_period: [{~U[2022-02-01T00:00:00Z], ~U[2022-02-01T22:00:00Z]}]
+          }
+        ],
+        facilities: [%{name: "Elevator 1", id: "1"}],
+        station_id_to_name: %{"place-bar" => "Bar Station", "place-foo" => "Foo Station"},
+        station_id_to_icons: %{"place-bar" => [:red], "place-foo" => [:red]},
+        now: ~U[2022-01-01T10:00:00Z],
+        stop_sequences: [["place-foo", "place-bar"]]
+      }
     }
   end
 
@@ -16,8 +140,149 @@ defmodule Screens.V2.WidgetInstance.ElevatorStatusTest do
   end
 
   describe "serialize/1" do
-    test "returns empty map", %{instance: instance} do
-      assert %{} == WidgetInstance.serialize(instance)
+    test "returns a detail page for one active at-home", %{
+      one_active_at_home_instance: instance
+    } do
+      expected_result = %{
+        pages: [
+          %Screens.V2.WidgetInstance.ElevatorStatus.DetailPage{
+            elevator_closure: %{
+              description: nil,
+              elevator_id: "1",
+              elevator_name: "Elevator 1",
+              timeframe: %{
+                active_period: [{~U[2022-01-01 00:00:00Z], ~U[2022-01-01 22:00:00Z]}],
+                happening_now: true
+              }
+            },
+            header_text: nil,
+            icons: [:red]
+          }
+        ]
+      }
+
+      assert expected_result == WidgetInstance.serialize(instance)
+    end
+
+    test "returns a list page for one active elsewhere", %{
+      one_active_elsewhere_instance: instance
+    } do
+      expected_result = %{
+        pages: [
+          %Screens.V2.WidgetInstance.ElevatorStatus.ListPage{
+            stations: [
+              %{
+                name: "Bar Station",
+                icons: [:red],
+                is_at_home_stop: false,
+                elevator_closures: [
+                  %{
+                    description: nil,
+                    elevator_id: "1",
+                    elevator_name: "Elevator 1",
+                    timeframe: %{
+                      active_period: [{~U[2022-01-01 00:00:00Z], ~U[2022-01-01 22:00:00Z]}],
+                      happening_now: true
+                    }
+                  }
+                ]
+              }
+            ]
+          }
+        ]
+      }
+
+      assert expected_result == WidgetInstance.serialize(instance)
+    end
+
+    test "returns a detail page for one upcoming at-home", %{
+      one_upcoming_at_home_instance: instance
+    } do
+      expected_result = %{
+        pages: [
+          %Screens.V2.WidgetInstance.ElevatorStatus.DetailPage{
+            elevator_closure: %{
+              description: nil,
+              elevator_id: "1",
+              elevator_name: "Elevator 1",
+              timeframe: %{
+                active_period: [{~U[2022-02-01 00:00:00Z], ~U[2022-02-01 22:00:00Z]}],
+                happening_now: false
+              }
+            },
+            header_text: nil,
+            icons: [:red]
+          }
+        ]
+      }
+
+      assert expected_result == WidgetInstance.serialize(instance)
+    end
+
+    test "returns a detail page for one upcoming on connecting line", %{
+      one_upcoming_on_connecting_line_instance: instance
+    } do
+      expected_result = %{
+        pages: [
+          %Screens.V2.WidgetInstance.ElevatorStatus.DetailPage{
+            elevator_closure: %{
+              description: nil,
+              elevator_id: "1",
+              elevator_name: "Elevator 1",
+              timeframe: %{
+                active_period: [{~U[2022-02-01 00:00:00Z], ~U[2022-02-01 22:00:00Z]}],
+                happening_now: false
+              }
+            },
+            header_text: nil,
+            icons: [:red]
+          }
+        ]
+      }
+
+      assert expected_result == WidgetInstance.serialize(instance)
+    end
+
+    test "returns ordered list of pages", %{instance: instance} do
+      expected_result = %{
+        pages: [
+          %Screens.V2.WidgetInstance.ElevatorStatus.DetailPage{
+            elevator_closure: %{
+              description: nil,
+              elevator_id: "1",
+              elevator_name: "Elevator 1",
+              timeframe: %{
+                active_period: [{~U[2022-01-01 00:00:00Z], ~U[2022-01-01 22:00:00Z]}],
+                happening_now: true
+              }
+            },
+            header_text: nil,
+            icons: [:red]
+          },
+          %Screens.V2.WidgetInstance.ElevatorStatus.ListPage{
+            stations: [
+              %{
+                name: "Bar Station",
+                icons: [:red],
+                is_at_home_stop: false,
+                elevator_closures: [
+                  %{
+                    description: nil,
+                    elevator_id: "1",
+                    elevator_name: "Elevator 1",
+                    timeframe: %{
+                      active_period: [{~U[2022-01-01 00:00:00Z], ~U[2022-01-01 22:00:00Z]}],
+                      happening_now: true
+                    }
+                  }
+                ]
+              }
+            ]
+          }
+        ]
+      }
+
+      assert expected_result == WidgetInstance.serialize(instance)
     end
   end
 
