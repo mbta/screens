52060e9055678c6b4b3820d240aa20ef807a2bee
Maddox <cmaddox@CTD24876CMADDOX.local>
2022-02-04 08:38:33 -0500

    Started on the DetailPage.

--- assets/src/components/v2/elevator_status.tsx
+++ assets/src/components/v2/elevator_status.tsx
@@ -1,11 +1,10 @@
 import React, { ComponentType, useEffect, useState } from "react";
+import FlexZonePageIndicator from "./flex/page_indicator";
 
 type Page = ListPage | DetailPage;
 
 interface DetailPage {
-  header_text: string;
-  icons: Icon[];
-  elevator_closure: Closure;
+  station: Station;
 }
 
 interface ListPage {
@@ -22,10 +21,12 @@ interface Station {
 interface Closure {
   elevator_name: string;
   elevator_id: string;
+  header_text: string;
   timeframe: {
     happening_now: boolean;
     active_period: ActivePeriod[];
   };
+  description: string;
 }
 
 interface ActivePeriod {
@@ -33,7 +34,15 @@ interface ActivePeriod {
   end: string;
 }
 
-type Icon = "orange" | "red" | "green" | "blue" | "silver" | "rail" | "bus";
+type Icon =
+  | "orange"
+  | "red"
+  | "green"
+  | "blue"
+  | "silver"
+  | "rail"
+  | "bus"
+  | "mattapan";
 
 interface Props {
   pages: Page[];
@@ -64,17 +73,38 @@ const ElevatorStatus: ComponentType<Props> = ({
   }, [pageIndex]);
 
   const page = pages[pageIndex];
+  let pageToRender;
   if (page == null) return null;
   if (instanceOfDetailPage(page)) {
-    return <DetailPageComponent detailPage={page as DetailPage} />;
+    pageToRender = <DetailPageComponent detailPage={page as DetailPage} />;
   } else if (instanceOfListPage(page)) {
-    return <ListPageComponent listPage={page as ListPage} />;
+    pageToRender = <ListPageComponent listPage={page as ListPage} />;
+  } else {
+    return null;
   }
-  return null;
+
+  return (
+    <>
+      <div className="elevator-status">
+        <div className="elevator-status__header">
+          <div className="elevator-status__header-text">Elevator Closures</div>
+          <img src="/images/elevator-status-elevator.svg" />
+        </div>
+        {pageToRender}
+        <div className="elevator-status__footer">
+          <div className="elevator-status__footer-text">
+            For more elevator alerts and directions to alternate accessible
+            paths, visit mbta.com/alerts/access or call 800-392-8100
+          </div>
+        </div>
+      </div>
+      <FlexZonePageIndicator numPages={pages.length} pageIndex={pageIndex} />
+    </>
+  );
 };
 
 const instanceOfDetailPage = (page: Page): page is DetailPage => {
-  return (page as DetailPage).header_text !== undefined;
+  return (page as DetailPage).station !== undefined;
 };
 
 const instanceOfListPage = (page: Page): page is ListPage => {
@@ -88,7 +118,45 @@ interface DetailPageProps {
 const DetailPageComponent: ComponentType<DetailPageProps> = ({
   detailPage,
 }) => {
-  return null;
+  const {
+    station: {
+      is_at_home_stop: isAtHomeStop,
+      name,
+      icons,
+      elevator_closures: elevatorClosures,
+    },
+  } = detailPage;
+  const {
+    header_text: headerText,
+    description,
+    timeframe,
+  } = elevatorClosures[0];
+  return (
+    <div className="detail-page">
+      <div className="detail-page__closure">
+        <div className="detail-page__closure-location">
+          <div className="detail-page__closure-icon">
+            <img src="/images/elevator-status-outage-black.svg" />
+          </div>
+          <div className="detail-page__closure-location-text">
+            {isAtHomeStop ? "At this station" : name}
+          </div>
+          <img src="/images/elevator-status-you-are-here.svg" />
+        </div>
+        <div className="detail-page__closure-header">{headerText}</div>
+        <div className="detail-page__timeframe">
+          <img src="/images/elevator-status-alert-black.svg" />
+          <div className="detail-page__timeframe-text-start">
+            {timeframe.happening_now ? "NOW" : "Upcoming"}
+          </div>
+          <div className="detail-page__timeframe-text-end">
+            Until further notice
+          </div>
+        </div>
+        <div className="detail-page__description">{description}</div>
+      </div>
+    </div>
+  );
 };
 
 interface ListPageProps {
