cbf099a4f56f5213f4ef35d0c9b5b322152f939a
Paul Kim <pkim@mbta.com>
2022-02-24 14:02:25 -0500

    rest of widget instance

--- lib/screens/v2/widget_instance/pre_fare_line_map.ex
+++ lib/screens/v2/widget_instance/pre_fare_line_map.ex
@@ -2,7 +2,6 @@ defmodule Screens.V2.WidgetInstance.PreFareLineMap do
   @moduledoc false
 
   alias Screens.Config.Screen
-  alias Screens.V2.WidgetInstance.PreFareLineMap
   alias Screens.V2.WidgetInstance
 
   defstruct screen: nil,
@@ -19,17 +18,34 @@ defmodule Screens.V2.WidgetInstance.PreFareLineMap do
 
   def serialize(%__MODULE__{asset_url: asset_url}), do: %{asset_url: asset_url}
 
-  def slot_names(%__MODULE__{slot_names: slot_names}), do: slot_names
+  def slot_names(_instance), do: [:main_content_left]
+
+  def priority(_instance), do: [2]
+
+  def widget_type(_instance), do: :pre_fare_line_map
+
+  def valid_candidate?(_instance), do: true
+
+  def audio_serialize(_instance), do: %{}
+
+  def audio_sort_key(_instance), do: 0
+
+  def audio_valid_candidate?(_instance), do: false
+
+  def audio_view(_instance), do: ScreensWeb.V2.Audio.PreFareLineMap
 
   defimpl Screens.V2.WidgetInstance do
-    def priority(%PreFareLineMap{priority: priority}), do: priority
-    def serialize(instance), do: serialize(instance)
-    def slot_names(instance), do: slot_names(instance)
-    def widget_type(_), do: :pre_fare_line_map
-    def valid_candidate?(_instance), do: true
-    def audio_serialize(_instance), do: %{}
-    def audio_sort_key(_instance), do: 0
-    def audio_valid_candidate?(_instance), do: false
-    def audio_view(_instance), do: ScreensWeb.V2.Audio.PreFareLineMap
+    alias Screens.V2.WidgetInstance.PreFareLineMap
+
+    def priority(instance), do: PreFareLineMap.priority(instance)
+    def serialize(instance), do: PreFareLineMap.serialize(instance)
+    def slot_names(instance), do: PreFareLineMap.slot_names(instance)
+    def widget_type(instance), do: PreFareLineMap.widget_type(instance)
+    def valid_candidate?(instance), do: PreFareLineMap.valid_candidate?(instance)
+    def audio_serialize(instance), do: PreFareLineMap.audio_serialize(instance)
+    @spec audio_sort_key(Screens.V2.WidgetInstance.PreFareLineMap.t()) :: 0
+    def audio_sort_key(instance), do: PreFareLineMap.audio_sort_key(instance)
+    def audio_valid_candidate?(instance), do: PreFareLineMap.audio_valid_candidate?(instance)
+    def audio_view(instance), do: PreFareLineMap.audio_view(instance)
   end
 end
