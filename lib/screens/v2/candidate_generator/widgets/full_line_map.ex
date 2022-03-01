a6093b4ecd49c3d564dad272f41711fa6609572c
Paul Kim <pkim@mbta.com>
2022-03-01 18:00:42 -0500

    credo

--- lib/screens/v2/candidate_generator/widgets/full_line_map.ex
+++ lib/screens/v2/candidate_generator/widgets/full_line_map.ex
@@ -3,8 +3,8 @@ defmodule Screens.V2.CandidateGenerator.Widgets.FullLineMap do
 
   alias Screens.Config.Screen
   alias Screens.Config.V2.{FullLineMap, PreFare}
-  alias Screens.V2.WidgetInstance.FullLineMap, as: FullLineMapWidget
   alias Screens.Util.Assets
+  alias Screens.V2.WidgetInstance.FullLineMap, as: FullLineMapWidget
 
   def full_line_map_instances(
         %Screen{app_params: %PreFare{full_line_map: full_line_map}} = config
