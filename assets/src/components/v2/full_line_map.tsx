a2a158fc262649521fcce0cc99267cad53389652
Paul Kim <pkim@mbta.com>
2022-03-08 16:06:28 -0500

    formatting and change to ImageProps

--- assets/src/components/v2/full_line_map.tsx
+++ assets/src/components/v2/full_line_map.tsx
@@ -4,27 +4,31 @@ interface Props {
   asset_urls: string[];
 }
 
-const intervalInMs = 10000
+const intervalInMs = 10000;
 
-const FullLineMap: ComponentType<Props> = ({asset_urls}) => {
+const FullLineMap: ComponentType<Props> = ({ asset_urls }) => {
   const [assetIndex, setAssetIndex] = useState(0);
 
   useEffect(() => {
-  if (assetIndex === asset_urls.length - 1) {
-    setTimeout(() => {setAssetIndex(0)}, intervalInMs)
-  } else {
-    setTimeout(() => {setAssetIndex((i) => i + 1)}, intervalInMs)
-  }
-}, [assetIndex])
+    if (assetIndex === asset_urls.length - 1) {
+      setTimeout(() => {
+        setAssetIndex(0);
+      }, intervalInMs);
+    } else {
+      setTimeout(() => {
+        setAssetIndex((i) => i + 1);
+      }, intervalInMs);
+    }
+  }, [assetIndex]);
 
   return <Image assetUrl={asset_urls[assetIndex]} />;
-}
+};
 
-interface ProperProps {
+interface ImageProps {
   assetUrl: string;
 }
 
-const Image: ComponentType<ProperProps> = ({ assetUrl }) => {
+const Image: ComponentType<ImageProps> = ({ assetUrl }) => {
   return (
     <div className="full-line-map-image__container">
       <img className="full-line-map-image__image" src={assetUrl} />
