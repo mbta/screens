2636d818128aa545244a84ad9146b4ad687d1ffa
Christian Maddox <christian.maddox93@gmail.com>
2022-04-01 16:26:04 -0400

    feat: Reconstructed Alert audio (#1276)
    * Basics for the Reconstructed Alert
    
    * Formatting
    
    * but i want credo to pretend it doesn't see the todos...
    
    * Apply suggestions from code review
    
    * Genericized some code
    
    * Fetch logic
    
    * Tests added
    
    * priority and slot_names for ReconstructedAlert widget_instance
    
    * Fixed things from a poor merge
    
    * formatting!
    
    * Fixed import
    
    * Changed stop sequence fetch to allow for filtering.
    
    * Fixed failing tests and add a new one.
    
    * Removed an inspect.
    
    * Changed variable name to match what it is used for.
    
    * Added a comment that explains each severity value.
    
    * Moved some common alert widget logic to a common module.
    
    * Moved some common code from SubwayStatus.
    
    * Updated serializer logic for reconstructed alerts.
    
    * Renamed common alert module.
    
    * Moved tests for moved function.
    
    * Changed some functions that aren't used elsewhere to private.
    
    * Added some additional filter logic to candidate generator.
    
    * Changed how we get terminal station name for alerts.
    
    * Fixed edge case with location_text.
    
    * Removed remedy from map. Frontend will handle it.
    
    * Added urgent key.
    
    * Tweaked get_destination a bit more to help issue text formatting.
    
    * Changed some alerts to show PIO message if multi lines affected.
    
    * Removed inspects.
    
    * Removed unnecessary assignment.
    
    * Removed unused function.
    
    * Changed issue text for issues with inline icons.
    
    * Finalized issue text for inside station closures.
    
    * Corrected some overlooked text.
    
    * Added a word to text.
    
    * Added and fixed tests.
    
    * Added a case for elsewhere. Never gonna need them.
    
    * Moved tests.
    
    * Fixed tests.
    
    * Changes to satisfy credo and dialyzer.
    
    * Fixed outside station closure issue text.
    
    * Created some helper functions so only one set of constants is needed.
    
    * Changed to helper function calls.
    
    * Added some RoutePill serialize functions for reconstructed alerts.
    
    * Changed routes to return a serialized list of RoutePills.
    
    * Added tests for new functions.
    
    * Removed an unused option.
    
    * Changed comment to @doc.
    
    * Refactored to reduce indentation.
    
    * Removed a duplicate function.
    
    * Added serialization for FreeTextLine.
    
    * Changed how cause is translated to text.
    
    * Fixed tests.
    
    * Changed default capitalization for cause.
    
    * Refactored to better Map function.
    
    * Refactored case to if/else. Thanks Jon.
    
    * Fixed typo and tests.
    
    * Added remedy and region to serialized map.
    
    * Made reconstructed_alert a valid audio candidate.
    
    * Added audio view for reconstructed_alert.
    
    * Fixed some text.
    
    * Added a sort to move GL to the back of the list.
    
    * Saving work.
    
    * Fixed render_affected_routes so it can handle all line combos.
    
    * Add audio_sort_key implementation.
    
    * Added a case to avoid error.
    
    * Fixed test for audio_serialize.
    
    * Fixed access error.
    
    Co-authored-by: Hannah Purcell <hpurcell@mbta.com>
    Co-authored-by: Hannah Purcell <69368883+hannahpurcell@users.noreply.github.com>
    Co-authored-by: Maddox <cmaddox@CTD24876CMADDOX.local>

---
+++ lib/screens_web/views/v2/audio/reconstructed_alert_view.ex
@@ -0,0 +1,112 @@
+defmodule ScreensWeb.V2.Audio.ReconstructedAlertView do
+  use ScreensWeb, :view
+
+  def render("_widget.ssml", %{
+        issue: issue,
+        remedy: remedy,
+        cause: cause,
+        region: :outside,
+        effect: :station_closure
+      }) do
+    ~E|<p><%= issue %> <%= cause %>. <%= remedy %>.</p>|
+  end
+
+  def render("_widget.ssml", %{
+        issue: issue,
+        routes: routes,
+        urgent: false,
+        effect: effect
+      })
+      when effect in [:delay, :moderate_delay] do
+    ~E|<p><%= render_affected_routes(routes) %> delay. <%= issue %>.</p>|
+  end
+
+  # Fix "alternate" pronunciation
+  def render("_widget.ssml", %{
+        issue: issue,
+        remedy: remedy,
+        cause: cause,
+        location: location,
+        routes: routes,
+        effect: effect
+      }) do
+    ~E|<p><%= render_affected_routes(routes) %> alert. <%= render_issue(issue) %> <%= location %> <%= cause %>. <%= remedy %>. <%= render_additional_info_for_effect(effect) %>.</p>|
+  end
+
+  defp render_issue(issue) do
+    case issue do
+      %{text: text} ->
+        text
+        |> Enum.map_join(" ", fn
+          %{icon: icon} -> "#{icon} line"
+          text -> text
+        end)
+
+      text ->
+        text
+    end
+  end
+
+  defp render_affected_routes(routes) do
+    non_gl_text = get_non_gl_text(routes)
+
+    gl_text = get_gl_text(routes)
+
+    cond do
+      String.length(non_gl_text) == 0 -> gl_text
+      String.length(gl_text) == 0 -> gl_text
+      true -> non_gl_text <> " and " <> gl_text
+    end
+  end
+
+  # Builds the text for GL as long as at least one branch is not affected.
+  # Uses hd() because there will only ever be one GL item in routes.
+  defp get_gl_text(routes) do
+    routes
+    |> Enum.filter(&(&1.color == :green and length(&1.branches) < 4))
+    |> Enum.map(fn
+      %{branches: [branch]} -> "Green Line: #{branch} Branch"
+      %{branches: [b1, b2]} -> "Green Line: #{b1} and #{b2} Branches"
+      %{branches: [b1, b2, b3]} -> "Green Line: #{b1}, #{b2}, and #{b3} Branches"
+    end)
+    |> hd()
+  end
+
+  # Builds the text for non-GL lines and/or GL where all branches are affected.
+  defp get_non_gl_text(routes) do
+    non_gl_lines =
+      routes
+      |> Enum.reject(&(&1.color == :green and length(&1.branches) < 4))
+      |> Enum.map(fn %{color: color} -> color end)
+
+    case non_gl_lines do
+      [] ->
+        ""
+
+      [route] ->
+        "#{route} Line"
+
+      routes when length(routes) == 2 ->
+        joined_text =
+          routes
+          |> Enum.join(" and ")
+
+        joined_text <> " Lines"
+
+      routes when length(routes) > 2 ->
+        all_but_last =
+          routes
+          |> Enum.take(length(routes) - 1)
+          |> Enum.join(", ")
+
+        all_but_last <> ", and #{List.last(routes)} Lines"
+    end
+  end
+
+  defp render_additional_info_for_effect(effect) do
+    case effect do
+      :shuttle -> "All shuttle buses are accessible"
+      _ -> ""
+    end
+  end
+end
