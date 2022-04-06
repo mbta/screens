50439360b44f1d368a82930caf9354f8bc300b48
Maddox <cmaddox@CTD24876CMADDOX.local>
2022-04-06 14:34:38 -0400

    Moved NoData component and styles to LCD folders.

--- assets/src/apps/v2/bus_shelter.tsx
+++ assets/src/apps/v2/bus_shelter.tsx
@@ -35,7 +35,7 @@ import SubwayStatus from "Components/v2/subway_status";
 import EvergreenContent from "Components/v2/evergreen_content";
 import Survey from "Components/v2/survey";
 
-import NoData from "Components/v2/bus_shelter/no_data";
+import NoData from "Components/v2/lcd/no_data";
 import DeparturesNoData from "Components/v2/bus_shelter/departures_no_data";
 
 import { FlexZoneAlert, FullBodyAlert } from "Components/v2/bus_shelter/alert";
