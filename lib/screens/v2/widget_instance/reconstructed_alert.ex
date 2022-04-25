7b529fda1ee5c165e5eb231ffa8cf2ef7fa0b5d2
Hannah Purcell <hpurcell@mbta.com>
2022-04-25 10:49:31 -0400

    Added comments

--- lib/screens/v2/widget_instance/reconstructed_alert.ex
+++ lib/screens/v2/widget_instance/reconstructed_alert.ex
@@ -95,6 +95,8 @@ defmodule Screens.V2.WidgetInstance.ReconstructedAlert do
       |> Enum.uniq()
       |> hd()
 
+    # When the alert is non-directional but the station is at the bounday:
+    # direction_id will be nil, but we still want to show the alert impacts one direction only
     if is_nil(direction_id) do
       informed_stop_ids = Enum.into(informed_entities, MapSet.new(), & &1.stop)
 
@@ -106,6 +108,7 @@ defmodule Screens.V2.WidgetInstance.ReconstructedAlert do
           do: headsign,
           else: false
       end)
+    # Otherwise, direction is provided, and we can find the destination tag from @route_directions
     else
       @route_directions
       |> Map.get(route_id)
