05019440f51dfdf08bbfdad4c6bad54f5c038cfe
Hannah Purcell <69368883+hannahpurcell@users.noreply.github.com>
2022-05-05 13:05:02 -0400

    Fix Elixir Sentry with backend logger (#1337)

--- lib/screens/application.ex
+++ lib/screens/application.ex
@@ -26,6 +26,7 @@ defmodule Screens.Application do
     # See https://hexdocs.pm/elixir/Supervisor.html
     # for other strategies and supported options
     opts = [strategy: :one_for_one, name: Screens.Supervisor]
+    {:ok, _} = Logger.add_backend(Sentry.LoggerBackend)
     Supervisor.start_link(children, opts)
   end
 
