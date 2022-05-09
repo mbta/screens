5dbffc4a0c9eaeb5f9ddcd7f4c97dcd7c02c8718
Hannah Purcell <hpurcell@mbta.com>
2022-05-09 11:57:39 -0400

    revert changes to app

--- lib/screens/application.ex
+++ lib/screens/application.ex
@@ -26,7 +26,6 @@ defmodule Screens.Application do
     # See https://hexdocs.pm/elixir/Supervisor.html
     # for other strategies and supported options
     opts = [strategy: :one_for_one, name: Screens.Supervisor]
-
     Supervisor.start_link(children, opts)
   end
 
