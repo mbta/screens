ea1a993e538983b3ab9468145706a9dd9deaa8b8
Hannah Purcell <hpurcell@mbta.com>
2022-05-09 12:17:14 -0400

    Remove added logger from application.ex

--- lib/screens/application.ex
+++ lib/screens/application.ex
@@ -26,7 +26,6 @@ defmodule Screens.Application do
     # See https://hexdocs.pm/elixir/Supervisor.html
     # for other strategies and supported options
     opts = [strategy: :one_for_one, name: Screens.Supervisor]
-    {:ok, _} = Logger.add_backend(Sentry.LoggerBackend)
     Supervisor.start_link(children, opts)
   end
 
