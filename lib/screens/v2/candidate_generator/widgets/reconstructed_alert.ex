70934403d3f00310142312512fe4dad3cafdc776
Hannah Purcell <hpurcell@mbta.com>
2022-05-12 13:41:09 -0400

    formatting

--- lib/screens/v2/candidate_generator/widgets/reconstructed_alert.ex
+++ lib/screens/v2/candidate_generator/widgets/reconstructed_alert.ex
@@ -99,12 +99,12 @@ defmodule Screens.V2.CandidateGenerator.Widgets.ReconstructedAlert do
     stop_ids =
       %{alert: alert}
       |> BaseAlert.informed_entities()
-      |> Enum.flat_map(fn %{stop: stop_id} -> 
-          case stop_id do
-            nil -> []
-            id -> [id]
-          end
-        end)
+      |> Enum.flat_map(fn %{stop: stop_id} ->
+        case stop_id do
+          nil -> []
+          id -> [id]
+        end
+      end)
 
     case stop_ids do
       [] ->
