f631ffde3eecb816591471eb768024845c04f3ce
Maddox <cmaddox@CTD24876CMADDOX.local>
2022-07-14 11:11:39 -0400

    Changed Sentry init to use empty string as DSN when not a real screen.

--- assets/src/util/sentry.tsx
+++ assets/src/util/sentry.tsx
@@ -9,13 +9,14 @@ const initSentry = (appString: string) => {
   const dataset = document.getElementById("app")?.dataset ?? {};
   const { sentry: sentryDsn, environmentName: env } = dataset;
 
-  if (sentryDsn && isRealScreen()) {
-    Sentry.init({
-      dsn: sentryDsn,
-      environment: env,
-    });
-    Sentry.captureMessage(`Sentry intialized for app: ${appString}`);
-  }
+  // Note: passing an empty string as the DSN sets up a "no-op SDK" that captures errors and lets you call its methods,
+  // but does not actually log anything to the Sentry service.
+  Sentry.init({
+    dsn: sentryDsn && isRealScreen() ? sentryDsn : "",
+    environment: env,
+  });
+
+  Sentry.captureMessage(`Sentry intialized for app: ${appString}`);
 };
 
 export default initSentry;
