e13256d729d58fa7f5a8022f5fd878b55e8299b3
Hannah Purcell <hpurcell@mbta.com>
2022-07-27 12:20:32 -0400

    Return to Raven!

--- assets/src/hooks/v2/use_api_response.tsx
+++ assets/src/hooks/v2/use_api_response.tsx
@@ -2,7 +2,8 @@ import { WidgetData } from "Components/v2/widget";
 import useDriftlessInterval from "Hooks/use_driftless_interval";
 import React, { useEffect, useState } from "react";
 import { useLocation } from "react-router-dom";
-import * as Sentry from "@sentry/browser";
+import Raven from "raven-js";
+Raven.config("https://45a59d5eb11a418f857e838eb4d6e73d@o89189.ingest.sentry.io/6061747").install();
 
 const MINUTE_IN_MS = 60_000;
 
@@ -57,7 +58,7 @@ const doFailureBuffer = (
       // This will trigger until a success API response is received.
       setApiResponse((prevApiResponse) => {
         if (prevApiResponse != null && prevApiResponse.state === "success") {
-          Sentry.captureMessage("Entering no-data state.");
+          Raven.captureMessage("Entering no-data state.");
         }
         return apiResponse;
       });
@@ -142,7 +143,7 @@ const useApiResponse = ({
       } else {
         setApiResponse((prevApiResponse) => {
           if (prevApiResponse != null && prevApiResponse.state !== "success") {
-            Sentry.captureMessage("Exiting no-data state.");
+            Raven.captureMessage("Exiting no-data state.");
           }
           return apiResponse;
         });
--- assets/src/util/sentry.tsx
+++ assets/src/util/sentry.tsx
@@ -1,5 +1,6 @@
-import * as Sentry from "@sentry/browser";
 import { isRealScreen } from "Util/util";
+import Raven from "raven-js";
+Raven.config("https://45a59d5eb11a418f857e838eb4d6e73d@o89189.ingest.sentry.io/6061747").install();
 
 /**
  * Initializes Sentry if the DSN is defined and this client is running on
@@ -7,16 +8,16 @@ import { isRealScreen } from "Util/util";
  */
 const initSentry = (appString: string) => {
   const dataset = document.getElementById("app")?.dataset ?? {};
+  // @ts-ignore
   const { sentry: sentryDsn, environmentName: env } = dataset;
 
   // Note: passing an empty string as the DSN sets up a "no-op SDK" that captures errors and lets you call its methods,
   // but does not actually log anything to the Sentry service.
-  Sentry.init({
-    dsn: sentryDsn && isRealScreen() ? sentryDsn : "",
-    environment: env,
-  });
 
-  Sentry.captureMessage(`Sentry intialized for app: ${appString}`);
+  if (isRealScreen()) {
+    Raven.config("https://45a59d5eb11a418f857e838eb4d6e73d@o89189.ingest.sentry.io/6061747").install();
+    Raven.captureMessage(`Sentry intialized for app: ${appString}`);
+  }
 };
 
 export default initSentry;
