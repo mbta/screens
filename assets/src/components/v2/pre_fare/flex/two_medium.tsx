d896cb6c331d098aaa5e63088f226615e1a86855
Maddox <cmaddox@CTD24876CMADDOX.local>
2022-08-11 16:35:18 -0400

    Added paging indicator and fixed CSS.

--- assets/src/components/v2/pre_fare/flex/two_medium.tsx
+++ assets/src/components/v2/pre_fare/flex/two_medium.tsx
@@ -1,15 +1,20 @@
 import React from "react";
 
 import Widget, { WidgetData } from "Components/v2/widget";
+import FlexZonePageIndicator from "Components/v2/flex/page_indicator";
 
 interface Props {
   medium_left: WidgetData;
   medium_right: WidgetData;
+  page_index: number;
+  num_pages: number;
 }
 
 const TwoMedium: React.ComponentType<Props> = ({
   medium_left: mediumLeft,
   medium_right: mediumRight,
+  num_pages: numPages,
+  page_index: pageIndex,
 }) => {
   return (
     <div className="flex-two-medium">
@@ -19,6 +24,7 @@ const TwoMedium: React.ComponentType<Props> = ({
       <div className="flex-two-medium__right">
         <Widget data={mediumRight} />
       </div>
+      <FlexZonePageIndicator numPages={numPages} pageIndex={pageIndex} />
     </div>
   );
 };
