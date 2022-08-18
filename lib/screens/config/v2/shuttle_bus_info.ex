aa196128f6d938ece1ca3e395c509bf571152fd8
Maddox <cmaddox@CTD24876CMADDOX.local>
2022-08-18 11:03:57 -0400

    Added a schedule for minute ranges to ShuttleBusInfo.

--- lib/screens/config/v2/shuttle_bus_info.ex
+++ lib/screens/config/v2/shuttle_bus_info.ex
@@ -1,10 +1,11 @@
 defmodule Screens.Config.V2.ShuttleBusInfo do
   @moduledoc false
 
+  alias Screens.Config.V2.ShuttleBusSchedule
   alias Screens.V2.WidgetInstance
 
   @type t :: %__MODULE__{
-          minutes_range_to_destination: String.t(),
+          minutes_range_to_destination_schedule: list(ShuttleBusSchedule.t()),
           destination: String.t(),
           arrow: arrow(),
           english_boarding_instructions: String.t(),
@@ -14,14 +15,16 @@ defmodule Screens.Config.V2.ShuttleBusInfo do
 
   @type arrow :: :n | :ne | :e | :se | :s | :sw | :w | :nw | nil
 
-  defstruct minutes_range_to_destination: nil,
+  defstruct minutes_range_to_destination_schedule: [],
             destination: nil,
             arrow: nil,
             english_boarding_instructions: nil,
             spanish_boarding_instructions: nil,
             priority: [99]
 
-  use Screens.Config.Struct, with_default: true
+  use Screens.Config.Struct,
+    children: [minutes_range_to_destination_schedule: {:list, ShuttleBusSchedule}],
+    with_default: true
 
   for arrow <- ~w[n ne e se s sw w nw]a do
     arrow_string = Atom.to_string(arrow)
