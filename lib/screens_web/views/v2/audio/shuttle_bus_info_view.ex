c036767b1e81f4ec30ff13f29c5ca36298f0ed48
Maddox <cmaddox@CTD24876CMADDOX.local>
2022-08-18 15:22:32 -0400

    Changed audio to match designs.

--- lib/screens_web/views/v2/audio/shuttle_bus_info_view.ex
+++ lib/screens_web/views/v2/audio/shuttle_bus_info_view.ex
@@ -3,8 +3,19 @@ defmodule ScreensWeb.V2.Audio.ShuttleBusInfoView do
 
   def render("_widget.ssml", %{
         minutes_range_to_destination: minutes_range_to_destination,
-        destination: destination
+        destination: destination,
+        audio_boarding_instructions: boarding_instructions
       }) do
-    ~E|<p>Estimated <%= minutes_range_to_destination %> minute to <%= destination %></p>|
+    ~E|
+    <p>Shuttle buses.</p>
+    <p><%= boarding_instructions %></p>
+    <p><%= render_time_estimate(minutes_range_to_destination, destination) %></p>
+    <p>All shuttle buses are accessible and free.</p>
+    <p>Accessible vans are also available upon request.</p>
+    |
+  end
+
+  defp render_time_estimate(minute_range, destination) do
+    "It's a #{minute_range} minute bus ride to #{destination}."
   end
 end
