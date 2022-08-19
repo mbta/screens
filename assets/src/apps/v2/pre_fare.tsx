d3e27cd3e52fc2fb4cd4af5e1335f555b031747f
Christian Maddox <christian.maddox93@gmail.com>
2022-08-19 09:33:22 -0400

    feat: Shuttle Bus Info widget frontend (#1445)
    * Added new slots for OL surge to PreFare template.
    
    * Added new slots to frontend.
    
    * Fixed test.
    
    * Changed slot name to match new slots.
    
    * Fixed surge slot dimensions to match design.
    
    * Created component for shuttle bus widget.
    
    * Added widget to PreFare frontend app.
    
    * Added opactiy to Spanish text.
    
    * Added ShuttleBusInfo widget config to admin table.
    
    * Added new component for the accessible icon.
    
    * Added accessible icon to Shuttle Bus widget.
    
    * Added new component for the free icon.
    
    * Added free icon to Shuttle Bus widget.
    
    * Added missing style for bg color.
    
    Co-authored-by: Maddox <cmaddox@CTD24876CMADDOX.local>

--- assets/src/apps/v2/pre_fare.tsx
+++ assets/src/apps/v2/pre_fare.tsx
@@ -40,6 +40,7 @@ import ReconstructedTakeover from "Components/v2/reconstructed_takeover";
 import MultiScreenPage from "Components/v2/multi_screen_page";
 import SimulationScreenPage from "Components/v2/simulation_screen_page";
 import SurgeBodyRight from "Components/v2/pre_fare/surge_body_right";
+import ShuttleBusInfo from "Components/v2/shuttle_bus_info";
 import BlueBikes from "Components/v2/blue_bikes";
 
 const TYPE_TO_COMPONENT = {
@@ -63,6 +64,7 @@ const TYPE_TO_COMPONENT = {
   no_data: NoData,
   reconstructed_large_alert: ReconstructedAlert,
   reconstructed_takeover: ReconstructedTakeover,
+  shuttle_bus_info: ShuttleBusInfo,
   blue_bikes: BlueBikes,
 };
 
