30be83d72fa91cbdc20a338cde3052dbcbc99344
Maddox <cmaddox@CTD24876CMADDOX.local>
2022-08-19 15:56:11 -0400

    Changed logic to return nothing if an error occurs in the function.

--- lib/screens/v2/candidate_generator/widgets/cr_departures.ex
+++ lib/screens/v2/candidate_generator/widgets/cr_departures.ex
@@ -16,48 +16,56 @@ defmodule Screens.V2.CandidateGenerator.Widgets.CRDepartures do
       )
 
   def departures_instances(
-        %Screen{app_params: %app{cr_departures: %CRDepartures{} = cr_departures}} = config,
+        %Screen{
+          app_params: %app{
+            cr_departures:
+              %CRDepartures{direction_to_destination: direction_to_destination, station: station} =
+                cr_departures
+          }
+        } = config,
         fetch_departures_fn,
         fetch_stop_name_fn,
         now
       )
       when app in [PreFare] do
-    opts = %{include_schedules: true}
+    with {:ok, departures_data} <- fetch_departures_fn.(direction_to_destination, station) do
+      destination = fetch_stop_name_fn.(cr_departures.destination)
 
-    params = %{
-      direction_id: cr_departures.direction_to_destination,
-      route_type: :rail,
-      stop_ids: [cr_departures.station]
-    }
+      # The Overnight and NoData widgets may not be relevant here
+      departures_instance =
+        cond do
+          departures_data == :error ->
+            %DeparturesNoData{screen: config, show_alternatives?: true}
 
-    destination = fetch_stop_name_fn.(cr_departures.destination)
+          departures_data == :overnight ->
+            %OvernightDepartures{}
 
-    {:ok, departures_data} = fetch_departures_fn.(opts, params)
+          true ->
+            %CRDeparturesWidget{
+              config: cr_departures,
+              departures_data: departures_data,
+              destination: destination,
+              now: now
+            }
+        end
 
-    # The Overnight and NoData widgets may not be relevant here
-    departures_instance =
-      cond do
-        departures_data == :error ->
-          %DeparturesNoData{screen: config, show_alternatives?: true}
-
-        departures_data == :overnight ->
-          %OvernightDepartures{}
-
-        true ->
-          %CRDeparturesWidget{
-            config: cr_departures,
-            departures_data: departures_data,
-            destination: destination,
-            now: now
-          }
-      end
-
-    [departures_instance]
+      [departures_instance]
+    else
+      :error -> []
+    end
   end
 
   def departures_instances(_, _, _, _), do: []
 
-  defp fetch_departures(opts, params) do
+  defp fetch_departures(direction_to_destination, station) do
+    opts = %{include_schedules: true}
+
+    params = %{
+      direction_id: direction_to_destination,
+      route_type: :rail,
+      stop_ids: [station]
+    }
+
     Departure.fetch(params, Keyword.new(opts))
   end
 end
