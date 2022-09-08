cd75dd98f332dd2cd4aa1a73d7f8b766d191b245
Maddox <cmaddox@CTD24876CMADDOX.local>
2022-09-08 08:53:08 -0400

    Finished overnight body.

--- assets/src/components/v2/cr_departures/overnight_cr_departures.tsx
+++ assets/src/components/v2/cr_departures/overnight_cr_departures.tsx
@@ -1,23 +1,36 @@
 import React, { ComponentType } from "react";
-import CRDeparturesHeader from "./cr_departures_header";
+import DeparturesTable from "Components/v2/cr_departures/cr_departures_table";
+import CRDeparturesHeader from "Components/v2/cr_departures/cr_departures_header";
 
 interface Props {
   direction: string;
   last_schedule_departure_time: string;
   last_schedule_headsign: string;
+  overnight_text_english: string;
+  overnight_text_spanish: string;
 }
 
 const OvernightCRDepartures: ComponentType<Props> = ({
   direction,
   last_schedule_departure_time: lastScheduleDepartureTime,
   last_schedule_headsign: lastScheduleHeadsign,
+  overnight_text_english: overnightTextEnglish,
+  overnight_text_spanish: overnightTextSpanish,
 }) => {
   return (
     <div className="overnight-cr-departures__container">
       <div className="overnight-cr-departures__card">
         <CRDeparturesHeader />
         <div className="overnight-cr-departures__body">
-          {lastScheduleDepartureTime}
+          <DeparturesTable departures={[]} />
+          <div className="overnight-cr-departures__body-text">
+            <div className="overnight-cr-departures__body-text--english">
+              {overnightTextEnglish}
+            </div>
+            <div className="overnight-cr-departures__body-text--spanish">
+              {overnightTextSpanish}
+            </div>
+          </div>
         </div>
         <div className="overnight-cr-departures__footer">
           {lastScheduleHeadsign}
