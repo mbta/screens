78a2f91c0b4b50f43e311814fa3efe6a9dbf44c1
Maddox <cmaddox@CTD24876CMADDOX.local>
2022-09-09 14:20:33 -0400

    Added a component for CR Departure time.

---
+++ assets/src/components/v2/cr_departures/cr_departure_time.tsx
@@ -0,0 +1,65 @@
+import BaseDepartureTime from "Components/eink/base_departure_time";
+import moment from "moment";
+import React from "react";
+import { TimeRepresentation } from "Util/time_representation";
+import { classWithModifier } from "Util/util";
+
+interface CRDepartureTimeProps {
+  departureType: "schedule" | "prediction";
+  time: string | TimeRepresentation;
+  isDelayed: boolean;
+}
+
+const CRDepartureTime = ({
+  departureType,
+  time,
+  isDelayed,
+}: CRDepartureTimeProps): JSX.Element => {
+  const formattedTime = moment(time as string).format("h:mm");
+
+  if (departureType === "schedule") {
+    return (
+      <div className="cr-departure-time">
+        <div className="cr-departure-time__text">{formattedTime}</div>
+        <div
+          className={classWithModifier("cr-departure-time__subtext", "english")}
+        >
+          Scheduled
+        </div>
+        <div
+          className={classWithModifier("cr-departure-time__subtext", "spanish")}
+        >
+          Programada
+        </div>
+      </div>
+    );
+  }
+
+  console.log(typeof time);
+
+  if (typeof time === "string" && isDelayed) {
+    return (
+      <div className="cr-departure-time">
+        <div className="cr-departure-time__text delayed">{formattedTime}</div>
+        <div
+          className={classWithModifier("cr-departure-time__subtext", "english")}
+        >
+          Delayed
+        </div>
+        <div
+          className={classWithModifier("cr-departure-time__subtext", "spanish")}
+        >
+          Atrasado
+        </div>
+      </div>
+    );
+  }
+
+  return (
+    <div className="cr-departure-time">
+      <BaseDepartureTime time={time as TimeRepresentation} hideAmPm />
+    </div>
+  );
+};
+
+export default CRDepartureTime;
