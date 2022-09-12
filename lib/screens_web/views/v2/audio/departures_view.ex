52189a4f966066760ca8dd3b38577662ec140cf5
Hannah Purcell <hpurcell@mbta.com>
2022-09-12 19:15:59 -0400

    remove inspect

--- lib/screens_web/views/v2/audio/departures_view.ex
+++ lib/screens_web/views/v2/audio/departures_view.ex
@@ -150,7 +150,6 @@ defmodule ScreensWeb.V2.Audio.DeparturesView do
   defp identity_render(value), do: ~E|<%= value %>|
 
   def build_text(value_renderers) do
-    IO.inspect(value_renderers)
     value_renderers
     |> Enum.reject(fn
       {value, renderer} when is_function(renderer) -> is_nil(value)
