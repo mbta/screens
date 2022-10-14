9a2640aeaa31621bff844b46705304e27c26c65c
cmaddox5 <christian.maddox93@gmail.com>
2022-10-14 10:36:58 -0400

    Removed some hardcoded values.

--- test/screens/screens_by_alert_test.exs
+++ test/screens/screens_by_alert_test.exs
@@ -31,15 +31,19 @@ defmodule Screens.ScreensByAlertTest do
       {:ok, pid} = ScreensByAlert.start_link()
       ScreensByAlert.put_data(1, [1])
       on_exit(fn -> Process.exit(pid, :done) end)
+
+      %{
+        ttl: Application.get_env(:screens, :screens_by_alert)[:screens_by_alert_ttl_seconds]
+      }
     end
 
     test "returns list with data when called before expiration" do
       assert [{1, _}] = ScreensByAlert.get_screens_by_alert(1)
     end
 
-    test "returns empty list when called after expiration" do
+    test "returns empty list when called after expiration", %{ttl: ttl} do
       assert [{1, _}] = ScreensByAlert.get_screens_by_alert(1)
-      Process.sleep(1001)
+      Process.sleep((ttl + 1) * 1000)
       assert [] = ScreensByAlert.get_screens_by_alert(1)
     end
   end
@@ -50,16 +54,19 @@ defmodule Screens.ScreensByAlertTest do
       ScreensByAlert.put_data(1, [1])
       on_exit(fn -> Process.exit(pid, :done) end)
 
-      %{last_updated: System.system_time(:second)}
+      %{
+        last_updated: System.system_time(:second),
+        ttl: Application.get_env(:screens, :screens_by_alert)[:screens_last_updated_ttl_seconds]
+      }
     end
 
     test "returns when screen was last updated", %{last_updated: last_updated} do
       assert last_updated == ScreensByAlert.get_screens_last_updated(1)
     end
 
-    test "returns nil after expiration", %{last_updated: last_updated} do
+    test "returns nil after expiration", %{last_updated: last_updated, ttl: ttl} do
       assert last_updated == ScreensByAlert.get_screens_last_updated(1)
-      Process.sleep(1001)
+      Process.sleep((ttl + 1) * 1000)
       assert is_nil(ScreensByAlert.get_screens_last_updated(1))
     end
   end
