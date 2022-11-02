51002adeebd9bfbbcc28a35807a27c3ef109224e
Jon Zimbel <jzimbel@mbta.com>
2022-11-02 12:12:34 -0400

    Use correct key for Memcache connection config

--- config/releases.exs
+++ config/releases.exs
@@ -64,7 +64,7 @@ config :sentry,
 config :ueberauth, Ueberauth.Strategy.Cognito, client_secret: cognito_client_secret
 config :screens, ScreensWeb.AuthManager, secret_key: screens_auth_secret
 
-config :screens, ScreensByAlert.Memcache,
+config :screens, Screens.ScreensByAlert.Memcache,
   connection_opts: [
     namespace: System.get_env("HOST"),
     hostname: System.get_env("MEMCACHED_HOST"),
