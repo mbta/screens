8ab652bda6f4e313859282db5c164b45e4be0b9d
cmaddox5 <christian.maddox93@gmail.com>
2023-01-11 14:57:41 -0500

    Added module for retrieved file from S3 used to determine time of last deploy.

---
+++ lib/screens/util/last_deploy.ex
@@ -0,0 +1,20 @@
+defmodule Screens.Util.LastDeployTime do
+  @moduledoc false
+
+  @bucket "mbta-screens"
+
+  def get_last_deploy_time() do
+    env = Application.get_env(:screens, :environment_name, "screens-prod")
+    path = "/#{env}/LAST_DEPLOY"
+
+    get_operation = ExAws.S3.get_object(@bucket, path)
+
+    case ExAws.request(get_operation) do
+      {:ok, %{last_modified: last_modified}} ->
+        last_modified
+
+      {:error, _} ->
+        nil
+    end
+  end
+end
