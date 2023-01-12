31214557dae685adcdd6087d02b354e50ee9320e
cmaddox5 <christian.maddox93@gmail.com>
2023-01-12 13:18:34 -0500

    Simplified fallback clause.

--- lib/screens/v2/screen_data/parameters.ex
+++ lib/screens/v2/screen_data/parameters.ex
@@ -35,16 +35,6 @@ defmodule Screens.V2.ScreenData.Parameters do
     dup_v2: 0
   }
 
-  @app_id_to_audio_interval_offset_seconds %{
-    bus_eink_v2: 0,
-    bus_shelter_v2: 0,
-    gl_eink_v2: 0,
-    solari_v2: 0,
-    solari_large_v2: 0,
-    pre_fare_v2: 0,
-    dup_v2: 0
-  }
-
   @spec get_candidate_generator(Screens.Config.Screen.t() | atom()) :: candidate_generator()
   def get_candidate_generator(%Screens.Config.Screen{app_id: app_id}) do
     get_candidate_generator(app_id)
@@ -81,7 +71,5 @@ defmodule Screens.V2.ScreenData.Parameters do
     interval_offset_seconds
   end
 
-  def get_audio_interval_offset_seconds(%Screens.Config.Screen{app_id: app_id}) do
-    Map.get(@app_id_to_audio_interval_offset_seconds, app_id)
-  end
+  def get_audio_interval_offset_seconds(_screen), do: 0
 end
