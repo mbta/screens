277ee87c5dee305e49b6d366048eec02f3ba8dbd
cmaddox5 <christian.maddox93@gmail.com>
2023-01-12 13:51:18 -0500

    Fixed tests so GenServer is not started more than once.

--- test/screens/screens_by_alert_test.exs
+++ test/screens/screens_by_alert_test.exs
@@ -3,34 +3,10 @@ defmodule Screens.ScreensByAlertTest do
   use ExUnit.Case
   alias Screens.ScreensByAlert
 
-  setup do
-    Application.stop(:screens)
-
-    on_exit(fn ->
-      Application.start(:screens)
-    end)
-  end
-
-  describe "start_link/1" do
-    test "returns {:ok, pid} when start_link is called" do
-      assert {:ok, pid} = ScreensByAlert.start_link([])
-      on_exit(fn -> Process.exit(pid, :done) end)
-    end
-  end
-
-  describe "put_data/2" do
-    test "returns :ok" do
-      {:ok, pid} = ScreensByAlert.start_link([])
-      assert :ok = ScreensByAlert.put_data(1, [1])
-      on_exit(fn -> Process.exit(pid, :done) end)
-    end
-  end
-
   describe "get_screens_by_alert/1" do
     setup do
-      {:ok, pid} = ScreensByAlert.start_link([])
       ScreensByAlert.put_data(1, [1])
-      on_exit(fn -> Process.exit(pid, :done) end)
+      on_exit(fn -> ScreensByAlert.put_data(1, []) end)
 
       %{
         screens_by_alert_ttl:
@@ -61,9 +37,8 @@ defmodule Screens.ScreensByAlertTest do
 
   describe "get_screens_last_updated/1" do
     setup do
-      {:ok, pid} = ScreensByAlert.start_link([])
       ScreensByAlert.put_data(1, [1])
-      on_exit(fn -> Process.exit(pid, :done) end)
+      on_exit(fn -> ScreensByAlert.put_data(1, []) end)
 
       %{
         last_updated: System.system_time(:second),
