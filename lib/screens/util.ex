2e405b7987c712f5a677a4dbcccbeb4d14355248
cmaddox5 <christian.maddox93@gmail.com>
2023-01-12 13:21:15 -0500

    Credo.

--- lib/screens/util.ex
+++ lib/screens/util.ex
@@ -167,7 +167,7 @@ defmodule Screens.Util do
 
   def outdated?(screen_id, client_refresh_timestamp) do
     {:ok, client_refresh_time, _} = DateTime.from_iso8601(client_refresh_timestamp)
-    refresh_if_loaded_before_time = State.refresh_if_loaded_before(screen_id) |> IO.inspect()
+    refresh_if_loaded_before_time = State.refresh_if_loaded_before(screen_id)
 
     case refresh_if_loaded_before_time do
       nil -> false
