e236ca4f9c8474e9b04cd539904ea7212d66690c
cmaddox5 <christian.maddox93@gmail.com>
2023-01-19 15:59:30 -0500

    Changed placeholder so we can see each layout.

--- lib/screens/v2/candidate_generator/dup.ex
+++ lib/screens/v2/candidate_generator/dup.ex
@@ -90,8 +90,7 @@ defmodule Screens.V2.CandidateGenerator.Dup do
 
   defp placeholder_instances do
     [
-      %Placeholder{slot_names: [:main_content_reduced_zero], color: :grey},
-      %Placeholder{slot_names: [:bottom_pane_zero], color: :blue},
+      %Placeholder{slot_names: [:full_rotation_zero], color: :green, priority: 1},
       %Placeholder{slot_names: [:main_content_one], color: :orange},
       %Placeholder{slot_names: [:main_content_reduced_two], color: :green},
       %Placeholder{slot_names: [:bottom_pane_two], color: :red}
