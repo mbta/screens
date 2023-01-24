5f4833381f8008ee72d744735f19c6b46ec63e36
cmaddox5 <christian.maddox93@gmail.com>
2023-01-24 10:57:48 -0500

    Fixed row dividers.

--- assets/src/components/v2/dup/departures/normal_section.tsx
+++ assets/src/components/v2/dup/departures/normal_section.tsx
@@ -5,17 +5,15 @@ import NoticeRow from "./notice_row";
 
 const NormalSection = ({ rows }) => {
   return (
-    <div>
-      <div className="departures-section">
-        {rows.map((row, index) => {
-          const { id, type, ...data } = row;
-          if (type === "departure_row") {
-            return <DepartureRow {...data} key={id} />;
-          } else if (type === "notice_row") {
-            return <NoticeRow row={row} key={"notice" + index} />;
-          }
-        })}
-      </div>
+    <div className="departures-section">
+      {rows.map((row, index) => {
+        const { id, type, ...data } = row;
+        if (type === "departure_row") {
+          return <DepartureRow {...data} key={id} />;
+        } else if (type === "notice_row") {
+          return <NoticeRow row={row} key={"notice" + index} />;
+        }
+      })}
     </div>
   );
 };
