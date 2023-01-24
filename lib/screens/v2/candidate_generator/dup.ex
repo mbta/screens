db78cdb405eed10d28ad4db10b639d15096e8604
cmaddox5 <christian.maddox93@gmail.com>
2023-01-24 09:48:48 -0500

    Cleaned up aliases.

--- lib/screens/v2/candidate_generator/dup.ex
+++ lib/screens/v2/candidate_generator/dup.ex
@@ -3,6 +3,7 @@ defmodule Screens.V2.CandidateGenerator.Dup do
 
   alias Screens.Config.Screen
   alias Screens.Config.V2.{Departures, Dup}
+  alias Screens.Config.V2.Dup
   alias Screens.Config.V2.Header.CurrentStopId
   alias Screens.Stops.Stop
   alias Screens.V2.CandidateGenerator
@@ -10,12 +11,6 @@ defmodule Screens.V2.CandidateGenerator.Dup do
   alias Screens.V2.Template.Builder
   alias Screens.V2.WidgetInstance.Departures, as: DeparturesWidget
   alias Screens.V2.WidgetInstance.{DeparturesNoData, NormalHeader, Placeholder}
-  alias Screens.Config.V2.Dup
-  alias Screens.Config.V2.Header.CurrentStopId
-  alias Screens.Stops.Stop
-  alias Screens.V2.CandidateGenerator
-  alias Screens.V2.Template.Builder
-  alias Screens.V2.WidgetInstance.{NormalHeader, Placeholder}
 
   @behaviour CandidateGenerator
 
