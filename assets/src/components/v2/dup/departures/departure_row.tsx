da95c3022ab7b3e6d353baf0ab9c4ccc08989373
cmaddox5 <christian.maddox93@gmail.com>
2023-01-24 10:42:13 -0500

    Cleaned up components.

--- assets/src/components/v2/dup/departures/departure_row.tsx
+++ assets/src/components/v2/dup/departures/departure_row.tsx
@@ -3,23 +3,16 @@ import React from "react";
 import RoutePill from "Components/v2/departures/route_pill";
 import Destination from "Components/v2/dup/departures/destination";
 import DepartureTimes from "Components/v2/dup/departures/departure_times";
-import DepartureAlerts from "Components/v2/departures/departure_alerts";
 
 const DepartureRow = ({
   headsign,
   route,
   times_with_crowding: timesWithCrowding,
-  inline_alerts: inlineAlerts,
 }) => {
   const routeText = Number(route.text);
   return (
     <div className="departure-row">
-      <div // Keep pill aligned to top if there is a variation for the headsign.
-        // Always aligning to top shifts destination text.
-        className={
-          "departure-row__route" + (headsign.variation ? "" : " center")
-        }
-      >
+      <div className={"departure-row__route"}>
         <RoutePill
           {...route}
           size={isNaN(routeText) || routeText > 200 ? "small" : "large"}
@@ -31,11 +24,6 @@ const DepartureRow = ({
       <div className="departure-row__time">
         <DepartureTimes timesWithCrowding={timesWithCrowding} />
       </div>
-      {inlineAlerts.length > 0 && (
-        <div className="departure-row__alerts">
-          <DepartureAlerts alerts={inlineAlerts} />
-        </div>
-      )}
     </div>
   );
 };
--- assets/src/components/v2/dup/departures/normal_departures.tsx
+++ assets/src/components/v2/dup/departures/normal_departures.tsx
@@ -1,148 +1,23 @@
-import React, {
-  useState,
-  forwardRef,
-  useLayoutEffect,
-  useRef,
-  useEffect,
-  useContext,
-} from "react";
+import React from "react";
 
 import NormalSection from "Components/v2/dup/departures/normal_section";
 import NoticeSection from "Components/v2/dup/departures/notice_section";
-import { LastFetchContext } from "../../screen_container";
-
-const NormalDeparturesRenderer = forwardRef(
-  ({ sections, sectionSizes }, ref) => {
-    return (
-      <div className="departures-container">
-        <div className="departures" ref={ref}>
-          {sections.map(({ type, ...data }, i) => {
-            if (type === "normal_section") {
-              const { rows } = data;
-              return (
-                <NormalSection rows={trimRows(rows, sectionSizes[i])} key={i} />
-              );
-            } else if (type === "notice_section") {
-              return <NoticeSection {...data} key={i} />;
-            }
-          })}
-        </div>
-      </div>
-    );
-  }
-);
-
-const trimRows = (rows, n) => {
-  const { trimmed, count } = rows.reduce(
-    ({ count, trimmed }, row: Row) => {
-      if (row.type == "notice_row") {
-        if (count < n) {
-          return { count: count + 1, trimmed: [...trimmed, row] };
-        } else {
-          return { count, trimmed };
-        }
-      }
-
-      const trimmedRow = {
-        ...row,
-        times_with_crowding: row.times_with_crowding.slice(0, n - count),
-      };
-      const addedCount = trimmedRow.times_with_crowding.length;
-
-      if (addedCount > 0) {
-        return { count: count + addedCount, trimmed: [...trimmed, trimmedRow] };
-      } else {
-        return { count, trimmed };
-      }
-    },
-    { count: 0, trimmed: [] }
-  );
-
-  return trimmed;
-};
-
-interface DepartureRow {
-  type: "departure_row";
-  times_with_crowding: any[];
-}
-
-interface NoticeRow {
-  type: "notice_row";
-  text: object;
-}
-
-type Row = DepartureRow | NoticeRow;
-
-const getInitialSectionSize = (data) => {
-  return data.rows.reduce((acc, row: Row) => {
-    switch (row.type) {
-      case "departure_row":
-        return acc + row.times_with_crowding.length;
-      case "notice_row":
-        return acc + 1;
-    }
-  }, 0);
-};
-
-const getInitialSectionSizes = (sections) => {
-  return sections.map((section) => getInitialSectionSize(section));
-};
-
-const NormalDeparturesSizer = ({ sections, onDoneSizing }) => {
-  const [tempSectionSizes, setTempSectionSizes] = useState(
-    getInitialSectionSizes(sections)
-  );
-  const ref = useRef();
-
-  useLayoutEffect(() => {
-    if (
-      ref.current &&
-      ref.current.clientHeight > ref.current.parentNode.parentNode.clientHeight
-    ) {
-      setTempSectionSizes((sectionSizes) => {
-        return [sectionSizes[0] - 1];
-      });
-    } else {
-      onDoneSizing(tempSectionSizes);
-    }
-  }, [sections, tempSectionSizes]);
-
-  return (
-    <NormalDeparturesRenderer
-      sections={sections}
-      sectionSizes={tempSectionSizes}
-      ref={ref}
-    />
-  );
-};
 
 const NormalDepartures = ({ sections }) => {
-  const [sectionSizes, setSectionSizes] = useState([]);
-  const lastFetch = useContext(LastFetchContext);
-
-  // Reset state each time we receive new props,
-  // so that section sizes are recalculated from scratch.
-  useEffect(() => {
-    setSectionSizes([]);
-  }, [sections]);
-
-  if (sectionSizes.length > 0) {
-    return (
-      <NormalDeparturesRenderer
-        sections={sections}
-        sectionSizes={sectionSizes}
-        key={lastFetch}
-      />
-    );
-  } else {
-    return (
-      <NormalDeparturesSizer
-        sections={sections}
-        onDoneSizing={setSectionSizes}
-        key={lastFetch}
-      />
-    );
-  }
+  return (
+    <div className="departures-container">
+      <div className="departures">
+        {sections.map(({ type, ...data }, i) => {
+          if (type === "normal_section") {
+            const { rows } = data;
+            return <NormalSection rows={rows} key={i} />;
+          } else if (type === "notice_section") {
+            return <NoticeSection {...data} key={i} />;
+          }
+        })}
+      </div>
+    </div>
+  );
 };
 
 export default NormalDepartures;
