fcbc18c1ce7659e9ca0da1f7ce47cc03658bef1a
cmaddox5 <christian.maddox93@gmail.com>
2023-01-24 13:55:51 -0500

    Fixed styling so v2 matches v1.

--- assets/src/components/v2/dup/departures/departure_times.tsx
+++ assets/src/components/v2/dup/departures/departure_times.tsx
@@ -4,11 +4,11 @@ import DepartureTime from "Components/v2/dup/departures/departure_time";
 
 const DepartureTimes = ({ timesWithCrowding }) => {
   return (
-    <div className="departure-times">
+    <>
       {timesWithCrowding.map(({ id, time, scheduled_time }) => (
         <DepartureTime scheduled_time={scheduled_time} time={time} key={id} />
       ))}
-    </div>
+    </>
   );
 };
 
