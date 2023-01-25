cca25e0febcd3a8fe130aba2ab6ab1a7a406ae35
cmaddox5 <christian.maddox93@gmail.com>
2023-01-25 11:06:52 -0500

    Removed unused alias.

--- lib/screens/v2/candidate_generator/widgets/departures.ex
+++ lib/screens/v2/candidate_generator/widgets/departures.ex
@@ -4,7 +4,7 @@ defmodule Screens.V2.CandidateGenerator.Widgets.Departures do
   alias Screens.Config.Screen
   alias Screens.Config.V2.Departures.Filter.RouteDirection
   alias Screens.Config.V2.Departures.{Filter, Query, Section}
-  alias Screens.Config.V2.{BusEink, BusShelter, Departures, Dup, GlEink, Solari, SolariLarge}
+  alias Screens.Config.V2.{BusEink, BusShelter, Departures, GlEink, Solari, SolariLarge}
   alias Screens.V2.Departure
   alias Screens.V2.WidgetInstance.Departures, as: DeparturesWidget
   alias Screens.V2.WidgetInstance.{DeparturesNoData, DeparturesNoService, OvernightDepartures}
