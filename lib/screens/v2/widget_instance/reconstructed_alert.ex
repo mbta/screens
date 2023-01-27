b14f269be5c7eb3ef4e276595c2f0a8862956b6d
cmaddox5 <christian.maddox93@gmail.com>
2023-01-27 08:33:27 -0500

    Fixed function.

--- lib/screens/v2/widget_instance/reconstructed_alert.ex
+++ lib/screens/v2/widget_instance/reconstructed_alert.ex
@@ -751,7 +751,7 @@ defmodule Screens.V2.WidgetInstance.ReconstructedAlert do
     def valid_candidate?(t), do: ReconstructedAlert.temporarily_override_alert(t)
     def audio_serialize(t), do: ReconstructedAlert.serialize(t)
     def audio_sort_key(t), do: ReconstructedAlert.audio_sort_key(t)
-    def audio_valid_candidate?(_instance), do: ReconstructedAlert.temporarily_override_alert(t)
+    def audio_valid_candidate?(t), do: ReconstructedAlert.temporarily_override_alert(t)
     def audio_view(_instance), do: ScreensWeb.V2.Audio.ReconstructedAlertView
   end
 
