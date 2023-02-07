c6cd5add8ea60c6c9aa37d14e202947430fe23f1
Hannah Purcell <hpurcell@mbta.com>
2023-02-07 15:05:47 -0500

    Simplify the dup_rotation_wrapper

--- assets/src/components/v2/dup/dup_rotation_wrapper.tsx
+++ assets/src/components/v2/dup/dup_rotation_wrapper.tsx
@@ -1,16 +1,11 @@
-import React, { ReactNode } from "react";
+import _ from "lodash";
+import React from "react";
 
 export const splitRotationFromPropNames = (WrappedComponent: React.ElementType, rotation: string) => {
-  return class extends React.Component {
-    render() {
+  return (props: any) => {
+      
+    const modifiedProps = _.mapKeys(props, (_value, key) => key.replace(`_${rotation}`, ""))
 
-      const modifiedProps = Object.keys(this.props).reduce((newProps: any, currentKey: string) => {
-        const newKey = currentKey.replace("_one", "").replace("_two", "").replace("_zero", "")
-        newProps[newKey] = this.props[currentKey as keyof Readonly<{}> & Readonly<{ children?: ReactNode; }>]
-        return newProps
-      }, {})
-
-      return <WrappedComponent {...modifiedProps} rotation={rotation} />;
-    }
-  }
+    return <WrappedComponent {...modifiedProps} rotation={rotation} />;
+}
 };
