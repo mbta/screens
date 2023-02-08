07d1450c0de72d14007993e5a826f0afa6052c74
Hannah Purcell <69368883+hannahpurcell@users.noreply.github.com>
2023-02-08 11:37:25 -0500

    Update assets/src/components/v2/dup/dup_rotation_wrapper.tsx
    Co-authored-by: Jon Zimbel <63608771+jzimbel-mbta@users.noreply.github.com>

--- assets/src/components/v2/dup/dup_rotation_wrapper.tsx
+++ assets/src/components/v2/dup/dup_rotation_wrapper.tsx
@@ -4,7 +4,8 @@ import React from "react";
 export const splitRotationFromPropNames = (WrappedComponent: React.ElementType, rotation: string) => {
   return (props: any) => {
       
-    const modifiedProps = _.mapKeys(props, (_value, key) => key.replace(`_${rotation}`, ""))
+    const rotationSuffixPattern = new RegExp(`_${rotation}$`);
+    const modifiedProps = _.mapKeys(props, (_value, key) => key.replace(rotationSuffixPattern, ""))
 
     return <WrappedComponent {...modifiedProps} rotation={rotation} />;
 }
