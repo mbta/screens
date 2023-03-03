5c2d3dc00e7625e767b73e5f700c597826be1cf3
Hannah Purcell <69368883+hannahpurcell@users.noreply.github.com>
2023-03-03 15:29:05 -0500

    Updated DUP simulation for v2 (#1631)

--- assets/src/apps/v2/dup.tsx
+++ assets/src/apps/v2/dup.tsx
@@ -11,7 +11,7 @@ import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import ScreenPage from "Components/v2/screen_page";
 import { MappingContext } from "Components/v2/widget";
 
-import NormalScreen from "Components/v2/dup/normal_screen";
+import NormalScreen, { NormalSimulation } from "Components/v2/dup/normal_screen";
 import Placeholder from "Components/v2/placeholder";
 import NormalHeader from "Components/v2/dup/normal_header";
 import NormalDepartures from "Components/v2/dup/departures/normal_departures";
@@ -25,9 +25,12 @@ import SplitBody from "Components/v2/dup/split_body";
 import { splitRotationFromPropNames } from "Components/v2/dup/dup_rotation_wrapper";
 import PartialAlert from "Components/v2/dup/partial_alert";
 import TakeoverAlert from "Components/v2/dup/takeover_alert";
+import SimulationScreenPage from "Components/v2/simulation_screen_page";
+import { LOADING_LAYOUT, ResponseMapper, ResponseMapperContext } from "Components/v2/screen_container";
 
 const TYPE_TO_COMPONENT = {
   screen_normal: NormalScreen,
+  simulation_screen_normal: NormalSimulation,
   rotation_normal_zero: splitRotationFromPropNames(RotationNormal, "zero"),
   rotation_normal_one: splitRotationFromPropNames(RotationNormal, "one"),
   rotation_normal_two: splitRotationFromPropNames(RotationNormal, "two"),
@@ -48,6 +51,30 @@ const TYPE_TO_COMPONENT = {
   takeover_alert: TakeoverAlert,
 };
 
+const DISABLED_LAYOUT = {
+  full_screen: {
+    type: "no_data",
+    show_alternatives: true,
+  },
+  type: "screen_takeover",
+};
+
+const FAILURE_LAYOUT = DISABLED_LAYOUT;
+
+const responseMapper: ResponseMapper = (apiResponse) => {
+  switch (apiResponse.state) {
+    case "success":
+    case "simulation_success":
+      return apiResponse.data;
+    case "disabled":
+      return DISABLED_LAYOUT;
+    case "failure":
+      return FAILURE_LAYOUT;
+    case "loading":
+      return LOADING_LAYOUT;
+  }
+};
+
 const App = (): JSX.Element => {
   return (
     <Router>
@@ -55,11 +82,20 @@ const App = (): JSX.Element => {
         <Route exact path="/v2/screen/dup_v2">
           <MultiScreenPage components={TYPE_TO_COMPONENT} />
         </Route>
+        <Route exact path="/v2/screen/:id/simulation">
+          <MappingContext.Provider value={TYPE_TO_COMPONENT}>
+            <ResponseMapperContext.Provider value={responseMapper}>
+              <SimulationScreenPage opts={{alternateView: true}} />
+            </ResponseMapperContext.Provider>
+          </MappingContext.Provider>
+        </Route>
         <Route path="/v2/screen/:id">
           <MappingContext.Provider value={TYPE_TO_COMPONENT}>
-            <Viewport>
-              <ScreenPage />
-            </Viewport>
+            <ResponseMapperContext.Provider value={responseMapper}>
+              <Viewport>
+                <ScreenPage />
+              </Viewport>
+            </ResponseMapperContext.Provider>
           </MappingContext.Provider>
         </Route>
       </Switch>
