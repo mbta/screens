9856d347da273c482113dec93b7a550606d46630
cmaddox5 <christian.maddox93@gmail.com>
2023-03-06 09:58:54 -0500

    Moved function so it can be used by other modules.

--- lib/screens/util.ex
+++ lib/screens/util.ex
@@ -3,6 +3,8 @@ defmodule Screens.Util do
 
   alias Screens.Config.State
 
+  @sl_route_ids ~w[741 742 743 746 749 751]
+
   def format_time(t) do
     t |> DateTime.truncate(:second) |> DateTime.to_iso8601()
   end
@@ -179,4 +181,22 @@ defmodule Screens.Util do
   def to_set(id) when is_binary(id), do: MapSet.new([id])
   def to_set(ids) when is_list(ids), do: MapSet.new(ids)
   def to_set(%MapSet{} = already_a_set), do: already_a_set
+
+  def get_color_for_route(route_id, route_type \\ nil)
+
+  def get_color_for_route("Red", _), do: :red
+  def get_color_for_route("Mattapan", _), do: :red
+  def get_color_for_route("Orange", _), do: :orange
+  def get_color_for_route("Green" <> _, _), do: :green
+  def get_color_for_route("Blue", _), do: :blue
+  def get_color_for_route("CR-" <> _, _), do: :purple
+  def get_color_for_route("Boat-" <> _, _), do: :teal
+
+  def get_color_for_route(route_id, _)
+      when route_id in @sl_route_ids,
+      do: :silver
+
+  def get_color_for_route(_, :rail), do: :purple
+  def get_color_for_route(_, :ferry), do: :teal
+  def get_color_for_route(_, _), do: :yellow
 end
