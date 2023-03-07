a2f13549bff73812d33fdaa8eb860808e923a6f5
cmaddox5 <christian.maddox93@gmail.com>
2023-03-07 11:01:12 -0500

    Added NoDataSection frontend component.

---
+++ assets/src/components/v2/dup/departures/no_data_section.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+import FreeText from "Components/v2/dup/dup_free_text";
+import NoConnection from "Components/v2/bundled_svg/no_connection";
+
+const NoDataSection = ({ text }) => {
+  return (
+    <div className="no-data-section">
+      <div className="no-data-section__row">
+        <FreeText lines={text} />
+        <NoConnection
+          className="no-data-section__no-connection-icon"
+          colorHex="#171F26"
+        />
+      </div>
+    </div>
+  );
+};
+
+export default NoDataSection;
