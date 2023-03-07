3c7afdaabbde528b4527e1bce8be24397d7bd11f
Hannah Purcell <hpurcell@mbta.com>
2023-03-07 17:13:34 -0500

    When using LinkArrow for "no data" state, saw improvement for takeover alert

--- assets/src/components/v2/dup/takeover_alert.tsx
+++ assets/src/components/v2/dup/takeover_alert.tsx
@@ -1,54 +1,8 @@
 import React from "react";
+import LinkArrow from "../bundled_svg/link_arrow";
 
 import FreeText, { FreeTextType } from "./dup_free_text";
 import NormalHeader from "./normal_header";
-
-const LinkArrow = ({ width, color }: any) => {
-  const height = 40;
-  const stroke = 8;
-  const headWidth = 40;
-
-  const d = [
-    "M",
-    stroke / 2,
-    height / 2,
-    "L",
-    width - headWidth,
-    height / 2,
-    "L",
-    width - headWidth,
-    stroke / 2,
-    "L",
-    width - stroke / 2,
-    height / 2,
-    "L",
-    width - headWidth,
-    height - stroke / 2,
-    "L",
-    width - headWidth,
-    height / 2,
-    "Z",
-  ].join(" ");
-
-  return (
-    <svg
-      xmlns="http://www.w3.org/2000/svg"
-      viewBox={`0 0 ${width} ${height}`}
-      width={`${width}px`}
-      height={`${height}px`}
-      version="1.1"
-    >
-      <path
-        stroke={color}
-        strokeWidth={stroke}
-        strokeLinecap="round"
-        strokeLinejoin="round"
-        fill={color}
-        d={d}
-      />
-    </svg>
-  );
-};
 
 interface TakeoverAlertProps {
   text: FreeTextType,
@@ -75,7 +29,7 @@ const TakeoverAlert = (alert: TakeoverAlertProps) => {
         </div>
         <div className="full-screen-alert__link">
           <div className="full-screen-alert__link-arrow">
-            <LinkArrow width="628" color="#64696e" />
+            <LinkArrow width={628} colorHex="#64696e" />
           </div>
           <div className="full-screen-alert__link-text">mbta.com/alerts</div>
         </div>
