7d3a0554b7eaca79cee572ca0a13f59bbd0d1502
cmaddox5 <christian.maddox93@gmail.com>
2023-03-09 08:12:10 -0500

    Fixed conditional.

--- lib/screens/v2/candidate_generator/dup/departures.ex
+++ lib/screens/v2/candidate_generator/dup/departures.ex
@@ -339,7 +339,7 @@ defmodule Screens.V2.CandidateGenerator.Dup.Departures do
           &(&1.route.id == route_id)
         )
 
-      if DateTime.compare(now, last_schedule_today.departure_time) == :gt or
+      if DateTime.compare(now, last_schedule_today.departure_time) == :gt and
            DateTime.compare(now, first_schedule_tomorrow.departure_time) == :lt do
         %Departure{schedule: first_schedule_tomorrow}
       end
