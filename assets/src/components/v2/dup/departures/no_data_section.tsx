ba114d83a544b515e8a24c9df49b15c5c3d9ba85
cmaddox5 <christian.maddox93@gmail.com>
2023-03-10 09:31:04 -0500

    Added section divider for no-data-section.

--- assets/src/components/v2/dup/departures/no_data_section.tsx
+++ assets/src/components/v2/dup/departures/no_data_section.tsx
@@ -5,7 +5,7 @@ import NoConnection from "Components/v2/bundled_svg/no_connection";
 
 const NoDataSection = ({ text }) => {
   return (
-    <div className="no-data-section">
+    <div className="departures-section no-data-section">
       <div className="no-data-section__row">
         <FreeText lines={text} />
         <NoConnection
