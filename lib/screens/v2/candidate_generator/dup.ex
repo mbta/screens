457775bb009519236eec664ad9b48e1c0c4d3deb
Jon Zimbel <63608771+jzimbel-mbta@users.noreply.github.com>
2023-03-20 15:40:05 -0400

    fix: Use a sane timeout for Task.async_stream calls (#1646)
    * Use a sane timeout for Task.async_stream calls
    
    * Use screen refresh rate as timeout value where it makes sense to do so

--- lib/screens/v2/candidate_generator/dup.ex
+++ lib/screens/v2/candidate_generator/dup.ex
@@ -86,7 +86,7 @@ defmodule Screens.V2.CandidateGenerator.Dup do
       fn -> departures_instances_fn.(config, now) end,
       fn -> evergreen_content_instances_fn.(config) end
     ]
-    |> Task.async_stream(& &1.(), ordered: false, timeout: :infinity)
+    |> Task.async_stream(& &1.(), ordered: false, timeout: 30_000)
     |> Enum.flat_map(fn {:ok, instances} -> instances end)
   end
 
