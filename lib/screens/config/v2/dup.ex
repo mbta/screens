9440c741f184548f41087fa51c9477f9f485a4eb
cmaddox5 <christian.maddox93@gmail.com>
2023-03-21 10:55:36 -0400

    Added stop_name as a header type for DUP v2 screen configs.

--- lib/screens/config/v2/dup.ex
+++ lib/screens/config/v2/dup.ex
@@ -1,11 +1,11 @@
 defmodule Screens.Config.V2.Dup do
   @moduledoc false
 
-  alias Screens.Config.V2.Header.CurrentStopId
+  alias Screens.Config.V2.Header.{CurrentStopId, CurrentStopName}
   alias Screens.Config.V2.{Departures, EvergreenContentItem}
 
   @type t :: %__MODULE__{
-          header: CurrentStopId.t(),
+          header: CurrentStopId.t() | CurrentStopName.t(),
           evergreen_content: list(EvergreenContentItem.t()),
           primary_departures: Departures.t(),
           secondary_departures: Departures.t()
@@ -21,7 +21,31 @@ defmodule Screens.Config.V2.Dup do
     children: [
       primary_departures: Departures,
       secondary_departures: Departures,
-      header: CurrentStopId,
       evergreen_content: {:list, EvergreenContentItem}
     ]
+
+  defp value_from_json("header", %{"type" => "current_stop_id"} = header) do
+    CurrentStopId.from_json(header)
+  end
+
+  defp value_from_json("header", %{"type" => "current_stop_name"} = header) do
+    CurrentStopName.from_json(header)
+  end
+
+  # When no type is provided, default to current_stop_id
+  defp value_from_json("header", header) do
+    CurrentStopId.from_json(header)
+  end
+
+  defp value_to_json(:header, %CurrentStopId{} = header) do
+    header
+    |> CurrentStopId.to_json()
+    |> Map.put(:type, :current_stop_id)
+  end
+
+  defp value_to_json(:header, %CurrentStopName{} = header) do
+    header
+    |> CurrentStopName.to_json()
+    |> Map.put(:type, :current_stop_name)
+  end
 end
