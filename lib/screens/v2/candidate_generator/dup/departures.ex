25f1d69980a3cf1cd00f0f5383b1825f2826527f
cmaddox5 <christian.maddox93@gmail.com>
2023-03-21 14:51:48 -0400

    Changed logic to get route_id from alert.

--- lib/screens/v2/candidate_generator/dup/departures.ex
+++ lib/screens/v2/candidate_generator/dup/departures.ex
@@ -448,15 +448,16 @@ defmodule Screens.V2.CandidateGenerator.Dup.Departures do
   defp get_overnight_schedules_for_section(
          stops_with_live_departures,
          params,
-         [%{type: type, id: id} | _] = routes,
-         _alert,
+         [%{type: type} | _] = routes,
+         alert,
          now,
          fetch_schedules_fn,
          fetch_vehicles_fn
        )
        when type in [:subway, :light_rail] do
+    informed_route = get_section_route_from_alert(params.stop_ids, alert)
     # If there are no vehicles operating on the route, assume we are overnight.
-    if fetch_vehicles_fn.(id, nil) == [] do
+    if fetch_vehicles_fn.(informed_route.id, nil) == [] do
       get_overnight_schedules_for_section(
         stops_with_live_departures,
         params,
