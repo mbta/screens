62fd097253eb04e55fa576de165b2c0d3f31051f
cmaddox5 <christian.maddox93@gmail.com>
2023-03-23 15:20:43 -0400

    Changed version file header references.

--- assets/src/components/v2/dup/normal_header.tsx
+++ assets/src/components/v2/dup/normal_header.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 import DefaultNormalHeader, { Icon } from "Components/v2/normal_header";
-import { DUP_VERSION } from "Components/dup/version";
+import { DUP_VERSION } from "Components/v2/dup/version";
 
 interface NormalHeaderProps {
   text: string;
@@ -11,8 +11,13 @@ interface NormalHeaderProps {
   code?: string;
 }
 
-const NormalHeader = ({text, time, color, accentPattern, code}: NormalHeaderProps) => {
-
+const NormalHeader = ({
+  text,
+  time,
+  color,
+  accentPattern,
+  code,
+}: NormalHeaderProps) => {
   return (
     <DefaultNormalHeader
       icon={Icon.logo}
