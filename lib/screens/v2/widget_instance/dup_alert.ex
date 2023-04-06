24b0588c2308894f5307611e6d999c955742aa3e
Jon Zimbel <63608771+jzimbel-mbta@users.noreply.github.com>
2023-04-06 13:21:11 -0400

    polish: Make DUP 3rd rotation layout depend on whether secondary departures are configured (#1680)
    * fix: Make third rotation layout depend on existence of secondary departures; move layout special cases into a single function
    
    * Tiny variable name fix
    
    * Reorganize layout logic to make more sense, hopefully?
    
    * Replace unnecessary `case` expr with `if`
    
    * "rotation" -> "page" for consistency with other nearby comments

--- lib/screens/v2/widget_instance/dup_alert.ex
+++ lib/screens/v2/widget_instance/dup_alert.ex
@@ -81,144 +81,159 @@ defmodule Screens.V2.WidgetInstance.DupAlert do
     end
   end
 
+  @spec valid_candidate?(t()) :: boolean()
   def valid_candidate?(%__MODULE__{} = t) do
     alert_layout(t) != :no_render
   end
 
+  # Inputs/output are stored as a table for readability.
+  # Table is adapted from the one in the product spec: https://www.notion.so/mbta-downtown-crossing/DUP-Alert-Widget-Specification-a82acff850ed4f2eb98a04e5f3e0fe52
+  # Compile-time code converts each table row to a key-value pair in a map.
+  # PrimarySections refers to the number of sections in `primary_departures` of DUP config.
+  #
+  # Delay alerts are handled separately, see `delay_alert_layout` below.
+  # Layouts for rotations one and two are derived from the layout for rotation zero, see `non_delay_alert_layout` below.
+  layout_zero_table = """
+  # INPUTS                                                            || OUTPUT
+  # Effect          Location            AffectedLines PrimarySections || LayoutZero
+    station_closure inside              1             1                  full_screen
+    station_closure inside              1             2                  partial
+    station_closure inside              2             2                  full_screen
+  # "Boundary" location is not possible for station closures.
+    shuttle         inside              1             1                  full_screen
+    shuttle         inside              1             2                  partial
+    shuttle         inside              2             2                  full_screen
+    shuttle         boundary_upstream   1             1                  partial
+    shuttle         boundary_downstream 1             1                  partial
+    shuttle         boundary_upstream   1             2                  partial
+    shuttle         boundary_downstream 1             2                  partial
+    suspension      inside              1             1                  full_screen
+    suspension      inside              1             2                  partial
+    suspension      inside              2             2                  full_screen
+    suspension      boundary_upstream   1             1                  partial
+    suspension      boundary_downstream 1             1                  partial
+    suspension      boundary_upstream   1             2                  partial
+    suspension      boundary_downstream 1             2                  partial
+  """
+
+  @parameters_to_layout_zero layout_zero_table
+                             |> String.split("\n", trim: true)
+                             |> Enum.reject(&String.starts_with?(&1, "#"))
+                             |> Enum.into(%{}, fn line ->
+                               [
+                                 effect,
+                                 location,
+                                 affected_line_count,
+                                 primary_section_count,
+                                 layout_zero
+                               ] = String.split(line, ~r/\s+/, trim: true)
+
+                               # Convert strings to appropriate types
+                               [effect, location, layout_zero] =
+                                 Enum.map(
+                                   [effect, location, layout_zero],
+                                   &String.to_existing_atom/1
+                                 )
+
+                               [affected_line_count, primary_section_count] =
+                                 Enum.map(
+                                   [affected_line_count, primary_section_count],
+                                   &String.to_integer/1
+                                 )
+
+                               parameters = %{
+                                 effect: effect,
+                                 location: location,
+                                 affected_line_count: affected_line_count,
+                                 primary_section_count: primary_section_count
+                               }
+
+                               {parameters, layout_zero}
+                             end)
+
+  defp get_layout_parameters(t) do
+    %{
+      effect: t.alert.effect,
+      location: BaseAlert.location(t),
+      affected_line_count: length(get_affected_lines(t)),
+      primary_section_count: t.primary_section_count
+    }
+  end
+
   @spec alert_layout(t()) :: :full_screen | :partial | :no_render
   defp alert_layout(t) do
-    effect = t.alert.effect
-    location = BaseAlert.location(t)
-    affected_line_count = length(get_affected_lines(t))
-    rotation_index = t.rotation_index
+    if t.alert.effect == :delay,
+      do: delay_alert_layout(t),
+      else: non_delay_alert_layout(t)
+  end
 
-    parameters = {effect, location, affected_line_count, t.primary_section_count, rotation_index}
-
+  defp delay_alert_layout(t) do
     delay_severity = t.alert.severity
 
-    if effect == :delay and delay_severity < 5 do
-      # We don't show delays < 20 minutes
-      log_layout_mismatch(t, parameters, delay_severity)
-      :no_render
+    if delay_severity >= 5 do
+      # All delays >= 20 minutes get a partial alert on all 3 rotations
+      :partial
     else
-      case do_alert_layout(parameters) do
-        :no_render ->
-          log_layout_mismatch(t, parameters, delay_severity)
-          :no_render
+      # We don't show delays < 20 minutes
+      log_layout_mismatch(t, delay_severity)
+      :no_render
+    end
+  end
 
-        layout ->
-          layout
-      end
+  defp non_delay_alert_layout(t) do
+    parameters = get_layout_parameters(t)
+
+    lookup_result = Map.fetch(@parameters_to_layout_zero, parameters)
+
+    case {t.rotation_index, lookup_result} do
+      {:zero, {:ok, layout_zero}} ->
+        # The first page's layout maps directly from `parameters`.
+        layout_zero
+
+      {:one, {:ok, _layout_zero}} ->
+        # The second page always uses full-screen layout, as long as the alert is relevant.
+        :full_screen
+
+      {:two, {:ok, layout_zero}} ->
+        # The third page's layout depends on whether or not the screen has secondary departures configured.
+        #
+        # If the screen has secondary departures configured, it always gets a partial alert.
+        # If the screen does not have secondary departures configured, it gets the same layout as the first page.
+        #
+        # This is because we do not want to hide secondary departures (e.g. bus, CR) if they exist.
+        has_secondary_departures = t.screen.app_params.secondary_departures.sections != []
+
+        if has_secondary_departures,
+          do: :partial,
+          else: layout_zero
+
+      {_rotation_index, :error} ->
+        # We don't know what to do under these conditions.
+        # Log it, and prevent the alert from appearing.
+        log_layout_mismatch(t, :not_applicable)
+        :no_render
     end
   end
 
   # The widget _should_ always be valid given the constraints of what alerts
   # we generate DUP alert widgets for, but in case one slips through, we
   # should know about it!
-  defp log_layout_mismatch(t, parameters, delay_severity) do
-    {effect, location, affected_line_count, total_section_count, rotation_index} = parameters
+  defp log_layout_mismatch(t, delay_severity) do
+    log_fields =
+      t
+      |> get_layout_parameters()
+      |> Map.merge(%{
+        delay_severity: delay_severity,
+        rotation_index: t.rotation_index,
+        alert_id: t.alert.id,
+        stop_id: t.screen.app_params.alerts.stop_id
+      })
 
-    labeled_parameters = [
-      effect: effect,
-      delay_severity: delay_severity,
-      location: location,
-      affected_line_count: affected_line_count,
-      total_section_count: total_section_count,
-      rotation_index: rotation_index
-    ]
-
-    identifiers = [alert_id: t.alert.id, stop_ids: Enum.join(t.screen.alerts.stop_ids, ",")]
-    log_fields = identifiers ++ labeled_parameters
-
-    Logger.info(
+    Logger.warn(
       "[DUP alert no matching layout] " <>
         Enum.map_join(log_fields, " ", fn {label, value} -> "#{label}=#{value}" end)
     )
   end
-
-  # Inputs/outputs are stored as a table for readability.
-  # Table is adapted from the one in the product spec: https://www.notion.so/mbta-downtown-crossing/DUP-Alert-Widget-Specification-a82acff850ed4f2eb98a04e5f3e0fe52
-  # Compile-time code converts each table row to `do_alert_layout` function clauses.
-  # TotalSections refers to the number of sections in `primary_departures` of DUP configs.
-  alert_layout_table = """
-  # INPUTS                                                          || OUTPUTS
-  # Effect          Location            AffectedLines TotalSections || LayoutZero  LayoutOne   LayoutTwo
-    station_closure inside              1             1                full_screen full_screen partial
-    station_closure inside              1             2                partial     full_screen partial
-    station_closure inside              2             2                full_screen full_screen partial
-  # "Boundary" location is not possible for station closures.
-    shuttle         inside              1             1                full_screen full_screen partial
-    shuttle         inside              1             2                partial     full_screen partial
-    shuttle         inside              2             2                full_screen full_screen partial
-    shuttle         boundary_upstream   1             1                partial     full_screen partial
-    shuttle         boundary_downstream 1             1                partial     full_screen partial
-    shuttle         boundary_upstream   1             2                partial     full_screen partial
-    shuttle         boundary_downstream 1             2                partial     full_screen partial
-    suspension      inside              1             1                full_screen full_screen partial
-    suspension      inside              1             2                partial     full_screen partial
-    suspension      inside              2             2                full_screen full_screen partial
-    suspension      boundary_upstream   1             1                partial     full_screen partial
-    suspension      boundary_downstream 1             1                partial     full_screen partial
-    suspension      boundary_upstream   1             2                partial     full_screen partial
-    suspension      boundary_downstream 1             2                partial     full_screen partial
-  # Delay alerts are handled separately, see `defp do_alert_layout({:delay, ...` below.
-  """
-
-  parameters_to_layout =
-    alert_layout_table
-    |> String.split("\n", trim: true)
-    |> Enum.reject(&String.starts_with?(&1, "#"))
-    |> Enum.flat_map(fn line ->
-      [
-        effect,
-        location,
-        affected_line_count,
-        total_section_count,
-        layout_zero,
-        layout_one,
-        layout_two
-      ] = String.split(line, ~r/\s+/, trim: true)
-
-      # Convert strings to appropriate types
-      [effect, location, layout_zero, layout_one, layout_two] =
-        Enum.map(
-          [effect, location, layout_zero, layout_one, layout_two],
-          &String.to_existing_atom/1
-        )
-
-      [affected_line_count, total_section_count] =
-        Enum.map([affected_line_count, total_section_count], &String.to_integer/1)
-
-      base_parameters = {effect, location, affected_line_count, total_section_count}
-
-      [
-        {Tuple.append(base_parameters, :zero), layout_zero},
-        {Tuple.append(base_parameters, :one), layout_one},
-        {Tuple.append(base_parameters, :two), layout_two}
-      ]
-    end)
-    |> Enum.into(%{})
-
-  @type layout_parameters ::
-          {Alert.effect(), location :: atom, affected_line_count :: 1..2,
-           total_section_count :: 1..2, rotation_index}
-
-  # Now, define function clauses for each row in the table.
-  @spec do_alert_layout(layout_parameters) :: :full_screen | :partial | :no_render
-  for {parameters, layout} <- parameters_to_layout do
-    parameters_ast = Macro.escape(parameters)
-    defp do_alert_layout(unquote(parameters_ast)), do: unquote(layout)
-  end
-
-  # Delays always use partial alerts on all 3 rotations of the screen.
-  defp do_alert_layout(
-         {:delay, _location, _affected_line_count, _total_section_count, _rotation_index}
-       ) do
-    :partial
-  end
-
-  # If matching inputs aren't found in the table, we assume the alert isn't relevant and do not render it.
-  defp do_alert_layout(_parameters), do: :no_render
 
   def get_affected_lines(t) do
     t
