f7eaefcf3ac31e5de5f36df985bf0f1c7d2945b5
Christian Maddox <christian.maddox93@gmail.com>
2023-04-26 11:05:00 -0400

    fix: headway-section only-child styling (#1720)
    * Added a layout prop so we know for sure what kind of headway section we are showing.
    
    * Fixed tests.

--- assets/src/components/v2/dup/departures/headway_section.tsx
+++ assets/src/components/v2/dup/departures/headway_section.tsx
@@ -1,9 +1,15 @@
 import React from "react";
 import FreeText from "Components/v2/free_text";
+import { classWithModifier } from "Util/util";
 
-const HeadwaySection = ({ text }) => {
+const HeadwaySection = ({ text, layout }) => {
   return (
-    <div className="departures-section headway-section">
+    <div
+      className={`departures-section ${classWithModifier(
+        "headway-section",
+        layout
+      )}`}
+    >
       <FreeText lines={text} />
     </div>
   );
