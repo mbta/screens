dac39a5ec3391a5f5c2fc6e59b5374e0829c5642
cmaddox5 <christian.maddox93@gmail.com>
2023-05-02 10:04:36 -0400

    Added a behaviour to be used by alerts CandidateGenerator modules.

---
+++ lib/screens/v2/alerts_candidate_generator_behaviour.ex
@@ -0,0 +1,28 @@
+defmodule Screens.V2.AlertsCandidateGeneratorBehaviour do
+  @moduledoc """
+  Behavior for fetching and filtering alerts in a CandidateGenerator.
+
+  - `fetch(opts, fetch_alerts_fn)` is called fetch alerts.
+  - `relevant_alerts(alerts, config, opts)` filters the list of alerts.
+  """
+
+  # Parameters
+  @type opts :: keyword()
+  @type fetch_alerts_fn :: fun()
+  @type config :: Screens.Config.Screen.t()
+
+  # Returns
+  @type alerts :: list()
+  @type facilities :: map()
+  @type fetch_result :: alerts() | {alerts(), facilities()}
+
+  @doc """
+  Fetches a list of alerts using the function provided in parameters.
+  """
+  @callback fetch(opts(), fetch_alerts_fn()) :: {:ok, fetch_result()}
+
+  @doc """
+  Filters alerts so only relevant alerts are considered when creating a WidgetInstance struct.
+  """
+  @callback relevant_alerts(alerts(), config(), opts()) :: alerts()
+end
