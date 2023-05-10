68c752ba6825f419537343e43ee3831d6a86444f
Jon Zimbel <jzimbel@mbta.com>
2023-05-10 10:27:53 -0400

    feat: Subway Status 2.0 - LCD Frontend

--- assets/src/hooks/v2/use_text_resizer.tsx
+++ assets/src/hooks/v2/use_text_resizer.tsx
@@ -1,34 +1,81 @@
-import { useEffect, useLayoutEffect, useRef, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 
-interface UseTextResizerArgs {
-  sizes: string[];
+interface UseTextResizerArgs<T> {
+  sizes: T[];
   maxHeight: number;
   resetDependencies: any[];
 }
 
-interface UseTextResizerReturn {
-  ref: React.MutableRefObject<null>;
-  size: string;
+interface UseTextResizerReturn<T> {
+  ref: React.RefObject<HTMLDivElement>;
+  size: T;
 }
 
 /**
- * This hook creates a ref to be placed on an element with text and uses the `useLayoutEffect` hook
- * to find the largest font size that still allows the text to fit in the element. If none of the sizes
- * fit, the smallest size is selected. This hook should be used for text resizing in a single containing
- * element with a single CSS class. It does not work well with containers that require more complicated
- * resizing, e.g. Subway Status rows.
- * @param sizes A list of CSS modifiers that represent the font size for the given element. Elements should be ordered smallest to largest.
+ * This hook creates a ref to be placed on an element with text and uses the `useEffect` hook
+ * to find the largest size that still allows the text to fit in the element. If none of the sizes
+ * fit, the smallest size is selected.
+ *
+ * This hook can be used with a list of CSS classes or modifiers for text resizing
+ * in a single containing element with a single CSS class.
+ *
+ * ```
+ * const {ref, size} = useTextResizer({
+ *   sizes: ["small", "medium", "large"],
+ *   maxHeight: 100,
+ *   resetDependencies: [textContent]
+ * });
+ *
+ * return <div className={classWithModifier("text-el", size)} ref={ref}>{textContent}</div>;
+ * ```
+ *
+ * The hook can also be given a list of arbitrary values (e.g. from an enum), which the calling
+ * component can use however it likes to adjust content and styles until the ref'd element
+ * has the desired maxHeight.
+ *
+ * ```
+ * enum SizingStep {
+ *   AbbreviateAndShrink,
+ *   Abbreviate,
+ *   FullSize
+ * }
+ *
+ * const {ref, size: selectedStep} = useTextResizer({
+ *   sizes: [SizingStep.AbbreviateAndShrink, SizingStep.Abbreviate, SizingStep.FullSize],
+ *   maxHeight: 100,
+ *   resetDependencies: [anything, that, affects, content, being, shown]
+ * });
+ *
+ * let abbreviate = false;
+ * let shrink = false;
+ * switch (selectedStep) {
+ *   case SizingStep.FullSize:
+ *     break;
+ *   case SizingStep.Abbreviate:
+ *     abbreviate = true;
+ *     break;
+ *   case SizingStep.AbbreviateAndShrink:
+ *     abbreviate = true;
+ *     shrink = true;
+ *     break;
+ * }
+ *
+ * return <div className={shrink ? "text-el--small" : "text-el"}>{abbreviate ? content.abbrev : content.full}</div>;
+ * ```
+ *
+ * @param sizes A list of values (CSS modifiers, enum members, or anything else) that represent the sizes for the given element.
+ *              Elements should be ordered smallest to largest.
  * @param maxHeight The maximum height of the container in which the text will be placed.
  * @param resetDependencies A list of dependencies that should be used to reset the sizeIndex.
- * @returns A ref and the selected CSS modifier.
+ * @returns A ref and the selected size value.
  */
-const useTextResizer = ({
+const useTextResizer = <T,>({
   sizes,
   maxHeight,
   resetDependencies,
-}: UseTextResizerArgs): UseTextResizerReturn => {
+}: UseTextResizerArgs<T>): UseTextResizerReturn<T> => {
   const [sizeIndex, setSizeIndex] = useState(sizes.length - 1);
-  const ref = useRef(null);
+  const ref = useRef<HTMLDivElement>(null);
 
   useEffect(() => {
     setSizeIndex(sizes.length - 1);
