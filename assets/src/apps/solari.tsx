f63bdd4ff793890c1c13c41ab1ff03ceef381513
Jon Zimbel <63608771+jzimbel-mbta@users.noreply.github.com>
2023-05-17 11:12:08 -0400

    Add Solari watchdog listener on Solari app root component mount (#1744)
    * Add Solari watchdog listener on Solari app root component mount
    
    * Use a cleanup function since the effect subscribes to an event

--- assets/src/apps/solari.tsx
+++ assets/src/apps/solari.tsx
@@ -5,7 +5,7 @@ declare function require(name: string): string;
 // tslint:disable-next-line
 require("../../css/solari.scss");
 
-import React from "react";
+import React, { useEffect } from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
@@ -20,6 +20,8 @@ import {
 } from "Components/eink/screen_page";
 
 const App = (): JSX.Element => {
+  useEffect(watchdogSubscriptionEffect, []);
+
   return (
     <Router>
       <Switch>
@@ -36,5 +38,22 @@ const App = (): JSX.Element => {
     </Router>
   );
 };
+
+const watchdogSubscriptionEffect = () => {
+  // Add a listener for "watchdog" events
+  window.addEventListener("message", handleWatchdogMessage);
+
+  // Return a cleanup function for React to call if the component re-renders, unmounts, etc.
+  return () => {
+    window.removeEventListener("message", handleWatchdogMessage);
+  };
+};
+
+const handleWatchdogMessage = (ev: MessageEvent) => {
+  // message is formatted this way {type:"watchdog", data: counter++ }
+  if (ev.data.type === "watchdog") {
+    (ev?.source as Window)?.postMessage(ev.data, "*");
+  }
+}
 
 ReactDOM.render(<App />, document.getElementById("app"));
