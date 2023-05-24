56156276d41693ea54bdfd85f09a9569399843f6
cmaddox5 <christian.maddox93@gmail.com>
2023-05-24 11:14:03 -0400

    Corrected filter.

--- lib/screens/v2/candidate_generator/widgets/reconstructed_alert.ex
+++ lib/screens/v2/candidate_generator/widgets/reconstructed_alert.ex
@@ -73,7 +73,13 @@ defmodule Screens.V2.CandidateGenerator.Widgets.ReconstructedAlert do
       moderate_delays =
         Enum.filter(
           relevant_alerts,
-          &(&1.effect == :delay and &1.severity >= 5)
+          &(Alert.get_alert_location_for_stop_id(
+              &1,
+              stop_id,
+              stop_sequences,
+              routes_at_stop,
+              is_terminal_station
+            ) in [:elsewhere] and (&1.effect == :delay and &1.severity >= 5))
         )
 
       common_parameters = [
