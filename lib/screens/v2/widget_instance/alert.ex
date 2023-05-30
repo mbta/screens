5ad596cfb601c15f7559951b47f79fa7eb81c191
Hannah Purcell <hpurcell@mbta.com>
2023-05-30 11:07:36 -0400

    Credo fix

--- lib/screens/v2/widget_instance/alert.ex
+++ lib/screens/v2/widget_instance/alert.ex
@@ -4,8 +4,8 @@ defmodule Screens.V2.WidgetInstance.Alert do
   alias Screens.Alerts.Alert
   alias Screens.Config.Screen
   alias Screens.LocationContext
-  alias Screens.V2.WidgetInstance
   alias Screens.V2.LocalizedAlert
+  alias Screens.V2.WidgetInstance  
   alias Screens.V2.WidgetInstance.Serializer.RoutePill
 
   defstruct screen: nil,
