8716f12c6e1b7c4303f85a7e96083fda5be3700e
cmaddox5 <christian.maddox93@gmail.com>
2023-06-08 09:30:20 -0400

    New alert helper function.

--- lib/screens/alerts/alert.ex
+++ lib/screens/alerts/alert.ex
@@ -573,4 +573,8 @@ defmodule Screens.Alerts.Alert do
   end
 
   def effect(%__MODULE__{effect: effect}), do: effect
+
+  def direction_id(%__MODULE__{informed_entities: informed_entities}) do
+    informed_entities |> Enum.map(& &1.direction_id) |> Enum.uniq() |> List.first()
+  end
 end
