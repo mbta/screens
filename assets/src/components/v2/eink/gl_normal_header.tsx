91638794e66edf47151318b6b4f699602beb6433
cmaddox5 <christian.maddox93@gmail.com>
2023-06-21 08:33:34 -0400

    Split out eink headers into separate components.

---
+++ assets/src/components/v2/eink/gl_normal_header.tsx
@@ -0,0 +1,30 @@
+import React, { ComponentType } from "react";
+
+import DefaultNormalHeader, { Icon } from "Components/v2/normal_header";
+
+interface Props {
+  icon: Icon;
+  text: string;
+  time: string;
+  show_to: boolean;
+}
+
+const NormalHeader: ComponentType<Props> = ({
+  icon,
+  text,
+  time,
+  show_to: showTo,
+}) => {
+  return (
+    <DefaultNormalHeader
+      icon={icon}
+      text={text}
+      time={time}
+      showUpdated
+      maxHeight={104}
+      showTo={showTo}
+    />
+  );
+};
+
+export default NormalHeader;
