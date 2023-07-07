63b950b6ab8790f98083b79638b5ed0864073c42
cmaddox5 <christian.maddox93@gmail.com>
2023-07-07 08:09:00 -0400

    Finished the solid line map and initial math needed for spacing.

--- assets/src/components/v2/disruption_diagram/disruption_diagram.tsx
+++ assets/src/components/v2/disruption_diagram/disruption_diagram.tsx
@@ -1,6 +1,16 @@
 import React, { ComponentType } from "react";
 
-type DisruptionDiagramData = ContinuousDisruptionDiagram | DiscreteDisruptionDiagram;
+const MAX_WIDTH = 904;
+const SLOT_WIDTH = 24;
+const LINE_HEIGHT = 24;
+const L = 13;
+// TODO: Make this based on headsign
+const R = 165;
+const W = MAX_WIDTH - (L + R);
+
+type DisruptionDiagramData =
+  | ContinuousDisruptionDiagram
+  | DiscreteDisruptionDiagram;
 
 interface DisruptionDiagramBase {
   line: LineColor;
@@ -35,7 +45,7 @@ interface MiddleSlot {
 }
 
 // Note the single ellipsis character, not 3 periods
-type Label = "…" | { full: string, abbrev: string };
+type Label = "…" | { full: string; abbrev: string };
 
 // End labels have hardcoded presentation, so we just send an ID for the client to use in
 // a lookup.
@@ -46,11 +56,160 @@ type Label = "…" | { full: string, abbrev: string };
 // so we can send actual text for those--it will be dynamically resized to fit.
 type EndLabelID = string;
 
-type LineColor =
-  | "blue"
-  | "orange"
-  | "red"
-  | "green";
+type LineColor = "blue" | "orange" | "red" | "green";
+
+const stopCircle = (
+  <circle
+    cx="14"
+    cy="14"
+    r="12"
+    fill="white"
+    stroke="#171F26"
+    strokeWidth="4"
+  />
+);
+
+const terminalCircle = (
+  <circle
+    cx="24"
+    cy="24"
+    r="20"
+    fill="white"
+    stroke="#171F26"
+    strokeWidth="8"
+  />
+);
+
+const homeStopIcon = (
+  <path
+    d="M3.15665 25.2076C1.61445 26.7498 1.61445 29.2502 3.15665 30.7924L25.2076 52.8434C26.7498 54.3856 29.2502 54.3855 30.7924 52.8433L52.8434 30.7924C54.3856 29.2502 54.3856 26.7498 52.8434 25.2076L30.7924 3.15665C29.2502 1.61445 26.7498 1.61445 25.2076 3.15665L3.15665 25.2076Z"
+    fill="#EE2E24"
+    stroke="#E6E4E1"
+    strokeWidth="4"
+  />
+);
+
+const homeStopTerminalIcon = (
+  <>
+    <path
+      d="M39.4605 4.26181C36.4447 1.24606 31.5553 1.24606 28.5395 4.26181L4.26181 28.5395C1.24606 31.5553 1.24606 36.4447 4.26181 39.4605L28.5395 63.7382C31.5553 66.7539 36.4447 66.7539 39.4605 63.7382L63.7382 39.4605C66.7539 36.4447 66.7539 31.5553 63.7382 28.5395L39.4605 4.26181Z"
+      fill="#EE2E24"
+      stroke="#E6E4E1"
+      strokeWidth="4"
+      strokeLinejoin="round"
+    />
+    <path
+      fillRule="evenodd"
+      clipRule="evenodd"
+      d="M18.0032 35.1702C17.2222 34.3892 17.2222 33.1229 18.0032 32.3418L32.3417 18.0033C33.1228 17.2223 34.3891 17.2223 35.1702 18.0033L49.5086 32.3418C50.2897 33.1229 50.2897 34.3892 49.5086 35.1702L35.1702 49.5087C34.3891 50.2898 33.1228 50.2898 32.3417 49.5087L18.0032 35.1702Z"
+      fill="white"
+    />
+  </>
+);
+
+interface EndSlotComponentProps {
+  slot: EndSlot;
+  line: LineColor;
+}
+
+const FirstSlotComponent: ComponentType<EndSlotComponentProps> = ({
+  slot,
+  line,
+}) => {
+  if (slot.type === "arrow") {
+    return (
+      <path
+        transform="translate(0 12)"
+        d="M35 0V24L19.554 24C19.1915 24 18.8358 23.9015 18.525 23.715L1.85831 13.715C0.563633 12.9382 0.563633 11.0618 1.85831 10.285L18.525 0.285015C18.8358 0.0985165 19.1915 0 19.554 0L35 0Z"
+        fill={line}
+      />
+    );
+  } else {
+    return (
+      <circle
+        cx={L + SLOT_WIDTH / 2}
+        cy="24"
+        r="20"
+        fill="white"
+        stroke="#171F26"
+        strokeWidth="8"
+      />
+    );
+  }
+};
+
+const LastSlotComponent: ComponentType<
+  EndSlotComponentProps & { x: number }
+> = ({ slot, line, x }) => {
+  if (slot.type === "arrow") {
+    return (
+      <path
+        transform={`translate(${x} 12)`}
+        width={204}
+        d="M0 24V0H59.446C59.8085 0 60.1642 0.0985159 60.475 0.285014L77.1417 10.285C78.4364 11.0618 78.4364 12.9382 77.1417 13.715L60.475 23.715C60.1642 23.9015 59.8085 24 59.446 24H0Z"
+        fill={line}
+      />
+    );
+  } else {
+    return (
+      <circle
+        cx={x}
+        cy="24"
+        r="20"
+        fill="white"
+        stroke="#171F26"
+        strokeWidth="8"
+      />
+    );
+  }
+};
+
+interface MiddleSlotComponentProps {
+  slot: MiddleSlot;
+  x: number;
+  spaceBetween: number;
+  line: LineColor;
+}
+
+const MiddleSlotComponent: ComponentType<MiddleSlotComponentProps> = ({
+  slot,
+  x,
+  spaceBetween,
+  line,
+}) => {
+  const background = (
+    <rect
+      width={SLOT_WIDTH + spaceBetween}
+      height={LINE_HEIGHT}
+      fill={line}
+      x={x}
+      y="12"
+    />
+  );
+
+  let icon;
+  if (slot.show_symbol) {
+    icon = (
+      <circle
+        cx={x}
+        cy="24"
+        r="10"
+        fill="white"
+        stroke={line}
+        strokeWidth="4"
+      />
+    );
+  } else {
+    icon = <div>…</div>;
+  }
+
+  return (
+    <>
+      {background}
+      {icon}
+    </>
+  );
+};
 
 /*
 Client is responsible for:
@@ -61,10 +220,62 @@ Client is responsible for:
 - sizing, spacing, positioning of edges/end arrows/shuttle dashes/the diagram as a whole within its container
 */
 
-const DisruptionDiagram: ComponentType<DisruptionDiagramData> = (_props) => {
+// R=165
+// L=11
+// W=728
+
+const DisruptionDiagram: ComponentType<DisruptionDiagramData> = ({
+  slots,
+  current_station_slot_index,
+  line,
+}) => {
+  const numStops = slots.length;
+  const spaceBetween = Math.min(
+    60,
+    (W - SLOT_WIDTH * numStops) / (numStops - 1)
+  );
+  const { 0: beginning, [slots.length - 1]: end, ...middle } = slots;
+  let x = 0;
+  const middleSlots = Object.values(middle).map((s, i) => {
+    x = (spaceBetween + SLOT_WIDTH) * (i + 1);
+    const slot = s as MiddleSlot;
+    const key = slot.label === "…" ? i : slot.label.full;
+    console.log(x);
+    return (
+      <MiddleSlotComponent
+        key={key}
+        slot={slot}
+        x={x}
+        spaceBetween={spaceBetween}
+        line={line}
+      />
+    );
+  });
+
+  x += spaceBetween + SLOT_WIDTH;
+
   return (
-    <div className="disruption-diagram">Diagram goes here!</div>
+    <svg
+      width="904px"
+      height="308px"
+      viewBox={[0, 0, 904, 308].join(" ")}
+      version="1.1"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <FirstSlotComponent slot={beginning} line={line} />
+      <rect
+        width={SLOT_WIDTH / 2 + spaceBetween}
+        height={LINE_HEIGHT}
+        fill={line}
+        x={L + SLOT_WIDTH / 2}
+        y="12"
+      />
+      {middleSlots}
+      <LastSlotComponent slot={end as EndSlot} x={x} line={line} />
+    </svg>
   );
 };
+
+export { ContinuousDisruptionDiagram };
 
 export default DisruptionDiagram;
--- assets/src/components/v2/pre_fare_single_screen_alert.tsx
+++ assets/src/components/v2/pre_fare_single_screen_alert.tsx
@@ -1,7 +1,15 @@
 import useTextResizer from "Hooks/v2/use_text_resizer";
 import React from "react";
 
-import { classWithModifier, classWithModifiers, formatCause, imagePath } from "Util/util";
+import {
+  classWithModifier,
+  classWithModifiers,
+  formatCause,
+  imagePath,
+} from "Util/util";
+import DisruptionDiagram, {
+  ContinuousDisruptionDiagram,
+} from "./disruption_diagram/disruption_diagram";
 
 interface PreFareSingleScreenAlertProps {
   issue: string;
@@ -15,54 +23,85 @@ interface PreFareSingleScreenAlertProps {
   updated_at: string;
 }
 
-const doFallbackLayout = (issue: string | null, remedy: string | null, pioSecondaryTextSize: string) => ( 
+const doFallbackLayout = (
+  issue: string | null,
+  remedy: string | null,
+  pioSecondaryTextSize: string
+) => (
   <>
     <img
       className="alert-card__pio-text__icon"
       src={imagePath("clock-negative.svg")}
     />
     {issue && <div className="alert-card__pio-text__main-text">{issue}</div>}
-    {remedy && <div className={classWithModifier("alert-card__pio-text__secondary-text", pioSecondaryTextSize)}>{remedy}</div>}
+    {remedy && (
+      <div
+        className={classWithModifier(
+          "alert-card__pio-text__secondary-text",
+          pioSecondaryTextSize
+        )}
+      >
+        {remedy}
+      </div>
+    )}
   </>
-)
+);
 
-const standardIssueSection = (issue: string, location: string | null, textSize: string) => (
+const standardIssueSection = (
+  issue: string,
+  location: string | null,
+  textSize: string
+) => (
   <div className="alert-card__issue">
-    <img
-      className="alert-card__icon"
-      src={imagePath("no-service-black.svg")}
-    />
+    <img className="alert-card__icon" src={imagePath("no-service-black.svg")} />
     <div>
-      <div className={classWithModifier("alert-card__content-block__text", textSize)}>
+      <div
+        className={classWithModifier(
+          "alert-card__content-block__text",
+          textSize
+        )}
+      >
         {issue}
       </div>
-      {location && <div className="alert-card__issue__location">{location}</div>}
+      {location && (
+        <div className="alert-card__issue__location">{location}</div>
+      )}
     </div>
   </div>
-)
+);
 
 const downstreamIssueSection = (issue: string) => (
   <div className="alert-card__issue">
-    <div className={classWithModifier("alert-card__content-block__text", "medium")}>{issue}</div>
+    <div
+      className={classWithModifier("alert-card__content-block__text", "medium")}
+    >
+      {issue}
+    </div>
   </div>
-)
+);
 
-const remedySection = (effect: string, remedy: string | null, contentTextSize: string) => (
+const remedySection = (
+  effect: string,
+  remedy: string | null,
+  contentTextSize: string
+) => (
   <div className="alert-card__remedy">
-    { effect === "shuttle" ?
+    {effect === "shuttle" ? (
       <>
         <div className="alert-card__remedy__shuttle-icons">
-          <img
-            className="alert-card__icon"
-            src={imagePath("bus-black.svg")}
-          />
+          <img className="alert-card__icon" src={imagePath("bus-black.svg")} />
           <img
             className="alert-card__isa-icon"
             src={imagePath("ISA_Blue.svg")}
-          /> 
+          />
         </div>
         <div>
-          <div className={classWithModifier("alert-card__content-block__text", contentTextSize)}>
+          <div
+            className={classWithModifier(
+              "alert-card__content-block__text",
+              contentTextSize
+            )}
+          >
             {remedy}
           </div>
           <div className="alert-card__body__accessibility-info--text">
@@ -70,51 +109,94 @@ const remedySection = (effect: string, remedy: string | null, contentTextSize: s
           </div>
         </div>
       </>
-      : <>
-        <img
-          className="alert-card__icon"
-          src={imagePath("nearby.svg")}
-        />
+    ) : (
+      <>
+        <img className="alert-card__icon" src={imagePath("nearby.svg")} />
         <div className="alert-card__remedy__text">{remedy}</div>
       </>
-    }
+    )}
   </div>
-)
+);
 
 const mapSection = () => {
-  <></>
-}
+  const props: ContinuousDisruptionDiagram = {
+    effect: "suspension",
+    line: "red",
+    current_station_slot_index: 2,
+    effect_region_slot_index_range: [3, 4],
+    slots: [
+      { type: "arrow", label_id: "place-alfcl" },
+      {
+        label: { full: "Kendall/MIT", abbrev: "Kendall/MIT" },
+        show_symbol: true,
+      },
+      {
+        label: { full: "Charles/MGH", abbrev: "Charles/MGH" },
+        show_symbol: true,
+      },
+      {
+        label: { full: "Park Street", abbrev: "Park St" },
+        show_symbol: true,
+      },
+      {
+        label: { full: "Downtown Crossing", abbrev: "Downt'n Xng" },
+        show_symbol: true,
+      },
+      {
+        label: { full: "South Station", abbrev: "South Sta" },
+        show_symbol: true,
+      },
+      { type: "arrow", label_id: "place-asmnl+place-brntn" },
+    ],
+  };
 
-const PreFareSingleScreenAlert: React.ComponentType<PreFareSingleScreenAlertProps> = (alert) => {
-  const { cause, region, effect, issue, location, remedy, routes, unaffected_routes, updated_at } = alert;
+  return <DisruptionDiagram {...props} />;
+};
 
-  /** 
-  * This switch statement picks the alert layout
-  * - fallback: icon, followed by a summary & pio text, or just the pio text
-  * - multiline: icon + route pill + text explaining the lines that are closed at the station
-  *              and then icon + route pill + text explaining normal service. Finally, the map section
-  * - standard: icon + issue, and icon + remedy, and then map section
-  * - downstream: map, issue without icon, then icon + remedy
-  **/
+const PreFareSingleScreenAlert: React.ComponentType<
+  PreFareSingleScreenAlertProps
+> = (alert) => {
+  const {
+    cause,
+    region,
+    effect,
+    issue,
+    location,
+    remedy,
+    routes,
+    unaffected_routes,
+    updated_at,
+  } = alert;
+
+  /**
+   * This switch statement picks the alert layout
+   * - fallback: icon, followed by a summary & pio text, or just the pio text
+   * - multiline: icon + route pill + text explaining the lines that are closed at the station
+   *              and then icon + route pill + text explaining normal service. Finally, the map section
+   * - standard: icon + issue, and icon + remedy, and then map section
+   * - downstream: map, issue without icon, then icon + remedy
+   **/
   const layoutRenderer = () => {
-    switch(true) {
+    switch (true) {
       case effect === "delay":
-        return fallbackLayout()
-      case (effect === "station_closure" || effect === "stop_closure") && region === "here":
-        return multiLineLayout()
+        return fallbackLayout();
+      case (effect === "station_closure" || effect === "stop_closure") &&
+        region === "here":
+        return multiLineLayout();
       case effect === "station_closure" || effect === "stop_closure":
-        return standardLayout()
-      case (region === "boundary" || region === "here")
-            && (effect === "shuttle" || effect === "suspension"):
-        return standardLayout()
-      case region === "outside" && (effect === "shuttle" || effect === "suspension"):
-        return downstreamLayout()  
+        return standardLayout();
+      case (region === "boundary" || region === "here") &&
+        (effect === "shuttle" || effect === "suspension"):
+        return standardLayout();
+      case region === "outside" &&
+        (effect === "shuttle" || effect === "suspension"):
+        return downstreamLayout();
       default:
-        return fallbackLayout()
+        return fallbackLayout();
     }
-  }
-  
-  // For the standard layout, issue font can be medium or large. 
+  };
+
+  // For the standard layout, issue font can be medium or large.
   // If remedy is "Seek alternate route", font size is static. Otherwise, it uses the same font size as
   // the issue.
   const standardLayout = () => {
@@ -130,17 +212,19 @@ const PreFareSingleScreenAlert: React.ComponentType<PreFareSingleScreenAlertProp
         {remedySection(effect, remedy, contentTextSize)}
         {mapSection()}
       </div>
-    )
-  }
+    );
+  };
 
   // In the downstream layout, the map is at the top, and the font size stays constant
   const downstreamLayout = () => (
-    <div className={classWithModifier("alert-card__content-block", "downstream")}>
+    <div
+      className={classWithModifier("alert-card__content-block", "downstream")}
+    >
       {mapSection()}
       {downstreamIssueSection(issue)}
       {remedySection(effect, remedy, "medium")}
     </div>
-  )
+  );
 
   // Covers the case where a station_closure only affects one line at a transfer station.
   // In the even rarer case that there are multiple branches in the routes list or unaffected routes list
@@ -159,49 +243,73 @@ const PreFareSingleScreenAlert: React.ComponentType<PreFareSingleScreenAlertProp
             className="alert-card__icon"
             src={imagePath("no-service-black.svg")}
           />
-          <div className={classWithModifier("alert-card__content-block__text", contentTextSize)}>
-            <img src={pillPath(routes[0])} className="alert-card__content-block__route-pill" />
+          <div
+            className={classWithModifier(
+              "alert-card__content-block__text",
+              contentTextSize
+            )}
+          >
+            <img
+              src={pillPath(routes[0])}
+              className="alert-card__content-block__route-pill"
+            />
             <span>trains are skipping this station</span>
           </div>
         </div>
         <div className="alert-card__issue">
-          <img
-            className="alert-card__icon"
-            src={imagePath("info.svg")}
-          />
-          <div className={classWithModifier("alert-card__content-block__text", contentTextSize)}>
-            {unaffected_routes.map(route => 
-             <img src={pillPath(route)} key={route} className="alert-card__content-block__route-pill" />
+          <img className="alert-card__icon" src={imagePath("info.svg")} />
+          <div
+            className={classWithModifier(
+              "alert-card__content-block__text",
+              contentTextSize
             )}
-           <span>trains stop as usual</span>
+          >
+            {unaffected_routes.map((route) => (
+              <img
+                src={pillPath(route)}
+                key={route}
+                className="alert-card__content-block__route-pill"
+              />
+            ))}
+            <span>trains stop as usual</span>
           </div>
         </div>
         {mapSection()}
       </div>
-    )
-  }
+    );
+  };
 
   const fallbackLayout = () => {
     // If there is more than 1 route in the banner, or the 1 route is longer than "GL·B"
     // the banner will be tall. Otherwise, it'll be 1-line
-    const bannerHeight = (routes.length > 1 || routes[0].length > 4) ? 368 : 200
+    const bannerHeight = routes.length > 1 || routes[0].length > 4 ? 368 : 200;
 
-    const screenHeight = 1720, footerHeight = 84, bottomMargin = 32, alertCardPadding = 120 + 32
+    const screenHeight = 1720,
+      footerHeight = 84,
+      bottomMargin = 32,
+      alertCardPadding = 120 + 32;
 
-    const maxTextHeight = screenHeight - footerHeight - bottomMargin - alertCardPadding - bannerHeight
+    const maxTextHeight =
+      screenHeight -
+      footerHeight -
+      bottomMargin -
+      alertCardPadding -
+      bannerHeight;
 
-    const { ref: pioTextBlockRef, size: pioSecondaryTextSize } = useTextResizer({
-      sizes: ["small", "medium"],
-      maxHeight: maxTextHeight,
-      resetDependencies: [issue, remedy],
-    });
+    const { ref: pioTextBlockRef, size: pioSecondaryTextSize } = useTextResizer(
+      {
+        sizes: ["small", "medium"],
+        maxHeight: maxTextHeight,
+        resetDependencies: [issue, remedy],
+      }
+    );
 
     return (
       <div className="alert-card__pio-text" ref={pioTextBlockRef}>
         {doFallbackLayout(issue, remedy, pioSecondaryTextSize)}
       </div>
-    )
-  }
+    );
+  };
 
   // TODO: When there is an SVG within the content-block, resize logic
   // doesn't kick in until the next data fetch. Should be fixed with better SVG loading
@@ -212,16 +320,21 @@ const PreFareSingleScreenAlert: React.ComponentType<PreFareSingleScreenAlertProp
       <div
         className={classWithModifiers("alert-container", [
           "single-page",
-          getAlertColor(routes)
+          getAlertColor(routes),
         ])}
       >
         <div className="alert-card">
-          <div className="alert-card__body">
-            {layoutRenderer()}
-          </div>
+          <div className="alert-card__body">{layoutRenderer()}</div>
           <div className="alert-card__footer">
-            { cause && <div className="alert-card__footer__cause">Cause: {formatCause(cause)}</div> }
-            <div>Updated <span className="alert-card__footer__datetime">{updated_at}</span></div>
+            {cause && (
+              <div className="alert-card__footer__cause">
+                Cause: {formatCause(cause)}
+              </div>
+            )}
+            <div>
+              Updated{" "}
+              <span className="alert-card__footer__datetime">{updated_at}</span>
+            </div>
           </div>
         </div>
       </div>
@@ -229,60 +342,121 @@ const PreFareSingleScreenAlert: React.ComponentType<PreFareSingleScreenAlertProp
   );
 };
 
-const pillPath = (pillFilename: string) => imagePath(`pills/${pillFilename}.svg`);
+const pillPath = (pillFilename: string) =>
+  imagePath(`pills/${pillFilename}.svg`);
 const getRouteColor = (route: string) => {
-  switch(route.substring(0, 2)) {
-    case "rl": return "red"
-    case "re": return "red"
-    case "ol": return "orange"
-    case "or": return "orange"
-    case "bl": return "blue"
-    case "gl": return "green"
-    case "gr": return "green"
-    default: return "yellow"
+  switch (route.substring(0, 2)) {
+    case "rl":
+      return "red";
+    case "re":
+      return "red";
+    case "ol":
+      return "orange";
+    case "or":
+      return "orange";
+    case "bl":
+      return "blue";
+    case "gl":
+      return "green";
+    case "gr":
+      return "green";
+    default:
+      return "yellow";
   }
-}
+};
 const getAlertColor = (routes: string[]) => {
-  const colors = routes.map(r => getRouteColor(r)).sort()
-  return colors[0] === colors.slice(-1)[0] ? colors[0] : "yellow"
-}
+  const colors = routes.map((r) => getRouteColor(r)).sort();
+  return colors[0] === colors.slice(-1)[0] ? colors[0] : "yellow";
+};
 
-const PreFareAlertBanner: React.ComponentType<{routes: any[]}> = ({routes}) => {
+const PreFareAlertBanner: React.ComponentType<{ routes: any[] }> = ({
+  routes,
+}) => {
   let banner;
 
-  if (routes.length === 1 && ["red-line", "orange-line", "blue-line", "green-line", "gl-b", "gl-c", "gl-d", "gl-e"].includes(routes[0])) {
+  if (
+    routes.length === 1 &&
+    [
+      "red-line",
+      "orange-line",
+      "blue-line",
+      "green-line",
+      "gl-b",
+      "gl-c",
+      "gl-d",
+      "gl-e",
+    ].includes(routes[0])
+  ) {
     // One destination, short text
-    const route = routes[0]
+    const route = routes[0];
 
-    banner = <div className={classWithModifiers("alert-banner", ["small", getRouteColor(route)])}>
-      <span className="alert-banner__attention-text">ATTENTION</span>
-      <img src={pillPath(route)} className="alert-banner__route-pill--short" />
-      <span>riders</span>
-    </div>
+    banner = (
+      <div
+        className={classWithModifiers("alert-banner", [
+          "small",
+          getRouteColor(route),
+        ])}
+      >
+        <span className="alert-banner__attention-text">ATTENTION</span>
+        <img
+          src={pillPath(route)}
+          className="alert-banner__route-pill--short"
+        />
+        <span>riders</span>
+      </div>
+    );
   } else if (routes.length === 1) {
     // One destination, long text
-    const route = routes[0]
+    const route = routes[0];
 
-    banner = <div className={classWithModifiers("alert-banner", ["large--one-route", getRouteColor(route)])}>
-      <span><span className="alert-banner__attention-text">ATTENTION,</span> riders to</span>
-      <img src={pillPath(route)} className="alert-banner__route-pill--long" />
-    </div>
+    banner = (
+      <div
+        className={classWithModifiers("alert-banner", [
+          "large--one-route",
+          getRouteColor(route),
+        ])}
+      >
+        <span>
+          <span className="alert-banner__attention-text">ATTENTION,</span>{" "}
+          riders to
+        </span>
+        <img src={pillPath(route)} className="alert-banner__route-pill--long" />
+      </div>
+    );
   } else if (routes.length === 2) {
     // Two destinations
-    banner = <div className={classWithModifiers("alert-banner", ["large--two-routes", getAlertColor(routes)])}>
-      <span><span className="alert-banner__attention-text">ATTENTION,</span> riders to</span>
-      {routes.map((route) => (
-        <img src={pillPath(route)} className="alert-banner__route-pill--long" />
-      ))}
-    </div>
+    banner = (
+      <div
+        className={classWithModifiers("alert-banner", [
+          "large--two-routes",
+          getAlertColor(routes),
+        ])}
+      >
+        <span>
+          <span className="alert-banner__attention-text">ATTENTION,</span>{" "}
+          riders to
+        </span>
+        {routes.map((route) => (
+          <img
+            src={pillPath(route)}
+            className="alert-banner__route-pill--long"
+          />
+        ))}
+      </div>
+    );
   } else {
     // Fallback
-    banner = <div className={classWithModifiers("alert-banner", ["small", "yellow"])}>
-      <span><span className="alert-banner__attention-text">ATTENTION,</span> riders</span>
-    </div>
+    banner = (
+      <div className={classWithModifiers("alert-banner", ["small", "yellow"])}>
+        <span>
+          <span className="alert-banner__attention-text">ATTENTION,</span>{" "}
+          riders
+        </span>
+      </div>
+    );
   }
 
   return banner;
-}
+};
 
 export default PreFareSingleScreenAlert;
