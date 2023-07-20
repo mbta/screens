339f701f9fe0e915bd083f57ce13cc6451f42ee1
Hannah Purcell <hpurcell@mbta.com>
2023-07-20 16:09:19 -0400

    Added console logs for temporary debugging

--- assets/src/components/v2/dup/version.tsx
+++ assets/src/components/v2/dup/version.tsx
@@ -1,1 +1,1 @@
-export const DUP_VERSION = "23.7.12.1";
+export const DUP_VERSION = "23.7.20.1";
--- assets/src/hooks/use_outfront_player_name.tsx
+++ assets/src/hooks/use_outfront_player_name.tsx
@@ -4,16 +4,24 @@ const useOutfrontPlayerName = () => {
   const [playerName, setPlayerName] = useState("");
 
   useEffect(() => {
+    console.log("Running the useEffect hook")
     if (parent?.parent?.mraid ?? false) {
+      console.log("  Within the condition: parent?.parent?.mraid == true")
       try {
+        console.log("   Within the try. Attempting to getDeviceInfo and parse the result")
         const info = JSON.parse(mraid.getDeviceInfo());
+        console.log("   result of getDeviceInfo: ", mraid.getDeviceInfo())
+        console.log("   result of JSON.parse: ", info)
         setPlayerName(info.deviceName)
       } catch (err) {
+        console.log("   Within the catch. Setting deviceName to `noMraid`")
         setPlayerName("noMraid");
       }
     }
+    // Will rerun if MRAID changes
   }, [parent?.parent?.mraid]);
   
+  if (playerName) console.log("useOutfrontPlayerName hook has finished. Returning current state of playerName: ", playerName)
   return playerName;
 };
 
