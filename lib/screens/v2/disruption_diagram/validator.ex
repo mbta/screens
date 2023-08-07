567e0a88e7e08c34911bb4b7caa90f280faf7a69
Jon Zimbel <jzimbel@mbta.com>
2023-08-07 18:32:42 -0400

    Reject 1-stop shuttles/suspensions for disruption diagram

--- lib/screens/v2/disruption_diagram/validator.ex
+++ lib/screens/v2/disruption_diagram/validator.ex
@@ -1,14 +1,11 @@
 defmodule Screens.V2.DisruptionDiagram.Validator do
   @moduledoc """
   Validates LocalizedAlerts for compatibility with disruption diagrams:
-  - The alert is a subway alert with an effect of shuttle, suspension, or station_closure
-  - The alert informs stops on only one subway route
-    - For BL & OL, this is the same as the line
-    - For RL, this is either the trunk, the Ashmont branch, or the Braintree branch. No combination of the three.
-    - For GL, this is any one branch, or just the trunk (Lechmere to Kenmore).
-    - No combinations of multiple lines, e.g. some stops on BL and some on OL.
-  - The alert does not inform an entire route (we only allow one end of the diagram to have a terminal stop)
-  - All stops informed by the alert are directly reachable from the home stop.
+  - The alert is a subway alert with an effect of shuttle, suspension, or station_closure.
+  - The alert does not inform an entire route.
+  - If the alert is a shuttle or suspension, it informs at least 2 stops.
+  - All stops informed by the alert are reachable from the home stop without any transfers.
+    - in other words, the alert informs stops on only one subway route.
   """
 
   alias Screens.Alerts.Alert
@@ -17,13 +14,32 @@ defmodule Screens.V2.DisruptionDiagram.Validator do
   @spec validate(LocalizedAlert.t()) :: :ok | {:error, reason :: String.t()}
   def validate(localized_alert) do
     with :ok <- validate_effect(localized_alert.alert.effect),
-         :ok <- validate_not_whole_route_disruption(localized_alert.alert) do
+         :ok <- validate_not_whole_route_disruption(localized_alert.alert),
+         :ok <- validate_stop_count(localized_alert.alert) do
       validate_informed_lines(localized_alert)
     end
   end
 
   defp validate_effect(effect) when effect in [:shuttle, :suspension, :station_closure], do: :ok
   defp validate_effect(effect), do: {:error, "invalid effect: #{effect}"}
+
+  defp validate_stop_count(%{effect: continuous_effect} = alert)
+       when continuous_effect in [:shuttle, :suspension] do
+    informed_stops =
+      for %{stop: stop, route: route} <- alert.informed_entities,
+          match?("place-" <> _, stop),
+          route in ~w[Blue Orange Red Green-B Green-C Green-D Green-E],
+          uniq: true,
+          do: stop
+
+    if length(informed_stops) >= 2 do
+      :ok
+    else
+      {:error, "#{continuous_effect} alert does not inform at least 2 stops"}
+    end
+  end
+
+  defp validate_stop_count(_), do: :ok
 
   defp validate_informed_lines(localized_alert) do
     localized_alert.alert
