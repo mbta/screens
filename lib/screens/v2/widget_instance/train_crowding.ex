84054cd0baf93e7dcc3ad23d00e46da78702e7f6
Hannah Purcell <hpurcell@mbta.com>
2023-08-24 15:23:41 -0400

    Formatting

--- lib/screens/v2/widget_instance/train_crowding.ex
+++ lib/screens/v2/widget_instance/train_crowding.ex
@@ -47,7 +47,9 @@ defmodule Screens.V2.WidgetInstance.TrainCrowding do
   end
 
   defp serialize_carriages(nil), do: nil
-  defp serialize_carriages(carriages), do: Enum.map(carriages, fn car -> serialize_occupancy_status(car) end)
+
+  defp serialize_carriages(carriages),
+    do: Enum.map(carriages, fn car -> serialize_occupancy_status(car) end)
 
   defp serialize_occupancy_status(:no_data_available), do: :no_data
   defp serialize_occupancy_status(:many_seats_available), do: :not_crowded
