cd4f3304b2e05bfc9639ec0670966c90ba3a295e
Hannah Purcell <69368883+hannahpurcell@users.noreply.github.com>
2023-08-25 15:46:58 -0400

    [feature] Crowding widget backend (#1834)
    * Stubbed out template & ol crowding widget config
    
    * Adjust triptych config, added screen type to stop file and evergreen candidategenerator
    
    * Change parsing of vehicle info, add logic for generating crowding widget
    
    * Add "enabled" flag to widget; if no predictions, turn off crowding widget
    
    * If an alert makes a temp terminal, suppress widget
    
    * Update carriage serializer with known data structure
    
    * Two vehicle parsers: bus & subway. Added occupancy_status types
    
    * Re-order function defs, handle nil carriages for a vehicle
    
    * Shouldn't remove parent_stop_id from vehicles

--- lib/screens/config/v2/triptych.ex
+++ lib/screens/config/v2/triptych.ex
@@ -1,16 +1,17 @@
 defmodule Screens.Config.V2.Triptych do
   @moduledoc false
 
-  alias Screens.Config.V2.{EvergreenContentItem, OLCrowding}
+  alias Screens.Config.V2.{EvergreenContentItem, TrainCrowding}
 
   @type t :: %__MODULE__{
-          ol_crowding: OLCrowding.t(),
+          train_crowding: TrainCrowding.t(),
           evergreen_content: list(EvergreenContentItem.t())
         }
 
-  defstruct ol_crowding: nil,
+  @enforce_keys [:train_crowding, :evergreen_content]
+  defstruct train_crowding: nil,
             evergreen_content: []
 
   use Screens.Config.Struct,
-    children: [ol_crowding: OLCrowding, evergreen_content: {:list, EvergreenContentItem}]
+    children: [train_crowding: TrainCrowding, evergreen_content: {:list, EvergreenContentItem}]
 end
---
+++ test/screens/v2/widget_instance/train_crowding_test.exs
@@ -0,0 +1,113 @@
+defmodule Screens.V2.WidgetInstance.TrainCrowdingTest do
+  use ExUnit.Case, async: true
+
+  alias Screens.Predictions.Prediction
+  alias Screens.V2.WidgetInstance.TrainCrowding, as: WidgetInstance
+  alias Screens.Vehicles.Vehicle
+
+  setup do
+    config =
+      struct(Screens.Config.Screen, %{
+        app_params:
+          struct(Screens.Config.V2.Triptych, %{
+            train_crowding: %Screens.Config.V2.TrainCrowding{
+              station_id: "place-masta",
+              direction_id: 1,
+              platform_position: 3,
+              front_car_direction: "right",
+              enabled: true
+            }
+          })
+      })
+
+    prediction =
+      struct(Prediction, %{
+        trip: %{
+          headsign: "Oak Grove"
+        },
+        vehicle:
+          struct(Vehicle, %{
+            stop_id: "10001",
+            current_status: :incoming_at,
+            carriages: [
+              :crushed_standing_room_only,
+              :few_seats_available,
+              :standing_room_only,
+              :many_seats_available,
+              :full,
+              :not_accepting_passengers
+            ]
+          })
+      })
+
+    widget = %WidgetInstance{
+      screen: config,
+      prediction: prediction,
+      now: ~U[2023-08-16 21:04:00Z]
+    }
+
+    %{widget: widget}
+  end
+
+  describe "serialize/1" do
+    test "serializes data", %{widget: widget} do
+      expected = %{
+        destination: "Oak Grove",
+        crowding: [:crowded, :not_crowded, :some_crowding, :not_crowded, :crowded, :disabled],
+        platform_position: 3,
+        front_car_direction: "right",
+        now: "2023-08-16T21:04:00Z"
+      }
+
+      assert expected == WidgetInstance.serialize(widget)
+    end
+  end
+
+  describe "priority/1" do
+    test "returns max priority", %{widget: widget} do
+      assert [1] == WidgetInstance.priority(widget)
+    end
+  end
+
+  describe "slot_names/1" do
+    test "returns [:full_screen]", %{widget: widget} do
+      assert [:full_screen] == WidgetInstance.slot_names(widget)
+    end
+  end
+
+  describe "widget_type/1" do
+    test "returns :train_crowding", %{widget: widget} do
+      assert :train_crowding == WidgetInstance.widget_type(widget)
+    end
+  end
+
+  describe "valid_candidate?/1" do
+    test "returns true", %{widget: widget} do
+      assert WidgetInstance.valid_candidate?(widget)
+    end
+  end
+
+  describe "audio_serialize/1" do
+    test "returns empty", %{widget: widget} do
+      assert %{} == WidgetInstance.audio_serialize(widget)
+    end
+  end
+
+  describe "audio_sort_key/1" do
+    test "returns [0]", %{widget: widget} do
+      assert [0] == WidgetInstance.audio_sort_key(widget)
+    end
+  end
+
+  describe "audio_valid_candidate?/1" do
+    test "returns false", %{widget: widget} do
+      assert not WidgetInstance.audio_valid_candidate?(widget)
+    end
+  end
+
+  describe "audio_view/1" do
+    test "returns nil", %{widget: widget} do
+      assert nil == WidgetInstance.audio_view(widget)
+    end
+  end
+end
