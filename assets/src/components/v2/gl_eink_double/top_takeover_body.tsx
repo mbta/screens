c223aad3ac2669e90553a497f5d5ba78ea8c59a3
cmaddox5 <christian.maddox93@gmail.com>
2023-09-13 13:13:24 -0400

    Corrected slot name in props.

--- assets/src/components/v2/gl_eink_double/top_takeover_body.tsx
+++ assets/src/components/v2/gl_eink_double/top_takeover_body.tsx
@@ -3,21 +3,21 @@ import React from "react";
 import Widget, { WidgetData } from "Components/v2/widget";
 
 interface Props {
-  full_body_top_screen: WidgetData;
+  full_main_content: WidgetData;
   main_content: WidgetData;
   flex_zone: WidgetData;
   footer: WidgetData;
 }
 
 const TopTakeoverBody: React.ComponentType<Props> = ({
-  full_body_top_screen: fullBodyTopScreen,
+  full_main_content: fullMainContent,
   flex_zone: flexZone,
   footer,
 }) => {
   return (
     <div className="body-top-takeover">
       <div className="body-top-takeover__top-screen">
-        <Widget data={fullBodyTopScreen} />
+        <Widget data={fullMainContent} />
       </div>
       <div className="body-top-takeover__flex-zone">
         <Widget data={flexZone} />
