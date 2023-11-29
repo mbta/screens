eff043468a21b3d6b7aa37d48feee82f789015eb
Jon Zimbel <jzimbel@mbta.com>
2023-11-29 15:32:14 -0500

    Use File.stat to save on config file reads in local dev

--- lib/screens/signs_ui_config/fetch/local.ex
+++ lib/screens/signs_ui_config/fetch/local.ex
@@ -3,9 +3,14 @@ defmodule Screens.SignsUiConfig.Fetch.Local do
 
   @impl true
   def fetch_config(current_version \\ nil) do
-    case File.read(local_config_path()) do
-      {:ok, contents} -> {:ok, contents, current_version}
-      _ -> :error
+    path = local_config_path()
+
+    with {:ok, last_modified} <- get_last_modified(path) do
+      if last_modified == current_version do
+        :unchanged
+      else
+        do_fetch(path, last_modified)
+      end
     end
   end
 
@@ -13,6 +18,20 @@ defmodule Screens.SignsUiConfig.Fetch.Local do
     case Application.get_env(:screens, :local_signs_ui_config_file_spec) do
       {:priv, file_name} -> Path.join(:code.priv_dir(:screens), file_name)
       {:test, file_name} -> Path.join(~w[#{File.cwd!()} test fixtures #{file_name}])
+    end
+  end
+
+  defp do_fetch(path, last_modified) do
+    case File.read(path) do
+      {:ok, contents} -> {:ok, contents, last_modified}
+      _ -> :error
+    end
+  end
+
+  defp get_last_modified(path) do
+    case File.stat(path) do
+      {:ok, %File.Stat{mtime: mtime}} -> {:ok, mtime}
+      {:error, _} -> :error
     end
   end
 end
