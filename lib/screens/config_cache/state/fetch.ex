92ed6757c5198dfe56074dad48412080af46c9ed
Jon Zimbel <jzimbel@mbta.com>
2023-11-29 22:23:01 -0500

    Delete old cache files

--- lib/screens/config_cache/state/fetch.ex
+++
@@ -1,7 +0,0 @@
-defmodule Screens.ConfigCache.State.Fetch do
-  @moduledoc false
-
-  @type version_id :: String.t() | nil
-
-  @callback fetch_config(version_id) :: {:ok, term(), version_id} | :unchanged | :error
-end
