99bba1d299d30f8ab0c4f106b51ad681b17157bb
cmaddox5 <christian.maddox93@gmail.com>
2023-12-11 08:29:20 -0500

    Improved speed Polly reads the text.

--- lib/screens_web/views/v2/audio/alert_view.ex
+++ lib/screens_web/views/v2/audio/alert_view.ex
@@ -2,6 +2,6 @@ defmodule ScreensWeb.V2.Audio.AlertView do
   use ScreensWeb, :view
 
   def render("_widget.ssml", %{description: description}) do
-    ~E|<p><s>Alert: <%= description %></s></p>|
+    ~E|<p>Alert:</p> <p><%= description %></p>|
   end
 end
