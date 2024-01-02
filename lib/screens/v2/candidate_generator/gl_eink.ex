25b7d9bbcd2f88551a65d3ef7c163a6c458b90c1
Christian Maddox <christian.maddox93@gmail.com>
2024-01-02 16:05:40 -0600

    tweak: Flex Zone takeover layout (#1952)
    * Added a new layout for flexzone takeovers.
    
    * Fixed test.
    
    * Yet another layout.

--- lib/screens/v2/candidate_generator/gl_eink.ex
+++ lib/screens/v2/candidate_generator/gl_eink.ex
@@ -52,6 +52,12 @@ defmodule Screens.V2.CandidateGenerator.GlEink do
               :full_body_top_screen,
               :full_body_bottom_screen
             ],
+            flex_zone_takeover: [
+              :left_sidebar,
+              :main_content,
+              :flex_zone_takeover,
+              :footer
+            ],
             bottom_takeover: [
               :left_sidebar,
               :main_content,
