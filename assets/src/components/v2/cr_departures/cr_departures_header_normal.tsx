f6823fc0ee0d046742873f53932071e98ba87360
cmaddox5 <christian.maddox93@gmail.com>
2024-04-11 14:41:05 -0400

    Changed header to show different text depending on a new is_free boolean.

---
+++ assets/src/components/v2/cr_departures/cr_departures_header_normal.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+import CRPill from "../../../../static/images/svgr_bundled/pills/commuter-rail.svg";
+import { getHexColor } from "Util/svg_utils";
+
+const CRDeparturesHeaderNormal = () => {
+  return (
+    <div className="departures-card__header">
+      <CRPill width="523" height="82" color={getHexColor("purple")} />
+      <div className="departures-card__header-text">Upcoming departures</div>
+    </div>
+  );
+};
+
+export default CRDeparturesHeaderNormal;
