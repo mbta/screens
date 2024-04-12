e292abd02eae975851464ade4e981c464b092dea
cmaddox5 <christian.maddox93@gmail.com>
2024-04-12 09:06:14 -0400

    Updated screens_config dependency.

--- mix.exs
+++ mix.exs
@@ -84,7 +84,7 @@ defmodule Screens.MixProject do
       {:telemetry_metrics, "~> 0.4"},
       {:screens_config,
        git: "https://github.com/mbta/screens-config-lib.git",
-       ref: "86684912f37a41885c8fc291f7e125394a1a7509"}
+       ref: "6599d0df9c5bebbfb762e0ae8ae6af6f6c0359d0"}
     ]
   end
 end
