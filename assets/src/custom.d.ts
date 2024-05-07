dc9ddb4a80472ccc4aa7675850eaade09957142d
cmaddox5 <christian.maddox93@gmail.com>
2024-05-07 13:28:10 -0400

    Moved declarations to existing file.

--- assets/src/custom.d.ts
+++ assets/src/custom.d.ts
@@ -1,6 +1,9 @@
-// Allow importing and using any SVG file as though it were a React component.
+// Allow importing and using any SVG, PNG, or JPG file as though it were a React component.
 
 declare module "*.svg" {
   const content: React.FunctionComponent<React.SVGAttributes<SVGElement>>;
   export default content;
 }
+
+declare module "*.png";
+declare module "*.jpg";
