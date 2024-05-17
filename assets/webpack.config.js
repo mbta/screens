82fd8905b4ba2e38270b92f250798a70ae83b761
cmaddox5 <christian.maddox93@gmail.com>
2024-05-17 14:48:13 -0400

    Simplified loader config.

--- assets/webpack.config.js
+++ assets/webpack.config.js
@@ -119,28 +119,12 @@ module.exports = () => [
           ],
         },
         {
-          test: /\.(png|jpe?g|gif)$/i,
-          use: [
-            {
-              loader: "file-loader",
-              options: {
-                name: "[name].[ext]",
-                outputPath: "/images/",
-                publicPath: "/images/",
-                useRelativePaths: true,
-              },
-            },
-          ],
-        },
-        {
-          test: /\.(webp)$/i,
+          test: /\.(png|jpe?g|gif|webp)$/i,
           use: [
             {
               loader: "file-loader",
               options: {
                 name: "/[folder]/[name].[ext]",
-                outputPath: "/images/",
-                publicPath: "/images/",
                 useRelativePaths: true,
               },
             },
