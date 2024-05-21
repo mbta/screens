f12e5d11aa4a059bda960021e323ef6f938cb713
sloane <1699281+sloanelybutsurely@users.noreply.github.com>
2024-05-21 13:23:06 -0400

    feat: render new busway headers when available

---
+++ assets/src/components/v2/departures/header.tsx
@@ -0,0 +1,40 @@
+import React, { useMemo } from "react";
+import cx from "classnames";
+import { imagePath } from "Util/util";
+
+type CardinalDirection = "n" | "ne" | "e" | "se" | "s" | "sw" | "w" | "nw";
+
+interface Props {
+  title: string | null;
+  direction: CardinalDirection | null;
+}
+
+const DirectionArrow = ({ direction }: { direction: CardinalDirection }) => {
+  const src = useMemo(() => imagePath("arrow-45.svg"), []);
+  return (
+    <img
+      src={src}
+      data-direction={direction}
+      className={cx("direction-arrow", {
+        "rotate-45": direction === "n",
+        "rotate-90": direction === "ne",
+        "rotate-135": direction === "e",
+        "rotate-180": direction === "se",
+        "rotate-225": direction === "s",
+        "rotate-270": direction === "sw",
+        "rotate-315": direction === "w",
+      })}
+    />
+  );
+};
+
+const Header = ({ title, direction }: Props) => {
+  return (
+    <header className="departures-header">
+      {(title || direction) && <span>{title}</span>}
+      {direction && <DirectionArrow direction={direction} />}
+    </header>
+  );
+};
+
+export default Header;
--- assets/src/components/v2/departures/normal_section.tsx
+++ assets/src/components/v2/departures/normal_section.tsx
@@ -3,11 +3,20 @@ import weakKey from "weak-key";
 
 import DepartureRow from "./departure_row";
 import NoticeRow from "./notice_row";
+import Header from "./header";
+
+import type { Direction } from "Components/solari/arrow";
 
 export type Layout = {
   base: number | null;
   max: number | null;
   min: number;
+};
+
+export type HeaderData = {
+  title: string | null;
+  arrow: Direction | null;
+  read_as: string | null;
 };
 
 export type Row =
@@ -16,13 +25,15 @@ export type Row =
 
 type NormalSection = {
   layout: Layout;
+  header?: HeaderData | null;
   rows: Row[];
 };
 
-const NormalSection: ComponentType<NormalSection> = ({ rows }) => {
+const NormalSection: ComponentType<NormalSection> = ({ rows, header }) => {
   return (
     <div>
       <div className="departures-section">
+        {header && <Header title={header.title} direction={header.arrow} />}
         {rows.map((row) => {
           if (row.type === "departure_row") {
             return <DepartureRow {...row} key={row.id} />;
