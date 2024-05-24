a75760ce99fcddd7f17a3879dc763842ca94137e
sloane <1699281+sloanelybutsurely@users.noreply.github.com>
2024-05-24 08:46:00 -0400

    refactor: use bundled svg for arrow
    the existing arrow svgs have additional padding around the actual arrow
    icon. this also adds an svg (copied from the non-bundled svgs) without
    any additional padding to make calculating the proper size of these
    easier.
    
    the existing bundled arrow svg can probably be replaced with ones
    without padding so long as they are rendered within a contain that has
    the desired padding. it is my opinion that it is more flexible to have
    icons _without_ padding and add back required padding based on context
    using CSS.

--- assets/src/components/v2/departures/header.tsx
+++ assets/src/components/v2/departures/header.tsx
@@ -1,6 +1,6 @@
-import React, { useMemo } from "react";
+import React from "react";
 import cx from "classnames";
-import { imagePath } from "Util/util";
+import Arrow45 from "Images/svgr_bundled/Arrow-45-no-padding.svg";
 
 type CardinalDirection = "n" | "ne" | "e" | "se" | "s" | "sw" | "w" | "nw";
 
@@ -9,23 +9,19 @@ interface Props {
   direction: CardinalDirection | null;
 }
 
-const DirectionArrow = ({ direction }: { direction: CardinalDirection }) => {
-  const src = useMemo(() => imagePath("arrow-45.svg"), []);
-  return (
-    <img
-      src={src}
-      className={cx("direction-arrow", {
-        "rotate-45": direction === "n",
-        "rotate-90": direction === "ne",
-        "rotate-135": direction === "e",
-        "rotate-180": direction === "se",
-        "rotate-225": direction === "s",
-        "rotate-270": direction === "sw",
-        "rotate-315": direction === "w",
-      })}
-    />
-  );
-};
+const DirectionArrow = ({ direction }: { direction: CardinalDirection }) => (
+  <Arrow45
+    className={cx("direction-arrow", {
+      "rotate-45": direction === "n",
+      "rotate-90": direction === "ne",
+      "rotate-135": direction === "e",
+      "rotate-180": direction === "se",
+      "rotate-225": direction === "s",
+      "rotate-270": direction === "sw",
+      "rotate-315": direction === "w",
+    })}
+  />
+);
 
 const Header = ({ title, direction }: Props) => {
   return (
