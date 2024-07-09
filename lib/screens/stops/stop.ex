c54d5181ee7adfd9ca29c897caaf9e5056a73ab6
Christian Maddox <christian.maddox93@gmail.com>
2024-07-09 13:30:49 -0400

    Refactor to pattern match
    Co-authored-by: sloane <1699281+sloanelybutsurely@users.noreply.github.com>

--- lib/screens/stops/stop.ex
+++ lib/screens/stops/stop.ex
@@ -367,14 +367,12 @@ defmodule Screens.Stops.Stop do
     case Screens.V3Api.get_json("stops/" <> stop_id, %{"include" => "child_stops"}) do
       {:ok, %{"included" => child_stop_data}} ->
         child_stop_data
-        |> Enum.filter(fn %{
-                            "attributes" => %{
-                              "location_type" => location_type,
-                              "vehicle_type" => vehicle_type
-                            }
-                          } ->
-          location_type == 0 and vehicle_type == 1
-        end)
+        |> Enum.filter(&match?(%{
+                                 "attributes" => %{
+                                   "location_type" => 0,
+                                   "vehicle_type" => 1
+                                 }
+                               }, &1))
         |> Enum.map(&Stops.Parser.parse_stop/1)
     end
   end
