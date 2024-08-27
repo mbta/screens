a4d5c1d52a7ee1b7958d0c3105d5a2b1aa8f533c
Cora Grant <cgrant2@mbta.com>
2024-08-27 13:35:29 -0400

    chore: enable frontend source maps in Sentry
    * Upload source maps in parallel as part of the deploy Action.
    
    * Removes unneeded options from TerserPlugin. There is no such option as
      `cache` (this did nothing?), and `parallel` defaults to true.
    
    * Removes an old script that appears to have been previously used to
      upload source maps, but was not referenced anywhere.

--- assets/webpack.config.js
+++ assets/webpack.config.js
@@ -6,6 +6,7 @@ const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const TerserPlugin = require("terser-webpack-plugin");
 const OptimizeCSSAssetsPlugin = require("optimize-css-assets-webpack-plugin");
 const CopyWebpackPlugin = require("copy-webpack-plugin");
+const { sentryWebpackPlugin } = require("@sentry/webpack-plugin");
 
 const common_export_body = {
   resolve: {
@@ -25,10 +26,7 @@ const common_export_body = {
   },
   devtool: "source-map",
   optimization: {
-    minimizer: [
-      new TerserPlugin({ cache: true, parallel: true, sourceMap: false }),
-      new OptimizeCSSAssetsPlugin({}),
-    ],
+    minimizer: [new TerserPlugin(), new OptimizeCSSAssetsPlugin()],
   },
 };
 
@@ -66,158 +64,174 @@ const common_babel_loader_plugins = [
   "@babel/plugin-proposal-do-expressions",
 ];
 
-module.exports = () => [
-  {
-    ...common_export_body,
-    entry: {
-      polyfills: "./src/polyfills.js",
-      bus_eink: "./src/apps/bus_eink.tsx",
-      gl_eink_single: "./src/apps/gl_eink_single.tsx",
-      gl_eink_double: "./src/apps/gl_eink_double.tsx",
-      solari: "./src/apps/solari.tsx",
-      dup: "./src/apps/dup.tsx",
-      admin: "./src/apps/admin.tsx",
-      bus_eink_v2: "./src/apps/v2/bus_eink.tsx",
-      gl_eink_v2: "./src/apps/v2/gl_eink.tsx",
-      busway_v2: "./src/apps/v2/busway.tsx",
-      solari_large_v2: "./src/apps/v2/solari_large.tsx",
-      dup_v2: "./src/apps/v2/dup.tsx",
-      bus_shelter_v2: "./src/apps/v2/bus_shelter.tsx",
-      pre_fare_v2: "./src/apps/v2/pre_fare.tsx",
-      triptych_v2: "./src/apps/v2/triptych.tsx",
-    },
-    module: {
-      rules: [
-        {
-          test: /\.ts(x?)$/,
-          exclude: /node_modules/,
-          use: {
-            loader: "babel-loader",
-            options: {
-              presets: [
-                ["@babel/preset-env", { targets: "> 0.25%" }],
-                "@babel/preset-react",
-                "@babel/preset-typescript",
-              ],
-              plugins: common_babel_loader_plugins,
-            },
-          },
-        },
-        ...common_rules,
-        {
-          test: /\.(woff(2)?|ttf|eot)(\?v=\d+\.\d+\.\d+)?$/,
-          use: [
-            {
-              loader: "file-loader",
-              options: {
-                name: "[name].[ext]",
-                outputPath: "fonts/",
-                publicPath: "../fonts/",
-                useRelativePaths: true,
-              },
-            },
-          ],
-        },
-        {
-          test: /\.(png|jpe?g|gif|webp)$/i,
-          use: [
-            {
-              loader: "file-loader",
-              options: {
-                name: "/[folder]/[name].[ext]",
-                useRelativePaths: true,
-              },
-            },
-          ],
-        },
-      ],
-    },
-    plugins: [
-      new MiniCssExtractPlugin({ filename: "../css/[name].css" }),
-      new CopyWebpackPlugin({ patterns: [{ from: "static/", to: "../" }] }),
-    ],
-  },
-  {
-    ...common_export_body,
-    entry: {
-      packaged_triptych_polyfills: "./src/polyfills.js",
-      packaged_triptych_v2: "./src/apps/v2/triptych.tsx",
-    },
-    module: {
-      rules: [
-        {
-          test: /\.ts(x?)$/,
-          exclude: /node_modules/,
-          use: {
-            loader: "babel-loader",
-            options: {
-              presets: [
-                // When no targets are specified: Babel will assume you are targeting the oldest browsers possible.
-                [
-                  "@babel/preset-env",
-                  {
-                    corejs: { version: 3, proposals: true },
-                    useBuiltIns: "usage",
-                  },
-                ],
-                "@babel/preset-react",
-                "@babel/preset-typescript",
-              ],
-              plugins: common_babel_loader_plugins,
-            },
-          },
-        },
-        ...common_rules,
-        {
-          test: /\.(woff(2)?|ttf|eot)(\?v=\d+\.\d+\.\d+)?$/,
-          use: [
-            {
-              loader: "file-loader",
-              options: {
-                name: "[name].[ext]",
-                outputPath: "fonts/",
-                publicPath: "fonts/",
-                useRelativePaths: true,
-              },
-            },
-          ],
-        },
-        {
-          test: /\.(png|jpe?g|gif)$/i,
-          use: [
-            {
-              loader: "file-loader",
+module.exports = (env, argv) => {
+  // Upload source maps to Sentry for prod builds. Must be the last plugin.
+  const appendPlugins =
+    argv.mode == "production"
+      ? [
+          sentryWebpackPlugin({
+            authToken: env.SENTRY_AUTH_TOKEN,
+            org: env.SENTRY_ORG,
+            project: env.SENTRY_PROJECT,
+          }),
+        ]
+      : [];
 
+  return [
+    {
+      ...common_export_body,
+      entry: {
+        polyfills: "./src/polyfills.js",
+        bus_eink: "./src/apps/bus_eink.tsx",
+        gl_eink_single: "./src/apps/gl_eink_single.tsx",
+        gl_eink_double: "./src/apps/gl_eink_double.tsx",
+        solari: "./src/apps/solari.tsx",
+        dup: "./src/apps/dup.tsx",
+        admin: "./src/apps/admin.tsx",
+        bus_eink_v2: "./src/apps/v2/bus_eink.tsx",
+        gl_eink_v2: "./src/apps/v2/gl_eink.tsx",
+        busway_v2: "./src/apps/v2/busway.tsx",
+        solari_large_v2: "./src/apps/v2/solari_large.tsx",
+        dup_v2: "./src/apps/v2/dup.tsx",
+        bus_shelter_v2: "./src/apps/v2/bus_shelter.tsx",
+        pre_fare_v2: "./src/apps/v2/pre_fare.tsx",
+        triptych_v2: "./src/apps/v2/triptych.tsx",
+      },
+      module: {
+        rules: [
+          {
+            test: /\.ts(x?)$/,
+            exclude: /node_modules/,
+            use: {
+              loader: "babel-loader",
               options: {
-                name: "[name].[ext]",
-                outputPath: "triptych_images/",
-                publicPath: "triptych_images/",
-                useRelativePaths: true,
+                presets: [
+                  ["@babel/preset-env", { targets: "> 0.25%" }],
+                  "@babel/preset-react",
+                  "@babel/preset-typescript",
+                ],
+                plugins: common_babel_loader_plugins,
               },
             },
-          ],
-        },
-        {
-          test: /\.(webp)$/i,
-          use: [
-            {
-              loader: "file-loader",
-              options: {
-                name: "/[folder]/[name].[ext]",
-                outputPath: "triptych_images/triptych_psas/",
-                publicPath: "triptych_images/triptych_psas/",
-                useRelativePaths: true,
+          },
+          ...common_rules,
+          {
+            test: /\.(woff(2)?|ttf|eot)(\?v=\d+\.\d+\.\d+)?$/,
+            use: [
+              {
+                loader: "file-loader",
+                options: {
+                  name: "[name].[ext]",
+                  outputPath: "fonts/",
+                  publicPath: "../fonts/",
+                  useRelativePaths: true,
+                },
               },
-            },
-          ],
-        },
+            ],
+          },
+          {
+            test: /\.(png|jpe?g|gif|webp)$/i,
+            use: [
+              {
+                loader: "file-loader",
+                options: {
+                  name: "/[folder]/[name].[ext]",
+                  useRelativePaths: true,
+                },
+              },
+            ],
+          },
+        ],
+      },
+      plugins: [
+        new MiniCssExtractPlugin({ filename: "../css/[name].css" }),
+        new CopyWebpackPlugin({ patterns: [{ from: "static/", to: "../" }] }),
+        ...appendPlugins,
       ],
     },
-    plugins: [
-      new MiniCssExtractPlugin({ filename: "../css/[name].css" }),
-      new CopyWebpackPlugin({
-        patterns: [{ from: "static/fonts", to: "../fonts" }],
-      }),
-    ],
-  },
-];
+    {
+      ...common_export_body,
+      entry: {
+        packaged_triptych_polyfills: "./src/polyfills.js",
+        packaged_triptych_v2: "./src/apps/v2/triptych.tsx",
+      },
+      module: {
+        rules: [
+          {
+            test: /\.ts(x?)$/,
+            exclude: /node_modules/,
+            use: {
+              loader: "babel-loader",
+              options: {
+                presets: [
+                  // When no targets are specified: Babel will assume you are targeting the oldest browsers possible.
+                  [
+                    "@babel/preset-env",
+                    {
+                      corejs: { version: 3, proposals: true },
+                      useBuiltIns: "usage",
+                    },
+                  ],
+                  "@babel/preset-react",
+                  "@babel/preset-typescript",
+                ],
+                plugins: common_babel_loader_plugins,
+              },
+            },
+          },
+          ...common_rules,
+          {
+            test: /\.(woff(2)?|ttf|eot)(\?v=\d+\.\d+\.\d+)?$/,
+            use: [
+              {
+                loader: "file-loader",
+                options: {
+                  name: "[name].[ext]",
+                  outputPath: "fonts/",
+                  publicPath: "fonts/",
+                  useRelativePaths: true,
+                },
+              },
+            ],
+          },
+          {
+            test: /\.(png|jpe?g|gif)$/i,
+            use: [
+              {
+                loader: "file-loader",
+
+                options: {
+                  name: "[name].[ext]",
+                  outputPath: "triptych_images/",
+                  publicPath: "triptych_images/",
+                  useRelativePaths: true,
+                },
+              },
+            ],
+          },
+          {
+            test: /\.(webp)$/i,
+            use: [
+              {
+                loader: "file-loader",
+                options: {
+                  name: "/[folder]/[name].[ext]",
+                  outputPath: "triptych_images/triptych_psas/",
+                  publicPath: "triptych_images/triptych_psas/",
+                  useRelativePaths: true,
+                },
+              },
+            ],
+          },
+        ],
+      },
+      plugins: [
+        new MiniCssExtractPlugin({ filename: "../css/[name].css" }),
+        new CopyWebpackPlugin({
+          patterns: [{ from: "static/fonts", to: "../fonts" }],
+        }),
+        ...appendPlugins,
+      ],
+    },
+  ];
+};
