0bcf0afa15bd705fac4bb3c5a481fe4adb2a7268
Cora Grant <cgrant2@mbta.com>
2024-09-16 14:28:50 -0400

    fix: duplicate header in DUP no-departures state
    The DUP `DeparturesNoData` widget was mapped to the same component as
    the full-screen `NoData` widget, which included a copy of the header;
    but the former was rendered in the "departures" slot, resulting in two
    copies of the header and the no-data message being cut off.

---
+++ assets/src/components/v2/dup/departures_no_data.tsx
@@ -0,0 +1,29 @@
+import React, { ComponentType } from "react";
+import { imagePath } from "Util/util";
+import LinkArrow from "../bundled_svg/link_arrow";
+
+const DeparturesNoData: ComponentType = () => {
+  return (
+    <div className="no-data__container">
+      <div className="no-data__body">
+        <div className="no-data__icon-container">
+          <img
+            className="no-data__icon-image"
+            src={imagePath("live-data-none.svg")}
+          />
+        </div>
+        <div className="no-data__message">
+          Live updates are temporarily unavailable
+        </div>
+      </div>
+      <div className="no-data__link">
+        <div className="no-data__link-arrow">
+          <LinkArrow width={375} colorHex="#a2a3a3" />
+        </div>
+        <div className="no-data__link-text">mbta.com/schedules</div>
+      </div>
+    </div>
+  );
+};
+
+export default DeparturesNoData;
