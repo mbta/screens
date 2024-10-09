aacf1bbcfc26746c980854371ae583ec0846c6e9
Kim <pkim@mbta.com>
2024-10-09 11:15:44 -0400

    Get placeholder to show

--- assets/src/apps/v2/elevator.tsx
+++ assets/src/apps/v2/elevator.tsx
@@ -9,6 +9,7 @@ require("../../../css/elevator_v2.scss");
 import React from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import NormalScreen from "Components/v2/elevator/normal_screen";
 import Placeholder from "Components/v2/placeholder";
 import EvergreenContent from "Components/v2/evergreen_content";
 import ScreenPage from "Components/v2/screen_page";
@@ -16,6 +17,7 @@ import { MappingContext } from "Components/v2/widget";
 import MultiScreenPage from "Components/v2/multi_screen_page";
 
 const TYPE_TO_COMPONENT = {
+  normal: NormalScreen,
   placeholder: Placeholder,
   evergreen_content: EvergreenContent,
 };
---
+++ assets/src/components/v2/elevator/normal_screen.tsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+import Widget, { WidgetData } from "Components/v2/widget";
+
+interface Props {
+  main_content: WidgetData;
+}
+
+const NormalScreen: React.ComponentType<Props> = ({
+  main_content: mainContent,
+}) => {
+  console.log(mainContent);
+  return (
+    <div className="screen-normal">
+      <div className="screen-normal__body">
+        <Widget data={mainContent} />
+      </div>
+    </div>
+  );
+};
+
+export default NormalScreen;
