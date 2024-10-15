f326362b5a68018dd3a41bba473e9b5ed5626e82
Cora Grant <cgrant2@mbta.com>
2024-10-15 13:41:19 -0400

    refactor: enable fetching RoutePatterns
    This makes RoutePatterns a resource with its own attributes that can be
    fetched directly, similar to Routes or Stops.

--- lib/screens/route_patterns/route_pattern.ex
+++ lib/screens/route_patterns/route_pattern.ex
@@ -1,11 +1,71 @@
 defmodule Screens.RoutePatterns.RoutePattern do
   @moduledoc false
 
-  alias Screens.RoutePatterns.RouteDirectionStops
+  alias Screens.RoutePatterns.{Parser, RouteDirectionStops}
   alias Screens.Routes.Route
+  alias Screens.RouteType
   alias Screens.Stops.Stop
   alias Screens.Trips.Trip
   alias Screens.V3Api
+
+  defstruct ~w[id canonical? direction_id typicality route stops]a
+
+  @type id :: String.t()
+  @type typicality :: 1 | 2 | 3 | 4 | 5
+
+  @type t :: %__MODULE__{
+          id: id(),
+          canonical?: boolean(),
+          direction_id: Trip.direction(),
+          typicality: typicality(),
+          route: Route.t(),
+          stops: [Stop.t()]
+        }
+
+  @type params :: %{
+          optional(:canonical?) => boolean(),
+          optional(:date) => Date.t(),
+          optional(:direction_id) => Trip.direction() | :both,
+          optional(:ids) => [id()],
+          optional(:route_ids) => [Route.id()],
+          optional(:route_type) => RouteType.t(),
+          optional(:stop_ids) => [Stop.id()],
+          optional(:typicality) => typicality()
+        }
+
+  @callback fetch(params()) :: {:ok, [t()]} | :error
+  def fetch(params, get_json_fn \\ &V3Api.get_json/2) do
+    # The API doesn't currently have some of these filters built-in
+    {filter_params, fetch_params} = Map.split(params, ~w[route_type typicality]a)
+
+    encoded_params =
+      fetch_params
+      |> Enum.flat_map(&encode_param/1)
+      |> Map.new()
+      |> Map.put("include", Enum.join(~w[route representative_trip.stops], ","))
+
+    case get_json_fn.("route_patterns", encoded_params) do
+      {:ok, response} ->
+        {:ok, Enum.reduce(filter_params, Parser.parse(response), &apply_filter/2)}
+
+      _ ->
+        :error
+    end
+  end
+
+  defp apply_filter({:route_type, type}, patterns),
+    do: Enum.filter(patterns, &(&1.route.type == type))
+
+  defp apply_filter({:typicality, typicality}, patterns),
+    do: Enum.filter(patterns, &(&1.typicality == typicality))
+
+  defp encode_param({:ids, ids}), do: [{"filter[id]", Enum.join(ids, ",")}]
+  defp encode_param({:route_ids, ids}), do: [{"filter[route]", Enum.join(ids, ",")}]
+  defp encode_param({:direction_id, :both}), do: []
+  defp encode_param({:direction_id, id}), do: [{"filter[direction_id]", to_string(id)}]
+  defp encode_param({:stop_ids, ids}), do: [{"filter[stop]", Enum.join(ids, ",")}]
+  defp encode_param({:canonical?, canonical?}), do: [{"filter[canonical]", to_string(canonical?)}]
+  defp encode_param({:date, date}), do: [{"filter[date]", Date.to_iso8601(date)}]
 
   @spec stops_by_route_and_direction(Route.id(), Trip.direction()) :: {:ok, [Stop.t()]} | :error
   def stops_by_route_and_direction(route_id, direction_id) do
--- test/screens/route_patterns/route_pattern_test.exs
+++ test/screens/route_patterns/route_pattern_test.exs
@@ -1,7 +1,244 @@
 defmodule Screens.RoutePatterns.RoutePatternTest do
   use ExUnit.Case, async: true
 
-  import Screens.RoutePatterns.RoutePattern
+  alias Screens.RoutePatterns.RoutePattern
+  alias Screens.Routes.Route
+  alias Screens.Stops.Stop
+
+  describe "fetch/2" do
+    test "fetches and parses route patterns" do
+      get_json_fn =
+        fn "route_patterns", %{"include" => "route,representative_trip.stops"} ->
+          {
+            :ok,
+            %{
+              "data" => [
+                %{
+                  "id" => "Blue-6-0",
+                  "attributes" => %{
+                    "canonical" => true,
+                    "direction_id" => 0,
+                    "typicality" => 1
+                  },
+                  "relationships" => %{
+                    "representative_trip" => %{
+                      "data" => %{"id" => "canonical-Blue-C1-0", "type" => "trip"}
+                    },
+                    "route" => %{"data" => %{"id" => "Blue", "type" => "route"}}
+                  }
+                },
+                %{
+                  "id" => "Blue-8-0",
+                  "attributes" => %{
+                    "canonical" => false,
+                    "direction_id" => 0,
+                    "typicality" => 3
+                  },
+                  "relationships" => %{
+                    "representative_trip" => %{
+                      "data" => %{"id" => "65198154", "type" => "trip"}
+                    },
+                    "route" => %{"data" => %{"id" => "Blue", "type" => "route"}}
+                  }
+                }
+              ],
+              "included" => [
+                %{
+                  "id" => "Blue",
+                  "type" => "route",
+                  "attributes" => %{
+                    "direction_destinations" => ["Bowdoin", "Wonderland"],
+                    "long_name" => "Blue Line",
+                    "short_name" => "",
+                    "type" => 1
+                  }
+                },
+                %{
+                  "id" => "canonical-Blue-C1-0",
+                  "type" => "trip",
+                  "relationships" => %{
+                    "stops" => %{
+                      "data" => [
+                        %{"id" => "70059", "type" => "stop"},
+                        %{"id" => "70838", "type" => "stop"}
+                      ]
+                    }
+                  }
+                },
+                %{
+                  "id" => "65198154",
+                  "type" => "trip",
+                  "relationships" => %{
+                    "stops" => %{
+                      "data" => [
+                        %{"id" => "70051", "type" => "stop"},
+                        %{"id" => "70838", "type" => "stop"}
+                      ]
+                    }
+                  }
+                },
+                %{
+                  "id" => "70059",
+                  "type" => "stop",
+                  "attributes" => %{
+                    "name" => "Wonderland",
+                    "platform_code" => "1",
+                    "platform_name" => "Bowdoin"
+                  }
+                },
+                %{
+                  "id" => "70051",
+                  "type" => "stop",
+                  "attributes" => %{
+                    "name" => "Orient Heights",
+                    "platform_code" => nil,
+                    "platform_name" => "Bowdoin"
+                  }
+                },
+                %{
+                  "id" => "70838",
+                  "type" => "stop",
+                  "attributes" => %{
+                    "name" => "Bowdoin",
+                    "platform_code" => nil,
+                    "platform_name" => "Exit Only"
+                  }
+                }
+              ]
+            }
+          }
+        end
+
+      blue_route = %Route{
+        id: "Blue",
+        short_name: "",
+        long_name: "Blue Line",
+        direction_destinations: ["Bowdoin", "Wonderland"],
+        type: :subway
+      }
+
+      bowdoin = %Stop{
+        id: "70838",
+        name: "Bowdoin",
+        platform_code: nil,
+        platform_name: "Exit Only"
+      }
+
+      orient_heights = %Stop{
+        id: "70051",
+        name: "Orient Heights",
+        platform_code: nil,
+        platform_name: "Bowdoin"
+      }
+
+      wonderland = %Stop{
+        id: "70059",
+        name: "Wonderland",
+        platform_code: "1",
+        platform_name: "Bowdoin"
+      }
+
+      expected = [
+        %RoutePattern{
+          id: "Blue-6-0",
+          canonical?: true,
+          direction_id: 0,
+          typicality: 1,
+          route: blue_route,
+          stops: [wonderland, bowdoin]
+        },
+        %RoutePattern{
+          id: "Blue-8-0",
+          canonical?: false,
+          direction_id: 0,
+          typicality: 3,
+          route: blue_route,
+          stops: [orient_heights, bowdoin]
+        }
+      ]
+
+      assert {:ok, expected} == RoutePattern.fetch(%{}, get_json_fn)
+    end
+
+    test "filters by route type or typicality" do
+      get_json_fn =
+        fn "route_patterns", %{"include" => "route,representative_trip.stops"} ->
+          {
+            :ok,
+            %{
+              "data" => [
+                %{
+                  "id" => "rp-blue",
+                  "attributes" => %{
+                    "canonical" => true,
+                    "direction_id" => 0,
+                    "typicality" => 1
+                  },
+                  "relationships" => %{
+                    "representative_trip" => %{
+                      "data" => %{"id" => "trip-blue", "type" => "trip"}
+                    },
+                    "route" => %{"data" => %{"id" => "Blue", "type" => "route"}}
+                  }
+                },
+                %{
+                  "id" => "rp-bus-1",
+                  "attributes" => %{
+                    "canonical" => false,
+                    "direction_id" => 0,
+                    "typicality" => 3
+                  },
+                  "relationships" => %{
+                    "representative_trip" => %{
+                      "data" => %{"id" => "trip-bus-1", "type" => "trip"}
+                    },
+                    "route" => %{"data" => %{"id" => "1", "type" => "route"}}
+                  }
+                }
+              ],
+              "included" => [
+                %{
+                  "id" => "Blue",
+                  "type" => "route",
+                  "attributes" => %{
+                    "direction_destinations" => ["Bowdoin", "Wonderland"],
+                    "long_name" => "Blue Line",
+                    "short_name" => "",
+                    "type" => 1
+                  }
+                },
+                %{
+                  "id" => "1",
+                  "type" => "route",
+                  "attributes" => %{
+                    "direction_destinations" => ["Harvard Square", "Nubian Station"],
+                    "long_name" => "Harvard Square - Nubian Station",
+                    "short_name" => "1",
+                    "type" => 3
+                  }
+                },
+                %{
+                  "id" => "trip-blue",
+                  "type" => "trip",
+                  "relationships" => %{"stops" => %{"data" => []}}
+                },
+                %{
+                  "id" => "trip-bus-1",
+                  "type" => "trip",
+                  "relationships" => %{"stops" => %{"data" => []}}
+                }
+              ]
+            }
+          }
+        end
+
+      assert {:ok, [%RoutePattern{id: "rp-bus-1"}]} =
+               RoutePattern.fetch(%{route_type: :bus}, get_json_fn)
+
+      assert {:ok, [%RoutePattern{id: "rp-blue"}]} =
+               RoutePattern.fetch(%{typicality: 1}, get_json_fn)
+    end
+  end
 
   describe "fetch_tagged_stop_sequences_through_stop/2" do
     test "returns {:ok, sequences} if fetch function returns {:ok, data}" do
@@ -44,7 +281,7 @@ defmodule Screens.RoutePatterns.RoutePatternTest do
       expected_stop_sequences = %{"route1" => [~w[1 2 3], ~w[3 2 1]], "route2" => [~w[5 6 7]]}
 
       assert {:ok, expected_stop_sequences} ==
-               fetch_tagged_stop_sequences_through_stop(stop_id, [], get_json_fn)
+               RoutePattern.fetch_tagged_stop_sequences_through_stop(stop_id, [], get_json_fn)
     end
 
     test "returns :error if fetch function returns :error" do
@@ -52,7 +289,8 @@ defmodule Screens.RoutePatterns.RoutePatternTest do
 
       get_json_fn = fn _, _ -> :error end
 
-      assert :error == fetch_tagged_stop_sequences_through_stop(stop_id, [], get_json_fn)
+      assert :error ==
+               RoutePattern.fetch_tagged_stop_sequences_through_stop(stop_id, [], get_json_fn)
     end
 
     test "returns filtered list if route_filters is provided" do
@@ -82,7 +320,11 @@ defmodule Screens.RoutePatterns.RoutePatternTest do
       expected_stop_sequences = %{"Orange" => [~w[5 6 7]]}
 
       assert {:ok, expected_stop_sequences} ==
-               fetch_tagged_stop_sequences_through_stop(stop_id, route_filters, get_json_fn)
+               RoutePattern.fetch_tagged_stop_sequences_through_stop(
+                 stop_id,
+                 route_filters,
+                 get_json_fn
+               )
     end
   end
 end
