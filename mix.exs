dd2bf7d96d312bd879b0ec151999716b7dc497b1
Cora Grant <cgrant2@mbta.com>
2024-11-14 16:51:15 -0500

    chore: update screens-config-lib (remove v1)
    This updates the library to the latest version, which removes config
    definitions for "v1" screens and the `v2_screen?` function. Support
    for "v1" screens in this app was already mostly removed in cd49ce0.

--- mix.exs
+++ mix.exs
@@ -94,7 +94,7 @@ defmodule Screens.MixProject do
       {:telemetry_metrics, "~> 0.4"},
       {:screens_config,
        git: "https://github.com/mbta/screens-config-lib.git",
-       ref: "588dacf1bfaf7aa6356f0ee741bb4b49176834cc"},
+       ref: "c0db78ecd43c633a55a1f24b268ae7b8a5214748"},
       {:nebulex, "~> 2.6"},
       {:remote_ip, "~> 1.2"},
       {:hackney_telemetry, "~> 0.2.0"},
