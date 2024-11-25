038557e99d9c00751d3c25110983683a16619299
cmaddox5 <christian.maddox93@gmail.com>
2024-11-25 14:51:55 -0500

    Use b tag instead of font-weight.

--- assets/src/components/v2/elevator/footer.tsx
+++ assets/src/components/v2/elevator/footer.tsx
@@ -3,9 +3,8 @@ import React from "react";
 const Footer = () => {
   return (
     <div className="footer">
-      For more info and alternate paths:{" "}
-      <span className="bold">mbta.com/alerts/access</span> or{" "}
-      <span className="bold">617-222-2828</span>
+      For more info and alternate paths: <b>mbta.com/alerts/access</b> or{" "}
+      <b>617-222-2828</b>
     </div>
   );
 };
