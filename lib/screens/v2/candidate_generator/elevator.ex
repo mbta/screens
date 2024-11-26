9346e88c5d1cd669cd031b6f1334f07e5e53f44c
cmaddox5 <christian.maddox93@gmail.com>
2024-11-26 13:42:39 -0500

    Use Enum.concat/1 instead of multiple ++ calls.

--- lib/screens/v2/candidate_generator/elevator.ex
+++ lib/screens/v2/candidate_generator/elevator.ex
@@ -31,8 +31,12 @@ defmodule Screens.V2.CandidateGenerator.Elevator do
         elevator_closure_instances_fn \\ &ElevatorClosures.elevator_status_instances/1,
         evergreen_content_instances_fn \\ &Evergreen.evergreen_content_instances/2
       ) do
-    [header_instance(config, now), footer_instance(config)] ++
-      elevator_closure_instances_fn.(config) ++ evergreen_content_instances_fn.(config, now)
+    Enum.concat([
+      header_instance(config, now),
+      footer_instance(config),
+      elevator_closure_instances_fn.(config),
+      evergreen_content_instances_fn.(config, now)
+    ])
   end
 
   def audio_only_instances(_widgets, _config), do: []
