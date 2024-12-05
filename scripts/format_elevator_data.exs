c6d3ed2b684e6879b2d7fb30f8a6046d1b7e6c5b
cmaddox5 <christian.maddox93@gmail.com>
2024-12-05 08:39:40 -0500

    Take a path as an arg instead of committing file.

--- scripts/format_elevator_data.exs
+++ scripts/format_elevator_data.exs
@@ -4,14 +4,18 @@
 # https://docs.google.com/spreadsheets/d/1lHogme-2SuDSgjrRK52k7yVgSFK-v_LMmUfkYgBJjIU/edit?gid=179933470#gid=179933470
 
 # To use this script:
-# 1. Download the spreadsheet above
-# 2. Rename downloaded file to `elevator_station_data.csv` and place in priv/elevators/
-# 3. Run `elixir scripts/format_elevator_data.exs
+# 1. Download the spreadsheet above to a temporary directory
+# 2. Run `elixir scripts/format_elevator_data.exs --path <path_to_csv>`
 
 Mix.install([{:jason, "~> 1.4"}, {:csv, "~> 3.2"}])
 
+{[path: path], _, _} =
+  System.argv()
+  |> OptionParser.parse(strict: [path: :string])
+
 formatted_data =
-  File.stream!("priv/elevators/elevator_station_data.csv")
+  path
+  |> File.stream!()
   |> CSV.decode(headers: true)
   |> Enum.map(fn
     {:ok, %{"elevator_id" => ""}} ->
@@ -26,5 +30,9 @@ formatted_data =
       %{id: id, station_id: station_id, nearby_redundancy?: category == "1 - Nearby"}
   end)
   |> Enum.reject(&is_nil/1)
+  |> Enum.sort_by(&{&1.station_id, &1.id})
 
-File.write("priv/elevators/elevator_redundancy_data.json", Jason.encode!(formatted_data, pretty: true))
+File.write(
+  "priv/elevators/elevator_redundancy_data.json",
+  Jason.encode!(formatted_data, pretty: true)
+)
