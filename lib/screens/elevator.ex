16da11859042def0d8ef4779812b671fff857aeb
Cora Grant <cgrant2@mbta.com>
2024-12-16 16:56:28 -0500

    chore: expose alternate elevator IDs
    This data is not currently available as a facility property in GTFS, so
    we need to load it via the spreadsheet instead of the API as originally
    expected.
    
    * Make the source file for elevator data an `@external_resource` so the
      module is correctly recompiled when it changes.
    
    * Add a test asserting that the module can load every elevator ID in the
      source file without crashing.

--- lib/screens/elevator.ex
+++ lib/screens/elevator.ex
@@ -1,40 +1,39 @@
 defmodule Screens.Elevator do
   @moduledoc """
-  Exposes hand-authored data about elevator accessibility that is currently owned by the Screens
-  team and not (yet?) available in GTFS.
+  Exposes hand-authored data about elevator accessibility that is not (yet?) available in GTFS.
   """
 
   alias Screens.Facilities.Facility
   alias Screens.Log
 
-  @enforce_keys ~w[id redundancy]a
+  @enforce_keys ~w[id alternate_ids redundancy]a
   defstruct @enforce_keys
 
-  @type t :: %__MODULE__{id: Facility.id(), redundancy: redundancy()}
+  @type t :: %__MODULE__{
+          id: Facility.id(),
+          alternate_ids: [Facility.id()],
+          redundancy: redundancy()
+        }
 
   @type redundancy :: :nearby | :in_station | {:other, summary :: String.t()}
 
-  @data :screens
-        |> :code.priv_dir()
-        |> Path.join("elevators.json")
-        |> File.read!()
-        |> Jason.decode!()
+  @data_path :screens |> :code.priv_dir() |> Path.join("elevators.json")
+  @data @data_path |> File.read!() |> Jason.decode!()
+  @external_resource @data_path
 
   @callback get(Facility.id()) :: t() | nil
   def get(id) do
     case Map.get(@data, id) do
-      %{"redundancy" => 1} ->
-        %__MODULE__{id: id, redundancy: :nearby}
-
-      %{"redundancy" => 2} ->
-        %__MODULE__{id: id, redundancy: :in_station}
-
-      %{"summary" => summary} ->
-        %__MODULE__{id: id, redundancy: {:other, summary}}
-
-      _other ->
+      nil ->
         Log.warning("elevator_redundancy_not_found", id: id)
         nil
+
+      %{"alternate_ids" => alternate_ids} = entry ->
+        %__MODULE__{id: id, alternate_ids: alternate_ids, redundancy: redundancy(entry)}
     end
   end
+
+  defp redundancy(%{"redundancy" => 1}), do: :nearby
+  defp redundancy(%{"redundancy" => 2}), do: :in_station
+  defp redundancy(%{"summary" => summary}), do: {:other, summary}
 end
--- test/screens/elevator_test.exs
+++ test/screens/elevator_test.exs
@@ -6,12 +6,29 @@ defmodule Screens.ElevatorTest do
   import ExUnit.CaptureLog
 
   describe "get/1" do
-    test "gets elevators in each redundancy category" do
-      assert Elevator.get("996") == %Elevator{id: "996", redundancy: :nearby}
-      assert Elevator.get("918") == %Elevator{id: "918", redundancy: :in_station}
+    test "works for every entry in the data" do
+      all_ids =
+        :screens
+        |> :code.priv_dir()
+        |> Path.join("elevators.json")
+        |> File.read!()
+        |> Jason.decode!()
+        |> Map.keys()
 
-      assert Elevator.get("780") ==
-               %Elevator{id: "780", redundancy: {:other, "Request assistance from conductor."}}
+      Enum.each(all_ids, fn id -> assert %Elevator{id: ^id} = Elevator.get(id) end)
+    end
+
+    test "returns alternate elevator IDs" do
+      assert %Elevator{alternate_ids: []} = Elevator.get("780")
+      assert %Elevator{alternate_ids: ~w[901 927 919]} = Elevator.get("918")
+    end
+
+    test "gets elevators in each redundancy category" do
+      assert %Elevator{redundancy: :nearby} = Elevator.get("996")
+      assert %Elevator{redundancy: :in_station} = Elevator.get("918")
+
+      assert %Elevator{redundancy: {:other, "Request assistance from conductor."}} =
+               Elevator.get("780")
     end
 
     test "returns nil and logs a warning when redundancy data does not exist" do
