07c9c3f63be8a716d74e53bbcbd559474d0f4520
robbie.sundstrom <rsundstrom@mbta.com>
2025-01-21 11:58:22 -0500

    remove unused variable

--- assets/src/hooks/v2/use_interval_paging.tsx
+++ assets/src/hooks/v2/use_interval_paging.tsx
@@ -1,4 +1,4 @@
-import { useState, useEffect, useRef, useCallback } from "react";
+import { useState, useEffect, useRef } from "react";
 
 interface UseIntervalPagingProps {
   numPages: number; // Total number of pages to cycle through
