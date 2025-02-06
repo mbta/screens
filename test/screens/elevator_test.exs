2b9179fbd56b129a31bdb43b21771577444c5ef0
Cora Grant <cgrant2@mbta.com>
2025-02-06 17:28:36 -0500

    fix: incorrect boldness in elevator outages
    For some elevators with in-station redundancy, the text "Accessible
    route available" would incorrectly be shown in the bold font reserved
    for more noteworthy redundancy categories.
    
    The reason is that we were expecting the redundancy category to be
    exactly `2`, but some elevators had categories like `2A` or `2B`. Since
    the exiting summary for all category-2 elevators is "Accessible route
    available", this resulted in screens still showing the correct phrasing,
    but more boldly than intended.

--- test/screens/elevator_test.exs
+++ test/screens/elevator_test.exs
@@ -27,7 +27,7 @@ defmodule Screens.ElevatorTest do
       assert %Elevator{entering_redundancy: :nearby} = Elevator.get("996")
       assert %Elevator{entering_redundancy: :in_station} = Elevator.get("918")
       assert %Elevator{entering_redundancy: :shuttle} = Elevator.get("816")
-      assert %Elevator{entering_redundancy: :other} = Elevator.get("958")
+      assert %Elevator{entering_redundancy: :other} = Elevator.get("970")
     end
 
     test "gets elevators in each exiting redundancy category" do
@@ -36,6 +36,19 @@ defmodule Screens.ElevatorTest do
 
       assert %Elevator{exiting_redundancy: {:other, "Request assistance from conductor."}} =
                Elevator.get("780")
+    end
+
+    test "handles sub-categories of in-station redundancy" do
+      assert %Elevator{entering_redundancy: :in_station, exiting_redundancy: :in_station} =
+               Elevator.get("842")
+
+      assert %Elevator{entering_redundancy: :in_station, exiting_redundancy: :in_station} =
+               Elevator.get("771")
+    end
+
+    test "only interprets sub-category 3B as shuttle for entering redundancy" do
+      assert %Elevator{entering_redundancy: :shuttle} = Elevator.get("945")
+      assert %Elevator{entering_redundancy: :other} = Elevator.get("958")
     end
 
     test "returns nil and logs a warning when redundancy data does not exist" do
