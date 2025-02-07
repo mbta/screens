cf829b14eedcd4e307e1119d4c38508f88a2ce9c
Cora Grant <cgrant2@mbta.com>
2025-02-07 18:27:22 -0500

    feat: upcoming closures for elevator screens
    When the screen's configured elevator is not currently closed, but has
    an upcoming planned closure, include a special page that explains this
    alongside the list of active closures. When this page is not the
    current one, the in-station summary banner shows the same information
    in a more concise format, so it is always visible.

--- assets/src/components/v2/elevator/closures.tsx
+++ assets/src/components/v2/elevator/closures.tsx
@@ -6,14 +6,19 @@ import makePersistent, {
   WrappedComponentProps,
 } from "Components/v2/persistent_wrapper";
 import PagingIndicators from "Components/v2/elevator/paging_indicators";
-import {
-  type StationWithClosures,
-  type Closure,
-} from "Components/v2/elevator/types";
+import { type StationWithClosures } from "Components/v2/elevator/types";
 import useIntervalPaging from "Hooks/v2/use_interval_paging";
-import NormalService from "Images/svgr_bundled/normal-service.svg";
+import CalendarIcon from "Images/svgr_bundled/calendar.svg";
+import CalendarAlertIcon from "Images/svgr_bundled/calendar-alert.svg";
+import NormalServiceIcon from "Images/svgr_bundled/normal-service.svg";
 import AccessibilityAlert from "Images/svgr_bundled/accessibility-alert.svg";
+import { hasOverflowX } from "Util/utils";
 import { CLOSURES_PAGING_INTERVAL_MS } from "./constants";
+
+type UpcomingClosureInfo = {
+  banner: { title: string; postfix: string | null };
+  details: { summary: string | null; titles: string[]; postfix: string | null };
+};
 
 interface ClosureRowProps {
   station: StationWithClosures;
@@ -65,14 +70,30 @@ const ClosureRow = ({
 };
 
 interface InStationSummaryProps {
-  closures: Closure[];
+  numClosures: number;
+  upcomingClosure?: UpcomingClosureInfo;
 }
 
-const InStationSummary = ({ closures }: InStationSummaryProps) => {
-  let text;
-  if (closures.length === 0) {
+const InStationSummary = ({
+  numClosures,
+  upcomingClosure,
+}: InStationSummaryProps) => {
+  let text: JSX.Element | string;
+
+  if (upcomingClosure) {
+    const {
+      banner: { title, postfix },
+    } = upcomingClosure;
+
+    text = (
+      <>
+        <b>{title}:</b> This elevator will be closed
+        {postfix ? `, ${postfix}` : " for maintenance"}
+      </>
+    );
+  } else if (numClosures === 0) {
     text = "All elevators at this station are currently working";
-  } else if (closures.length === 1) {
+  } else if (numClosures === 1) {
     text = (
       <>
         <b>This elevator is working.</b> Another elevator at this station is
@@ -82,39 +103,71 @@ const InStationSummary = ({ closures }: InStationSummaryProps) => {
   } else {
     text = (
       <>
-        <b>This elevator is working.</b> {closures.length} other elevators at
-        this station are down.
+        <b>This elevator is working.</b> {numClosures} other elevators at this
+        station are down.
       </>
     );
   }
 
   return (
-    <>
-      <div className="in-station-summary">
-        <span className="text">{text}</span>
-        <span>
-          <NormalService height={72} width={72} fill="#145A06" />
-        </span>
-      </div>
-    </>
+    <div className="in-station-summary">
+      <span className="text">{text}</span>
+      <span>
+        {upcomingClosure ? (
+          <CalendarIcon height={72} width={72} />
+        ) : (
+          <NormalServiceIcon height={72} width={72} fill="#145A06" />
+        )}
+      </span>
+    </div>
   );
 };
 
-interface CurrentClosuresProps extends WrappedComponentProps {
-  stations: StationWithClosures[];
-  stationId: string;
-}
+const UpcomingClosure = ({
+  closure: {
+    details: { summary, titles, postfix },
+  },
+}: {
+  closure: UpcomingClosureInfo;
+}) => {
+  const titleRef = useRef<HTMLDivElement>(null);
+  const [titleIndex, setTitleIndex] = useState(0);
+  const title = titles[titleIndex];
 
-const CurrentClosures = ({
-  stations,
-  stationId,
-  updateVisibleData,
-}: CurrentClosuresProps) => {
-  const ref = useRef<HTMLDivElement>(null);
+  useLayoutEffect(() => {
+    if (titleIndex < titles.length - 1 && hasOverflowX(titleRef))
+      setTitleIndex(titleIndex + 1);
+  }, [titles]);
 
-  const sortedStations = [...stations].sort((a, b) => {
-    const aInStation = a.id === stationId;
-    const bInStation = b.id === stationId;
+  return (
+    <div className="upcoming-closure">
+      <CalendarAlertIcon width={224} />
+      <div className="upcoming-closure__title" ref={titleRef}>
+        {title}:
+      </div>
+      <div>This elevator will be closed for maintenance.</div>
+
+      <div>
+        {summary ?? (
+          <>
+            Visit <b>mbta.com/elevators</b> for information about alternate
+            routes.
+          </>
+        )}
+      </div>
+
+      <div className="upcoming-closure__postfix">{postfix}</div>
+    </div>
+  );
+};
+
+const sortStations = (
+  stations: StationWithClosures[],
+  thisStationId: string,
+): StationWithClosures[] =>
+  [...stations].sort((a, b) => {
+    const aInStation = a.id === thisStationId;
+    const bInStation = b.id === thisStationId;
 
     // Sort all in-station closures above other closures. Within each of those
     // groups, sort by station name.
@@ -127,11 +180,47 @@ const CurrentClosures = ({
     }
   });
 
-  // Each index represents a page number and each value represents the number of
-  // rows on the corresponding page index.
-  const [rowCountsPerPage, setRowCountsPerPage] = useState<number[]>([]);
+interface Props extends WrappedComponentProps {
+  id: string;
+  stations_with_closures: StationWithClosures[];
+  station_id: string;
+  upcoming_closure?: UpcomingClosureInfo;
+}
 
-  const numPages = Object.keys(rowCountsPerPage).length;
+const Closures = ({
+  stations_with_closures: stations,
+  station_id: stationId,
+  upcoming_closure: upcomingClosure,
+  updateVisibleData,
+}: Props) => {
+  const numClosuresInStation = stations
+    .filter((s) => s.id === stationId)
+    .flatMap((s) => s.closures).length;
+
+  const listRef = useRef<HTMLDivElement>(null);
+
+  // Number of rows on each page index of the closure list (not counting the
+  // page for an upcoming closure, if there is one).
+  const [pageRowCounts, setPageRowCounts] = useState<number[]>([]);
+
+  // Let the browser lay out the closures into flexbox columns (pages) for us,
+  // then look at their distinct `offsetLeft` values to determine how many pages
+  // we have and how many items are on each.
+  useLayoutEffect(() => {
+    if (!listRef.current) return;
+
+    const rowCounts = Object.values(
+      _.countBy(
+        listRef.current.children,
+        (child) => (child as HTMLElement).offsetLeft,
+      ),
+    );
+
+    setPageRowCounts(rowCounts);
+  }, [stations]);
+
+  const numListPages = Object.keys(pageRowCounts).length;
+  const numPages = upcomingClosure ? numListPages + 1 : numListPages;
 
   const pageIndex = useIntervalPaging({
     numPages,
@@ -139,105 +228,76 @@ const CurrentClosures = ({
     updateVisibleData,
   });
 
-  const numOffsetRows = Object.keys(rowCountsPerPage).reduce((acc, key) => {
-    if (parseInt(key) === pageIndex) {
-      return acc;
-    } else {
-      return acc + rowCountsPerPage[key];
-    }
-  }, 0);
+  const isUpcomingClosurePage = upcomingClosure && pageIndex == 0;
 
-  useLayoutEffect(() => {
-    if (!ref.current) return;
+  // The current page within the pages of the closure list. Note when the
+  // current page is the upcoming closure page, this is -1, which correctly
+  // positions the closure list entirely off-screen (it must always be rendered
+  // somewhere, so we know how many pages there are for the page indicators).
+  const listPageIndex = upcomingClosure ? pageIndex - 1 : pageIndex;
 
-    const offsets = Array.from(ref.current.children).map((closure) => {
-      return (closure as HTMLDivElement).offsetLeft;
-    });
+  const listOffsetStyle = {
+    "--closure-list-offset": listPageIndex,
+  } as React.CSSProperties;
 
-    const rowCounts: number[] = [];
+  const numRowsOffPage = pageRowCounts
+    .filter((_, index) => index != listPageIndex)
+    .reduce((a, b) => a + b, 0);
 
-    _.uniq(offsets).forEach((uo) => {
-      rowCounts.push(offsets.filter((o) => o === uo).length);
-    });
-
-    setRowCountsPerPage(rowCounts);
-  }, [stations]);
-
-  // Track which closure row will be at the top of each page to apply special styling
-  const firstRowsOnPages = [0];
-  for (let i = 0; i < rowCountsPerPage.length - 1; i++) {
-    firstRowsOnPages.push(firstRowsOnPages[i] + rowCountsPerPage[i]);
-  }
+  // Determine the index of the first row on each page of the closure list, for
+  // styling purposes. Each page's first index is the sum of row counts for all
+  // preceding pages.
+  const firstRowIndices = pageRowCounts.map((_rowCount, pageNum, rowCounts) =>
+    rowCounts.slice(0, pageNum).reduce((a, b) => a + b, 0),
+  );
 
   return (
-    <div className="closures-list">
-      <div className="header-container">
-        <div className="header">
-          <div className="header__title">MBTA Elevator Closures</div>
-          <div>
-            <AccessibilityAlert height={128} width={155} />
-          </div>
-        </div>
-      </div>
+    <div className="elevator-closures">
+      <InStationSummary
+        numClosures={numClosuresInStation}
+        upcomingClosure={isUpcomingClosurePage ? undefined : upcomingClosure}
+      />
 
-      <div className="closure-list-container">
-        {
-          <div
-            className="closure-list"
-            style={
-              {
-                "--closure-list-offset": pageIndex,
-              } as React.CSSProperties
-            }
-            ref={ref}
-          >
-            {sortedStations.map((station, index) => (
-              <ClosureRow
-                station={station}
-                isCurrentStation={station.id == stationId}
-                key={station.id}
-                isFirstRowOnPage={firstRowsOnPages.includes(index)}
-              />
-            ))}
+      <div className="closures-info">
+        {isUpcomingClosurePage ? (
+          <UpcomingClosure closure={upcomingClosure} />
+        ) : (
+          <div className="header-container">
+            <div className="header">
+              <div className="header__title">MBTA Elevator Closures</div>
+              <div>
+                <AccessibilityAlert height={128} width={155} />
+              </div>
+            </div>
           </div>
-        }
+        )}
+
+        <div className="closure-list-container">
+          {
+            <div className="closure-list" ref={listRef} style={listOffsetStyle}>
+              {sortStations(stations, stationId).map((station, index) => (
+                <ClosureRow
+                  isCurrentStation={station.id == stationId}
+                  isFirstRowOnPage={firstRowIndices.includes(index)}
+                  key={station.id}
+                  station={station}
+                />
+              ))}
+            </div>
+          }
+        </div>
       </div>
 
       {numPages > 1 && (
         <div className="paging-info-container">
-          <div className="more-elevators-text">
-            +{numOffsetRows} more elevator{numOffsetRows !== 1 ? "s" : ""}
-          </div>
+          {!isUpcomingClosurePage && numRowsOffPage > 0 && (
+            <div className="more-elevators-text">
+              +{numRowsOffPage} more elevator{numRowsOffPage !== 1 ? "s" : ""}
+            </div>
+          )}
           <PagingIndicators numPages={numPages} pageIndex={pageIndex} />
         </div>
       )}
-    </div>
-  );
-};
-
-interface Props extends WrappedComponentProps {
-  id: string;
-  stations_with_closures: StationWithClosures[];
-  station_id: string;
-}
-
-const Closures = ({
-  stations_with_closures: stations,
-  station_id: stationId,
-  updateVisibleData,
-}: Props) => {
-  return (
-    <div className="elevator-closures">
-      <InStationSummary
-        closures={stations
-          .filter((s) => s.id === stationId)
-          .flatMap((s) => s.closures)}
-      />
-      <CurrentClosures
-        stations={stations}
-        stationId={stationId}
-        updateVisibleData={updateVisibleData}
-      />
     </div>
   );
 };
