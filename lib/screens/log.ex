070d8e5f7ac7bb012cdaf3e9ec6934c04991b1ea
Dean Shi <dshi@mbta.com>
2025-02-12 12:06:50 -0500

    Revert log.ex info code

--- lib/screens/log.ex
+++ lib/screens/log.ex
@@ -5,19 +5,15 @@ defmodule Screens.Log do
 
   def error(tag, data \\ []), do: log(:error, tag, data)
   def warning(tag, data \\ []), do: log(:warning, tag, data)
-  def info(tag, data \\ []), do: log(:info, tag, data)
 
-  @spec log(:error | :warning | :info, String.t(), Enumerable.t({String.Chars.t(), term()})) ::
-          :ok
+  @spec log(:error | :warning, String.t(), Enumerable.t({String.Chars.t(), term()})) :: :ok
   defp log(level, tag, data) do
     Logger.log(
       level,
       tag <> " " <> Enum.map_join(data, " ", fn {key, value} -> "#{key}=#{inspect(value)}" end)
     )
 
-    if level != :info do
-      _ = Sentry.capture_message(tag, extra: Map.new(data), level: level, result: :none)
-    end
+    _ = Sentry.capture_message(tag, extra: Map.new(data), level: level, result: :none)
 
     :ok
   end
