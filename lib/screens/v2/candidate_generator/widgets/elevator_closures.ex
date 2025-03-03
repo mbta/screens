8a4ad754e1508722a6c57e837bd6f809a98daeae
robbie.sundstrom <rsundstrom@mbta.com>
2025-03-03 17:48:35 -0500

    Add logic into pre-fare widget generator to filter elevators with redundancy

--- lib/screens/v2/candidate_generator/widgets/elevator_closures.ex
+++ lib/screens/v2/candidate_generator/widgets/elevator_closures.ex
@@ -1,13 +1,38 @@
 defmodule Screens.V2.CandidateGenerator.Widgets.ElevatorClosures do
   @moduledoc false
 
-  alias Screens.Alerts.Alert
+  alias Screens.Alerts.{Alert, InformedEntity}
+  alias Screens.Elevator
+  alias Screens.Facilities.Facility
   alias Screens.LocationContext
+  alias Screens.Report
   alias Screens.Routes.Route
   alias Screens.Stops.Stop
   alias Screens.V2.WidgetInstance.ElevatorStatus, as: ElevatorStatusWidget
   alias ScreensConfig.Screen
   alias ScreensConfig.V2.{ElevatorStatus, PreFare}
+  alias Screens.Stops.Stop
+
+  import Screens.Inject
+
+  @elevator injected(Elevator)
+
+  defmodule Closure do
+    @moduledoc false
+    # Internal struct used while generating widgets. Represents a single elevator which is closed.
+
+    @type t :: %__MODULE__{
+            id: Facility.id(),
+            alert_id: String.t(),
+            name: String.t(),
+            station_id: Stop.id(),
+            periods: [Alert.active_period()],
+            elevator: Elevator.t() | nil
+          }
+
+    @enforce_keys ~w[id alert_id name station_id periods elevator]a
+    defstruct @enforce_keys
+  end
 
   def elevator_status_instances(
         %Screen{
@@ -22,12 +47,24 @@ defmodule Screens.V2.CandidateGenerator.Widgets.ElevatorClosures do
     with {:ok, location_context} <- fetch_location_context_fn.(PreFare, parent_station_id, now),
          {:ok, parent_station_map} <- Stop.fetch_parent_station_name_map(),
          {:ok, alerts} <- fetch_elevator_alerts_with_facilities_fn.() do
-      elevator_closures = relevant_alerts(alerts)
-      icon_map = get_icon_map(elevator_closures, parent_station_id)
+      elevator_alerts = relevant_alerts(alerts)
+
+      icon_map = get_icon_map(elevator_alerts, parent_station_id)
+
+      elevator_closures =
+        elevator_alerts
+        |> Enum.flat_map(&elevator_closure/1)
+
+      elevator_closure_ids =
+        elevator_closures
+        |> Enum.filter(&relevant_closure?(&1, elevator_closures))
+        |> Enum.map(fn %Closure{alert_id: alert_id} -> alert_id end)
 
       [
         %ElevatorStatusWidget{
-          alerts: elevator_closures,
+          alerts:
+            elevator_alerts
+            |> Enum.filter(fn %Alert{id: id} -> id in elevator_closure_ids end),
           location_context: location_context,
           screen: config,
           now: now,
@@ -84,5 +121,54 @@ defmodule Screens.V2.CandidateGenerator.Widgets.ElevatorClosures do
       %Screens.Routes.Route{type: type} -> type
     end)
     |> Enum.uniq()
+  end
+
+  defp elevator_closure(%Alert{
+         id: alert_id,
+         active_period: active_periods,
+         effect: :elevator_closure,
+         informed_entities: entities
+       }) do
+    # We expect there is a 1:1 relationship between `elevator_closure` alerts and individual
+    # out-of-service elevators. Log a warning if our assumptions don't hold.
+    stations_and_facilities =
+      entities
+      |> Enum.filter(&(InformedEntity.parent_station?(&1) and not is_nil(&1.facility)))
+      |> Enum.map(fn %{facility: facility, stop: station_id} -> {station_id, facility} end)
+      |> Enum.uniq()
+
+    case stations_and_facilities do
+      [] ->
+        []
+
+      [{station_id, %{id: id, name: name}}] ->
+        [
+          %Closure{
+            id: id,
+            alert_id: alert_id,
+            name: name,
+            station_id: station_id,
+            periods: active_periods,
+            elevator: @elevator.get(id)
+          }
+        ]
+
+      _multiple ->
+        Report.warning("elevator_closure_affects_multiple", alert_id: alert_id)
+        []
+    end
+  end
+
+  # If we couldn't find alternate/redundancy data for an elevator, assume it's relevant.
+  defp relevant_closure?(%Closure{elevator: nil}, _closures), do: true
+
+  # If any of a closed elevator's alternates are also closed, it's always relevant.
+  defp relevant_closure?(
+         %Closure{
+           elevator: %Elevator{alternate_ids: alternate_ids, exiting_redundancy: redundancy}
+         },
+         closures
+       ) do
+    Enum.any?(closures, fn %Closure{id: id} -> id in alternate_ids end) or redundancy != :nearby
   end
 end
