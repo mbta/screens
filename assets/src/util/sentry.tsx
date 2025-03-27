fea204c9078bb6985a460d933ba2544e5de244e3
Kim <pkim@mbta.com>
2025-03-27 18:21:12 -0400

    Custom transport for enabling offline caching of Sentry events

--- assets/src/util/sentry.tsx
+++ assets/src/util/sentry.tsx
@@ -3,7 +3,7 @@ import { isRealScreen } from "Util/utils";
 import { getDataset } from "Util/dataset";
 // Previously tried @sentry/react and @sentry/browser as the SDK, but the QtWeb browser on e-inks could not
 // use them. Raven is an older stable SDK that better works with older browsers.
-import Raven, { RavenOptions } from "raven-js";
+import Raven, { RavenOptions, RavenTransportOptions } from "raven-js";
 
 // https://docs.sentry.io/clients/javascript/usage/#raven-js-additional-context
 type LogLevel = "info" | "warning" | "error";
@@ -19,6 +19,58 @@ const captureException = (ex: unknown, options?: RavenOptions) => {
   Raven.captureException(ex, options);
 };
 
+const offline_events_key = "sentry_offline_events";
+
+// Store offline Sentry events in localStorage for later retry
+function cacheEvent(options: RavenTransportOptions) {
+  try {
+    const cached = localStorage.getItem(offline_events_key);
+    const events = cached ? JSON.parse(cached) : [];
+    events.push({
+      ...options,
+      data: {
+        ...options.data,
+        extra: {
+          ...(options.data.extra || {}),
+          cached: new Date().toISOString(),
+        },
+      },
+    });
+    localStorage.setItem(offline_events_key, JSON.stringify(events));
+  } catch {}
+}
+
+function resendCachedEvents() {
+  const cached = localStorage.getItem(offline_events_key);
+  if (!cached) return;
+
+  // Retry each event in the cache
+  JSON.parse(cached).forEach((event: RavenTransportOptions) => {
+    fetch(event.url, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(event.data),
+    });
+  });
+
+  localStorage.removeItem(offline_events_key);
+}
+
+function customTransport(options: RavenTransportOptions) {
+  fetch(options.url, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(options.data),
+  }).catch(() => {
+    // Cache the event only if the fetch was rejected, indicating that the network was likely unavailable
+    cacheEvent(options);
+  });
+}
+
 /**
  * Initializes Sentry if the DSN is defined AND this client is running on
  * a real production screen.
@@ -27,7 +79,11 @@ const initSentry = (appString: string) => {
   const { sentry: sentryDsn, environmentName: env } = getDataset();
 
   if (sentryDsn && isRealScreen()) {
+    Raven.setTransport(customTransport);
     Raven.config(sentryDsn, { environment: env }).install();
+
+    // Set listener to retry any cached Sentry events any time the browser transitions from offline to online
+    window.addEventListener("online", resendCachedEvents);
 
     if (isDup()) {
       const today = new Date();
