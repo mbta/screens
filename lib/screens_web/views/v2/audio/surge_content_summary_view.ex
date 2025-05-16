3111ed3ad95078653ab8a5c6f4776004639683e4
Cora Grant <cgrant2@mbta.com>
2025-05-16 15:18:52 -0400

    chore: remove ShuttleBusInfo and old "surge" logic
    * Remove the `ShuttleBusInfo` widget and related logic/components. This
      was developed specifically for the 2022 Orange Line "Super Surge" and
      has not been used since then.
    
    * Remove some special-case logic related to specific past disruptions
      other than the Super Surge.

--- lib/screens_web/views/v2/audio/surge_content_summary_view.ex
+++
@@ -1,9 +0,0 @@
-defmodule ScreensWeb.V2.Audio.SurgeContentSummaryView do
-  use ScreensWeb, :view
-
-  def render("_widget.ssml", _) do
-    ~E|<p>No Orange Line trains are in service.</p>
-    <p>Alternative travel options include the Commuter Rail, <s>blue bikes</s>, and shuttle buses.
-    Riders can take the Commuter Rail for free, and free Bluebike passes are available.</p>|
-  end
-end
