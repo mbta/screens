mbta/screens

commit 4efc67d88585e5819fbe98fa8fdecff03d7c3a61

feat: support multi-stop alerts configuration
These are the minimal changes required to support Bus E-ink screens
(only) being configured to display alerts for multiple stops.

Presentation may not be ideal due to conditions involving the screen's
"home stop" always being considered false (as there is no single "home
stop"), but since we have exactly one use case for this feature and
don't expect that to change soon, the amount of effort applied here is
appropriate to the impact.

lib/screens/v2/location_context.ex
@@ -19,12 +19,12 @@ defmodule Screens.LocationContext do
             alert_route_types: []
 
   @type t :: %__MODULE__{
-          home_stop: Stop.id(),
-          # Stop sequences through this stop, keyed under their associated routes
+          home_stop: Stop.id() | nil,
+          # Stop sequences through the stops, keyed under their associated routes
           tagged_stop_sequences: %{Route.id() => list(list(Stop.id()))},
           upstream_stops: MapSet.t(Stop.id()),
           downstream_stops: MapSet.t(Stop.id()),
-          # Routes serving this stop
+          # Routes serving the stops
           routes: list(%{route_id: Route.id(), active?: boolean()}),
           # Route types we care about for the alerts of this screen type / place
           alert_route_types: list(RouteType.t())
@@ -33,35 +33,48 @@ defmodule Screens.LocationContext do
   @type screen_type :: BusEink | BusShelter | Dup | GlEink | PreFare
 
   @doc """
-  Fetches all the location context for a screen given its app type, stop id, and time
+  Fetches all the location context for a screen given its app type, home stop ID, and time.
+
+  Note: Multiple stop IDs are accepted, but this is intended only for edge cases where a screen
+  serves a cluster of standalone stops conceptually similar to a parent station but not actually
+  modeled as one. In this case, `home_stop` will be `nil`. This is only allowed on screen types
+  where we know it will not cause major issues, and has intentionally minimal support on those
+  screen types (e.g. "does this alert affect the home stop" conditions will always be false).
   """
-  @spec fetch(screen_type(), Stop.id(), DateTime.t()) :: {:ok, t()} | :error
-  def fetch(app, stop_id, now) do
+  @spec fetch(screen_type(), Stop.id() | [Stop.id()], DateTime.t()) :: {:ok, t()} | :error
+  def fetch(app, stop_id, now) when is_binary(stop_id), do: do_fetch(app, [stop_id], now)
+  def fetch(BusEink = app, stop_ids, now) when is_list(stop_ids), do: do_fetch(app, stop_ids, now)
+
+  defp do_fetch(app, stop_ids, now) do
     Screens.Telemetry.span(
       ~w[screens location_context fetch]a,
-      %{app: app, stop_id: stop_id},
+      %{app: app, stop_ids: stop_ids},
       fn ->
-        with alert_route_types <- route_type_filter(app, stop_id),
-             {:ok, routes_at_stop} <- routes_with_active(stop_id, alert_route_types, now),
-             route_ids_at_stop = Enum.map(routes_at_stop, & &1.route_id),
+        with alert_route_types <- route_type_filter(app, stop_ids),
+             {:ok, routes_at_stops} <- routes_with_active(stop_ids, alert_route_types, now),
+             route_ids_at_stop = Enum.map(routes_at_stops, & &1.route_id),
              {:ok, tagged_stop_sequences} <-
-               fetch_tagged_stop_sequences_by_app(app, stop_id, route_ids_at_stop) do
+               fetch_tagged_stop_sequences_by_app(app, stop_ids, route_ids_at_stop) do
           stop_sequences = untag_stop_sequences(tagged_stop_sequences)
 
           {
             :ok,
             %__MODULE__{
-              home_stop: stop_id,
+              home_stop:
+                case stop_ids do
+                  [single] -> single
+                  _multiple -> nil
+                end,
               tagged_stop_sequences: tagged_stop_sequences,
-              upstream_stops: upstream_stop_id_set(stop_id, stop_sequences),
-              downstream_stops: downstream_stop_id_set(stop_id, stop_sequences),
-              routes: routes_at_stop,
+              upstream_stops: upstream_stop_id_set(stop_ids, stop_sequences),
+              downstream_stops: downstream_stop_id_set(stop_ids, stop_sequences),
+              routes: routes_at_stops,
               alert_route_types: alert_route_types
             }
           }
         else
           :error ->
-            Report.error("location_context_fetch_error", stop_id: stop_id)
+            Report.error("location_context_fetch_error", stop_ids: stop_ids)
             :error
         end
       end
@@ -69,18 +82,20 @@ defmodule Screens.LocationContext do
   end
 
   # NOTE: only public due to use in tests. Should be treated as private.
-  @spec upstream_stop_id_set(String.t(), list(list(Stop.id()))) :: MapSet.t(Stop.id())
-  def upstream_stop_id_set(stop_id, stop_sequences) do
-    stop_sequences
-    |> Enum.flat_map(fn stop_sequence -> Util.slice_before(stop_sequence, stop_id) end)
-    |> MapSet.new()
-  end
+  @spec upstream_stop_id_set([Stop.id()], [[Stop.id()]]) :: MapSet.t(Stop.id())
+  def upstream_stop_id_set(stop_ids, stop_sequences),
+    do: sliced_stop_id_set(stop_ids, stop_sequences, &Util.slice_before/2)
 
   # NOTE: only public due to use in tests. Should be treated as private.
-  @spec downstream_stop_id_set(String.t(), list(list(Stop.id()))) :: MapSet.t(Stop.id())
-  def downstream_stop_id_set(stop_id, stop_sequences) do
-    stop_sequences
-    |> Enum.flat_map(fn stop_sequence -> Util.slice_after(stop_sequence, stop_id) end)
+  @spec downstream_stop_id_set([Stop.id()], [[Stop.id()]]) :: MapSet.t(Stop.id())
+  def downstream_stop_id_set(stop_ids, stop_sequences),
+    do: sliced_stop_id_set(stop_ids, stop_sequences, &Util.slice_after/2)
+
+  defp sliced_stop_id_set(stop_ids, stop_sequences, slice_fn) do
+    stop_ids
+    |> Enum.flat_map(fn stop_id ->
+      Enum.flat_map(stop_sequences, fn stop_sequence -> slice_fn.(stop_sequence, stop_id) end)
+    end)
     |> MapSet.new()
   end
 
@@ -100,18 +115,18 @@ defmodule Screens.LocationContext do
 
   # Returns the route types we care about for the alerts of this screen type / place.
   # NOTE: only public due to use in tests. Should be treated as private.
-  @spec route_type_filter(screen_type(), String.t()) :: list(RouteType.t())
+  @spec route_type_filter(screen_type(), [Stop.id()]) :: list(RouteType.t())
   def route_type_filter(app, _) when app in [BusEink, BusShelter], do: [:bus]
   def route_type_filter(GlEink, _), do: [:light_rail]
   # Ashmont should not show Mattapan alerts for PreFare or Dup
-  def route_type_filter(app, "place-asmnl") when app in [PreFare, Dup], do: [:subway]
+  def route_type_filter(app, ["place-asmnl"]) when app in [PreFare, Dup], do: [:subway]
   def route_type_filter(PreFare, _), do: [:light_rail, :subway]
   # WTC is a special bus-only case
-  def route_type_filter(Dup, "place-wtcst"), do: [:bus]
+  def route_type_filter(Dup, ["place-wtcst"]), do: [:bus]
   def route_type_filter(Dup, _), do: [:light_rail, :subway]
 
-  defp routes_with_active(stop_id, route_types, now) do
-    params = %{route_types: route_types, stop_id: stop_id}
+  defp routes_with_active(stop_ids, route_types, now) do
+    params = %{route_types: route_types, stop_ids: stop_ids}
 
     with {:ok, routes} <- Route.fetch(params),
          {:ok, routes_today} <- params |> Map.put(:date, now) |> Route.fetch() do
@@ -124,26 +139,26 @@ defmodule Screens.LocationContext do
     end
   end
 
-  defp fetch_tagged_stop_sequences_by_app(app, stop_id, _route_ids)
+  defp fetch_tagged_stop_sequences_by_app(app, stop_ids, _route_ids)
        when app in [BusEink, BusShelter, GlEink] do
-    fetch_tagged_stop_sequences_through_stop(stop_id)
+    fetch_tagged_stop_sequences_through_stops(stop_ids)
   end
 
-  defp fetch_tagged_stop_sequences_by_app(Dup, stop_id, route_ids) do
-    fetch_tagged_parent_station_sequences_through_stop(stop_id, route_ids)
+  defp fetch_tagged_stop_sequences_by_app(Dup, stop_ids, route_ids) do
+    fetch_tagged_parent_station_sequences_through_stops(stop_ids, route_ids)
   end
 
-  defp fetch_tagged_stop_sequences_by_app(PreFare, stop_id, route_ids) do
+  defp fetch_tagged_stop_sequences_by_app(PreFare, stop_ids, route_ids) do
     # We limit results to canonical route patterns only--no stop sequences for nonstandard patterns.
-    fetch_tagged_parent_station_sequences_through_stop(stop_id, route_ids, true)
+    fetch_tagged_parent_station_sequences_through_stops(stop_ids, route_ids, true)
   end
 
   # Returns a map from route ID to a list of stop sequences of that route, for all routes serving
   # stop, in all applicable directions.
-  @spec fetch_tagged_stop_sequences_through_stop(Stop.id(), [Route.id()]) ::
+  @spec fetch_tagged_stop_sequences_through_stops([Stop.id()], [Route.id()]) ::
           {:ok, %{Route.id() => [[Stop.id()]]}} | :error
-  defp fetch_tagged_stop_sequences_through_stop(stop_id, route_ids \\ []) do
-    case RoutePattern.fetch(%{stop_ids: [stop_id], route_ids: route_ids}) do
+  defp fetch_tagged_stop_sequences_through_stops(stop_ids, route_ids \\ []) do
+    case RoutePattern.fetch(%{stop_ids: stop_ids, route_ids: route_ids}) do
       {:ok, patterns} -> {:ok, get_tagged_stop_sequences(patterns)}
       _ -> :error
     end
@@ -154,10 +169,14 @@ defmodule Screens.LocationContext do
   #
   # If no parent station data exists, platform ID is returned instead. Only stop sequences for
   # direction ID 0 are returned. Assumes that all stop sequences in result are platforms.
-  @spec fetch_tagged_parent_station_sequences_through_stop(Stop.id(), [Route.id()], boolean()) ::
+  @spec fetch_tagged_parent_station_sequences_through_stops([Stop.id()], [Route.id()], boolean()) ::
           {:ok, %{Route.id() => [[Stop.id()]]}} | :error
-  defp fetch_tagged_parent_station_sequences_through_stop(stop_id, route_ids, canonical? \\ false) do
-    params = %{stop_ids: [stop_id], route_ids: route_ids}
+  defp fetch_tagged_parent_station_sequences_through_stops(
+         stop_ids,
+         route_ids,
+         canonical? \\ false
+       ) do
+    params = %{stop_ids: stop_ids, route_ids: route_ids}
     params = if(canonical?, do: Map.put(params, :canonical?, true), else: params)
 
     case RoutePattern.fetch(params) do
@@ -170,7 +189,7 @@ defmodule Screens.LocationContext do
   # NOTE: only public due to use in tests. Should be treated as private.
   @spec untag_stop_sequences(%{Route.id() => [[Stop.id()]]}) :: [[Stop.id()]]
   def untag_stop_sequences(tagged_stop_sequences),
-    do: Enum.flat_map(tagged_stop_sequences, &elem(&1, 1))
+    do: Enum.flat_map(tagged_stop_sequences, fn {_route_id, sequences} -> sequences end)
 
   defp get_tagged_stop_sequences(route_patterns) do
     route_patterns

lib/screens/v2/candidate_generator/widgets/alerts.ex
@@ -3,13 +3,9 @@ defmodule Screens.V2.CandidateGenerator.Widgets.Alerts do
 
   alias Screens.Alerts.Alert
   alias Screens.LocationContext
-  alias Screens.Util
   alias Screens.V2.WidgetInstance.Alert, as: AlertWidget
-  alias ScreensConfig.Alerts
+  alias ScreensConfig.{Alerts, MultiStopAlerts}
   alias ScreensConfig.Screen
-  alias ScreensConfig.Screen.{BusEink, BusShelter, GlEink}
-
-  @alert_supporting_screen_types [BusEink, BusShelter, GlEink]
 
   @relevant_effects MapSet.new(
                       ~w[shuttle station_closure stop_closure suspension detour stop_moved snow_route elevator_closure]a
@@ -17,37 +13,39 @@ defmodule Screens.V2.CandidateGenerator.Widgets.Alerts do
 
   @spec alert_instances(Screen.t()) :: list(AlertWidget.t())
   def alert_instances(
-        %Screen{app_params: %app{alerts: %Alerts{stop_id: stop_id}}} = config,
+        %Screen{app_params: %app{alerts: alerts_config}} = config,
         now \\ DateTime.utc_now(),
         fetch_alerts_by_stop_and_route_fn \\ &Alert.fetch_by_stop_and_route/2,
         fetch_location_context_fn \\ &LocationContext.fetch/3
-      )
-      when app in @alert_supporting_screen_types do
-    with {:ok, location_context} <- fetch_location_context_fn.(app, stop_id, now),
-         reachable_stop_ids = local_and_downstream_stop_ids(location_context, stop_id),
+      ) do
+    stop_ids =
+      case alerts_config do
+        %Alerts{stop_id: stop_id} -> [stop_id]
+        %MultiStopAlerts{stop_ids: stop_ids} -> stop_ids
+      end
+
+    with {:ok, location_context} <- fetch_location_context_fn.(app, stop_ids, now),
+         reachable_stop_ids = local_and_downstream_stop_ids(location_context, stop_ids),
          route_ids <- LocationContext.route_ids(location_context),
          {:ok, alerts} <- fetch_alerts_by_stop_and_route_fn.(reachable_stop_ids, route_ids) do
       alerts
       |> relevant_alerts(reachable_stop_ids, route_ids, now)
-      |> Enum.map(fn alert ->
-        %AlertWidget{
-          alert: alert,
-          screen: config,
-          location_context: location_context,
-          now: now
-        }
-      end)
+      |> Enum.map(
+        &%AlertWidget{alert: &1, screen: config, location_context: location_context, now: now}
+      )
     else
       :error -> []
     end
   end
 
   @doc """
-  Filters out alerts whose effects we are not interested in, as well as those that do not inform at least one of:
+  Filters out alerts whose effects we are not interested in, as well as those that do not inform
+  at least one of:
+
   - an entire route type, e.g. bus or light rail
-  - a route that serves the home stop, scoped to either the home stop or a downstream stop
-  - a downstream stop or the home stop
-  - a route that serves the home stop
+  - a route that serves the local stops, scoped to local or downstream stops
+  - a local or downstream stop
+  - a route that serves a local stop
 
   (list describes the `relevant_ie?` function clauses in order)
   """
@@ -79,18 +77,9 @@ defmodule Screens.V2.CandidateGenerator.Widgets.Alerts do
     |> Enum.to_list()
   end
 
-  defp local_and_downstream_stop_ids(location_context, home_stop)
-       when location_context.tagged_stop_sequences == nil do
-    [home_stop]
-  end
-
-  defp local_and_downstream_stop_ids(location_context, home_stop) do
-    downstream_stop_ids =
-      location_context
-      |> LocationContext.stop_sequences()
-      |> Enum.flat_map(&Util.slice_after(&1, home_stop))
-      |> Enum.uniq()
-
-    [home_stop | downstream_stop_ids]
-  end
+  defp local_and_downstream_stop_ids(
+         %LocationContext{downstream_stops: downstream_stops},
+         home_stop_ids
+       ),
+       do: home_stop_ids |> MapSet.new() |> MapSet.union(downstream_stops) |> MapSet.to_list()
 end

test/screens/v2/candidate_generator/widgets/alerts_test.exs
@@ -6,8 +6,8 @@ defmodule Screens.V2.CandidateGenerator.Widgets.AlertsTest do
   alias Screens.Alerts.Alert
   alias Screens.LocationContext
   alias Screens.V2.WidgetInstance.Alert, as: AlertWidget
-  alias ScreensConfig.{Alerts, Screen}
-  alias ScreensConfig.Screen.{BusShelter, Busway}
+  alias ScreensConfig.{Alerts, MultiStopAlerts, Screen}
+  alias ScreensConfig.Screen.{BusEink, BusShelter}
 
   defp ie(opts \\ []) do
     %{stop: opts[:stop], route: opts[:route], route_type: opts[:route_type]}
@@ -19,14 +19,17 @@ defmodule Screens.V2.CandidateGenerator.Widgets.AlertsTest do
   describe "alert_instances/4" do
     setup do
       now = ~U[2021-01-01T00:00:00Z]
-
       stop_id = "1265"
-
+      multi_stop_ids = [stop_id, "1260"]
       app = BusShelter
-
+      multi_stop_app = BusEink
       config = struct(Screen, %{app_params: struct(app, %{alerts: %Alerts{stop_id: stop_id}})})
 
-      bad_config = struct(Screen, %{app_params: struct(Busway)})
+      multi_stop_config =
+        struct(Screen, %{
+          app_params:
+            struct(multi_stop_app, %{alerts: %MultiStopAlerts{stop_ids: multi_stop_ids}})
+        })
 
       routes_at_stop = [
         %{route_id: "22", active?: true},
@@ -47,7 +50,7 @@ defmodule Screens.V2.CandidateGenerator.Widgets.AlertsTest do
         %Alert{
           id: "1",
           effect: :stop_closure,
-          informed_entities: [ie(stop: "1265")],
+          informed_entities: [ie(stop: "1265"), ie(stop: "11531")],
           active_period: [{now, nil}]
         },
         %Alert{
@@ -62,25 +65,47 @@ defmodule Screens.V2.CandidateGenerator.Widgets.AlertsTest do
           informed_entities: [ie(stop: "1265")],
           active_period: [{now, nil}]
         },
-        %Alert{id: "4", effect: :stop_closure, informed_entities: [], active_period: [{now, nil}]}
+        %Alert{
+          id: "4",
+          effect: :stop_closure,
+          informed_entities: [],
+          active_period: [{now, nil}]
+        },
+        %Alert{
+          id: "5",
+          effect: :stop_closure,
+          informed_entities: [ie(stop: "1262")],
+          active_period: [{now, nil}]
+        }
       ]
 
       location_context = %LocationContext{
         home_stop: stop_id,
         tagged_stop_sequences: tagged_stop_sequences,
-        upstream_stops: LocationContext.upstream_stop_id_set(stop_id, stop_sequences),
-        downstream_stops: LocationContext.downstream_stop_id_set(stop_id, stop_sequences),
+        upstream_stops: LocationContext.upstream_stop_id_set([stop_id], stop_sequences),
+        downstream_stops: LocationContext.downstream_stop_id_set([stop_id], stop_sequences),
         routes: routes_at_stop,
-        alert_route_types: LocationContext.route_type_filter(app, stop_id)
+        alert_route_types: LocationContext.route_type_filter(app, [stop_id])
+      }
+
+      multi_stop_context = %LocationContext{
+        home_stop: nil,
+        tagged_stop_sequences: tagged_stop_sequences,
+        upstream_stops: LocationContext.upstream_stop_id_set(multi_stop_ids, stop_sequences),
+        downstream_stops: LocationContext.downstream_stop_id_set(multi_stop_ids, stop_sequences),
+        routes: routes_at_stop,
+        alert_route_types: LocationContext.route_type_filter(multi_stop_app, multi_stop_ids)
       }
 
       %{
         config: config,
-        bad_config: bad_config,
+        multi_stop_config: multi_stop_config,
         location_context: location_context,
+        multi_stop_context: multi_stop_context,
         now: now,
         fetch_alerts_fn: fn _, _ -> {:ok, alerts} end,
         fetch_location_context_fn: fn _, _, _ -> {:ok, location_context} end,
+        fetch_multi_stop_context_fn: fn _, _, _ -> {:ok, multi_stop_context} end,
         x_fetch_alerts_fn: fn _, _ -> :error end,
         x_fetch_location_context_fn: fn _, _, _ -> :error end
       }
@@ -107,7 +132,7 @@ defmodule Screens.V2.CandidateGenerator.Widgets.AlertsTest do
             alert: %Alert{
               id: "1",
               effect: :stop_closure,
-              informed_entities: [ie(stop: "1265")],
+              informed_entities: [ie(stop: "1265"), ie(stop: "11531")],
               active_period: [{now, nil}]
             }
           },
@@ -135,24 +160,6 @@ defmodule Screens.V2.CandidateGenerator.Widgets.AlertsTest do
                )
     end
 
-    test "fails when passed config for an unsupported screen type", context do
-      %{
-        bad_config: bad_config,
-        now: now,
-        fetch_alerts_fn: fetch_alerts_fn,
-        fetch_location_context_fn: fetch_location_context_fn
-      } = context
-
-      assert_raise FunctionClauseError, fn ->
-        alert_instances(
-          bad_config,
-          now,
-          fetch_alerts_fn,
-          fetch_location_context_fn
-        )
-      end
-    end
-
     test "returns empty list if any query fails", context do
       %{
         config: config,
@@ -178,6 +185,57 @@ defmodule Screens.V2.CandidateGenerator.Widgets.AlertsTest do
                  x_fetch_alerts_fn,
                  fetch_location_context_fn
                )
+    end
+
+    test "returns alert widgets for multiple stops", context do
+      %{
+        multi_stop_config: config,
+        multi_stop_context: location_context,
+        now: now,
+        fetch_alerts_fn: fetch_alerts_fn,
+        fetch_multi_stop_context_fn: fetch_location_context_fn
+      } = context
+
+      expected_common_data = %{screen: config, location_context: location_context, now: now}
+
+      expected_widgets = [
+        struct(
+          %AlertWidget{
+            alert: %Alert{
+              id: "1",
+              effect: :stop_closure,
+              informed_entities: [ie(stop: "1265"), ie(stop: "11531")],
+              active_period: [{now, nil}]
+            }
+          },
+          expected_common_data
+        ),
+        struct(
+          %AlertWidget{
+            alert: %Alert{
+              id: "2",
+              effect: :stop_closure,
+              informed_entities: [ie(route: "22")],
+              active_period: [{now, nil}]
+            }
+          },
+          expected_common_data
+        ),
+        struct(
+          %AlertWidget{
+            alert: %Alert{
+              id: "5",
+              effect: :stop_closure,
+              informed_entities: [ie(stop: "1262")],
+              active_period: [{now, nil}]
+            }
+          },
+          expected_common_data
+        )
+      ]
+
+      assert expected_widgets ==
+               alert_instances(config, now, fetch_alerts_fn, fetch_location_context_fn)
     end
   end
 

test/screens/v2/candidate_generator/widgets/reconstructed_alert_test.exs
@@ -104,10 +104,10 @@ defmodule Screens.V2.CandidateGenerator.Widgets.ReconstructedAlertTest do
       location_context = %LocationContext{
         home_stop: stop_id,
         tagged_stop_sequences: tagged_stop_sequences,
-        upstream_stops: LocationContext.upstream_stop_id_set(stop_id, stop_sequences),
-        downstream_stops: LocationContext.downstream_stop_id_set(stop_id, stop_sequences),
+        upstream_stops: LocationContext.upstream_stop_id_set([stop_id], stop_sequences),
+        downstream_stops: LocationContext.downstream_stop_id_set([stop_id], stop_sequences),
         routes: routes_at_stop,
-        alert_route_types: LocationContext.route_type_filter(app, stop_id)
+        alert_route_types: LocationContext.route_type_filter(app, [stop_id])
       }
 
       %{

test/screens/v2/localized_alert_test.exs
@@ -33,7 +33,7 @@ defmodule Screens.V2.LocalizedAlertTest do
       widget
       | location_context: %{
           widget.location_context
-          | alert_route_types: LocationContext.route_type_filter(app_config_module, stop_id),
+          | alert_route_types: LocationContext.route_type_filter(app_config_module, [stop_id]),
             home_stop: stop_id
         }
     }
@@ -52,9 +52,12 @@ defmodule Screens.V2.LocalizedAlertTest do
           widget.location_context
           | tagged_stop_sequences: tagged_sequences,
             upstream_stops:
-              LocationContext.upstream_stop_id_set(widget.location_context.home_stop, sequences),
+              LocationContext.upstream_stop_id_set([widget.location_context.home_stop], sequences),
             downstream_stops:
-              LocationContext.downstream_stop_id_set(widget.location_context.home_stop, sequences)
+              LocationContext.downstream_stop_id_set(
+                [widget.location_context.home_stop],
+                sequences
+              )
         }
     }
   end

test/screens/v2/widget_instance/alert_test.exs
@@ -42,7 +42,7 @@ defmodule Screens.V2.WidgetInstance.AlertTest do
       widget
       | location_context: %{
           widget.location_context
-          | alert_route_types: LocationContext.route_type_filter(app_config_module, stop_id),
+          | alert_route_types: LocationContext.route_type_filter(app_config_module, [stop_id]),
             home_stop: stop_id
         }
     }
@@ -61,9 +61,12 @@ defmodule Screens.V2.WidgetInstance.AlertTest do
           widget.location_context
           | tagged_stop_sequences: tagged_sequences,
             upstream_stops:
-              LocationContext.upstream_stop_id_set(widget.location_context.home_stop, sequences),
+              LocationContext.upstream_stop_id_set([widget.location_context.home_stop], sequences),
             downstream_stops:
-              LocationContext.downstream_stop_id_set(widget.location_context.home_stop, sequences)
+              LocationContext.downstream_stop_id_set(
+                [widget.location_context.home_stop],
+                sequences
+              )
         }
     }
   end

test/screens/v2/widget_instance/dup_special_case_alert_test.exs
@@ -919,10 +919,11 @@ defmodule Screens.V2.WidgetInstance.DupSpecialCaseAlertTest do
              %LocationContext{
                home_stop: stop_id,
                tagged_stop_sequences: tagged_stop_sequences,
-               upstream_stops: LocationContext.upstream_stop_id_set(stop_id, stop_sequences),
-               downstream_stops: LocationContext.downstream_stop_id_set(stop_id, stop_sequences),
+               upstream_stops: LocationContext.upstream_stop_id_set([stop_id], stop_sequences),
+               downstream_stops:
+                 LocationContext.downstream_stop_id_set([stop_id], stop_sequences),
                routes: routes(stop_id),
-               alert_route_types: LocationContext.route_type_filter(Dup, stop_id)
+               alert_route_types: LocationContext.route_type_filter(Dup, [stop_id])
              }}
         end
       }

test/screens/v2/widget_instance/elevator_status_test.exs
@@ -84,8 +84,8 @@ defmodule Screens.V2.WidgetInstance.ElevatorStatusTest do
     location_context = %LocationContext{
       home_stop: home_station_id,
       tagged_stop_sequences: tagged_stop_sequences,
-      upstream_stops: LocationContext.upstream_stop_id_set(home_station_id, stop_sequences),
-      downstream_stops: LocationContext.downstream_stop_id_set(home_station_id, stop_sequences),
+      upstream_stops: LocationContext.upstream_stop_id_set([home_station_id], stop_sequences),
+      downstream_stops: LocationContext.downstream_stop_id_set([home_station_id], stop_sequences),
       routes: [],
       alert_route_types: []
     }

commit 6675bcc761f6cd91d4b038981b6056af4321b668

fix: crash fetching location context
Fixes an issue introduced in 4efc67d8 where fetching data would crash
for Bus Shelter and GL E-ink screens.

lib/screens/v2/location_context.ex
@@ -43,7 +43,8 @@ defmodule Screens.LocationContext do
   """
   @spec fetch(screen_type(), Stop.id() | [Stop.id()], DateTime.t()) :: {:ok, t()} | :error
   def fetch(app, stop_id, now) when is_binary(stop_id), do: do_fetch(app, [stop_id], now)
-  def fetch(BusEink = app, stop_ids, now) when is_list(stop_ids), do: do_fetch(app, stop_ids, now)
+  def fetch(app, [_] = stop_ids, now), do: do_fetch(app, stop_ids, now)
+  def fetch(BusEink = app, [_ | _] = stop_ids, now), do: do_fetch(app, stop_ids, now)
 
   defp do_fetch(app, stop_ids, now) do
     Screens.Telemetry.span(