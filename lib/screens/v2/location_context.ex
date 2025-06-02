mbta/screens

commit ba06cfa505c493896dd9384a4222d388bdd5589d

refactor: relocate LocationContext-specific logic
Certain public APIs of `Route` and `RoutePattern` were essentially
internal logic of `LocationContext` and never used elsewhere. Move
this "in-house" to simplify subsequent changes.

lib/screens/v2/location_context.ex
@@ -42,11 +42,11 @@ defmodule Screens.LocationContext do
       %{app: app, stop_id: stop_id},
       fn ->
         with alert_route_types <- route_type_filter(app, stop_id),
-             {:ok, routes_at_stop} <-
-               Route.serving_stop_with_active(stop_id, now, alert_route_types),
+             {:ok, routes_at_stop} <- routes_with_active(stop_id, alert_route_types, now),
+             route_ids_at_stop = Enum.map(routes_at_stop, & &1.route_id),
              {:ok, tagged_stop_sequences} <-
-               fetch_tagged_stop_sequences_by_app(app, stop_id, routes_at_stop) do
-          stop_sequences = RoutePattern.untag_stop_sequences(tagged_stop_sequences)
+               fetch_tagged_stop_sequences_by_app(app, stop_id, route_ids_at_stop) do
+          stop_sequences = untag_stop_sequences(tagged_stop_sequences)
 
           {
             :ok,
@@ -68,17 +68,7 @@ defmodule Screens.LocationContext do
     )
   end
 
-  # Returns the route types we care about for the alerts of this screen type / place
-  @spec route_type_filter(screen_type(), String.t()) :: list(RouteType.t())
-  def route_type_filter(app, _) when app in [BusEink, BusShelter], do: [:bus]
-  def route_type_filter(GlEink, _), do: [:light_rail]
-  # Ashmont should not show Mattapan alerts for PreFare or Dup
-  def route_type_filter(app, "place-asmnl") when app in [PreFare, Dup], do: [:subway]
-  def route_type_filter(PreFare, _), do: [:light_rail, :subway]
-  # WTC is a special bus-only case
-  def route_type_filter(Dup, "place-wtcst"), do: [:bus]
-  def route_type_filter(Dup, _), do: [:light_rail, :subway]
-
+  # NOTE: only public due to use in tests. Should be treated as private.
   @spec upstream_stop_id_set(String.t(), list(list(Stop.id()))) :: MapSet.t(Stop.id())
   def upstream_stop_id_set(stop_id, stop_sequences) do
     stop_sequences
@@ -86,6 +76,7 @@ defmodule Screens.LocationContext do
     |> MapSet.new()
   end
 
+  # NOTE: only public due to use in tests. Should be treated as private.
   @spec downstream_stop_id_set(String.t(), list(list(Stop.id()))) :: MapSet.t(Stop.id())
   def downstream_stop_id_set(stop_id, stop_sequences) do
     stop_sequences
@@ -97,9 +88,7 @@ defmodule Screens.LocationContext do
   Returns IDs of routes that serve this location.
   """
   @spec route_ids(t()) :: list(Route.id())
-  def route_ids(%__MODULE__{} = t) do
-    Route.route_ids(t.routes)
-  end
+  def route_ids(%__MODULE__{routes: routes}), do: Enum.map(routes, & &1.route_id)
 
   @doc """
   Returns the stop sequences of routes that serve this location.
@@ -107,24 +96,110 @@ defmodule Screens.LocationContext do
   Generally, this means they go from north/east -> south/west.
   """
   @spec stop_sequences(t()) :: list(list(Stop.id()))
-  def stop_sequences(%__MODULE__{} = t) do
-    RoutePattern.untag_stop_sequences(t.tagged_stop_sequences)
+  def stop_sequences(%__MODULE__{} = t), do: untag_stop_sequences(t.tagged_stop_sequences)
+
+  # Returns the route types we care about for the alerts of this screen type / place.
+  # NOTE: only public due to use in tests. Should be treated as private.
+  @spec route_type_filter(screen_type(), String.t()) :: list(RouteType.t())
+  def route_type_filter(app, _) when app in [BusEink, BusShelter], do: [:bus]
+  def route_type_filter(GlEink, _), do: [:light_rail]
+  # Ashmont should not show Mattapan alerts for PreFare or Dup
+  def route_type_filter(app, "place-asmnl") when app in [PreFare, Dup], do: [:subway]
+  def route_type_filter(PreFare, _), do: [:light_rail, :subway]
+  # WTC is a special bus-only case
+  def route_type_filter(Dup, "place-wtcst"), do: [:bus]
+  def route_type_filter(Dup, _), do: [:light_rail, :subway]
+
+  defp routes_with_active(stop_id, route_types, now) do
+    params = %{route_types: route_types, stop_id: stop_id}
+
+    with {:ok, routes} <- Route.fetch(params),
+         {:ok, routes_today} <- params |> Map.put(:date, now) |> Route.fetch() do
+      route_ids_today = MapSet.new(routes_today, fn %Route{id: id} -> id end)
+
+      {
+        :ok,
+        Enum.map(routes, fn %Route{id: id} -> %{route_id: id, active?: id in route_ids_today} end)
+      }
+    end
   end
 
-  defp fetch_tagged_stop_sequences_by_app(app, stop_id, _routes_at_stop)
+  defp fetch_tagged_stop_sequences_by_app(app, stop_id, _route_ids)
        when app in [BusEink, BusShelter, GlEink] do
-    RoutePattern.fetch_tagged_stop_sequences_through_stop(stop_id)
+    fetch_tagged_stop_sequences_through_stop(stop_id)
   end
 
-  defp fetch_tagged_stop_sequences_by_app(Dup, stop_id, routes_at_stop) do
-    route_ids = Route.route_ids(routes_at_stop)
-    RoutePattern.fetch_tagged_parent_station_sequences_through_stop(stop_id, route_ids)
+  defp fetch_tagged_stop_sequences_by_app(Dup, stop_id, route_ids) do
+    fetch_tagged_parent_station_sequences_through_stop(stop_id, route_ids)
   end
 
-  defp fetch_tagged_stop_sequences_by_app(PreFare, stop_id, routes_at_stop) do
-    route_ids = Route.route_ids(routes_at_stop)
-
+  defp fetch_tagged_stop_sequences_by_app(PreFare, stop_id, route_ids) do
     # We limit results to canonical route patterns only--no stop sequences for nonstandard patterns.
-    RoutePattern.fetch_tagged_parent_station_sequences_through_stop(stop_id, route_ids, true)
+    fetch_tagged_parent_station_sequences_through_stop(stop_id, route_ids, true)
+  end
+
+  # Returns a map from route ID to a list of stop sequences of that route, for all routes serving
+  # stop, in all applicable directions.
+  @spec fetch_tagged_stop_sequences_through_stop(Stop.id(), [Route.id()]) ::
+          {:ok, %{Route.id() => [[Stop.id()]]}} | :error
+  defp fetch_tagged_stop_sequences_through_stop(stop_id, route_ids \\ []) do
+    case RoutePattern.fetch(%{stop_ids: [stop_id], route_ids: route_ids}) do
+      {:ok, patterns} -> {:ok, get_tagged_stop_sequences(patterns)}
+      _ -> :error
+    end
+  end
+
+  # Returns a map from route ID to a list of stop sequences of that route. Stop sequences are
+  # described in terms of parent station IDs, not platform IDs.
+  #
+  # If no parent station data exists, platform ID is returned instead. Only stop sequences for
+  # direction ID 0 are returned. Assumes that all stop sequences in result are platforms.
+  @spec fetch_tagged_parent_station_sequences_through_stop(Stop.id(), [Route.id()], boolean()) ::
+          {:ok, %{Route.id() => [[Stop.id()]]}} | :error
+  defp fetch_tagged_parent_station_sequences_through_stop(stop_id, route_ids, canonical? \\ false) do
+    params = %{stop_ids: [stop_id], route_ids: route_ids}
+    params = if(canonical?, do: Map.put(params, :canonical?, true), else: params)
+
+    case RoutePattern.fetch(params) do
+      {:ok, []} -> :error
+      {:ok, patterns} -> {:ok, get_tagged_parent_station_sequences(patterns)}
+      _ -> :error
+    end
+  end
+
+  # NOTE: only public due to use in tests. Should be treated as private.
+  @spec untag_stop_sequences(%{Route.id() => [[Stop.id()]]}) :: [[Stop.id()]]
+  def untag_stop_sequences(tagged_stop_sequences),
+    do: Enum.flat_map(tagged_stop_sequences, &elem(&1, 1))
+
+  defp get_tagged_stop_sequences(route_patterns) do
+    route_patterns
+    |> Enum.map(fn %RoutePattern{route: %Route{id: route_id}, stops: stops} ->
+      {route_id, Enum.map(stops, fn %Stop{id: id} -> id end)}
+    end)
+    |> Enum.group_by(&elem(&1, 0), &elem(&1, 1))
+  end
+
+  defp get_tagged_parent_station_sequences(route_patterns) do
+    platform_to_station_map =
+      route_patterns
+      |> Enum.flat_map(& &1.stops)
+      |> Enum.map(fn
+        %Stop{id: id, parent_station: nil} -> {id, id}
+        %Stop{id: id, parent_station: parent_id} -> {id, parent_id}
+      end)
+      |> Map.new()
+
+    route_patterns
+    |> get_tagged_stop_sequences()
+    |> Map.new(fn {route_id, stop_sequences} ->
+      station_sequences =
+        stop_sequences
+        |> Enum.map(fn stops -> Enum.map(stops, &Map.fetch!(platform_to_station_map, &1)) end)
+        # Dedup the stop sequences (both directions are listed, but we only need 1)
+        |> Enum.uniq_by(&MapSet.new/1)
+
+      {route_id, station_sequences}
+    end)
   end
 end

lib/screens/route_patterns/route_pattern.ex
@@ -87,146 +87,4 @@ defmodule Screens.RoutePatterns.RoutePattern do
       _ -> :error
     end
   end
-
-  @doc """
-  Returns a map from route ID to a list of stop sequences of that route, for all
-  routes serving stop, in all applicable directions.
-  """
-  @spec fetch_tagged_stop_sequences_through_stop(Stop.id()) ::
-          {:ok, %{Route.id() => list(list(Stop.id()))}} | :error
-  def fetch_tagged_stop_sequences_through_stop(
-        stop_id,
-        route_filters \\ [],
-        get_json_fn \\ &V3Api.get_json/2
-      ) do
-    params = %{
-      "include" => "representative_trip.stops,route",
-      "filter[stop]" => stop_id
-    }
-
-    params =
-      if length(route_filters) > 0,
-        do: Map.put(params, "filter[route]", Enum.join(route_filters, ",")),
-        else: params
-
-    case get_json_fn.("route_patterns", params) do
-      {:ok, result} ->
-        {:ok, get_tagged_stop_sequences_from_result(result)}
-
-      _ ->
-        :error
-    end
-  end
-
-  @doc """
-  Returns a map from route ID to a list of stop sequences of that route. Stop sequences
-  are described in terms of parent station IDs, not platform IDs.
-
-  Pass `true` for `canonical_only?` to limit results to canonical route patterns.
-  With `canonical_only? = true`,
-  - For most routes (everything but Red Line), only one stop sequence will be in the list.
-  - For Red Line, the list will contain one stop sequence for the Ashmont branch and one for the Braintree branch.
-
-  Pass `false` for `canonical_only?` to limit results to *non-canonical* route patterns. (You probably don't want to do this!)
-
-  If no parent station data exists, platform_id is returned instead.
-  Only stop sequences for direction ID 0 are returned.
-  Assumes that all stop sequences in result are platforms.
-  """
-  @spec fetch_tagged_parent_station_sequences_through_stop(Stop.id(), list(String.t())) ::
-          {:ok, %{Route.id() => list(list(Stop.id()))}} | :error
-  def fetch_tagged_parent_station_sequences_through_stop(
-        stop_id,
-        route_filters,
-        canonical_only? \\ nil,
-        get_json_fn \\ &V3Api.get_json/2
-      ) do
-    params = %{
-      "include" => "representative_trip.stops,route",
-      "filter[stop]" => stop_id,
-      "filter[direction_id]" => 0,
-      "filter[route]" => Enum.join(route_filters, ",")
-    }
-
-    params =
-      if is_boolean(canonical_only?),
-        do: Map.put(params, "filter[canonical]", canonical_only?),
-        else: params
-
-    case get_json_fn.("route_patterns", params) do
-      {:ok, %{"data" => []}} ->
-        :error
-
-      {:ok, result} ->
-        {:ok, get_tagged_parent_station_sequences_from_result(result)}
-
-      _ ->
-        :error
-    end
-  end
-
-  @doc """
-  Given a map from route ID to stop sequences of that route, returns a flat list
-  of all of the stop sequences.
-
-  ```
-  iex> untag_stop_sequences(%{"route1" => [sequence1, sequence2], "route2" => [sequence3]})
-  [sequence1, sequence2, sequence3]
-  ```
-  """
-  @spec untag_stop_sequences(%{Route.id() => list(list(Stop.id()))}) :: list(list(Stop.id()))
-  def untag_stop_sequences(tagged_stop_sequences) do
-    Enum.flat_map(tagged_stop_sequences, &elem(&1, 1))
-  end
-
-  defp get_tagged_stop_sequences_from_result(result) do
-    result["included"]
-    |> Enum.filter(&(&1["type"] == "trip"))
-    |> Enum.map(fn trip ->
-      route = trip["relationships"]["route"]["data"]["id"]
-      sequence = Enum.map(trip["relationships"]["stops"]["data"], & &1["id"])
-
-      {route, sequence}
-    end)
-    |> Enum.group_by(&elem(&1, 0), &elem(&1, 1))
-  end
-
-  defp get_platform_to_station_map_from_result(result) do
-    result
-    |> get_in([
-      "included",
-      Access.filter(&(&1["type"] == "stop"))
-    ])
-    |> Enum.map(fn %{
-                     "relationships" => %{
-                       "parent_station" => %{"data" => parent_station_data}
-                     },
-                     "id" => platform_id
-                   } ->
-      if parent_station_data,
-        do: {platform_id, parent_station_data["id"]},
-        else: {platform_id, platform_id}
-    end)
-    |> Enum.into(%{})
-  end
-
-  defp get_tagged_parent_station_sequences_from_result(result) do
-    platform_to_station_map = get_platform_to_station_map_from_result(result)
-
-    result
-    |> get_tagged_stop_sequences_from_result()
-    |> Map.new(fn {route_id, stop_sequences} ->
-      station_sequences =
-        stop_sequences
-        |> Enum.map(&platforms_to_stations(&1, platform_to_station_map))
-        # Dedup the stop sequences (both directions are listed, but we only need 1)
-        |> Enum.uniq_by(&MapSet.new/1)
-
-      {route_id, station_sequences}
-    end)
-  end
-
-  defp platforms_to_stations(stop_sequence, platform_to_station_map) do
-    Enum.map(stop_sequence, &Map.fetch!(platform_to_station_map, &1))
-  end
 end

lib/screens/routes/route.ex
@@ -65,38 +65,6 @@ defmodule Screens.Routes.Route do
     end
   end
 
-  @doc """
-  Fetches routes that serve the given stop, also determining whether each route has any scheduled
-  service at the stop on the current day. Only route IDs and the `active?` flag are returned.
-  """
-  @spec serving_stop_with_active(Stop.id()) ::
-          {:ok, list(%{route_id: id(), active?: boolean()})} | :error
-  def serving_stop_with_active(
-        stop_id,
-        now \\ DateTime.utc_now(),
-        type_filters \\ [],
-        get_json_fn \\ &V3Api.get_json/2,
-        fetch_routes_fn \\ &fetch_routes/3
-      ) do
-    Screens.Telemetry.span(
-      ~w[screens routes route serving_stop_with_active]a,
-      %{stop_id: stop_id, type_filters: type_filters},
-      fn ->
-        with {:ok, routes} <- fetch_routes_fn.(stop_id, get_json_fn, type_filters),
-             {:ok, active_route_ids} <- fetch_active_route_ids(stop_id, now, get_json_fn) do
-          active_set = MapSet.new(active_route_ids)
-
-          routes_at_stop =
-            Enum.map(routes, &%{route_id: &1.id, active?: MapSet.member?(active_set, &1.id)})
-
-          {:ok, routes_at_stop}
-        else
-          :error -> :error
-        end
-      end
-    )
-  end
-
   defp format_query_param({:ids, ids}) when is_list(ids) do
     [{"filter[id]", Enum.join(ids, ",")}]
   end
@@ -130,23 +98,6 @@ defmodule Screens.Routes.Route do
   end
 
   defp format_query_param(_), do: []
-
-  defp fetch_routes(stop_id, get_json_fn, type_filters) do
-    case fetch([stop_id: stop_id, route_types: type_filters], get_json_fn) do
-      {:ok, routes} -> {:ok, routes}
-      :error -> :error
-    end
-  end
-
-  defp fetch_active_route_ids(stop_id, now, get_json_fn) do
-    case fetch([stop_id: stop_id, date: now], get_json_fn) do
-      {:ok, routes} -> {:ok, Enum.map(routes, & &1.id)}
-      :error -> :error
-    end
-  end
-
-  @spec route_ids(list(%{route_id: id(), active?: boolean()})) :: list(id())
-  def route_ids(routes), do: Enum.map(routes, & &1.route_id)
 
   @spec color(id()) :: color()
   @spec color(id(), RouteType.t() | nil) :: color()

lib/screens/v2/candidate_generator/dup/alerts.ex
@@ -6,7 +6,6 @@ defmodule Screens.V2.CandidateGenerator.Dup.Alerts do
   alias Screens.Alerts.Alert
   alias Screens.LocationContext
   alias Screens.Report
-  alias Screens.Routes.Route
   alias Screens.Stops.Stop
   alias Screens.V2.LocalizedAlert
   alias Screens.V2.WidgetInstance.{DupAlert, DupSpecialCaseAlert}
@@ -51,7 +50,7 @@ defmodule Screens.V2.CandidateGenerator.Dup.Alerts do
         end
 
       with {:ok, location_context} <- fetch_location_context_fn.(Dup, stop_id, now),
-           route_ids <- Route.route_ids(location_context.routes),
+           route_ids <- LocationContext.route_ids(location_context),
            {:ok, alerts} <- fetch_alerts_fn.(route_ids: route_ids) do
         alerts
         |> relevant_alerts(config, location_context, now)

lib/screens/v2/candidate_generator/widgets/alerts.ex
@@ -3,7 +3,6 @@ defmodule Screens.V2.CandidateGenerator.Widgets.Alerts do
 
   alias Screens.Alerts.Alert
   alias Screens.LocationContext
-  alias Screens.Routes.Route
   alias Screens.Util
   alias Screens.V2.WidgetInstance.Alert, as: AlertWidget
   alias ScreensConfig.Alerts
@@ -26,12 +25,8 @@ defmodule Screens.V2.CandidateGenerator.Widgets.Alerts do
       when app in @alert_supporting_screen_types do
     with {:ok, location_context} <- fetch_location_context_fn.(app, stop_id, now),
          reachable_stop_ids = local_and_downstream_stop_ids(location_context, stop_id),
-         route_ids <- Route.route_ids(location_context.routes),
-         {:ok, alerts} <-
-           fetch_alerts_by_stop_and_route_fn.(
-             reachable_stop_ids,
-             route_ids
-           ) do
+         route_ids <- LocationContext.route_ids(location_context),
+         {:ok, alerts} <- fetch_alerts_by_stop_and_route_fn.(reachable_stop_ids, route_ids) do
       alerts
       |> relevant_alerts(reachable_stop_ids, route_ids, now)
       |> Enum.map(fn alert ->

lib/screens/v2/candidate_generator/widgets/reconstructed_alert.ex
@@ -3,7 +3,6 @@ defmodule Screens.V2.CandidateGenerator.Widgets.ReconstructedAlert do
 
   alias Screens.Alerts.Alert
   alias Screens.LocationContext
-  alias Screens.Routes.Route
   alias Screens.Stops.Stop
   alias Screens.V2.LocalizedAlert
   alias Screens.V2.WidgetInstance.ReconstructedAlert
@@ -59,7 +58,7 @@ defmodule Screens.V2.CandidateGenerator.Widgets.ReconstructedAlert do
 
     # Filtering by subway and light_rail types
     with {:ok, location_context} <- fetch_location_context_fn.(PreFare, stop_id, now),
-         route_ids <- Route.route_ids(location_context.routes),
+         route_ids <- LocationContext.route_ids(location_context),
          {:ok, alerts} <- fetch_alerts_fn.(route_ids: route_ids) do
       relevant_alerts = relevant_alerts(alerts, location_context, now)
       is_terminal_station = terminal?(stop_id, LocationContext.stop_sequences(location_context))

lib/screens/v2/localized_alert.ex
@@ -132,7 +132,7 @@ defmodule Screens.V2.LocalizedAlert do
 
   # Only route is not nil (route type ignored)
   defp informed_entity_to_zone(%{stop: nil, route: route}, context) do
-    route_ids = Route.route_ids(context.routes)
+    route_ids = LocationContext.route_ids(context)
     if route in route_ids, do: [:upstream, :home_stop, :downstream], else: []
   end
 
@@ -142,7 +142,7 @@ defmodule Screens.V2.LocalizedAlert do
 
   # Both stop and route are not nil (route type ignored)
   defp informed_entity_to_zone(%{stop: _stop, route: route} = informed_entity, context) do
-    route_ids = Route.route_ids(context.routes)
+    route_ids = LocationContext.route_ids(context)
 
     if route in route_ids do
       informed_entity_to_zone(%{informed_entity | route: nil}, context)
@@ -277,11 +277,7 @@ defmodule Screens.V2.LocalizedAlert do
       }) do
     rts = location_context.alert_route_types
     home_stop = location_context.home_stop
-
-    route_set =
-      location_context.routes
-      |> Route.route_ids()
-      |> MapSet.new()
+    route_set = location_context |> LocationContext.route_ids() |> MapSet.new()
 
     # allows us to pattern match against the empty set
     empty_set = MapSet.new()

test/screens/route_patterns/route_pattern_test.exs
@@ -317,92 +317,4 @@ defmodule Screens.RoutePatterns.RoutePatternTest do
                RoutePattern.fetch(%{typicality: 1}, get_json_fn)
     end
   end
-
-  describe "fetch_tagged_stop_sequences_through_stop/2" do
-    test "returns {:ok, sequences} if fetch function returns {:ok, data}" do
-      stop_id = "1265"
-
-      params = %{
-        "include" => "representative_trip.stops,route",
-        "filter[stop]" => stop_id
-      }
-
-      data = %{
-        "included" => [
-          %{"type" => "stop"},
-          %{
-            "type" => "trip",
-            "relationships" => %{
-              "stops" => %{"data" => [%{"id" => "1"}, %{"id" => "2"}, %{"id" => "3"}]},
-              "route" => %{"data" => %{"id" => "route1"}}
-            }
-          },
-          %{
-            "type" => "trip",
-            "relationships" => %{
-              "stops" => %{"data" => [%{"id" => "3"}, %{"id" => "2"}, %{"id" => "1"}]},
-              "route" => %{"data" => %{"id" => "route1"}}
-            }
-          },
-          %{
-            "type" => "trip",
-            "relationships" => %{
-              "stops" => %{"data" => [%{"id" => "5"}, %{"id" => "6"}, %{"id" => "7"}]},
-              "route" => %{"data" => %{"id" => "route2"}}
-            }
-          }
-        ]
-      }
-
-      get_json_fn = fn _, ^params -> {:ok, data} end
-
-      expected_stop_sequences = %{"route1" => [~w[1 2 3], ~w[3 2 1]], "route2" => [~w[5 6 7]]}
-
-      assert {:ok, expected_stop_sequences} ==
-               RoutePattern.fetch_tagged_stop_sequences_through_stop(stop_id, [], get_json_fn)
-    end
-
-    test "returns :error if fetch function returns :error" do
-      stop_id = "1265"
-
-      get_json_fn = fn _, _ -> :error end
-
-      assert :error ==
-               RoutePattern.fetch_tagged_stop_sequences_through_stop(stop_id, [], get_json_fn)
-    end
-
-    test "returns filtered list if route_filters is provided" do
-      stop_id = "1265"
-      route_filters = ["Orange"]
-
-      params = %{
-        "include" => "representative_trip.stops,route",
-        "filter[stop]" => stop_id,
-        "filter[route]" => Enum.join(route_filters, ",")
-      }
-
-      data = %{
-        "included" => [
-          %{
-            "type" => "trip",
-            "relationships" => %{
-              "stops" => %{"data" => [%{"id" => "5"}, %{"id" => "6"}, %{"id" => "7"}]},
-              "route" => %{"data" => %{"id" => "Orange"}}
-            }
-          }
-        ]
-      }
-
-      get_json_fn = fn _, ^params -> {:ok, data} end
-
-      expected_stop_sequences = %{"Orange" => [~w[5 6 7]]}
-
-      assert {:ok, expected_stop_sequences} ==
-               RoutePattern.fetch_tagged_stop_sequences_through_stop(
-                 stop_id,
-                 route_filters,
-                 get_json_fn
-               )
-    end
-  end
 end

commit db7b4735df55835f6f82cdbb3274f7e6f13d82f3

fix: ReconstructedAlert crash on Pre-Fares
ba06cfa5 introduced a regression where the "stop sequences" of a
`LocationContext` could be entire `Stop` structs instead of stop IDs,
on screen types that use parent station sequences (DUPs and Pre-Fares).
This could cause later logic to crash when it expected to find IDs.

lib/screens/v2/location_context.ex
@@ -206,7 +206,7 @@ defmodule Screens.LocationContext do
       |> Enum.flat_map(& &1.stops)
       |> Enum.map(fn
         %Stop{id: id, parent_station: nil} -> {id, id}
-        %Stop{id: id, parent_station: parent_id} -> {id, parent_id}
+        %Stop{id: id, parent_station: %Stop{id: parent_id}} -> {id, parent_id}
       end)
       |> Map.new()
 