ab3d60e2edee07288a432ccd5c1e836e0b992859
Cora Grant <cgrant2@mbta.com>
2025-07-25 16:37:07 -0400

    refactor: DupAlert layout selection logic
    The layout of a DUP alert is determined by certain rules. These were
    implemented by essentially pre-applying them to every combination of
    inputs to create a TSV-like "lookup table", which was then parsed into
    a more usable form. This was over-complicated given how straightforward
    the rules themselves are (and how irrelevant much of the data in the
    table was as a result). Expressing them directly as code results in a
    more concise and legible implementation, and makes it easier to change
    the rules in the future.

--- lib/screens/v2/widget_instance/dup_alert.ex
+++ lib/screens/v2/widget_instance/dup_alert.ex
@@ -5,10 +5,9 @@ defmodule Screens.V2.WidgetInstance.DupAlert do
 
   alias Screens.Alerts.Alert
   alias Screens.LocationContext
-  alias Screens.Report
   alias Screens.V2.LocalizedAlert
   alias Screens.V2.WidgetInstance.DupAlert.Serialize
-  alias ScreensConfig.Screen
+  alias ScreensConfig.{Departures, Screen, Screen.Dup}
 
   @enforce_keys [:screen, :alert, :location_context, :rotation_index, :stop_name]
   defstruct @enforce_keys
@@ -27,13 +26,11 @@ defmodule Screens.V2.WidgetInstance.DupAlert do
 
   @spec priority(t()) :: Screens.V2.WidgetInstance.priority()
   def priority(t) do
-    # For DUP alerts, priority is based on layout.
     # Overnight mode should take priority over partial alerts but not full_screen.
     # Giving full_screen the highest priority so it will always show over overnight.
-    if alert_layout(t) == :full_screen do
-      [1]
-    else
-      [1, 2]
+    case alert_layout(t) do
+      :full_screen -> [1]
+      :partial -> [1, 2]
     end
   end
 
@@ -42,9 +39,6 @@ defmodule Screens.V2.WidgetInstance.DupAlert do
     case alert_layout(t) do
       :full_screen -> Serialize.serialize_full_screen(t)
       :partial -> Serialize.serialize_partial(t)
-      # This case will never match since it would be filtered out at `valid_candidate?`,
-      # but Dialyzer doesn't know that.
-      :no_render -> %{}
     end
   end
 
@@ -54,9 +48,6 @@ defmodule Screens.V2.WidgetInstance.DupAlert do
       case alert_layout(t) do
         :full_screen -> "full_rotation"
         :partial -> "bottom_pane"
-        # This case will never match since it would be filtered out at `valid_candidate?`,
-        # but Dialyzer doesn't know that.
-        :no_render -> "bottom_pane"
       end
 
     # Returns e.g. [:full_rotation_zero], [:bottom_pane_one], ...
@@ -68,160 +59,61 @@ defmodule Screens.V2.WidgetInstance.DupAlert do
     case alert_layout(t) do
       :full_screen -> :takeover_alert
       :partial -> :partial_alert
-      # This case will never match since it would be filtered out at `valid_candidate?`,
-      # but Dialyzer doesn't know that.
-      :no_render -> :partial_alert
     end
   end
 
   @spec valid_candidate?(t()) :: boolean()
-  def valid_candidate?(%__MODULE__{} = t), do: alert_layout(t) != :no_render
+  def valid_candidate?(%__MODULE__{}), do: true
 
-  # Inputs/output are stored as a table for readability.
-  # Table is adapted from the one in the product spec: https://www.notion.so/mbta-downtown-crossing/DUP-Alert-Widget-Specification-a82acff850ed4f2eb98a04e5f3e0fe52
-  # Compile-time code converts each table row to a key-value pair in a map.
-  # PrimarySections refers to the number of sections in `primary_departures` of DUP config.
-  #
-  # Delay alerts are handled separately, see `delay_alert_layout` below.
-  # Layouts for rotations one and two are derived from the layout for rotation zero, see `non_delay_alert_layout` below.
-  layout_zero_table = """
-  # INPUTS                                                            || OUTPUT
-  # Effect          Location            AffectedLines PrimarySections || LayoutZero
-    station_closure inside              1             1                  full_screen
-    station_closure inside              1             2                  partial
-    station_closure inside              2             2                  full_screen
-  # "Boundary" location is not possible for station closures.
-    shuttle         inside              1             1                  full_screen
-    shuttle         inside              1             2                  partial
-    shuttle         inside              2             2                  full_screen
-    shuttle         boundary_upstream   1             1                  partial
-    shuttle         boundary_downstream 1             1                  partial
-    shuttle         boundary_upstream   1             2                  partial
-    shuttle         boundary_downstream 1             2                  partial
-    suspension      inside              1             1                  full_screen
-    suspension      inside              1             2                  partial
-    suspension      inside              2             2                  full_screen
-    suspension      boundary_upstream   1             1                  partial
-    suspension      boundary_downstream 1             1                  partial
-    suspension      boundary_upstream   1             2                  partial
-    suspension      boundary_downstream 1             2                  partial
-  """
+  # Determine the desired layout for this alert. Follows the rules documented here:
+  # https://www.notion.so/mbta-downtown-crossing/DUP-Alert-Widget-Specification-17cf5d8d11ea80399a7fe3c4f13a511f
+  @spec alert_layout(t()) :: :full_screen | :partial
+  defp alert_layout(%__MODULE__{rotation_index: :zero} = t), do: rotation_zero_layout(t)
 
-  @parameters_to_layout_zero layout_zero_table
-                             |> String.split("\n", trim: true)
-                             |> Enum.reject(&String.starts_with?(&1, "#"))
-                             |> Enum.into(%{}, fn line ->
-                               [
-                                 effect,
-                                 location,
-                                 affected_line_count,
-                                 primary_section_count,
-                                 layout_zero
-                               ] = String.split(line, ~r/\s+/, trim: true)
-
-                               # Convert strings to appropriate types
-                               [effect, location, layout_zero] =
-                                 Enum.map(
-                                   [effect, location, layout_zero],
-                                   &String.to_atom/1
-                                 )
-
-                               [affected_line_count, primary_section_count] =
-                                 Enum.map(
-                                   [affected_line_count, primary_section_count],
-                                   &String.to_integer/1
-                                 )
-
-                               parameters = %{
-                                 effect: effect,
-                                 location: location,
-                                 affected_line_count: affected_line_count,
-                                 primary_section_count: primary_section_count
-                               }
-
-                               {parameters, layout_zero}
-                             end)
-
-  defp get_layout_parameters(t) do
-    %{
-      effect: t.alert.effect,
-      location: LocalizedAlert.location(t),
-      affected_line_count: length(get_affected_lines(t)),
-      primary_section_count: length(t.screen.app_params.primary_departures.sections)
-    }
+  defp alert_layout(%__MODULE__{rotation_index: :one} = t) do
+    if eliminated_service_type(t) == :none, do: :partial, else: :full_screen
   end
 
-  @spec alert_layout(t()) :: :full_screen | :partial | :no_render
-  defp alert_layout(t) do
-    if t.alert.effect == :delay,
-      do: delay_alert_layout(t),
-      else: non_delay_alert_layout(t)
+  # When no secondary departures are configured, departures in this rotation will be the same as
+  # those in rotation zero, so use the same layout.
+  defp alert_layout(
+         %__MODULE__{
+           rotation_index: :two,
+           screen: %Screen{app_params: %Dup{secondary_departures: %Departures{sections: []}}}
+         } = t
+       ),
+       do: rotation_zero_layout(t)
+
+  defp alert_layout(%__MODULE__{rotation_index: :two}), do: :partial
+
+  defp rotation_zero_layout(t) do
+    if eliminated_service_type(t) == :all, do: :full_screen, else: :partial
   end
 
-  defp delay_alert_layout(t) do
-    delay_severity = t.alert.severity
-
-    if delay_severity >= 5 do
-      # All delays >= 20 minutes get a partial alert on all 3 rotations
-      :partial
-    else
-      # We don't show delays < 20 minutes
-      log_layout_mismatch(t, delay_severity)
-      :no_render
-    end
+  # Determine whether this alert "eliminates service" entirely, partially, or not at all, at the
+  # screen's station, for the routes in primary departures.
+  @spec eliminated_service_type(t()) :: :all | :some | :none
+  defp eliminated_service_type(
+         %__MODULE__{
+           alert: %Alert{effect: effect},
+           screen: %Screen{
+             app_params: %Dup{primary_departures: %Departures{sections: primary_sections}}
+           }
+         } = t
+       )
+       when effect in [:shuttle, :station_closure, :suspension] do
+    # Assume primary departures is always configured with a section for each subway line at the
+    # screen's station. So if we're `inside` a disruption and it affects the same number of lines
+    # as the number we show departures for, that means all service is eliminated. Otherwise, only
+    # some is (either we're at a boundary and there's still service in one direction, or we're at
+    # a transfer station and the alert only affects one line).
+    if LocalizedAlert.location(t) == :inside and
+         length(get_affected_lines(t)) == length(primary_sections),
+       do: :all,
+       else: :some
   end
 
-  defp non_delay_alert_layout(t) do
-    parameters = get_layout_parameters(t)
-
-    lookup_result = Map.fetch(@parameters_to_layout_zero, parameters)
-
-    case {t.rotation_index, lookup_result} do
-      {:zero, {:ok, layout_zero}} ->
-        # The first page's layout maps directly from `parameters`.
-        layout_zero
-
-      {:one, {:ok, _layout_zero}} ->
-        # The second page always uses full-screen layout, as long as the alert is relevant.
-        :full_screen
-
-      {:two, {:ok, layout_zero}} ->
-        # The third page's layout depends on whether or not the screen has secondary departures configured.
-        #
-        # If the screen has secondary departures configured, it always gets a partial alert.
-        # If the screen does not have secondary departures configured, it gets the same layout as the first page.
-        #
-        # This is because we do not want to hide secondary departures (e.g. bus, CR) if they exist.
-        has_secondary_departures = t.screen.app_params.secondary_departures.sections != []
-
-        if has_secondary_departures,
-          do: :partial,
-          else: layout_zero
-
-      {_rotation_index, :error} ->
-        # We don't know what to do under these conditions.
-        # Log it, and prevent the alert from appearing.
-        log_layout_mismatch(t, :not_applicable)
-        :no_render
-    end
-  end
-
-  # The widget _should_ always be valid given the constraints of what alerts
-  # we generate DUP alert widgets for, but in case one slips through, we
-  # should know about it!
-  defp log_layout_mismatch(t, delay_severity) do
-    Report.warning(
-      "dup_alert_no_matching_layout",
-      t
-      |> get_layout_parameters()
-      |> Map.merge(%{
-        delay_severity: delay_severity,
-        rotation_index: t.rotation_index,
-        alert_id: t.alert.id,
-        stop_id: t.screen.app_params.alerts.stop_id
-      })
-    )
-  end
+  defp eliminated_service_type(_other), do: :none
 
   def get_affected_lines(t) do
     t
