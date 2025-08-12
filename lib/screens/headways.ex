mbta/screens

commit 6986ffb877a8e5c3feb6f49ea0f23ea6e88ea5ce

chore: look up headway values by stop
Instead of manually configuring a "headway key" for every departures
section, we'd like to be able to translate stop IDs (which departures
already must be configured with) into headway keys.

Currently the mapping of "headway key" to stop IDs only exists
implicitly in the PA/ESS sign configuration in `realtime_signs`, and
then only for stations that have PA/ESS signs. So for now, we need to
encode this information ourselves within Screens.

lib/screens/headways.ex
@@ -1,0 +1,207 @@
+defmodule Screens.Headways do
+  @moduledoc """
+  Looks up headway values to be displayed on screens for stops with frequent service, for example
+  when no live departures are available ("Trains every X-Y minutes").
+  """
+
+  alias Screens.Routes.Route
+  alias Screens.SignsUiConfig.Cache, as: SignsUi
+  alias Screens.Stops.Stop
+
+  # Compact mapping of stop IDs to headway keys, leaning on the fact that subway stop IDs happen
+  # to be numeric and often contiguous as we "traverse" the line in a given direction.
+  @stops %{
+    blue_trunk: [70038..70060],
+    glx_medford: [70505..70514],
+    glx_union: [70503..70504],
+    green_b: [
+      70106..70107,
+      70110..70117,
+      70120..70121,
+      70124..70131,
+      70134..70135,
+      70144..70149,
+      170_136..170_137,
+      170_140..170_141
+    ],
+    green_c: [70211..70220, 70223..70238],
+    green_d: [70160..70183, 70186..70187],
+    green_e: [70239..70258, 70260..70260],
+    green_trunk: [70151..70159, 70196..70208, 70501..70502, 71150..71151],
+    mattapan_trunk: [70261..70261, 70263..70276],
+    orange_trunk: [70001..70036, 70278..70279],
+    red_ashmont: [70085..70094],
+    red_braintree: [70095..70105],
+    red_trunk: [70061..70061, 70063..70084]
+  }
+
+  # Mapping of parent station IDs to headway keys, for stations with a single unambiguous key.
+  # For brevity, omits the "place-" prefix which is currently common to all parent station IDs.
+  @stations %{
+    blue_trunk: ~w[wondl rbmnl bmmnl sdmnl orhte wimnl aport mvbcl aqucl bomnl],
+    glx_medford: ~w[mdftf balsq mgngl gilmn esomr],
+    glx_union: ~w[unsqu],
+    green_b: ~w[
+      bland
+      buest
+      bucen
+      amory
+      babck
+      brico
+      harvd
+      grigg
+      alsgr
+      wrnst
+      wascm
+      sthld
+      chswk
+      chill
+      sougr
+      lake
+    ],
+    green_c: ~w[
+      smary
+      hwsst
+      kntst
+      stpul
+      cool
+      sumav
+      bndhl
+      fbkst
+      bcnwa
+      tapst
+      denrd
+      engav
+      clmnl
+    ],
+    green_d: ~w[
+      fenwy
+      longw
+      bvmnl
+      brkhl
+      bcnfd
+      rsmnl
+      chhil
+      newto
+      newtn
+      eliot
+      waban
+      woodl
+      river
+    ],
+    green_e: ~w[
+      prmnl
+      symcl
+      nuniv
+      mfa
+      lngmd
+      brmnl
+      fenwd
+      mispk
+      rvrwy
+      bckhl
+      hsmnl
+    ],
+    green_trunk: ~w[lech spmnl boyls armnl coecl hymnl kencl],
+    orange_trunk: ~w[
+      ogmnl
+      mlmnl
+      welln
+      astao
+      sull
+      ccmnl
+      chncl
+      tumnl
+      bbsta
+      masta
+      rugg
+      rcmnl
+      jaksn
+      sbmnl
+      grnst
+      forhl
+    ],
+    red_ashmont: ~w[shmnl fldcr smmnl asmnl],
+    red_braintree: ~w[nqncy wlsta qnctr qamnl brntn],
+    red_trunk: ~w[alfcl davis portr harsq cntsq knncl chmnl sstat brdwy andrw jfk]
+  }
+
+  # Mapping of parent station and route IDs to headway keys, for parent stations which serve more
+  # than one line.
+  @multi_stations %{
+    blue_trunk: {~w[Blue], ~w[state gover]},
+    green_trunk: {~w[Green-B Green-C Green-D Green-E], ~w[north haecl gover pktrm]},
+    orange_trunk: {~w[Orange], ~w[north haecl state dwnxg]},
+    red_trunk: {~w[Red], ~w[pktrm dwnxg]}
+  }
+
+  @type range :: {low :: pos_integer(), high :: pos_integer()}
+
+  @doc """
+  Gets headway values for a stop. The stop should be a stopping location (`location_type == 0`).
+  Returns `nil` otherwise, or if no headways are configured for the stop.
+  """
+  @spec get(Stop.id()) :: range() | nil
+  @spec get(Stop.id(), DateTime.t()) :: range() | nil
+  def get(stop_id, at \\ DateTime.utc_now()) do
+    case headway_key(stop_id) do
+      nil -> nil
+      key -> key |> SignsUi.headways() |> Map.get(period(at))
+    end
+  end
+
+  @doc """
+  Gets headway values for a stop, with a route provided to disambiguate when the stop is a parent
+  station served by multiple routes.
+
+  ⚠️ Included for compatibility with existing DUP departures logic. Prefer `get/2`.
+  """
+  @callback get_with_route(Stop.id(), Route.id()) :: range() | nil
+  @callback get_with_route(Stop.id(), Route.id(), DateTime.t()) :: range() | nil
+  def get_with_route(stop_id, route_id, at \\ DateTime.utc_now()) do
+    case headway_key(stop_id, route_id) do
+      nil -> nil
+      key -> key |> SignsUi.headways() |> Map.get(period(at))
+    end
+  end
+
+  @spec headway_key(Stop.id()) :: SignsUi.headway_key() | nil
+  @spec headway_key(Stop.id(), Route.id()) :: SignsUi.headway_key() | nil
+  for {key, ranges} <- @stops, range <- ranges, stop_id <- range do
+    defp headway_key(unquote(to_string(stop_id))), do: unquote(to_string(key))
+    defp headway_key(unquote(to_string(stop_id)), _route_id), do: unquote(to_string(key))
+  end
+
+  for {key, stations} <- @stations, station <- stations do
+    defp headway_key("place-" <> unquote(station), _route_id), do: unquote(to_string(key))
+  end
+
+  for {key, {route_ids, stations}} <- @multi_stations,
+      route_id <- route_ids,
+      station <- stations do
+    defp headway_key("place-" <> unquote(station), unquote(route_id)), do: unquote(to_string(key))
+  end
+
+  defp headway_key(_stop_id), do: nil
+  defp headway_key(_stop_id, _route_id), do: nil
+
+  @spec period(DateTime.t()) :: :peak | :off_peak | :saturday | :sunday
+  defp period(datetime) do
+    local_dt = DateTime.shift_zone!(datetime, "America/New_York")
+
+    # Subtract 3 hours, since the service day starts/ends at 3:00 AM
+    day_of_week = local_dt |> DateTime.add(-3, :hour) |> Date.day_of_week()
+
+    time = {local_dt.hour, local_dt.minute}
+    am_peak? = time >= {7, 0} and time < {9, 0}
+    pm_peak? = time >= {16, 0} and time <= {18, 30}
+    peak_time? = am_peak? or pm_peak?
+
+    case {day_of_week, peak_time?} do
+      {6, _} -> :saturday
+      {7, _} -> :sunday
+      {_, true} -> :peak
+      {_, false} -> :off_peak
+    end
+  end
+end

test/screens/headways_test.exs
@@ -1,0 +1,49 @@
+defmodule Screens.HeadwaysTest do
+  use ExUnit.Case, async: true
+
+  alias Screens.Headways
+
+  # Headway values used in assertions come from: test/fixtures/signs_ui_config.json
+
+  @monday ~D[2024-11-04]
+  @zone "America/New_York"
+
+  defp local_dt(day_of_week \\ 1, hour \\ 12) do
+    # Note the default of Monday at 12:00 is in the `off_peak` period
+    @monday |> Date.add(day_of_week - 1) |> DateTime.new!(Time.new!(hour, 0, 0), @zone)
+  end
+
+  describe "get/2" do
+    @blue_trunk "70042"
+    @green_d "70160"
+    @red_ashmont "70088"
+
+    test "returns nil for a stop with no defined headways" do
+      assert Headways.get("nonexistent", local_dt()) == nil
+    end
+
+    test "returns nil for a parent station" do
+      assert Headways.get("place-north", local_dt()) == nil
+    end
+
+    test "returns the correct value for a given stop" do
+      assert Headways.get(@blue_trunk, local_dt()) == {9, 13}
+      assert Headways.get(@green_d, local_dt()) == {7, 13}
+      assert Headways.get(@red_ashmont, local_dt()) == {24, 32}
+    end
+
+    test "returns the correct value for a given time period" do
+      assert Headways.get(@blue_trunk, local_dt(1, 8)) == {5, 7}
+      assert Headways.get(@blue_trunk, local_dt(1, 17)) == {5, 7}
+      assert Headways.get(@blue_trunk, local_dt(6)) == {10, 14}
+      assert Headways.get(@blue_trunk, local_dt(7)) == {12, 16}
+    end
+  end
+
+  describe "get_with_route/3" do
+    test "returns the correct value for a combination of parent station and route" do
+      assert Headways.get_with_route("place-north", "Green-D", local_dt()) == {7, 13}
+      assert Headways.get_with_route("place-north", "Orange", local_dt()) == {9, 11}
+    end
+  end
+end

config/test.exs
@@ -165,6 +165,9 @@ config :screens, Screens.V2.ScreenData,
 
 config :screens, Screens.V2.CandidateGenerator.DupNew, stop_module: Screens.Stops.MockStop
 
+config :screens, Screens.V2.CandidateGenerator.Dup.Departures,
+  headways_module: Screens.MockHeadways
+
 config :screens, Screens.V2.RDS,
   departure_module: Screens.V2.MockDeparture,
   route_pattern_module: Screens.RoutePatterns.MockRoutePattern,

lib/screens/signs_ui_config/cache.ex
@@ -5,11 +5,12 @@ defmodule Screens.SignsUiConfig.Cache do
 
   use Screens.Cache.Client, table: :signs_ui_config
 
+  alias Screens.Headways
+
   @type entry :: {{:headways, headway_key()}, headway_values()}
 
   @type headway_key :: String.t()
-  @type headway_values :: %{optional(atom()) => time_range()}
-  @type time_range :: {low :: integer(), high :: integer()}
+  @type headway_values :: %{optional(atom()) => Headways.range()}
 
   @spec headways(headway_key()) :: headway_values()
   def headways(key) do

lib/screens/util.ex
@@ -3,26 +3,6 @@ defmodule Screens.Util do
 
   alias Screens.Config.Cache
 
-  @spec time_period(DateTime.t()) :: :peak | :off_peak | :saturday | :sunday
-  def time_period(utc_time) do
-    {:ok, dt} = DateTime.shift_zone(utc_time, "America/New_York")
-
-    # Subtract 3 hours, since the service day ends at 3 AM
-    day_of_week = dt |> DateTime.add(-3, :hour) |> Date.day_of_week()
-
-    t = {dt.hour, dt.minute}
-    am_rush? = t >= {7, 0} and t < {9, 0}
-    pm_rush? = t >= {16, 0} and t <= {18, 30}
-    rush_hour? = am_rush? or pm_rush?
-
-    case {day_of_week, rush_hour?} do
-      {6, _} -> :saturday
-      {7, _} -> :sunday
-      {_, true} -> :peak
-      {_, false} -> :off_peak
-    end
-  end
-
   @doc """
   Similar to Enum.group_by, except it returns a list of {key, value} tuples instead of a map to maintain order.
   Order of the groups is determined by the position of the first occurrence of a member of that group.

lib/screens/v2/candidate_generator/dup/departures.ex
@@ -5,7 +5,6 @@ defmodule Screens.V2.CandidateGenerator.Dup.Departures do
 
   alias Screens.Alerts.Alert
   alias Screens.Routes.Route
-  alias Screens.SignsUiConfig
   alias Screens.Util
   alias Screens.V2.CandidateGenerator.Widgets
   alias Screens.V2.Departure
@@ -13,9 +12,11 @@ defmodule Screens.V2.CandidateGenerator.Dup.Departures do
   alias Screens.V2.WidgetInstance.{DeparturesNoData, OvernightDepartures}
   alias ScreensConfig.Screen
   alias ScreensConfig.V2.Departures
-  alias ScreensConfig.V2.Departures.{Header, Headway, Layout, Query, Section}
+  alias ScreensConfig.V2.Departures.{Header, Layout, Query, Section}
   alias ScreensConfig.V2.Departures.Query.Params
   alias ScreensConfig.V2.Dup
+
+  @headways Application.compile_env(:screens, [__MODULE__, :headways_module], Screens.Headways)
 
   def departures_instances(
         %Screen{
@@ -138,7 +139,6 @@ defmodule Screens.V2.CandidateGenerator.Dup.Departures do
          %{
            departures: departures,
            alert_informed_entities: alert_informed_entities,
-           headway: headway,
            stop_ids: stop_ids,
            routes: routes,
            params: params
@@ -169,7 +169,7 @@ defmodule Screens.V2.CandidateGenerator.Dup.Departures do
         )
       end
 
-    headway_mode = get_headway_mode(stop_ids, headway, alert_informed_entities, now)
+    headway_mode = get_headway_mode(stop_ids, routes, alert_informed_entities, now)
 
     cond do
       # All routes in section are overnight
@@ -246,8 +246,7 @@ defmodule Screens.V2.CandidateGenerator.Dup.Departures do
   end
 
   defp get_section_data(
-         %Section{query: %Query{params: %Params{stop_ids: stop_ids} = params}, headway: headway} =
-           section,
+         %Section{query: %Query{params: %Params{stop_ids: stop_ids} = params}} = section,
          fetch_departures_fn,
          fetch_alerts_fn,
          fetch_routes_fn,
@@ -287,7 +286,6 @@ defmodule Screens.V2.CandidateGenerator.Dup.Departures do
           %{
             departures: section_departures,
             alert_informed_entities: alert_informed_entities,
-            headway: headway,
             stop_ids: stop_ids,
             routes: routes,
             params: params
@@ -346,12 +344,7 @@ defmodule Screens.V2.CandidateGenerator.Dup.Departures do
 
   defp get_headway_mode(_, _, [], _), do: :inactive
 
-  defp get_headway_mode(
-         stop_ids,
-         %Headway{headway_id: headway_id},
-         informed_entities,
-         current_time
-       ) do
+  defp get_headway_mode(stop_ids, routes, informed_entities, current_time) do
     # Use all informed_entities from relevant alerts to decide whether there's
     # any reason to go into headway mode.
     # For example, a NB suspension and SB shuttle from Aquarium shouldn't use headway mode
@@ -363,15 +356,14 @@ defmodule Screens.V2.CandidateGenerator.Dup.Departures do
         not (branch_station?(stop_ids) and branch_alert?(interpreted_alert))
 
     if headway_mode? do
-      headways = SignsUiConfig.Cache.headways(headway_id)
-      current_time_period = Screens.Util.time_period(current_time)
+      all_headways =
+        for stop_id <- stop_ids, %{id: route_id} <- routes do
+          @headways.get_with_route(stop_id, route_id, current_time)
+        end
 
-      case headways do
-        %{^current_time_period => {lo, hi}} ->
-          {:active, {lo, hi}, interpreted_alert.headsign}
-
-        _ ->
-          :inactive
+      case Enum.uniq(all_headways) do
+        [{lo, hi}] -> {:active, {lo, hi}, interpreted_alert.headsign}
+        _ -> :inactive
       end
     else
       :inactive

mix.exs
@@ -94,7 +94,7 @@ defmodule Screens.MixProject do
       {:telemetry_metrics, "~> 0.4"},
       {:screens_config,
        git: "https://github.com/mbta/screens-config-lib.git",
-       ref: "8ec6e1684a129b089edc5e867a32dfc90028b2e0"},
+       ref: "588dacf1bfaf7aa6356f0ee741bb4b49176834cc"},
       {:nebulex, "~> 2.6"},
       {:remote_ip, "~> 1.2"},
       {:hackney_telemetry, "~> 0.2.0"},

test/screens/util_test.exs
@@ -117,12 +117,4 @@ defmodule Screens.UtilTest do
       assert expected == get_service_date_today(now)
     end
   end
-
-  test "time_period" do
-    tz = "America/New_York"
-    assert DateTime.new!(~D[2020-03-20], ~T[18:00:00], tz) |> time_period() == :peak
-    assert DateTime.new!(~D[2020-03-21], ~T[02:00:00], tz) |> time_period() == :off_peak
-    assert DateTime.new!(~D[2020-03-21], ~T[08:00:00], tz) |> time_period() == :saturday
-    assert DateTime.new!(~D[2020-03-22], ~T[12:00:00], tz) |> time_period() == :sunday
-  end
 end

commit e873a60758b1656f3153fc39a924f95da8d72159

fix: DUP subway headways appearing in bus sections
When a DUP was configured with secondary bus departures, and on
the "boundary" of an active alert for the subway route in primary
departures, the secondary sections would incorrectly display headway
messages for the subway route instead of the (still present) bus
departures.

This issue was introduced in 6986ffb and caused by an incorrect
implementation of headway key lookups, where if a station didn't have
any transfers to other subway routes, it was assumed that the station
served no other routes at all (including e.g. bus routes).

This updates the behavior of headway lookups to be more conservative,
fixing the issue but with the side effect that sections not configured
with an explicit route ID filter will never show headways.

lib/screens/headways.ex
@@ -36,104 +36,85 @@ defmodule Screens.Headways do
     red_trunk: [70061..70061, 70063..70084]
   }
 
-  # Mapping of parent station IDs to headway keys, for stations with a single unambiguous key.
-  # For brevity, omits the "place-" prefix which is currently common to all parent station IDs.
+  # Mapping of parent station IDs and route IDs to headway keys. For brevity, omits the "place-"
+  # prefix which is currently common to all parent station IDs.
   @stations %{
-    blue_trunk: ~w[wondl rbmnl bmmnl sdmnl orhte wimnl aport mvbcl aqucl bomnl],
-    glx_medford: ~w[mdftf balsq mgngl gilmn esomr],
-    glx_union: ~w[unsqu],
-    green_b: ~w[
-      bland
-      buest
-      bucen
-      amory
-      babck
-      brico
-      harvd
-      grigg
-      alsgr
-      wrnst
-      wascm
-      sthld
-      chswk
-      chill
-      sougr
-      lake
-    ],
-    green_c: ~w[
-      smary
-      hwsst
-      kntst
-      stpul
-      cool
-      sumav
-      bndhl
-      fbkst
-      bcnwa
-      tapst
-      denrd
-      engav
-      clmnl
-    ],
-    green_d: ~w[
-      fenwy
-      longw
-      bvmnl
-      brkhl
-      bcnfd
-      rsmnl
-      chhil
-      newto
-      newtn
-      eliot
-      waban
-      woodl
-      river
-    ],
-    green_e: ~w[
-      prmnl
-      symcl
-      nuniv
-      mfa
-      lngmd
-      brmnl
-      fenwd
-      mispk
-      rvrwy
-      bckhl
-      hsmnl
-    ],
-    green_trunk: ~w[lech spmnl boyls armnl coecl hymnl kencl],
-    orange_trunk: ~w[
-      ogmnl
-      mlmnl
-      welln
-      astao
-      sull
-      ccmnl
-      chncl
-      tumnl
-      bbsta
-      masta
-      rugg
-      rcmnl
-      jaksn
-      sbmnl
-      grnst
-      forhl
-    ],
-    red_ashmont: ~w[shmnl fldcr smmnl asmnl],
-    red_braintree: ~w[nqncy wlsta qnctr qamnl brntn],
-    red_trunk: ~w[alfcl davis portr harsq cntsq knncl chmnl sstat brdwy andrw jfk]
-  }
-
-  # Mapping of parent station and route IDs to headway keys, for parent stations which serve more
-  # than one line.
-  @multi_stations %{
-    blue_trunk: {~w[Blue], ~w[state gover]},
-    green_trunk: {~w[Green-B Green-C Green-D Green-E], ~w[north haecl gover pktrm]},
-    orange_trunk: {~w[Orange], ~w[north haecl state dwnxg]},
-    red_trunk: {~w[Red], ~w[pktrm dwnxg]}
+    blue_trunk: {
+      ~w[Blue],
+      ~w[wondl rbmnl bmmnl sdmnl orhte wimnl aport mvbcl aqucl state gover bomnl]
+    },
+    glx_medford: {~w[Green-E], ~w[mdftf balsq mgngl gilmn esomr]},
+    glx_union: {~w[Green-D], ~w[unsqu]},
+    green_b: {
+      ~w[Green-B],
+      ~w[
+        bland
+        buest
+        bucen
+        amory
+        babck
+        brico
+        harvd
+        grigg
+        alsgr
+        wrnst
+        wascm
+        sthld
+        chswk
+        chill
+        sougr
+        lake
+      ]
+    },
+    green_c: {
+      ~w[Green-C],
+      ~w[smary hwsst kntst stpul cool sumav bndhl fbkst bcnwa tapst denrd engav clmnl]
+    },
+    green_d: {
+      ~w[Green-D],
+      ~w[fenwy longw bvmnl brkhl bcnfd rsmnl chhil newto newtn eliot waban woodl river]
+    },
+    green_e: {
+      ~w[Green-E],
+      ~w[prmnl symcl nuniv mfa lngmd brmnl fenwd mispk rvrwy bckhl hsmnl]
+    },
+    # Not all of these stations are served by all Green Line branches, but "trunk" is the most
+    # granular division of the Green Line we have to work with
+    green_trunk: {
+      ~w[Green-B Green-C Green-D Green-E],
+      ~w[lech spmnl north haecl gover pktrm boyls armnl coecl hymnl kencl]
+    },
+    orange_trunk: {
+      ~w[Orange],
+      ~w[
+        ogmnl
+        mlmnl
+        welln
+        astao
+        sull
+        ccmnl
+        north
+        haecl
+        state
+        dwnxg
+        chncl
+        tumnl
+        bbsta
+        masta
+        rugg
+        rcmnl
+        jaksn
+        sbmnl
+        grnst
+        forhl
+      ]
+    },
+    red_trunk: {
+      ~w[Red],
+      ~w[alfcl davis portr harsq cntsq knncl chmnl pktrm dwnxg sstat brdwy andrw jfk]
+    },
+    red_ashmont: {~w[Red], ~w[shmnl fldcr smmnl asmnl]},
+    red_braintree: {~w[Red], ~w[nqncy wlsta qnctr qamnl brntn]}
   }
 
   @type range :: {low :: pos_integer(), high :: pos_integer()}
@@ -173,11 +154,7 @@ defmodule Screens.Headways do
     defp headway_key(unquote(to_string(stop_id)), _route_id), do: unquote(to_string(key))
   end
 
-  for {key, stations} <- @stations, station <- stations do
-    defp headway_key("place-" <> unquote(station), _route_id), do: unquote(to_string(key))
-  end
-
-  for {key, {route_ids, stations}} <- @multi_stations,
+  for {key, {route_ids, stations}} <- @stations,
       route_id <- route_ids,
       station <- stations do
     defp headway_key("place-" <> unquote(station), unquote(route_id)), do: unquote(to_string(key))

test/screens/headways_test.exs
@@ -42,6 +42,7 @@ defmodule Screens.HeadwaysTest do
 
   describe "get_with_route/3" do
     test "returns the correct value for a combination of parent station and route" do
+      assert Headways.get_with_route("place-aport", "Blue", local_dt()) == {9, 13}
       assert Headways.get_with_route("place-north", "Green-D", local_dt()) == {7, 13}
       assert Headways.get_with_route("place-north", "Orange", local_dt()) == {9, 11}
     end