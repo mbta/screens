ed43f689855ab2d5b234929adc5cb74e347f84fa
Dean <deanshi@users.noreply.github.com>
2025-08-14 12:09:56 -0400

    Added Silver Line headway mapping to stop_ids (#2633)
    - Also added headway logic for parsing the stop_id and route_id into the headway key
    - Added tests for the new silver line headway logic

--- test/screens/headways_test.exs
+++ test/screens/headways_test.exs
@@ -17,6 +17,10 @@ defmodule Screens.HeadwaysTest do
     @blue_trunk "70042"
     @green_d "70160"
     @red_ashmont "70088"
+    @sl_one_terminal_a "17092"
+    @sl_two_drydock_ave "31259"
+    @sl_three_airport "7096"
+    @congress_st_at_wtc "17096"
 
     test "returns nil for a stop with no defined headways" do
       assert Headways.get("nonexistent", local_dt()) == nil
@@ -30,6 +34,9 @@ defmodule Screens.HeadwaysTest do
       assert Headways.get(@blue_trunk, local_dt()) == {9, 13}
       assert Headways.get(@green_d, local_dt()) == {7, 13}
       assert Headways.get(@red_ashmont, local_dt()) == {24, 32}
+      assert Headways.get(@sl_one_terminal_a, local_dt()) == {1, 3}
+      assert Headways.get(@sl_two_drydock_ave, local_dt()) == {1, 3}
+      assert Headways.get(@sl_three_airport, local_dt()) == {7, 9}
     end
 
     test "returns the correct value for a given time period" do
@@ -41,10 +48,19 @@ defmodule Screens.HeadwaysTest do
   end
 
   describe "get_with_route/3" do
-    test "returns the correct value for a combination of parent station and route" do
+    test "returns the correct value for a combination of parent station and route for Rail" do
       assert Headways.get_with_route("place-aport", "Blue", local_dt()) == {9, 13}
       assert Headways.get_with_route("place-north", "Green-D", local_dt()) == {7, 13}
       assert Headways.get_with_route("place-north", "Orange", local_dt()) == {9, 11}
+    end
+
+    test "returns the correct value for a combination of station id and route for the Silver Line" do
+      assert Headways.get_with_route("place-chels", "743", local_dt()) == {7, 9}
+      assert Headways.get_with_route("place-crtst", "746", local_dt()) == {1, 3}
+      assert Headways.get_with_route("place-crtst", "743", local_dt()) == {7, 9}
+      assert Headways.get_with_route(@congress_st_at_wtc, "742", local_dt()) == {1, 3}
+      assert Headways.get_with_route(@congress_st_at_wtc, "743", local_dt()) == {7, 9}
+      assert Headways.get_with_route("place-crtst", "751", local_dt()) == nil
     end
   end
 end
