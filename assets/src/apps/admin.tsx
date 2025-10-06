d7a3e84ba0208db3be3f521781f5ee9cf2ed5475
Cora Grant <cgrant2@mbta.com>
2025-10-06 17:22:23 -0400

    wip

--- assets/src/apps/admin.tsx
+++ assets/src/apps/admin.tsx
@@ -3,65 +3,38 @@ import "../../css/admin.scss";
 import { type ComponentType, StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter as Router, NavLink, Route, Routes } from "react-router";
-import weakKey from "weak-key";
 
-import {
-  AllScreensTable,
-  BusEinkScreensTable,
-  GLEinkScreensTable,
-  BuswayScreensTable,
-  BusShelterScreensTable,
-  PreFareScreensTable,
-  DupScreensTable,
-  ElevatorScreensTable,
-} from "Components/admin/admin_tables";
+import ScreensTable from "Components/admin/table";
 import AdminScreenConfigForm from "Components/admin/admin_screen_config_form";
 import ImageManager from "Components/admin/admin_image_manager";
 import Devops from "Components/admin/devops";
 import Inspector from "Components/admin/inspector";
 import Tools from "Components/admin/tools";
 
-const routes: [string, string, ComponentType][][] = [
-  [["inspector", "🔍", Inspector]],
-  [["", "All Screens", AllScreensTable]],
-  [
-    ["bus-eink-screens", "Bus E-ink", BusEinkScreensTable],
-    ["bus-shelter-screens", "Bus Shelter", BusShelterScreensTable],
-    ["dup-screens", "DUP", DupScreensTable],
-    ["elevator-screens", "Elevator", ElevatorScreensTable],
-    ["gl-eink-screens", "GL E-ink", GLEinkScreensTable],
-    ["pre-fare-screens", "Pre-Fare", PreFareScreensTable],
-    ["busway-screens", "Sectional", BuswayScreensTable],
-  ],
-  [
-    ["screens-json-editor", "Config Editor", AdminScreenConfigForm],
-    ["devops", "Devops", Devops],
-    ["image-manager", "Image Manager", ImageManager],
-    ["tools", "Tools", Tools],
-  ],
+const routes: [string, string, ComponentType][] = [
+  ["inspector", "🔍", Inspector],
+  ["", "Screens Table", ScreensTable],
+  ["screens-json-editor", "JSON Editor", AdminScreenConfigForm],
+  ["devops", "Devops", Devops],
+  ["image-manager", "Image Manager", ImageManager],
+  ["tools", "Tools", Tools],
 ];
 
 const App = (): JSX.Element => {
   return (
     <Router basename="admin">
       <div className="admin-navbar">
-        {routes.map((group) => (
-          <div key={weakKey(group)} className="admin-navbar__group">
-            {group.map(([path, label]) => (
-              <NavLink to={path} key={path}>
-                {label}
-              </NavLink>
-            ))}
-          </div>
+        {routes.map(([path, label]) => (
+          <NavLink to={path} key={path}>
+            {label}
+          </NavLink>
         ))}
       </div>
 
       <Routes>
-        {routes.map((group) =>
-          group.map(([path, , Component]) => (
-            <Route path={path} key={path} element={<Component />} />
-          )),
-        )}
+        {routes.map(([path, , Component]) => (
+          <Route path={path} key={path} element={<Component />} />
+        ))}
       </Routes>
     </Router>
   );
