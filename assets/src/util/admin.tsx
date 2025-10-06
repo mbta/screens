5c98cb3a8db54afefdf5270118924ad3e28f8eb3
Cora Grant <cgrant2@mbta.com>
2025-10-06 17:22:23 -0400

    wip

--- assets/src/util/admin.tsx
+++ assets/src/util/admin.tsx
@@ -10,14 +10,40 @@ export type Config = {
 };
 
 export type Screen = {
-  app_id: string;
+  app_id: AppId;
   app_params: JSONObject;
   device_id: string | null;
   disabled: boolean;
   hidden_from_screenplay: boolean;
   name: string;
+  location: string;
+  tags: string[];
   vendor: string;
 };
+
+export type ScreenWithId = { id: string; config: Screen };
+
+export const SCREEN_APPS = {
+  bus_eink_v2: { name: "Bus E-ink", hasAudio: true, variants: [] },
+  bus_shelter_v2: { name: "Bus Shelter", hasAudio: true, variants: [] },
+  busway_v2: { name: "Sectional", hasAudio: true, variants: [] },
+  dup_v2: { name: "DUP", hasAudio: false, variants: ["new_departures"] },
+  elevator_v2: { name: "Elevator", hasAudio: false, variants: [] },
+  gl_eink_v2: { name: "GL E-ink", hasAudio: true, variants: [] },
+  pre_fare_v2: { name: "Pre-Fare", hasAudio: true, variants: [] },
+} as const;
+
+export type AppId = keyof typeof SCREEN_APPS;
+
+export const SCREEN_VENDORS = [
+  "c3ms",
+  "gds",
+  "lg_mri",
+  "mercury",
+  "mimo",
+  "outfront",
+  "solari",
+];
 
 /**
  * Set of attributes for forms and form elements which disable "auto" browser
