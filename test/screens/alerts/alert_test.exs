mbta/screens

commit 31bd15d596ec6fbd33d69bf775e1f504404a25cf

fix: interpreting alerts as directional
`Alert.direction_id/1` would incorrectly return a non-nil value if
there happened to be any stopping locations (which would always have
a non-nil direction) ahead of the first parent station in the informed
entities. Since entities are never explicitly sorted, this could
sometimes be the case and sometimes not, even for the same alert.

The immediate fix is to only consider parent stations, though this
approach is still flawed, as is the entire notion that one can assign a
single "direction" to an alert. It should work well enough for how
alert data is currently structured and the kinds of alerts we are
likely to see, though.

lib/screens/alerts/alert.ex
@@ -389,8 +389,13 @@ defmodule Screens.Alerts.Alert do
     )
   end
 
-  def direction_id(%__MODULE__{informed_entities: informed_entities}),
-    do: List.first(informed_entities).direction_id
+  @doc """
+  Very imperfectly determine whether an alert only affects one direction of service. Assumes this
+  is the case if any affected parent station is only affected in one direction.
+  """
+  @spec direction_id(t()) :: Trip.direction() | nil
+  def direction_id(%__MODULE__{informed_entities: entities}),
+    do: Enum.find(entities, &InformedEntity.parent_station?/1).direction_id
 
   def informed_parent_stations(%__MODULE__{
         informed_entities: informed_entities

test/screens/alerts/alert_test.exs
@@ -290,4 +290,64 @@ defmodule Screens.Alerts.AlertTest do
       assert :error == Alert.fetch_by_stop_and_route(stop_ids, route_ids, x_get_json_fn2)
     end
   end
+
+  describe "direction_id/1" do
+    test "returns nil when all informed parent stations are affected in both directions" do
+      alert = %Alert{
+        informed_entities: [
+          %{
+            activities: ~w[board exit ride]a,
+            direction_id: 0,
+            facility: nil,
+            route: "1",
+            route_type: nil,
+            stop: "12345"
+          },
+          %{
+            activities: ~w[board exit ride]a,
+            direction_id: nil,
+            facility: nil,
+            route: "1",
+            route_type: nil,
+            stop: "place-a"
+          }
+        ]
+      }
+
+      assert Alert.direction_id(alert) == nil
+    end
+
+    test "returns the direction ID of the first directionally-informed parent station" do
+      alert = %Alert{
+        informed_entities: [
+          %{
+            activities: ~w[board exit ride]a,
+            direction_id: 0,
+            facility: nil,
+            route: "1",
+            route_type: nil,
+            stop: "12345"
+          },
+          %{
+            activities: ~w[board exit ride]a,
+            direction_id: 1,
+            facility: nil,
+            route: "1",
+            route_type: nil,
+            stop: "place-a"
+          },
+          %{
+            activities: ~w[board exit ride]a,
+            direction_id: 0,
+            facility: nil,
+            route: "1",
+            route_type: nil,
+            stop: "place-b"
+          }
+        ]
+      }
+
+      assert Alert.direction_id(alert) == 1
+    end
+  end
 end

commit 0e3c6f30fe58484850bf04c27157bc126450ac57

fix: Pre-Fare crash on whole-route alerts
The fix to "alert direction" logic in 31bd15d introduced a regression
when dealing with whole-route alerts, or in general, alerts that do not
affect any specific parent stations. Improve this logic so it does not
crash and gives the answer we want in more scenarios.

lib/screens/alerts/alert.ex
@@ -391,11 +391,27 @@ defmodule Screens.Alerts.Alert do
 
   @doc """
   Very imperfectly determine whether an alert only affects one direction of service. Assumes this
-  is the case if any affected parent station is only affected in one direction.
+  is the case if the alert affects a whole direction of some route, or it affects parent stations
+  all in the same direction.
   """
   @spec direction_id(t()) :: Trip.direction() | nil
-  def direction_id(%__MODULE__{informed_entities: entities}),
-    do: Enum.find(entities, &InformedEntity.parent_station?/1).direction_id
+  def direction_id(%__MODULE__{informed_entities: entities}) do
+    direction_from_whole_direction_entities =
+      Enum.find_value(entities, fn %{direction_id: direction_id} = entity ->
+        if InformedEntity.whole_direction?(entity), do: direction_id
+      end)
+
+    direction_from_parent_station_entities =
+      case entities
+           |> Enum.filter(&InformedEntity.parent_station?/1)
+           |> Enum.map(& &1.direction_id)
+           |> Enum.uniq() do
+        [direction_id] when not is_nil(direction_id) -> direction_id
+        _other -> nil
+      end
+
+    direction_from_whole_direction_entities || direction_from_parent_station_entities
+  end
 
   def informed_parent_stations(%__MODULE__{
         informed_entities: informed_entities

test/screens/alerts/alert_test.exs
@@ -317,7 +317,9 @@ defmodule Screens.Alerts.AlertTest do
       assert Alert.direction_id(alert) == nil
     end
 
-    test "returns the direction ID of the first directionally-informed parent station" do
+    test "returns nil when informed parent stations have a mix of directions" do
+      # Typical disruption with "endpoints" where only one direction of service is affected and
+      # a "middle" where both directions are
       alert = %Alert{
         informed_entities: [
           %{
@@ -326,11 +328,70 @@ defmodule Screens.Alerts.AlertTest do
             facility: nil,
             route: "1",
             route_type: nil,
-            stop: "12345"
+            stop: "place-a"
+          },
+          %{
+            activities: ~w[board exit ride]a,
+            direction_id: nil,
+            facility: nil,
+            route: "1",
+            route_type: nil,
+            stop: "place-b"
           },
           %{
             activities: ~w[board exit ride]a,
             direction_id: 1,
+            facility: nil,
+            route: "1",
+            route_type: nil,
+            stop: "place-c"
+          }
+        ]
+      }
+
+      assert Alert.direction_id(alert) == nil
+    end
+
+    test "returns nil when informed parent stations all have different non-nil directions" do
+      # Scenario where the disruption has "endpoints" but they're adjacent stations
+      alert = %Alert{
+        informed_entities: [
+          %{
+            activities: ~w[board exit ride]a,
+            direction_id: 0,
+            facility: nil,
+            route: "1",
+            route_type: nil,
+            stop: "place-a"
+          },
+          %{
+            activities: ~w[board exit ride]a,
+            direction_id: 1,
+            facility: nil,
+            route: "1",
+            route_type: nil,
+            stop: "place-b"
+          }
+        ]
+      }
+
+      assert Alert.direction_id(alert) == nil
+    end
+
+    test "returns the direction ID when all parent stations are affected in the same direction" do
+      alert = %Alert{
+        informed_entities: [
+          %{
+            activities: ~w[board exit ride]a,
+            direction_id: nil,
+            facility: nil,
+            route: "1",
+            route_type: nil,
+            stop: "12345"
+          },
+          %{
+            activities: ~w[board exit ride]a,
+            direction_id: 0,
             facility: nil,
             route: "1",
             route_type: nil,
@@ -343,6 +404,40 @@ defmodule Screens.Alerts.AlertTest do
             route: "1",
             route_type: nil,
             stop: "place-b"
+          }
+        ]
+      }
+
+      assert Alert.direction_id(alert) == 0
+    end
+
+    test "returns nil when there are no specific affected stops and no affected direction" do
+      alert = %Alert{
+        informed_entities: [
+          %{
+            activities: ~w[board exit ride]a,
+            direction_id: nil,
+            facility: nil,
+            route: "1",
+            route_type: nil,
+            stop: nil
+          }
+        ]
+      }
+
+      assert Alert.direction_id(alert) == nil
+    end
+
+    test "returns the direction ID when a whole direction is affected" do
+      alert = %Alert{
+        informed_entities: [
+          %{
+            activities: ~w[board exit ride]a,
+            direction_id: 1,
+            facility: nil,
+            route: "1",
+            route_type: nil,
+            stop: nil
           }
         ]
       }