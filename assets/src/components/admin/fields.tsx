a37fa7bcf31b81a8a870652609d0f0ff39d3b84b
Cora Grant <cgrant2@mbta.com>
2025-11-04 11:50:02 -0500

    wip: extract

---
+++ assets/src/components/admin/fields.tsx
@@ -0,0 +1,50 @@
+import { type AppId, SCREEN_APPS, SCREEN_VENDORS } from "Util/admin";
+
+import {
+  type Cell,
+  buildSelectInput,
+  CheckboxInput,
+  InspectorLink,
+  JsonInput,
+  StringInput,
+} from "./fields/cells";
+
+import {
+  type Filter,
+  BooleanFilter,
+  buildSelectFilter,
+  StringFilter,
+} from "./fields/filters";
+
+type Field = {
+  label: string;
+  path: string;
+  cell: Cell;
+  filter?: Filter;
+};
+
+const filteredBoolean = { cell: CheckboxInput, filter: BooleanFilter };
+const filteredString = { cell: StringInput, filter: StringFilter };
+
+const baseFields: Field[] = [
+  { label: "ID", path: "id", cell: InspectorLink },
+  {
+    label: "App",
+    path: "app_id",
+    cell: ({ value }) => SCREEN_APPS[value as AppId].name,
+  },
+  { label: "Name", path: "name", ...filteredString },
+  { label: "Location", path: "location", ...filteredString },
+  {
+    label: "Vendor",
+    path: "vendor",
+    cell: buildSelectInput([null, ...SCREEN_VENDORS]),
+    filter: buildSelectFilter(SCREEN_VENDORS),
+  },
+  { label: "Device ID", path: "device_id", ...filteredString },
+  { label: "Disabled?", path: "disabled", ...filteredBoolean },
+  { label: "Hidden?", path: "hidden_from_screenplay", ...filteredBoolean },
+  { label: "App Params", path: "app_params", cell: JsonInput },
+];
+
+export { baseFields };
