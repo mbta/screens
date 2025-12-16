b670d39de93b29acc380e5feebdd9239c731d346
Brett Heath-Wlaz <bheathwlaz@mbta.com>
2025-12-16 12:12:00 -0500

    implement new layout

--- assets/src/components/subway_status/lcd_subway_status.tsx
+++ assets/src/components/subway_status/lcd_subway_status.tsx
@@ -1,5 +1,6 @@
 import { type ComponentType } from "react";
-import { classWithModifier, classWithModifiers } from "Util/utils";
+import * as fp from "lodash/fp";
+import { classWithModifier } from "Util/utils";
 import {
   Alert,
   ExtendedSection,
@@ -7,8 +8,6 @@ import {
   SubwayStatusData,
   SubwayStatusPill,
   adjustAlertForContractedStatus,
-  isContracted,
-  isContractedWith1Alert,
   isExtended,
   useSubwayStatusTextResizer,
 } from "./subway_status_common";
@@ -22,38 +21,30 @@ const LcdSubwayStatus: ComponentType<SubwayStatusData> = (props) => {
 
   return (
     <div className="subway-status">
-      <LineStatus section={blue} showRule />
-      <LineStatus section={orange} showRule />
-      <LineStatus section={red} showRule />
-      <LineStatus section={green} showRule={shouldShowLastRule(props)} />
+      {showHeader([blue, orange, red, green]) && (
+        <div className="subway-status__header">Subway Status</div>
+      )}
+      <LineStatus section={blue} />
+      <LineStatus section={orange} />
+      <LineStatus section={red} />
+      <LineStatus section={green} />
     </div>
   );
 };
 
-interface WithRule {
-  showRule: boolean;
-}
+type LineStatusProps = { section: Section };
 
-type LineStatusProps = { section: Section } & WithRule;
-
-const LineStatus: ComponentType<LineStatusProps> = ({ section, showRule }) => {
+const LineStatus: ComponentType<LineStatusProps> = ({ section }) => {
   const alerts =
     section.type === "contracted"
       ? section.alerts.map(adjustAlertForContractedStatus)
       : [section.alert];
 
   return (
-    <div
-      className={classWithModifiers("subway-status_status", [
-        section.type,
-        alerts.length === 1 ? "one-alert" : "two-alerts",
-        alerts?.[1]?.route_pill && "has-second-pill",
-      ])}
-    >
+    <div className="subway-status__line">
       {alerts.map((alert, index) => (
         <BasicAlert alert={alert} type={section.type} key={index} />
       ))}
-      {showRule && <div className="subway-status_status_rule" />}
     </div>
   );
 };
@@ -66,49 +57,27 @@ interface BasicAlertProps {
 }
 
 const BasicAlert = ({ alert, type }: BasicAlertProps) => {
-  const { ref, status, location, isLastStep } = useSubwayStatusTextResizer(
-    alert,
-    type,
-  );
-
-  const containerClassName = classWithModifier(
-    "subway-status_alert",
-    alert.route_pill ? "has-pill" : "no-pill",
-  );
-
-  const sizerClassName = classWithModifier(
-    "subway-status_alert-sizer",
-    isLastStep && "hide-overflow",
-  );
-
-  const textContainerClassName = classWithModifiers(
-    "subway-status_alert_text-container",
-    [
-      isLastStep && "hide-overflow",
-      alert.route_pill?.branches &&
-        `${alert.route_pill.branches.length}-branches`,
-    ],
-  );
-
-  const statusTextClassName = classWithModifier(
-    "subway-status_alert_status-text",
-    status === NORMAL_STATUS && "normal-service",
-  );
+  const { ref, status, location } = useSubwayStatusTextResizer(alert, type);
 
   return (
-    <div className={containerClassName}>
-      <div className={sizerClassName} ref={ref}>
-        <div className="subway-status_alert_route-pill-container">
-          {alert.route_pill && <RoutePill routePill={alert.route_pill} />}
-        </div>
-        <div className={textContainerClassName}>
-          {status && <span className={statusTextClassName}>{status}</span>}
-          {location && (
-            <span className="subway-status_alert_location-text">
-              {location}
-            </span>
-          )}
-        </div>
+    <div className="subway-status__alert" ref={ref}>
+      <div className="subway-status__pill-container">
+        {alert.route_pill && <RoutePill routePill={alert.route_pill} />}
+      </div>
+      <div className={classWithModifier("subway-status__text-container", type)}>
+        {status && (
+          <span
+            className={classWithModifier(
+              "subway-status__status-text",
+              status === NORMAL_STATUS && "normal",
+            )}
+          >
+            {status}
+          </span>
+        )}
+        {location && (
+          <span className="subway-status__location-text">{location}</span>
+        )}
       </div>
     </div>
   );
@@ -155,33 +124,20 @@ const convertLocationlessExtendedAlertToContracted = (
     ? { type: "contracted", alerts: [section.alert] }
     : section;
 
-/**
- * Determines whether we should show the last rule, below the Green Line section.
- *
- * We show the last rule as long as the last pill in the pill column is no lower
- * than where it would be if all statuses were normal.
- *
- * This is the case when the two following conditions are met:
- * - The first 3 sections (BL, OL, RL) are contracted with 1 alert (or normal status)
- * - The GL section is *either*:
- *   - extended, *or*
- *   - contracted without a separate pill on its second alert (or just no second alert)
- *
- * In all other cases, we do not show the last rule.
- */
-const shouldShowLastRule = ({ blue, orange, red, green }: SubwayStatusData) => {
-  const firstThree = [blue, orange, red];
-
-  const firstThreeContracted = firstThree.every(isContractedWith1Alert);
-  const glExtended = isExtended(green);
-  const glContractedWithNoSecondPill =
-    isContracted(green) && !green.alerts?.[1]?.route_pill;
-
-  return firstThreeContracted && (glExtended || glContractedWithNoSecondPill);
-};
-
 const isExtendedWithNoLocation = (
   section: Section,
 ): section is ExtendedSection => isExtended(section) && !section.alert.location;
+
+const showHeader = (sections: Section[]) => {
+  // Note: This computation is tied to the CSS, and may need to change when it does.
+  const height = fp.sumBy((section) => {
+    if (section.type === "contracted") {
+      return section.alerts.length === 1 ? 120 : 216;
+    } else {
+      return 160;
+    }
+  }, sections);
+  return height < 530;
+};
 
 export default LcdSubwayStatus;
