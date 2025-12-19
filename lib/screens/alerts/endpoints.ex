29dad616486b6052edc3046cf3fe1ba741740f8b
robbie.sundstrom <rsundstrom@mbta.com>
2025-12-18 23:12:21 -0500

    add specs to endpoints.ex

--- lib/screens/alerts/endpoints.ex
+++ lib/screens/alerts/endpoints.ex
@@ -5,14 +5,16 @@ defmodule Screens.Alerts.Endpoints do
 
   alias Screens.Alerts.InformedEntity
   alias Screens.Routes.Route
+  alias Screens.Stops.Stop
   alias Screens.Stops.Subway
   alias Screens.V2.WidgetInstance.SubwayStatus.Serialize.Utils
 
   @typep direction_id_t() :: 0 | 1
   @typep direction_name_map_t() :: %{forward: String.t(), backward: String.t()}
-  @type endpoints :: {{String.t(), String.t()}, {String.t(), String.t()}}
+  @type endpoints_t :: %{:abbrev => String.t(), :full => String.t()}
+  @typep traversal_fun_t() :: (UnrootedPolytree.Edges.t() -> [UnrootedPolytree.Node.t()])
 
-  @spec get([InformedEntity.t()], Route.id()) :: Subway.station()
+  @spec get([InformedEntity.t()], Route.id()) :: endpoints_t() | nil
   def get(informed_entities, route_id) do
     case informed_entities
          |> Subway.affected_stops_on_route(route_id)
@@ -53,8 +55,8 @@ defmodule Screens.Alerts.Endpoints do
   # unique stop, then that means that stop is the unambiguous endpoint
   # in that direction - if there are multiple, then there is no
   # unambiguous endpoint in that direction.
-  defp to_endpoints(nil, _direction_names), do: nil
-
+  @spec to_endpoints([[Subway.station()]], direction_name_map_t()) ::
+          {Subway.station_names(), Subway.station_names()}
   defp to_endpoints(stop_lists, direction_names) do
     stop_tree =
       stop_lists
@@ -83,12 +85,32 @@ defmodule Screens.Alerts.Endpoints do
     end
   end
 
-  @spec tuple_for_direction_name(String.t()) :: {String.t(), String.t()}
+  @spec direction_names_for_route(Route.id(), direction_id_t()) :: direction_name_map_t()
+  defp direction_names_for_route(route_id, direction_id) do
+    Utils.route_directions()
+    |> Map.get(route_id)
+    |> to_forward_backward_direction_map(direction_id)
+  end
+
+  # Given an alert, returns the affected direction ID, if there is one
+  # provided in the alert. If there are multiple, returns one at
+  # random. If there aren't any, defaults to 0.
+  @spec affected_direction_id([InformedEntity.t()]) :: direction_id_t()
+  defp affected_direction_id(entities) do
+    entities
+    |> Enum.reject(&Kernel.is_nil/1)
+    |> List.first(0)
+    |> Map.get("direction_id", 0)
+  end
+
+  # Given a direction, constructs full and abbreviated naming
+  @spec tuple_for_direction_name(String.t()) :: Subway.station_names()
   defp tuple_for_direction_name(direction_name), do: {direction_name <> " Stops", direction_name}
 
   # Traverses an UnrootedPolytree of stops using the `previous` field
   # on each node in order to traverse backwards to the first affected
   # stop. See `traverse_from_nodes/3` for more info.
+  @spec first_stops(UnrootedPolytree.t()) :: [Subway.station_names()]
   defp first_stops(stop_tree) do
     stop_tree
     |> traverse_from_nodes(stop_tree.starting_nodes, & &1.previous)
@@ -98,7 +120,7 @@ defmodule Screens.Alerts.Endpoints do
   # Traverses an UnrootedPolytree of stops using the `next` field on
   # each node in order to traverse forwards to the last affected
   # stop. See `traverse_from_nodes/3` for more info.
-  @spec last_stops(UnrootedPolytree.t()) :: []
+  @spec last_stops(UnrootedPolytree.t()) :: [Subway.station_names()]
   defp last_stops(stop_tree) do
     stop_tree
     |> traverse_from_nodes(stop_tree.starting_nodes, & &1.next)
@@ -112,6 +134,8 @@ defmodule Screens.Alerts.Endpoints do
   # It de-duplicates identical node ID's, so even if there are two
   # branches, if they arrive at the same stop in the end, then it only
   # returns that node once.
+  @spec traverse_from_nodes(UnrootedPolytree.t(), [Stop.id()], traversal_fun_t()) ::
+          [UnrootedPolytree.Node.t()]
   defp traverse_from_nodes(unrooted_polytree, node_ids, traversal_fun) do
     node_ids
     |> Enum.flat_map(&(unrooted_polytree |> traverse_from_node(&1, traversal_fun)))
@@ -120,6 +144,8 @@ defmodule Screens.Alerts.Endpoints do
 
   # Helper function used by `traverse_from_nodes/3` to traverse an
   # UnrootedPolytree from a single node.
+  @spec traverse_from_node(UnrootedPolytree.t(), Stop.id(), traversal_fun_t()) ::
+          [UnrootedPolytree.Node.t()]
   defp traverse_from_node(unrooted_polytree, node_id, traversal_fun) do
     unrooted_polytree
     |> UnrootedPolytree.edges_for_id(node_id)
@@ -138,12 +164,6 @@ defmodule Screens.Alerts.Endpoints do
     end
   end
 
-  defp direction_names_for_route(route_id, direction_id) do
-    Utils.route_directions()
-    |> Map.get(route_id)
-    |> to_forward_backward_direction_map(direction_id)
-  end
-
   # Converts a route's direction map into an
   # orientation-aware %{forward: _, backward: _} direction map, based
   # on the direction ID provided.
@@ -154,15 +174,4 @@ defmodule Screens.Alerts.Endpoints do
 
   defp to_forward_backward_direction_map(direction_names, 1),
     do: %{forward: Enum.at(direction_names, 1), backward: Enum.at(direction_names, 0)}
-
-  # Given an alert, returns the affected direction ID, if there is one
-  # provided in the alert. If there are multiple, returns one at
-  # random. If there aren't any, defaults to 0.
-  # @spec affected_direction_id(InformedEntity.t()) :: direction_id_t()
-  defp affected_direction_id(entities) do
-    entities
-    |> Enum.reject(&Kernel.is_nil/1)
-    |> List.first(0)
-    |> Map.get("direction_id", 0)
-  end
 end
