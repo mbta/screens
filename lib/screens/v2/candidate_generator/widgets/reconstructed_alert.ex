fcc575395018b77839d256ff4091e46bf32782cb
robbie.sundstrom <rsundstrom@mbta.com>
2026-03-23 13:59:47 -0400

    fix misspelling

--- lib/screens/v2/candidate_generator/widgets/reconstructed_alert.ex
+++ lib/screens/v2/candidate_generator/widgets/reconstructed_alert.ex
@@ -169,7 +169,7 @@ defmodule Screens.V2.CandidateGenerator.Widgets.ReconstructedAlert do
     with [informed_parent_station] <- Alert.informed_parent_stations(alert),
          platforms <- fetch_subway_platforms_for_stop_fn.(informed_parent_station.stop.id),
          :partial_closure <- Alert.station_closure_type(alert, platforms) do
-      infomed_stop_ids =
+      informed_stop_ids =
         Enum.map(informed_entities, fn
           %InformedEntity{stop: %Stop{id: stop_id}} -> stop_id
           _ -> nil
