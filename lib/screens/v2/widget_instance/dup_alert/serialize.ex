1370442a184e60ae4c41af530307286c3a2c7881
robbie.sundstrom <rsundstrom@mbta.com>
2026-03-27 16:25:50 -0400

    PR comment to use intersection of MapSet

--- lib/screens/v2/widget_instance/dup_alert/serialize.ex
+++ lib/screens/v2/widget_instance/dup_alert/serialize.ex
@@ -239,10 +239,11 @@ defmodule Screens.V2.WidgetInstance.DupAlert.Serialize do
       t
       |> DupAlert.child_stops_for_affected_line()
       |> Enum.map(& &1.id)
+      |> MapSet.new()
 
     platform_names =
       t.alert.informed_entities
-      |> Enum.filter(&(&1.stop.id in child_stop_ids and &1.stop.platform_name))
+      |> Enum.filter(&(MapSet.member?(child_stop_ids, &1.stop.id) and &1.stop.platform_name))
       |> Enum.map(& &1.stop.platform_name)
 
     case platform_names do
