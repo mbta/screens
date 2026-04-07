mbta/screens

commit 8fad02c6fe10b31b39f0bbfc0f3603e3ddf69ad4

chore: centralize screen request handling/logging
== Motivation ==

We'd like to improve the metadata included in HTTP request logs, to get
a better sense of overall application performance. A specific question
that's difficult to answer with current logging is: "How long does an
average data request take to produce, broken down by screen type?"

An obstacle to adding this metadata is that prior to this change there
is no single, central location where parameters for the various types
of requests are translated into a screen configuration (the data
structure that would tell us, among other things, what type of screen
we are processing a request for). Screen configs and request parameters
are loaded and processed ad-hoc in different ways across different
controllers/actions, and sometimes the same screen config is loaded
from cache more than once in the same request, by different modules
(the existing `LogScreenData` module being one of these).

== Changes ==

* Introduce `Plug.ScreenRequest`, a one-stop shop for loading a screen
  config and setting up all assigns needed to handle a "screen request"
  generally. This also pulls out some values we are interested in and
  sets them as logger metadata.

* The above gives us everything `LogScreenData` used to provide and
  more, but in a format different from what existing Splunk alerts and
  dashboards expect. Cordon off the old format in a separate module,
  `Plug.LegacyLogging`, which can be straightforwardly deleted once we
  start recognizing the new format in Splunk.

* Refactor controllers to directly use the screen config and parameters
  assigned by `Plug.ScreenRequest`.

* Refactor `ScreenData` and `ScreenAudioData` to accept a screen config
  instead of a screen ID, allowing this to be passed in from controller
  actions that already have it via `Plug.ScreenRequest`.

* In `ScreenApiController`, handle "disabled" and "outdated" responses
  using inline plugs, simplifying the code for each individual action.

lib/screens/v2/screen_data.ex
@@ -11,87 +11,68 @@ defmodule Screens.V2.ScreenData do
   import Screens.Inject
   import Screens.V2.Template.Guards, only: [is_slot_id: 1, is_paged_slot_id: 1]
 
-  @config_cache injected(Screens.Config.Cache)
   @parameters injected(Screens.V2.ScreenData.Parameters)
 
   @type t :: %{type: atom()}
   @type simulation_data :: %{full_page: t(), flex_zone: [t()]}
   @type variants(data) :: {data, %{String.t() => data}}
-  @type screen_id :: String.t()
   @type options :: [
-          generator_variant: String.t(),
-          pending_config: Screen.t(),
+          generator_variant: String.t() | nil,
           run_all_variants?: boolean(),
-          update_visible_alerts?: boolean()
+          update_visible_alerts_for_screen_id: String.t()
         ]
 
-  @callback get(screen_id()) :: t()
-  @callback get(screen_id(), options()) :: t()
-  def get(screen_id, opts \\ []) do
-    select_variant(screen_id, opts, &layout_to_data/2)
-  end
+  @callback get(Screen.t()) :: t()
+  @callback get(Screen.t(), options()) :: t()
+  def get(screen, opts \\ []), do: select_variant(screen, opts, &layout_to_data/2)
 
-  @spec simulation(screen_id()) :: simulation_data()
-  @spec simulation(screen_id(), options()) :: simulation_data()
-  def simulation(screen_id, opts \\ []) do
-    select_variant(screen_id, opts, &layout_to_simulation_data/2)
-  end
+  @spec simulation(Screen.t()) :: simulation_data()
+  @spec simulation(Screen.t(), options()) :: simulation_data()
+  def simulation(screen, opts \\ []),
+    do: select_variant(screen, opts, &layout_to_simulation_data/2)
 
-  @spec variants(screen_id()) :: variants(t())
-  @spec variants(screen_id(), options()) :: variants(t())
-  def variants(screen_id, opts \\ []) do
-    all_variants(screen_id, opts, &layout_to_data/2)
-  end
+  @spec variants(Screen.t()) :: variants(t())
+  def variants(screen), do: all_variants(screen, &layout_to_data/2)
 
-  @spec simulation_variants(screen_id()) :: variants(simulation_data())
-  @spec simulation_variants(screen_id(), options()) :: variants(simulation_data())
-  def simulation_variants(screen_id, opts \\ []) do
-    all_variants(screen_id, opts, &layout_to_simulation_data/2)
-  end
+  @spec simulation_variants(Screen.t()) :: variants(simulation_data())
+  def simulation_variants(screen), do: all_variants(screen, &layout_to_simulation_data/2)
 
-  @spec select_variant(screen_id(), options(), (Layout.t(), Screen.t() -> data)) :: data
+  @spec select_variant(Screen.t(), options(), (Layout.t(), Screen.t() -> data)) :: data
         when data: t() | simulation_data()
-  defp select_variant(screen_id, opts, then_fn) do
-    config = get_config(screen_id, opts)
+  defp select_variant(screen, opts, then_fn) do
     selected_variant = Keyword.get(opts, :generator_variant)
 
     if Keyword.get(opts, :run_all_variants?, false) do
-      other_variants = List.delete([nil | @parameters.variants(config)], selected_variant)
+      other_variants = List.delete([nil | @parameters.variants(screen)], selected_variant)
 
       Enum.each(other_variants, fn variant ->
         {:ok, _pid} =
           Task.Supervisor.start_child(ParallelRunSupervisor, fn ->
-            config |> Layout.generate(variant) |> then_fn.(config)
+            screen |> Layout.generate(variant) |> then_fn.(screen)
           end)
       end)
     end
 
-    config
+    screen
     |> Layout.generate(selected_variant)
-    |> tap(&update_visible_alerts(&1, screen_id, config, opts))
-    |> then_fn.(config)
+    |> tap(&update_visible_alerts(&1, screen, opts))
+    |> then_fn.(screen)
   end
 
-  @spec all_variants(screen_id(), options(), (Layout.t(), Screen.t() -> data)) ::
-          {data, %{atom() => data}}
+  @spec all_variants(Screen.t(), (Layout.t(), Screen.t() -> data)) :: {data, %{atom() => data}}
         when data: t() | simulation_data()
-  defp all_variants(screen_id, opts, then_fn) do
-    config = get_config(screen_id, opts)
-
+  defp all_variants(screen, then_fn) do
     ParallelRunSupervisor
     |> Task.Supervisor.async_stream(
-      [nil | @parameters.variants(config)],
+      [nil | @parameters.variants(screen)],
       fn variant ->
-        {variant, config |> Layout.generate(variant) |> then_fn.(config)}
+        {variant, screen |> Layout.generate(variant) |> then_fn.(screen)}
       end
     )
     |> Enum.map(fn {:ok, result} -> result end)
     |> Enum.split(1)
     |> then(fn {[{nil, default}], variants} -> {default, Map.new(variants)} end)
   end
-
-  defp get_config(screen_id, opts),
-    do: Keyword.get_lazy(opts, :pending_config, fn -> @config_cache.screen(screen_id) end)
 
   @spec layout_to_data(Layout.t(), Screen.t()) :: t()
   defp layout_to_data(layout, config) do
@@ -187,8 +168,10 @@ defmodule Screens.V2.ScreenData do
 
   def update_visible_alerts(_, _, %Screen{hidden_from_screenplay: true}, _opts), do: :ok
 
-  def update_visible_alerts({_layout, instance_map}, screen_id, _config, opts) do
-    if Keyword.get(opts, :update_visible_alerts?, false) do
+  def update_visible_alerts({_layout, instance_map}, _screen, opts) do
+    screen_id = Keyword.get(opts, :update_visible_alerts_for_screen_id, nil)
+
+    if not is_nil(screen_id) do
       alert_ids =
         instance_map
         |> Map.values()

assets/src/hooks/use_api_response.ts
@@ -12,10 +12,9 @@ import { WidgetData } from "Components/widget";
 import useDriftlessInterval from "Hooks/use_driftless_interval";
 import { getDatasetValue } from "Util/dataset";
 import { sendToInspector, useReceiveFromInspector } from "Util/inspector";
-import { isDup } from "Util/outfront";
+import { getRotationIndex, isDup } from "Util/outfront";
 import { getScreenSide, isRealScreen } from "Util/utils";
 import { report } from "Util/sentry";
-import { ROTATION_INDEX } from "Components/dup/rotation_index";
 import { DUP_VERSION } from "Components/dup/version";
 import useRefreshRate from "./use_refresh_rate";
 
@@ -121,17 +120,6 @@ const isSuccess = (
 ): response is Success | SimulationSuccess =>
   ["success", "simulation_success"].includes(response.state);
 
-const loggingParams = () => {
-  if (isDup()) {
-    return {
-      rotation_index: ROTATION_INDEX.toString(),
-      version: DUP_VERSION,
-    };
-  } else {
-    return {};
-  }
-};
-
 const useApiPath = (screenId: string, appendPath?: string): string => {
   return useMemo(() => {
     const base = isDup() ? OUTFRONT_BASE_URI : document.baseURI;
@@ -146,17 +134,17 @@ const useApiPath = (screenId: string, appendPath?: string): string => {
 
     const url = new URL(path, base);
 
-    const datasetParams: Record<string, string | null | undefined> = {
+    const params: Record<string, string | null | undefined> = {
       is_real_screen: isRealScreen() ? "true" : null,
       last_refresh: getDatasetValue("lastRefresh"),
-      requestor:
-        getDatasetValue("requestor") ?? (isRealScreen() ? "real_screen" : null),
+      requestor: getDatasetValue("requestor"),
+      rotation_index: getRotationIndex(),
       screen_side: getScreenSide(),
       variant: getDatasetValue("variant"),
-      ...loggingParams(),
+      version: isDup() ? DUP_VERSION : null,
     };
 
-    for (const [key, value] of Object.entries(datasetParams)) {
+    for (const [key, value] of Object.entries(params)) {
       if (value) url.searchParams.append(key, value);
     }
 

config/config.exs
@@ -21,8 +21,23 @@ config :logger,
 
 # Configures Elixir's Logger
 config :logger, :console,
-  format: "$time $metadata[$level] $message\n",
-  metadata: [:remote_ip, :request_id]
+  format: "$time [$level] $message $metadata\n",
+  metadata: ~w[
+    app_id
+    is_pending
+    is_real_screen
+    ofm_app_package_version
+    remote_ip
+    requestor
+    request_id
+    request_type
+    response_type
+    rotation_index
+    screen_id
+    screen_side
+    variant
+    vendor
+  ]a
 
 # Disable default Phoenix logger; using Logster instead
 config :phoenix, :logger, false

config/dev.exs
@@ -73,8 +73,8 @@ config :screens, ScreensWeb.Endpoint,
     ]
   ]
 
-# Do not include metadata nor timestamps in development logs
-config :logger, :console, format: "[$level] $message\n"
+# Do not include timestamps in development logs
+config :logger, :console, format: "[$level] $message $metadata\n"
 
 # Set a higher stacktrace during development. Avoid configuring such
 # in production as building large stacktraces may be expensive.

lib/screens/audio.ex
@@ -5,8 +5,8 @@ defmodule Screens.Audio do
 
   @lexicon_names ["mbtalexicon"]
 
-  @spec synthesize(String.t(), keyword()) :: {:ok, binary()} | :error
-  def synthesize(ssml_string, log_meta) do
+  @spec synthesize(String.t()) :: {:ok, binary()} | :error
+  def synthesize(ssml_string) do
     result =
       ssml_string
       |> ExAws.Polly.synthesize_speech(lexicon_names: @lexicon_names, text_type: "ssml")
@@ -17,7 +17,7 @@ defmodule Screens.Audio do
         {:ok, audio_data}
 
       {:error, error} ->
-        Report.error("synthesize_ssml_failed", [error: error, string: ssml_string] ++ log_meta)
+        Report.error("synthesize_ssml_failed", error: error, string: ssml_string)
         :error
     end
   end

lib/screens/config/cache.ex
@@ -16,45 +16,10 @@ defmodule Screens.Config.Cache do
 
   def ok?, do: table_exists?()
 
-  def refresh_if_loaded_before(screen_id) do
-    with_table default: nil do
-      [[last_deploy_timestamp]] = :ets.match(@table, {:last_deploy_timestamp, :"$1"})
-
-      refresh_if_loaded_before =
-        case :ets.match(@table, {screen_id, %{refresh_if_loaded_before: :"$1"}}) do
-          [[timestamp]] -> timestamp
-          [] -> nil
-        end
-
-      case {last_deploy_timestamp, refresh_if_loaded_before} do
-        {nil, nil} ->
-          nil
-
-        {nil, refresh_if_loaded_before} ->
-          refresh_if_loaded_before
-
-        {last_deploy_timestamp, nil} ->
-          last_deploy_timestamp
-
-        {last_deploy_timestamp, refresh_if_loaded_before} ->
-          Enum.max([last_deploy_timestamp, refresh_if_loaded_before], DateTime)
-      end
-    end
-  end
-
   def last_deploy_timestamp do
     with_table do
       [[last_deploy_timestamp]] = :ets.match(@table, {:last_deploy_timestamp, :"$1"})
       last_deploy_timestamp
-    end
-  end
-
-  def disabled?(screen_id) do
-    with_table default: false do
-      case :ets.match(@table, {{:screen, screen_id}, %{disabled: :"$1"}}) do
-        [[disabled]] -> disabled
-        [] -> false
-      end
     end
   end
 

lib/screens/log_screen_data.ex
@@ -1,125 +1,0 @@
-defmodule Screens.LogScreenData do
-  @moduledoc false
-
-  alias Screens.Config.Cache
-  alias ScreensConfig.Screen
-
-  def log_page_load(screen_id, params) do
-    screen_side = params["screen_side"]
-
-    if real_screen?(params) do
-      data =
-        %{screen_id: screen_id, screen_name: screen_name_for_id(screen_id)}
-        |> insert_screen_side(screen_side)
-
-      log_message("[screen page load]", data)
-    end
-  end
-
-  def log_data_request(
-        screen_id,
-        last_refresh,
-        params
-      ) do
-    requestor = params["requestor"]
-
-    if real_screen?(params) or not is_nil(requestor) do
-      screen_side = params["screen_side"]
-      rotation_index = params["rotation_index"]
-      ofm_app_package_version = params["version"]
-
-      data =
-        %{
-          screen_id: screen_id,
-          screen_name: screen_name_for_id(screen_id),
-          last_refresh: last_refresh
-        }
-        |> insert_screen_side(screen_side)
-        |> insert_requestor(requestor)
-        |> insert_dup_rotation_index(rotation_index)
-        |> insert_version(ofm_app_package_version)
-
-      log_message("[screen data request]", data)
-    end
-  end
-
-  def log_audio_request(screen_id, params) do
-    if real_screen?(params) do
-      data = %{
-        screen_id: screen_id,
-        screen_name: screen_name_for_id(screen_id)
-      }
-
-      log_message("[screen audio request]", data)
-    end
-  end
-
-  def log_frontend_error(screen_id, error_message, stack_trace) do
-    data = %{
-      screen_id: screen_id,
-      error_message: error_message,
-      stack_trace: stack_trace
-    }
-
-    log_message("[screen frontend error]", data)
-  end
-
-  def log_api_response(response, screen_id, last_refresh, params)
-
-  def log_api_response(
-        %{status: status},
-        screen_id,
-        last_refresh,
-        params
-      ) do
-    log_api_response_success(screen_id, last_refresh, params, status)
-  end
-
-  def log_api_response(status, screen_id, last_refresh, params) do
-    log_api_response_success(screen_id, last_refresh, params, status)
-  end
-
-  defp log_api_response_success(screen_id, last_refresh, params, status) do
-    screen_side = params["screen_side"]
-
-    if real_screen?(params) do
-      data =
-        %{
-          screen_id: screen_id,
-          screen_name: screen_name_for_id(screen_id),
-          last_refresh: last_refresh
-        }
-        |> insert_screen_side(screen_side)
-
-      log_message("[screen api response #{status}]", data)
-    end
-
-    :ok
-  end
-
-  def log_message(message, data), do: Logster.info([message | Keyword.new(data)])
-
-  defp screen_name_for_id(screen_id) do
-    case Cache.screen(screen_id) do
-      %Screen{name: name} -> name
-      nil -> "UNKNOWN_SCREEN"
-    end
-  end
-
-  defp insert_screen_side(data, nil), do: data
-  defp insert_screen_side(data, screen_side), do: Map.put(data, :screen_side, screen_side)
-
-  defp insert_requestor(data, nil), do: data
-  defp insert_requestor(data, requestor), do: Map.put(data, :requestor, requestor)
-
-  defp insert_dup_rotation_index(data, nil), do: data
-
-  defp insert_dup_rotation_index(data, rotation_index),
-    do: Map.put(data, :page_number, rotation_index)
-
-  defp insert_version(data, nil), do: data
-  defp insert_version(data, version), do: Map.put(data, :ofm_app_package_version, version)
-
-  defp real_screen?(%{"is_real_screen" => "true"}), do: true
-  defp real_screen?(_), do: false
-end

lib/screens/report.ex
@@ -4,10 +4,11 @@ defmodule Screens.Report do
   def error(tag, data \\ []), do: log(:error, tag, data)
   def warning(tag, data \\ []), do: log(:warning, tag, data)
 
-  @spec log(:error | :warning, String.t(), Enumerable.t({String.Chars.t(), term()})) :: :ok
+  @spec log(:error | :warning, String.t(), keyword()) :: :ok
   defp log(level, tag, data) do
     Logster.log(level, [tag | data])
-    _ = Sentry.capture_message(tag, extra: Map.new(data), level: level, result: :none)
+    extra = Logger.metadata() |> Keyword.merge(data) |> Map.new()
+    _ = Sentry.capture_message(tag, extra: extra, level: level, result: :none)
     :ok
   end
 end

commit 7f3335a2e67c302e532f58a9c35df100f8f6ac83

fix: updating visible alerts for hidden screens
Regression introduced in 8fad02c6. The number of arguments in the second
clause of a function was changed, but not in the first clause, causing
that one to never match. The compiler didn't warn about this because
the function was mistakenly defined as public. Make it private as well
as fixing the argument count.

lib/screens/v2/screen_data.ex
@@ -166,9 +166,9 @@ defmodule Screens.V2.ScreenData do
     end
   end
 
-  def update_visible_alerts(_, _, %Screen{hidden_from_screenplay: true}, _opts), do: :ok
+  defp update_visible_alerts(_, %Screen{hidden_from_screenplay: true}, _opts), do: :ok
 
-  def update_visible_alerts({_layout, instance_map}, _screen, opts) do
+  defp update_visible_alerts({_layout, instance_map}, _screen, opts) do
     screen_id = Keyword.get(opts, :update_visible_alerts_for_screen_id, nil)
 
     if not is_nil(screen_id) do