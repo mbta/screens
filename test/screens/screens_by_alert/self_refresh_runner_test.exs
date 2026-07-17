d615d702b787f72cd30acc6e91b8623167517c30
Cora Grant <cgrant2@mbta.com>
2026-07-17 17:35:48 -0400

    perf: reduce duplicate self-refresh runs
    The primary change here is that `ScreensByAlert` can now keep track of
    screen IDs for which a data refresh is in progress. At the *start* of a
    screen data request or self-refresh, the relevant screen IDs are added
    to this list, and when displayed alert IDs are updated for a screen ID,
    it is removed from the list. In case data refreshing does not complete
    successfully, screen IDs also expire from the list after a short time.
    
    This dramatically reduces the resources used by the self-refresh runner
    on screens that are already being refreshed by either a data request or
    another instance's self-refresh runner, though it does not *guarantee*
    this never happens; doing so would require a lock/transaction mechanism,
    which Memcache does not have. This is essentially a partial, quick-fix
    version of the upcoming "resilience" changes, for which we plan to use
    Redis, which does support these.
    
    Other changes:
    
    * Increase the TTL on alerts data from 40 to 45 seconds, allowing us to
      increase the buffer time between when the self-refresh runner picks up
      a screen and its alerts data expires from 5 to 10 seconds. This should
      improve the reliability of the alerts-on-screens feature, since screen
      data often does take more than 5 seconds to generate.
    
    * In addition to "normal" screen data requests, also update alerts data
      on simulation requests. Unclear why we didn't do this before; the data
      should be identical and equally valid. This mostly cuts resource usage
      in non-prod environments, where most requests are from Screenplay.

--- test/screens/screens_by_alert/self_refresh_runner_test.exs
+++ test/screens/screens_by_alert/self_refresh_runner_test.exs
@@ -19,23 +19,27 @@ defmodule Screens.ScreensByAlert.SelfRefreshRunnerTest do
       %{"1001" => now - 61, "1002" => now - 62, "1301" => now - 63, "1401" => now - 64}
     end)
 
-    # Only the 2 most-outdated screens should be refreshed, per the batch size in test
+    # Pretend another instance is refreshing 1301. Per the batch size in test, the 2 most outdated
+    # screens should be refreshed (that are not already being refreshed), which are 1401 and 1002.
+    expect(ScreensByAlert.Mock, :get_in_progress, fn _screen_ids -> ["1301"] end)
+    expect(ScreensByAlert.Mock, :put_in_progress, fn ~w[1401 1002] -> :ok end)
+
     expect(ScreenData.Mock, :get, fn %Screen{app_id: :bus_shelter_v2},
                                      [update_visible_alerts_for_screen_id: "1401"] ->
       %{type: :x}
     end)
 
-    expect(ScreenData.Mock, :get, fn %Screen{app_id: :busway_v2},
-                                     [update_visible_alerts_for_screen_id: "1301"] ->
+    expect(ScreenData.Mock, :get, fn %Screen{app_id: :bus_eink_v2},
+                                     [update_visible_alerts_for_screen_id: "1002"] ->
       raise "oops"
     end)
 
-    screen_ids = MapSet.new(~w[1301 1401])
+    screen_ids = MapSet.new(~w[1002 1401])
     assert {:noreply, ^screen_ids} = SelfRefreshRunner.handle_info(:check, MapSet.new())
 
     # Wait longer than the default 100ms to avoid occasional timeouts
     assert_receive({:done, :ok, "1401"}, 200)
-    assert_receive({:done, :exit, "1301"}, 200)
+    assert_receive({:done, :exit, "1002"}, 200)
   end
 
   test "skips refreshing if any refreshes are in progress" do
