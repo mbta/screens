f7bdbffdbe1729fd905f20deba4b2db7e13bdbc6
robbie.sundstrom <rsundstrom@mbta.com>
2026-07-27 15:34:39 -0400

    PR comment fixes

--- scripts/sync_s3_env.sh
+++ scripts/sync_s3_env.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 # Syncs two environments S3 buckets and their config files.
-# Can sync from prod as a source, but to prod as a destination.
+# Can sync from prod as a source, but not to prod as a destination.
 # Any of dev, dev-green, or dev-blue can be the source or destination.
 
 set -eu
@@ -40,7 +40,7 @@ case $SOURCE_ENV in
 prod | dev | dev-green | dev-blue) true ;;
 *)
   echo "Usage: $0 [-D] -s <screens-env-source-name> -d <screens-env-dest-name>" >&2
-  echo "<screens-env-source-name> should be one of the following: prod | dev | dev-green" >&2
+  echo "<screens-env-source-name> should be one of the following: prod | dev | dev_blue | dev-green" >&2
   exit 1
   ;;
 esac
