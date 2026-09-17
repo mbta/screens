e3fd7c1ac3f480c84d8074c643ad86ad6cc3b34d
Cora Grant <cgrant2@mbta.com>
2026-09-17 16:00:55 -0400

    fix: timeout reporting for RPC'd widget generation
    `Nebulex.Distributed.RPC` wraps any exception thrown by the remote node
    in its own error struct. This meant if the remote node threw
    `CandidateGenerator.Timeout`, the calling node would return a generic
    500 to the client instead of a 503, which would then not be logged as a
    timeout in our Splunk dashboard.
    
    Fix this by using `:erpc.call/5` directly. `Nebulex.Distributed.RPC`
    does not seem intended for use by consumers of the library anyway, and
    this was straightforward enough.
    
    Also limit the scope of RPC errors for which we will retry generation
    locally; most possible errors would represent a bug in our code, which
    we'd want to be alerted to intead of ignoring.

--- lib/screens/v2/screen_data/cache.ex
+++ lib/screens/v2/screen_data/cache.ex
@@ -23,7 +23,7 @@ defmodule Screens.V2.ScreenData.Cache do
 
     @type error :: {:error, Nebulex.Error.t()}
 
-    @callback call(node(), module(), atom(), [any()], timeout()) :: any()
+    @callback call(node(), module(), atom(), [any()], timeout()) :: {:ok, any()} | {:error, any()}
     @callback delete_all(Nebulex.Cache.query_spec()) :: {:ok, non_neg_integer()} | error()
     @callback fetch(key()) :: {:ok, value()} | error()
     @callback find_node(key()) :: {:ok, node()} | error()
@@ -35,18 +35,24 @@ defmodule Screens.V2.ScreenData.Cache do
       use Nebulex.Cache, otp_app: :screens, adapter: Nebulex.Adapters.Partitioned
     end
 
-    defdelegate call(node, mod, fun, args, timeout), to: Nebulex.Distributed.RPC
     defdelegate delete_all(query), to: Adapter
     defdelegate fetch(key), to: Adapter
     defdelegate find_node(key), to: Adapter
     defdelegate put(key, value, opts \\ []), to: Adapter
     defdelegate transaction(fun, opts \\ []), to: Adapter
+
+    @doc "Wrapper for `:erpc.call/5` that returns an `:ok`/`:error` tuple instead of raising."
+    def call(node, mod, fun, args, timeout) do
+      {:ok, :erpc.call(node, mod, fun, args, timeout)}
+    rescue
+      e in ErlangError -> {:error, e.original}
+    end
   end
 
   defmodule Meta do
     @moduledoc "Metadata returned about the internal cache operations."
     @type t :: %__MODULE__{
-            what: :hit | :miss | {:error, operation :: atom(), Nebulex.Error.t()},
+            what: :hit | :miss | {:error, operation :: atom(), Nebulex.Error.t() | nil},
             where: :local | :remote
           }
     defstruct what: :miss, where: :local
@@ -99,9 +105,18 @@ defmodule Screens.V2.ScreenData.Cache do
         # instead of retrying the generation ourself, since the app may be overloaded and at that
         # point the client has been waiting on us for a long time.
         case @store.call(node, __MODULE__, :instances, [id, screen], 20_000) do
-          {:error, %Nebulex.Error{reason: {:rpc, {:error, :timeout}}}} -> raise Timeout
-          {:error, e} -> {generate(screen), %Meta{what: {:error, :rpc, e}, where: :remote}}
-          {instances, %Meta{} = meta} -> {instances, %Meta{meta | where: :remote}}
+          {:ok, {instances, %Meta{} = meta}} ->
+            {instances, %Meta{meta | where: :remote}}
+
+          # If unable to reach the remote node, generating instances locally might still work.
+          {:error, {:erpc, :noconnection}} ->
+            {generate(screen), %Meta{what: {:error, :no_connection, nil}, where: :remote}}
+
+          {:error, {:erpc, :timeout}} ->
+            raise Timeout
+
+          {:error, {:exception, Timeout, stacktrace}} ->
+            reraise Timeout, stacktrace
         end
 
       {:error, error} ->
